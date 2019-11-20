2019e1441428f4fe578d9accfb0b075d26482f5e
Internal Developer <internal-developer@intel.com>
2019-11-20 16:13:42 +0200

    remove replicated file
    Change-Id: I1fbdccfc9c7c24929a2b5ab4556a023f840389a5

--- habana_kernels/tests/test_hpu_device.py
+++
@@ -1,9 +0,0 @@
-import torch
-torch.ops.load_library("libhabana_device.so")
-torch.ops.load_library("libhabana_kernels.so")
-
-def test_hpu_device():
-    device = torch.device('habana')
-    tensor = torch.randn(5)
-
-    tensor.to(device)
