63f42c33eccff5456eec41a698c10ff2969ad500
Internal Developer <internal-developer@intel.com>
2019-12-03 13:51:15 +0200

    implmement memocpy to and from HPU, it works
    Change-Id: Ied7cd721106c9d7828a97e44bd86befc52443301

--- habana_kernels/basic_kernels.cpp
+++ habana_kernels/basic_kernels.cpp
@@ -1,6 +1,8 @@
 #include <ATen/InferSize.h>
 #include <torch/script.h>
+#include "habana_device/HPUCheck.h"
 #include "habana_device/HPUContext.h"
+#include "synapse/include/synapse_api.h"
 
 using namespace torch;
 
@@ -24,7 +26,7 @@ Tensor empty_habana(
   // original comment:
   // is_variable should have been 'unpacked'  TODO: remove this when Variable
   // and Tensor are merged
-  AT_ASSERT(!options.is_variable());
+  // AT_ASSERT(!options.is_variable());
   // TORCH_CHECK(!optional_memory_format.has_value(),"'memory_format' argument
   // is incompatible with HABANA tensor");
   TORCH_CHECK(!options.pinned_memory(), "Only dense CPU tensors can be pinned");
@@ -77,6 +79,102 @@ Tensor view_habana(const Tensor& self, IntArrayRef size) {
   return self_;
 }
 
+// cpu->hpu and hpu->cpu copy implementation
+Tensor& hpu_copy_(Tensor& self, const Tensor& src, bool non_blocking) {
+  // TODO: (from torch code) this should be handled during dispatch, but that's
+  // missing...
+  Tensor& dst = self;
+  TORCH_CHECK(dst.defined(), "dst is undefined");
+  TORCH_CHECK(src.defined(), "src is undefined");
+  TORCH_CHECK(
+      dst.nbytes() == src.nbytes(), "src and dst buffers size don't match");
+
+  int device_id = -1;
+
+  const auto src_device = src.device().type();
+  const auto dst_device = dst.device().type();
+  synDmaDir dma_type;
+  void* mapped_addr = nullptr;
+  if (src_device == c10::DeviceType::CPU &&
+      dst_device == c10::DeviceType::HABANA) {
+    device_id = dst.device().index();
+    dma_type = synDmaDir::HOST_TO_DRAM;
+    mapped_addr = src.data_ptr();
+    TORCH_HABANA_CHECK(
+        synHostMap(device_id, dst.nbytes(), mapped_addr),
+        "Synapse failed to map tensor");
+  } else if (
+      src_device == c10::DeviceType::HABANA &&
+      dst_device == c10::DeviceType::CPU) {
+    device_id = src.device().index();
+    dma_type = synDmaDir::DRAM_TO_HOST;
+    mapped_addr = dst.data_ptr();
+    TORCH_HABANA_CHECK(
+        synHostMap(device_id, dst.nbytes(), mapped_addr),
+        "Synapse failed to map tensor");
+  } else if (
+      src_device == c10::DeviceType::HABANA &&
+      dst_device == c10::DeviceType::HABANA) {
+    device_id = dst.device().index();
+    TORCH_CHECK(
+        dst.device().index() == src.device().index(),
+        "Tensors can't be copied between devices using hpu_copy_");
+    dma_type = synDmaDir::DRAM_TO_DRAM;
+  } else {
+    TORCH_CHECK(
+        false,
+        "hpu_copy_ doesn't support ",
+        src_device,
+        " to ",
+        dst_device,
+        "copy");
+  }
+
+  synStreamHandle stream{};
+  TORCH_HABANA_CHECK(
+      synStreamCreate(&stream, device_id, 0), "Creating synapse stream failed");
+
+  TORCH_HABANA_CHECK(
+      synMemCopyAsync(
+          stream,
+          reinterpret_cast<uint64_t>(src.data_ptr()),
+          src.nbytes(),
+          reinterpret_cast<uint64_t>(dst.data_ptr()),
+          dma_type),
+      "Synapse DMA: ",
+      dma_type,
+      "start failed");
+
+  TORCH_HABANA_CHECK(
+      synStreamSynchronize(stream), "Stream synchronization failed");
+
+  TORCH_HABANA_CHECK(
+      synHostUnmap(device_id, mapped_addr), "Synapse failed to unmap tensor");
+  TORCH_HABANA_CHECK(
+      synStreamDestroy(stream), "Destroying synapse stream failed");
+
+  return dst;
+}
+
+// - func: _copy_from(Tensor self, Tensor dst, bool non_blocking=False) ->
+// Tensor
+//   use_c10_dispatcher: full
+//   dispatch: {}
+// Tensor hpu__copy_from(
+//     const Tensor& self,
+//     const Tensor& dst,
+//     bool non_blocking) {
+//   TORCH_CHECK(self.defined(), "self is undefined");
+//   TORCH_CHECK(dst.defined(), "src is undefined");
+
+//   if (self.is_same(dst)) {
+//     return dst;
+//   }
+
+//   throw "UNIMPLEMENTED";
+//   return dst;
+// }
+
 static auto registry =
     torch::RegisterOperators()
         .op("habana_kernels::set_one", &set_one)
@@ -85,7 +183,21 @@ static auto registry =
                     "aten::empty.memory_format(int[] size, *, ScalarType? dtype=None, Layout? layout=None, Device? device=None, bool? pin_memory=None, MemoryFormat? memory_format=None) -> Tensor")
                 .impl_unboxedOnlyKernel<decltype(empty_habana), &empty_habana>(
                     TensorTypeId::HABANATensorId)
+                .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA))
+        .op(torch::RegisterOperators::options()
+                .schema(
+                    "aten::copy_(Tensor(a!) self, Tensor src, bool non_blocking=False) -> Tensor(a!)")
+                .impl_unboxedOnlyKernel<decltype(hpu_copy_), &hpu_copy_>(
+                    TensorTypeId::HABANATensorId)
                 .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA));
+// .op(torch::RegisterOperators::options()
+//         .schema(
+//             "aten::_copy_from(Tensor self, Tensor dst, bool
+//             non_blocking=False) -> Tensor")
+//         .impl_unboxedOnlyKernel<
+//             decltype(hpu__copy_from),
+//             &hpu__copy_from>(TensorTypeId::HABANATensorId)
+//         .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA));
 // .op(torch::RegisterOperators::options()
 //         .schema("aten::view(Tensor(a) self, int[] size) -> Tensor(a)")
 //         .impl_unboxedOnlyKernel<decltype(view_habana), &view_habana>(
