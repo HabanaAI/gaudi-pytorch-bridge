ae9ff36afded1caed3f7ad49aaf06fdd7fcba61d
Internal Developer <internal-developer@intel.com>
2020-01-28 16:37:17 +0200

    test for relu bwd.
    It is implemented using threshold op
    
    Change-Id: I2e3ec849b766feb1e63eea219301519df8f24953

--- tests/test_hpu_unary_ops.py
+++ tests/test_hpu_unary_ops.py
@@ -25,5 +25,15 @@ def test_hpu_unary_op(N, H, W, C, unary_op):
     evaluate_fwd_kernel(kernel=unary_op, kernel_params=kernel_params)
 
 
+@pytest.mark.parametrize("N, H, W, C", test_case_list)
+@pytest.mark.parametrize("unary_op", unary_op_list)
+def test_hpu_unary_op_fwd_bwd(N, H, W, C, unary_op):
+    # TODO: extend that test to all features
+    kernel_params_fwd = {'input': torch.randn(N, C, H, W, requires_grad=True)}
+    bwd_tensors = [torch.randn(N, C, H, W)]
+    evaluate_fwd_bwd_kernel(kernel=unary_op, tensor_list_bwd=bwd_tensors,
+                            kernel_params_fwd=kernel_params_fwd)
+
+
 if __name__ == '__main__':
     test_hpu_unary_op(*test_case_list[0], unary_op_list[0])
