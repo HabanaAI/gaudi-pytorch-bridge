4a0d71fa4584187e866b0d8281241f32cf295a7c
Internal Developer <internal-developer@intel.com>
2020-02-04 13:11:54 +0200

    implement _local_scalar_dense_hpu
    Change-Id: I44738372b6b925e2b0e98c9ad79076811a176a63

---
+++ habana_kernels/scalar.cpp
@@ -0,0 +1,44 @@
+#include <torch/script.h>
+
+#include "habana_device/hpu_cached_devices.h"
+#include "habana_helpers/logging.h"
+
+using namespace torch;
+
+namespace at {
+namespace native {
+
+Scalar _local_scalar_dense_hpu(const Tensor& self) {
+  LOG_FUNC_BEGIN;
+  Scalar r;
+  // Note: this macro expands to more types than HPU supports, but this is not
+  // an issue
+  // Note: this kernel intentionally doesn't check if numel == 1, dunno why,
+  // it's just pytorch
+  AT_DISPATCH_ALL_TYPES_AND(
+      at::ScalarType::BFloat16,
+      self.scalar_type(),
+      "_local_scalar_dense_hpu",
+      [&] {
+        scalar_t value;
+        synapse_helpers::HPURegistrar::get_device(self.device().index())
+            .copy_data_to_host(
+                reinterpret_cast<synapse_helpers::device_ptr>(self.data_ptr()),
+                &value,
+                self.nbytes());
+        r = Scalar(value);
+      });
+  LOG_FUNC_END;
+  return r;
+}
+
+} // namespace native
+} // namespace at
+
+static auto registry = torch::RegisterOperators().op(
+    torch::RegisterOperators::options()
+        .schema("aten::_local_scalar_dense(Tensor self) -> Scalar")
+        .impl_unboxedOnlyKernel<
+            decltype(at::native::_local_scalar_dense_hpu),
+            &at::native::_local_scalar_dense_hpu>(TensorTypeId::HABANATensorId)
+        .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA));
