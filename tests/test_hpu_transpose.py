437b23f212a5b527c0858b88bf2c07d9f8b2ac3c
Internal Developer <internal-developer@intel.com>
2020-04-12 13:54:20 +0300

    PyTorch implementation for transpose.
    torch.transpose
    torch.transpose_
    t()
    t_()
    In place versions are implemented through a combination of
    out-of-place transpose and HPU memcpy to self tensor.
    
    Change-Id: I4287c5819b8ae40fd02f06155b2609cfd841bdda

---
+++ tests/test_hpu_transpose.py
@@ -0,0 +1,82 @@
+import torch
+import numpy as np
+import pytest
+from test_utils import evaluate_fwd_kernel, evaluate_fwd_inplace_kernel
+
+# N - batch
+# H - input height
+# W - input width
+# C - input channels
+#1, 2, 3, 4
+test_case_list = [
+    # N, H, W, C
+    (8, 224, 224, 3),
+]
+
+test_case_t_list = [
+    #H, W
+    (2, 3),
+]
+
+t_op_list = [
+    # op, op params dict
+    (torch.transpose, {'input':torch.randn((8, 224, 224, 3)), 'dim0':0, 'dim1':3}),
+    (torch.transpose, {'input':torch.randint(1, 24, (2, 3, 4), dtype=torch.int), 'dim0':1, 'dim1':2}),
+    (torch.transpose, {'input':torch.randint(1, 24, (1, 3, 2), dtype=torch.int), 'dim0':-1, 'dim1':-3}),
+    (torch.transpose, {'input':torch.randn(3, 4), 'dim0':1, 'dim1':0}),
+    (torch.transpose, {'input':torch.randn(2), 'dim0':0, 'dim1':0}),
+]
+
+transpose_inplace_op_list = [
+    ('transpose_',{}),
+]
+
+t_inplace_op_list = [
+    ('t_',)
+]   
+
+@pytest.mark.parametrize("N, H, W, C", test_case_list)
+@pytest.mark.parametrize("t_op, kernel_params_fwd", t_op_list)
+def test_hpu_transpose(N, H, W, C, t_op, kernel_params_fwd):
+    print('In T = {}'.format(kernel_params_fwd['input']))
+    print('Expected Out T = {}'.format(torch.transpose(kernel_params_fwd['input'],
+                                        kernel_params_fwd['dim0'],kernel_params_fwd['dim1'])))
+    evaluate_fwd_kernel(kernel=t_op, kernel_params=kernel_params_fwd)
+
+@pytest.mark.parametrize("N, H, W, C", test_case_list)
+@pytest.mark.parametrize("transpose_inplace_op, kernel_params_fwd", transpose_inplace_op_list)
+def test_hpu_transpose_inplace(N, H, W, C, transpose_inplace_op, kernel_params_fwd):
+    in_out_tensor = torch.randn(N, C, H, W)
+    kernel_params_fwd = {'dim0':0, 'dim1':2}
+    evaluate_fwd_inplace_kernel(in_out_tensor=in_out_tensor, kernel_name=transpose_inplace_op, kernel_params=kernel_params_fwd)
+    in_out_tensor = torch.randn(N, C, H, W)
+    kernel_params_fwd = {'dim0':-1, 'dim1':-2}
+    evaluate_fwd_inplace_kernel(in_out_tensor=in_out_tensor, kernel_name=transpose_inplace_op, kernel_params=kernel_params_fwd)
+    in_out_tensor = torch.randn(H, W)
+    kernel_params_fwd = {'dim0':0, 'dim1':1}
+    evaluate_fwd_inplace_kernel(in_out_tensor=in_out_tensor, kernel_name=transpose_inplace_op, kernel_params=kernel_params_fwd)
+
+@pytest.mark.parametrize("H, W", test_case_t_list)
+@pytest.mark.parametrize("t_inplace_op", t_inplace_op_list)
+def test_hpu_t_inplace(H, W, t_inplace_op):
+    in_out_tensor = torch.randint(1, 24, (H, W), dtype=torch.int)
+    evaluate_fwd_inplace_kernel(in_out_tensor=in_out_tensor, kernel_name='t_', kernel_params=None)
+    in_out_tensor = torch.randint(1, 24, (1,), dtype=torch.int)
+    evaluate_fwd_inplace_kernel(in_out_tensor=in_out_tensor, kernel_name='t_', kernel_params=None)
+
+@pytest.mark.parametrize("N, H, W, C", test_case_list)
+def test_hpu_transpose_of_transpose(N, H, W, C):
+    hpu = torch.device('habana')
+    cpu = torch.device('cpu')
+    in_out_tensor = torch.randn(1, 2, 3, 4)
+    hputensor = in_out_tensor.to(hpu)
+    thputensor = hputensor.transpose_(0, 2)
+    tthputensor = thputensor.transpose_(0, 2)
+    print (torch.eq(hputensor, tthputensor).to(cpu))
+    
+
+if __name__ == '__main__':
+    test_hpu_transpose(*test_case_list[0],*t_op_list[0])
+    test_hpu_t_inplace(*test_case_t_list[0], t_inplace_op_list[0])
+    test_hpu_transpose_inplace(*test_case_list[0], *transpose_inplace_op_list[0])
+    test_hpu_transpose_of_transpose(*test_case_list[0])
