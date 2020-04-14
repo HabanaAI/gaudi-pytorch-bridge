f55386effd60cceaec619f471d929c78f2307675
Internal Developer <internal-developer@intel.com>
2020-04-14 15:30:33 +0300

    Work around CI issues
    Temporarily recreate the synapse_helpers_abi11 target and disable
    Abseil targets in order to fix CI. Our CI currently doesn't run builds
    in containers which makes CMake's package registry contain stale paths.
    
    Change-Id: Ie5c23b5e052fb83abe66ebe36a2c9da967ed141e

--- cmake/AddNPUStackDependencies.cmake
+++ cmake/AddNPUStackDependencies.cmake
@@ -14,6 +14,12 @@ set_target_properties(Synapse PROPERTIES
   INTERFACE_INCLUDE_DIRECTORIES "$ENV{SYNAPSE_ROOT}/include"
   )
 
-add_library(tpc_kernels SHARED IMPORTED)
-set_target_properties(tpc_kernels PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "$ENV{TPC_KERNELS_ROOT}/include")
+add_library(synapse_helpers_abi11 SHARED)
+set_target_properties(synapse_helpers_abi11 PROPERTIES
+  IMPORTED_LOCATION "$ENV{BUILD_ROOT_LATEST}/libsynapse_helpers.so.1"
+  INTERFACE_INCLUDE_DIRECTORIES "$ENV{TF_MODULES_ROOT};$ENV{ABSEIL_CPP_INCLUDE_DIR};$ENV{TPC_KERNELS_ROOT}/include"
+  INTERFACE_COMPILE_DEFINITIONS GENERIC_HELPERS
+  LINKER_LANGUAGE CXX
+  )
+add_library(synapse_helpers::synapse_helpers_abi11 ALIAS synapse_helpers_abi11)
+target_link_libraries(synapse_helpers_abi11 INTERFACE Synapse)
