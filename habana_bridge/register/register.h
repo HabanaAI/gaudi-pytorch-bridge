1bd3b27c9aa4ba669d1c19108df673b4f4890a6d
Internal Developer <internal-developer@intel.com>
2020-04-30 13:09:24 +0300

    Create directory structure for graph mode fusion
    Change-Id: I8491205a52fc7fe5e8eecfed744a36e96049b113
    Signed-off-by: Internal Developer <internal-developer@intel.com>

---
+++ habana_bridge/register/register.h
@@ -0,0 +1,10 @@
+/******************************************************************************
+ * Copyright (C) 2020 HabanaLabs, Ltd.
+ * All Rights Reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ ******************************************************************************
+ */
+#pragma once
