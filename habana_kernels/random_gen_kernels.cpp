edef4b72f00eadfda546a9219c3bb70648dad22f
Internal Developer <internal-developer@intel.com>
2020-05-20 15:49:43 +0300

    bernoulli RNG
    Change-Id: I135e9a2306298dd9050973454cdf324fb1dff8ed

--- habana_kernels/random_gen_kernels.cpp
+++ habana_kernels/random_gen_kernels.cpp
@@ -22,6 +22,18 @@
 
 using namespace torch;
 
+uint32_t get_seed_hpu(CPUGenerator* gen) {
+  if (gen == nullptr) {
+    gen = at::detail::getDefaultCPUGenerator();
+  }
+
+  // Acquire lock when using random generators
+  std::lock_guard<std::mutex> lock(gen->mutex_);
+  auto seed = gen->random();
+
+  return seed;
+}
+
 /*******************************************************************
 *@brief Implements uniform distribution generation kernel
 *INPUTS
@@ -35,21 +47,8 @@ void uniform_hpu(
     const Tensor& self,
     double from = 0,
     double to = 1,
-    Generator* gen = nullptr) {
+    CPUGenerator* gen = nullptr) {
   LOG_FUNC_BEGIN;
-  uint64_t seed;
-  uint32_t seed_u32;
-
-  if (gen == nullptr) {
-    seed = at::detail::getNonDeterministicRandom(false);
-  } else {
-    // Acquire lock when using random generators
-    std::lock_guard<std::mutex> lock(gen->mutex_);
-    seed = gen->current_seed();
-  }
-
-  // Convert to 32 bit as TPC kernel supports only 32 bit seed
-  seed_u32 = (uint32_t)(seed & 0xFFFFFFFF);
 
   std::vector<const at::Tensor*> pt_inputs{};
   std::vector<const at::Tensor*> pt_outputs{&self};
@@ -57,7 +56,7 @@ void uniform_hpu(
   ns_RandomUniform::Params params;
   params.low = static_cast<float>(from);
   params.high = static_cast<float>(to);
-  params.seed = seed_u32;
+  params.seed = get_seed_hpu(gen);
 
   synapse_simple_generic_kernel(
       pt_outputs,
@@ -83,21 +82,8 @@ void normal_hpu(
     const Tensor& self,
     double mean = 0,
     double std = 1,
-    Generator* gen = nullptr) {
+    CPUGenerator* gen = nullptr) {
   LOG_FUNC_BEGIN;
-  uint64_t seed;
-  uint32_t seed_u32;
-
-  if (gen == nullptr) {
-    seed = at::detail::getNonDeterministicRandom(false);
-  } else {
-    // Acquire lock when using random generators
-    std::lock_guard<std::mutex> lock(gen->mutex_);
-    seed = gen->current_seed();
-  }
-
-  // Convert to 32 bit as TPC kernel supports only 32 bit seed
-  seed_u32 = (uint32_t)(seed & 0xFFFFFFFF);
 
   std::vector<const at::Tensor*> pt_inputs{};
   std::vector<const at::Tensor*> pt_outputs{&self};
@@ -105,7 +91,7 @@ void normal_hpu(
   ns_RandomNormal::Params params;
   params.mean = static_cast<float>(mean);
   params.stddev = static_cast<float>(std);
-  params.seed = seed_u32;
+  params.seed = get_seed_hpu(gen);
 
   synapse_simple_generic_kernel(
       pt_outputs,
@@ -116,6 +102,104 @@ void normal_hpu(
       SynapsePassType::FORWARD_PASS);
 
   LOG_FUNC_END;
+}
+
+/*******************************************************************
+*@brief Implements Bernoulli distribution generation kernel
+@param[in] self - output tensor with probablities, 1-4D,
+BF16/FP32
+@param[in] gen - Generator class for seed (optional)
+@param[out] - Tensor same shape as self with 0/1 entries generated based on
+input probabilities , I16/I32, 1-4D
+*******************************************************************/
+Tensor bernoulli_hpu(const Tensor& self, CPUGenerator* gen = nullptr) {
+  LOG_FUNC_BEGIN;
+
+  Tensor output =
+      at::empty(self.sizes(), self.options().dtype(c10::ScalarType::Int));
+
+  std::vector<const at::Tensor*> pt_inputs{&self};
+  std::vector<const at::Tensor*> pt_outputs{&output};
+
+  ns_RandomBernoulli::Params params;
+  params.seed = get_seed_hpu(gen);
+
+  synapse_simple_generic_kernel(
+      pt_outputs,
+      pt_inputs,
+      "random_bernoulli",
+      &params,
+      sizeof(params),
+      SynapsePassType::FORWARD_PASS);
+
+  LOG_FUNC_END;
+
+  return output;
+}
+
+/*******************************************************************
+*@brief Implements Bernoulli distribution generation kernel
+*INPUTS self.bernoulli_(p=0.5, *, generator=None) → Tensor
+*Fills each location of self with an independent sample from
+Bernoulli(p).
+@param[in, out] self - output tensor with probablities, 1-4D,
+BF16/FP32.
+@param[in] gen - Generator class for seed (optional)
+*******************************************************************/
+Tensor& bernoulli_scalar_hpu(
+    Tensor& self,
+    float p,
+    CPUGenerator* gen = nullptr) {
+  LOG_FUNC_BEGIN;
+
+  auto self_scalar_type = self.scalar_type();
+
+  TORCH_CHECK(
+      (self_scalar_type == c10::ScalarType::Int) ||
+          (self_scalar_type == c10::ScalarType::Float),
+      "Expected float or int data type");
+
+  Scalar p_converted = p;
+  auto p_tensor = habana_helpers::scalar_to_device_tensor(
+      p_converted,
+      self.options().dtype(c10::ScalarType::Float),
+      self.ndimension());
+  auto expanded_p_tensor = p_tensor.expand(self.sizes());
+
+  Tensor* output_ptr;
+  Tensor self_int;
+
+  std::vector<const at::Tensor*> pt_inputs{&expanded_p_tensor};
+
+  if (self_scalar_type == c10::ScalarType::Float) {
+    self_int =
+        at::empty(self.sizes(), self.options().dtype(c10::ScalarType::Int));
+    output_ptr = &self_int;
+  } else {
+    // Int
+    output_ptr = &self;
+  }
+
+  std::vector<const at::Tensor*> pt_outputs{output_ptr};
+
+  ns_RandomBernoulli::Params params;
+  params.seed = get_seed_hpu(gen);
+
+  synapse_simple_generic_kernel(
+      pt_outputs,
+      pt_inputs,
+      "random_bernoulli",
+      &params,
+      sizeof(params),
+      SynapsePassType::FORWARD_PASS);
+
+  if (self_scalar_type == c10::ScalarType::Float) {
+    habana_helpers::copy_data_within_device(self_int, self);
+  }
+
+  LOG_FUNC_END;
+
+  return self;
 }
 
 static auto registry =
@@ -131,4 +215,18 @@ static auto registry =
                     "aten::normal_(Tensor(a!) self, float mean=0, float std=1, *, Generator? generator=None) -> Tensor(a!)")
                 .impl_unboxedOnlyKernel<decltype(normal_hpu), &normal_hpu>(
                     DispatchKey::HABANATensorId)
+                .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA))
+        .op(torch::RegisterOperators::options()
+                .schema(
+                    "aten::bernoulli(Tensor self, *, Generator? generator=None) -> Tensor")
+                .impl_unboxedOnlyKernel<
+                    decltype(bernoulli_hpu),
+                    &bernoulli_hpu>(DispatchKey::HABANATensorId)
+                .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA))
+        .op(torch::RegisterOperators::options()
+                .schema(
+                    "aten::bernoulli_.float(Tensor(a!) self, float p=0.5, *, Generator? generator=None) -> Tensor(a!)")
+                .impl_unboxedOnlyKernel<
+                    decltype(bernoulli_scalar_hpu),
+                    &bernoulli_scalar_hpu>(DispatchKey::HABANATensorId)
                 .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA));
--- tests/test_hpu_random_gen.py
+++ tests/test_hpu_random_gen.py
@@ -41,18 +41,17 @@ def test_hpu_rand_gen_uniform_fwd(N, H, W, C, min, max, seed):
     output2 = in2_hpu.uniform_(a, b, generator=g)
     output2_hpu = output2.to(cpu).detach().numpy()
 
-    # Assert if the two tensors are same for same seed
+    # verify if the two tensors are same for same seed
     testing.assert_equal(output1_hpu, output2_hpu)
 
-    # Assert if the output values exceeds the range
+    # verify if the output values exceeds the range
     testing.assert_equal((np.min(output2_hpu) >= a) & (np.max(output2_hpu) <= b), True)
 
-    # Assert if two tensors are different for different seeds
+    # verify if two tensors are different for different seeds
     input3 = torch.empty(N, C, H, W, dtype=torch.float)
     in3_hpu = input3.to(hpu)
     output3 = in3_hpu.uniform_(a, b)
     output3_hpu = output3.to(cpu).detach().numpy()
-
     testing.assert_equal(np.any(np.not_equal(output2_hpu, output3_hpu)), True)
 
 
@@ -74,19 +73,69 @@ def test_hpu_rand_gen_normal_fwd(N, H, W, C, mean, std, seed):
     output2 = in2_hpu.normal_(mean=mean, std=std, generator=g)
     output2_hpu = output2.to(cpu).detach().numpy()
 
-    # Assert if the two tensors are same for same seed
+    # verify if the two tensors are same for same seed
     testing.assert_equal(output1_hpu, output2_hpu)
 
-    # Assert if the output values exceeds the range (used 5 sigma range)
+    # verify if the output values exceeds the range (used 5 sigma range)
     testing.assert_equal((np.min(output2_hpu) >= (mean - 5 * std)) & (np.max(output2_hpu) <= (mean + 5 * std)), True)
 
-    # Assert if two tensors are different for different seeds
+    # verify if two tensors are different for different seeds
     input3 = torch.empty(N, C, H, W, dtype=torch.float)
     in3_hpu = input3.to(hpu)
     output3 = in3_hpu.normal_(mean=mean, std=std)
     output3_hpu = output3.to(cpu).detach().numpy()
 
     testing.assert_equal(np.any(np.not_equal(output2_hpu, output3_hpu)), True)
+
+    torch.manual_seed(seed)
+    # verify if two tensors are same across runs for the same manual seed
+    output4 = in3_hpu.normal_(mean=mean, std=std)
+    output4_hpu = output4.to(cpu).detach().numpy()
+    print(output4_hpu)
+
+    torch.manual_seed(seed)
+    output5 = in3_hpu.normal_(mean=mean, std=std)
+    output5_hpu = output5.to(cpu).detach().numpy()
+
+    testing.assert_equal(output4_hpu, output5_hpu)
+
+
+@pytest.mark.parametrize("N, H, W, C, min, max, seed", test_cast_list_4d)
+def test_hpu_rand_gen_bernoulli_fwd(N, H, W, C, min, max, seed):
+    # CPU and HPU uses different algorithm for RNG. Hence they are not compared
+    # Instead basic sanity like range and staleness are checked
+    torch.manual_seed(seed)
+    g = torch.Generator()
+
+    input = torch.empty(N, C, H, W, dtype=torch.float)
+    in_hpu = input.uniform_(0, 1).to(hpu)
+    g.manual_seed(seed)
+    output1 = torch.bernoulli(in_hpu, generator=g)
+    output1_hpu = output1.to(cpu).detach().numpy()
+
+    g.manual_seed(seed)
+    output2 = torch.bernoulli(in_hpu, generator=g)
+    output2_hpu = output2.to(cpu).detach().numpy()
+
+    # verify if the two tensors are same for same seed
+    testing.assert_equal(output1_hpu, output2_hpu)
+
+    # verify if the output values exceeds the range
+    testing.assert_equal((np.min(output2_hpu) >= 0) & (np.max(output2_hpu) <= 1), True)
+
+    # Test bernoulli._float
+    input_i32 = torch.empty((N, C, H, W), dtype=torch.int32)
+    input_i32_hpu = input_i32.to(hpu)
+    output1 = input_i32_hpu.bernoulli(0.5, generator=g)
+    output1_hpu = output1.to(cpu).detach().numpy()
+
+    input_f32 = torch.empty((N, C, H, W), dtype=torch.float32)
+    input_f32_hpu = input_f32.to(hpu)
+    output2 = input_f32_hpu.bernoulli(0.5, generator=g)
+    output2_hpu = output2.to(cpu).detach().numpy()
+
+    # verify if the two tensors are same for same seed
+    testing.assert_equal(output1_hpu, output2_hpu)
 
 
 if __name__ == '__main__':
