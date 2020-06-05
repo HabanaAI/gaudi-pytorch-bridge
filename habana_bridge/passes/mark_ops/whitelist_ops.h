2ce0fb08d2363bca6bd8634120034bd1bd7f01ee
Internal Developer <internal-developer@intel.com>
2020-06-05 16:02:17 +0300

    Ensuring matmul in torchscript calls mm instead of default matmul
    * Removing aten::matmul from whitelist ops since we do not support it for now
    
    Change-Id: Id54aa0df55badcb08886574fbde49cb0fe33d5ab
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- habana_bridge/passes/mark_ops/whitelist_ops.h
+++ habana_bridge/passes/mark_ops/whitelist_ops.h
@@ -49,7 +49,6 @@ const std::unordered_set<std::string> HabanaWhiteList::HabanaWhiteListOps = {
         "aten::convolution_backward_overrideable",
         "aten::fill_",
         "aten::mm",
-        "aten::matmul",
         "aten::addmm",
         "aten::native_batch_norm",
         "aten::native_batch_norm_backward",
