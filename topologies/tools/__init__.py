39843fbbdbe3a9f8c03f1bbfd95d3fb921573d6c
Internal Developer <internal-developer@intel.com>
2020-06-19 19:39:00 +0300

    Introduce TensorProbe tool, add config options
    Add support for TensorProbe tool to dump relevant tensors for, say,
    convergence analysis/Debug.
    
    This tool has two parts:
    
    1.ModelParamsDump: Dumps model level tensors like:
    model input, target, outout, loss, trainable params, gradients etc.
    
    2.Hooks
    Using the pytorch hooks framework, this tool can dump intermediate
    i/o tensors and gradients b/w pytorch nn modules of the model
    
    Use the following env variables to configure the tool:
    
    TP_HOOKS_ENABLE :
    1 - Enable; 0 - Disable
    
    TP_HOOKS_ITER_INDICES_TO_DUMP:
    iteration indices for which the intermediate tensors are to be dumped.
    'ALL' -dump for all iterations
    Or,
    list of comma separated indices - specific iters to dump.
    Eg:  0,1,6,9
    
    TP_MODEL_PARAM_DUMP_ENABLE :
    1 - Enable; 0 - Disable
    
    TP_MODEL_PARAM_DUMP_ITER_INDICES_TO_DUMP:
    Similar def as above, but for model level tensors.
    
    TP_DATA_DUMP_PATH:
    path of directory to dump the tensor data into
    
    Change-Id: Ie29bbf843124733076cd79796e23cbde937ee2e7
    Signed-off-by: Internal Developer <internal-developer@intel.com>

---
+++ topologies/tools/__init__.py
@@ -0,0 +1,3 @@
+from .model_info import *
+from .tensor_probe import *
+from .train_metadata import *
---
+++ topologies/tools/train_metadata.py
@@ -0,0 +1,64 @@
+import sys
+from .tensor_probe import *
+
+#TrainMetaData is to set additional configurations/flags on top of those offered by the standard training script.
+#example uses include specifying the number of steps to train rather than training a full epoch.
+class TrainMetaData():
+    def __init__(self, model, device):
+        self.current_train_step = 0
+        self.current_eval_step = 0
+        #use a large value for num_train_steps  by default so that if the num_train_steps is not set,
+        #the default behaviour of running training for all the iterations is maintained.
+        self.num_train_steps = sys.maxsize
+        self.num_eval_steps = sys.maxsize
+        self.logging = True #Enable - default
+        self.save_checkpt = True
+        self.ParamsDump = ModelParamsDump()
+        self.hooks = tp_hooks_register(model, device)
+
+    def increment_train_step(self):
+        self.current_train_step += 1
+        self.ParamsDump.increment_curr_iter_idx()
+        tp_hooks_increment_iteration_idx(self.hooks)
+        return self.current_train_step
+
+    def increment_eval_step(self):
+        self.current_eval_step += 1
+        return self.current_eval_step
+
+    def set_num_train_steps(self, x):
+        self.num_train_steps = x
+
+    def set_num_eval_steps(self, x):
+        self.num_eval_steps = x
+
+    def end_train(self):
+        if (self.current_train_step == self.num_train_steps):
+            return True
+        else:
+            return False
+
+    def end_eval(self):
+        if (self.current_eval_step == self.num_eval_steps):
+            return True
+        else:
+            return False
+
+    def end_train_n_eval(self):
+        if (self.end_train() and self.end_eval()):
+            return True
+        else:
+            return False
+
+    def set_logging(self, x):
+        self.logging = x
+
+    def is_logging(self):
+        return self.logging
+
+    #Enable/disable saving of checkpoint/model
+    def set_save_checkpoint_enable(self, enable=True):
+        self.save_checkpt = enable
+
+    def is_save_checkpoint(self):
+        return self.save_checkpt
