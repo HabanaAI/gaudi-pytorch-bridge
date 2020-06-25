27fa420439070cf407d81d71d9ae1aa8a8c0568f
Internal Developer <internal-developer@intel.com>
2020-06-25 14:13:19 +0300

    Batch norm uses buffer with Long Integer            datatype which is not supported in Synapse.
               Redirecting c10::ScalarType::Long to
               synDataType::syn_type_int32 for resnet50
    
    Change-Id: I3466e14913d45bf6cb2cfa39b01f1b9592e58a54

--- pytorch_helpers/habana_helpers/tensor_utils.cpp
+++ pytorch_helpers/habana_helpers/tensor_utils.cpp
@@ -101,7 +101,7 @@ synDataType habana_helpers::pytorch_to_synapse_type(
       {c10::ScalarType::Char, synDataType::syn_type_int8},
       {c10::ScalarType::Short, synDataType::syn_type_int16},
       {c10::ScalarType::Int, synDataType::syn_type_int32},
-      //   {c10::ScalarType::Long , synDataType::},
+      {c10::ScalarType::Long , synDataType::syn_type_int32},
       {c10::ScalarType::Float, synDataType::syn_type_float},
       //   {c10::ScalarType::Double , synDataType::},
       {c10::ScalarType::Bool, synDataType::syn_type_int8},
