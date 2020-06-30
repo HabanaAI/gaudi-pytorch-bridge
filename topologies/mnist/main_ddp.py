6209aca17e1eaaeb8c337fedd2dccfeb97210737
Internal Developer <internal-developer@intel.com>
2020-06-30 14:48:12 +0300

    Add more flags to distributed MNIST code
        - Add training and evaluation steps
        - Add more logging options
    
    Change-Id: Ib79ebb546b7c3efe161eef3ee1a2cf6669cb51f1
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- topologies/mnist/main_ddp.py
+++ topologies/mnist/main_ddp.py
@@ -11,6 +11,8 @@ import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.multiprocessing as mp
 import os
+import sys
+import time
 
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
@@ -22,6 +24,76 @@ def setup(rank, world_size):
 
 def cleanup():
     dist.destroy_process_group()
+
+class TrainMetaData():
+    def __init__(self):
+        self.current_train_step = 0
+        self.current_eval_step = 0
+        self.num_train_steps = sys.maxsize
+        self.num_eval_steps = sys.maxsize
+        self.logging = True #Enable - default
+
+    def increment_train_step(self):
+        self.current_train_step += 1
+        return self.current_train_step
+
+    def increment_eval_step(self):
+        self.current_eval_step += 1
+        return self.current_eval_step
+
+    def set_num_train_steps(self, x):
+        self.num_train_steps = x
+
+    def set_num_eval_steps(self, x):
+        self.num_eval_steps = x
+
+    def end_train(self):
+        if (self.current_train_step == self.num_train_steps):
+            return True
+        else:
+            return False
+
+    def end_eval(self):
+        if (self.current_eval_step == self.num_eval_steps):
+            return True
+        else:
+            return False
+
+    def end_train_n_eval(self):
+        if (self.end_train() and self.end_eval()):
+            return True
+        else:
+            return False
+
+    def set_logging(self, x):
+        self.logging = x
+
+    def is_logging(self):
+        return self.logging
+
+    @staticmethod
+    def accuracy(output, target, topk=(1,)):
+        """
+        Computes the accuracy over the k top
+        predictions for the specified values of k
+        """
+        with torch.no_grad():
+            maxk = max(topk)
+            batch_size = target.size(0)
+
+            _, pred = output.topk(maxk, 1, True, True)
+
+            pred = pred.t()
+            pred_cpu = torch.tensor(pred, device='cpu')
+            target_cpu = torch.tensor(target, device='cpu')
+
+            correct = pred_cpu.eq(target_cpu[None])
+
+            res = []
+            for k in topk:
+                correct_k = correct[:k].flatten().sum(dtype=torch.float32)
+                res.append(correct_k * (100.0 / batch_size))
+            return res
 
 class Net(nn.Module):
     def __init__(self):
@@ -42,13 +114,14 @@ class Net(nn.Module):
         return F.log_softmax(x, dim=1)
 
 
-def train(args, model, device, train_loader, optimizer, epoch, rank):
+def train(args, model, device, train_loader, optimizer, epoch, rank, trainMetaData):
     model.train()
     if rank == 0:
         with open('mnistpy.log', 'w') as file:  # reset file
             file.write('')
 
     for batch_idx, (data, target) in enumerate(train_loader):
+        iter_timer_start = time.time()
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
@@ -56,15 +129,26 @@ def train(args, model, device, train_loader, optimizer, epoch, rank):
         loss_cpu = loss
         loss.backward()
         optimizer.step()
+        iter_duration = time.time() - iter_timer_start
+        acc1, acc5 = trainMetaData.accuracy(output, target, topk=(1, 5))
+        log_msg = 'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f} '\
+                  'acc1: {:.6f} acc5: {:.6f} time: {:.6f}\n'.format(
+                  epoch, batch_idx * len(data), len(train_loader.dataset),
+                  100. * batch_idx / len(train_loader),
+                  loss_cpu.to(torch.device('cpu')).item(), acc1, acc5,
+                  iter_duration)
+
+        print(log_msg)
         # if batch_idx % args.log_interval == 0:
-        if rank == 0:
+        if rank == 0 and trainMetaData.is_logging():
             with open('mnistpy.log', 'a') as file:
-                file.write('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\n'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
-                    100. * batch_idx / len(train_loader), loss_cpu.to(torch.device('cpu')).item()))
+                file.write(log_msg)
+        trainMetaData.increment_train_step()
+        if trainMetaData.end_train() is True:
+            break
 
 
-def test(args, model, device, test_loader):
+def test(args, model, device, test_loader, trainMetaData):
     model.eval()
     test_loss = 0
     correct = 0
@@ -75,8 +159,15 @@ def test(args, model, device, test_loader):
             # sum up batch loss
             test_loss += F.nll_loss(output, target, reduction='sum').item()
             # get the index of the max log-probability
-            pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            output_cpu = output
+            pred = output_cpu.to(torch.device('cpu')).argmax(dim=1, keepdim=True)
+            target_cpu = target
+            target_cpu = target_cpu.to(torch.device('cpu'))
+            new_view = target_cpu.view_as(pred)
+            correct += pred.eq(new_view).sum().item()
+            trainMetaData.increment_eval_step()
+            if trainMetaData.end_eval() is True:
+                break
 
     test_loss /= len(test_loader.dataset)
 
@@ -107,11 +198,17 @@ def main(rank, world_size):
 
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
+    parser.add_argument('--num-train-steps', type=int, default=sys.maxsize, metavar='T',
+                        help='number of steps for training')
+    parser.add_argument('--num-eval-steps', type=int, default=sys.maxsize, metavar='E',
+                        help='number of steps for evaluation')
+    parser.add_argument('--no-log', action='store_true', default=False,
+                        help='disable log')
     args = parser.parse_args()
 
     use_habana = not args.no_habana
     if use_habana:
-        torch.ops.load_library("libhabana_pytorch_plugin.so")
+        torch.ops.load_library(os.environ.get("BUILD_ROOT_LATEST") + "/libhabana_pytorch_plugin.so")
 
     torch.manual_seed(args.seed)
 
@@ -139,12 +236,19 @@ def main(rank, world_size):
 
     model = Net().to(device)
     model = DDP(model)
+    trainMetaData = TrainMetaData()
+    trainMetaData.set_num_train_steps(args.num_train_steps)
+    trainMetaData.set_num_eval_steps(args.num_eval_steps)
+    log = not args.no_log
+    trainMetaData.set_logging(True if log else False)
     optimizer = optim.SGD(model.parameters(), lr=args.lr,
                           momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch, rank)
-        test(args, model, device, test_loader)
+        train(args, model, device, train_loader, optimizer, epoch, rank, trainMetaData)
+        test(args, model, device, test_loader, trainMetaData)
+        if (trainMetaData.end_train_n_eval()):
+            break
 
     if args.save_model and rank==0:
         torch.save(model.state_dict(), "mnist_cnn.pt")
