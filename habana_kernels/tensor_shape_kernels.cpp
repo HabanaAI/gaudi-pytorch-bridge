f438cdb24dd795d62d0f192eb6522e5f8929315c
Internal Developer <internal-developer@intel.com>
2020-07-06 12:22:18 +0300

    Use transpose as guid and fix params calc
    Change-Id: I09357c5e90ef3d0da5aeff3c1cfa036eea1879c3

--- habana_kernels/tensor_shape_kernels.cpp
+++ habana_kernels/tensor_shape_kernels.cpp
@@ -292,9 +292,7 @@ inline void recalc_strides(
  * @param dim0 - second dimension to swap
  ***************************************************************************/
 TransposeOperator::TransposeOperator(int device_id, c10::ScalarType scalarType)
-    : HabanaOperator(
-          "transpose_fwd_" +
-          habana_helpers::name_suffix_from_type(scalarType)) {
+    : HabanaOperator("transpose") {
   this->CreateSynContext(device_id);
 }
 
@@ -499,9 +497,7 @@ inline int is_hpu_supported_transpose_type(const c10::ScalarType pt_type) {
  * @param dims_ - permute dims array
  ************************************************************************/
 PermuteOperator::PermuteOperator(int device_id, c10::ScalarType scalarType)
-    : HabanaOperator(
-          "transpose_fwd_" +
-          habana_helpers::name_suffix_from_type(scalarType)) {
+    : HabanaOperator("transpose") {
   this->CreateSynContext(device_id);
 }
 
@@ -544,8 +540,8 @@ void PermuteOperator::AllocateAndAddSynapseNode(
   params.tensorDim = self.dim();
   // params.permute has to be populated in a reverse order for HPU FCD-LCD order
   for (int i = 0; i < self.dim(); i++) {
-    params.permutation[self.dim() - 1 - dims[i]] =
-        static_cast<TransposePermutationDim>(self.dim() - 1 - i);
+    params.permutation[i] = static_cast<TransposePermutationDim>(
+        self.dim() - dims[dims.size() - i - 1] - 1);
   }
   for (int i = self.dim(); i < MAX_DIMENSIONS_NUM; i++) {
     params.permutation[i] = static_cast<TransposePermutationDim>(i);
