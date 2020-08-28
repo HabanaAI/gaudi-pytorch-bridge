25d5c7a19ddbecbc863ed6b149dc129b714e8231
Internal Developer <internal-developer@intel.com>
2020-08-28 14:16:41 +0300

    Fix for checkpoint load for resnet
    Change-Id: I8c5037b0e30a5b600e436434ff03642dad0e5a9c

--- topologies/resnet/train.py
+++ topologies/resnet/train.py
@@ -211,10 +211,7 @@ def permute_params(model, to_filters_last):
                 if to_filters_last:
                     param.data = param.data.permute((2,3,1,0))
                 else:
-                    s = list(param.data.shape) # param data shape (KCRS)
-                    sh = [s[2], s[3], s[1],s[0]] # update to RSCK
-                    vh = torch.reshape(param.data, sh) # reshape the tensor in RSCK
-                    param.data = vh.permute((3,2,0,1)) # permute RSCK to KCRS
+                    param.data = param.data.permute((3,2,0,1)) # permute RSCK to KCRS
 
 
 #Data loader worker init function
