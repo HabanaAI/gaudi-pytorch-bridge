Fix broadcast call and resnet optimization
Script changes to avoid broadcast at the beginning of iteration
Distributed Data Parallel was not called correctly for Resnet
Add events for broadcast in rank 0 node also. Ported from TF

Change-Id: I661e7a512e655ddbcc3605891c5a5db4231f8066

topologies/demo/resnet/train.py
@@ -292,7 +292,7 @@ def main(args):
 
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_step_size, gamma=args.lr_gamma)
 
-    model_for_train = model_for_eval = model
+    model_for_eval = model
     if args.run_trace_mode:
         sample_trace_tensor = enable_tracing(device)
 
@@ -311,10 +311,13 @@ def main(args):
 
     if args.distributed:
         if args.device == 'habana':
-            model = torch.nn.parallel.DistributedDataParallel(model)
+            model = torch.nn.parallel.DistributedDataParallel(model, bucket_cap_mb=100, broadcast_buffers=False)
         else:
             model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
         model_without_ddp = model.module
+
+    model_for_train = model
+
     if args.resume:
         if(device==torch.device('habana')):
             permute_params(model_without_ddp, False)