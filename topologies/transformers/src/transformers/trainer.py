e281664c754e604c75737f5598ab643f7b33cdb9
Internal Developer <internal-developer@intel.com>
2020-10-01 09:28:15 +0300

    Support for enabling input dumping in BERT
    Change-Id: Ifed5abffd51f37cb2a2255bdeae62839508bced4
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- topologies/transformers/src/transformers/trainer.py
+++ topologies/transformers/src/transformers/trainer.py
@@ -524,6 +524,12 @@ class Trainer:
                     continue
 
                 device = self.args.device
+
+                input_keys = ('attention_mask', 'input_ids', 'token_type_ids')
+                input_dict = {k: inputs[k] for k in input_keys if k in inputs}
+                target = inputs['labels']
+
+                tp_probe_tensors_iteration_start(model, device, target, input_dict, self.trainMetaData.ParamsDump, False)
                 tr_loss_cpu, output_cpu  = self._training_step(model, inputs, optimizer)
                 tp_probe_tensors_iteration_end(model, device, output_cpu, tr_loss_cpu, self.trainMetaData.ParamsDump, False)
                 tr_loss += tr_loss_cpu
