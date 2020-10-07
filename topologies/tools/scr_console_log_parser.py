f2966f5b04b87490a705da42de09a79f2105f52f
Internal Developer <internal-developer@intel.com>
2020-10-07 18:30:39 +0300

    Tools: Resnet console log parser: Parse validation logs
    Add support for parsing validation phase logs and plotting
    loss/acc curves.
    Since Validation phase logs dont have 'epoch' entry, the
    epoch number from the preceeding train phase if available is
    used for plotting validation phase loss/acc curves. If there
    is no preceeding train phase, 0 is used as epoch number.
    
    Change-Id: I123710691eda6e7f67c1bf0a62f88515dfac5995
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- topologies/tools/scr_console_log_parser.py
+++ topologies/tools/scr_console_log_parser.py
@@ -12,6 +12,31 @@ matplotlib.use('agg')
 log_info_list = []
 
 def process(args):
+    def parse_line(line):
+        bd = {}
+        m = line.split('  ')
+        for i in m:
+            try:
+                x = i.split(':')
+                if x[0].strip() in ['loss', 'acc1', 'acc5']:
+                    y = x[1].strip().split(' ')
+                    bd[x[0].strip() + '_median'] = y[0].strip()
+                    bd[x[0].strip() + '_avg'] = re.sub('[\\(|\\)]', '', y[1].strip())
+                else:
+                    bd[x[0].strip()] = re.sub('[\\[|\\]]', '', x[1].strip())
+            except:
+                pass
+        return bd
+    def write_csv_file(out_file, vl):
+        # writing data into respective csv file
+        #out_file = os.path.join(args.out_dir, dev + '.csv')
+        #k = k + 1
+        headers = vl[0].keys()# vl is an array of dicts of parsed log entries. use the first entry to get the header
+        with open(out_file, 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer.writeheader()
+            writer.writerows(vl)
+
     # Parse all the log files and create some meta data like device, model, batchsize etc
     # if there are multiple log files of same device, append the device name with an order number.
     def log_meta_data(args):
@@ -52,41 +77,35 @@ def process(args):
     #=============================def log_meta_data(args) ends ==================================================
 
     log_meta_data(args)
-    out_file_list = []
+    out_file_list_t = []
+    out_file_list_v = []
     k =0
     for file in args.files:
-        vl = []
+        vl_t = [] # for training
+        vl_v = [] # for validation
+        epoch = 0
         with open(file.name) as f:
             for line in f:
-                bd = {}
                 if 'Epoch:' in line and 'eta' in line:
-                    if 'Test' in line: # TODO add handling of testi/val iteration parsing
-                        continue
-                    else:
-                        m = line.split('  ')
-                        for i in m:
-                            try:
-                                x = i.split(':')
-                                if x[0].strip() in ['loss', 'acc1', 'acc5']:
-                                    y = x[1].strip().split(' ')
-                                    bd[x[0].strip() + '_median'] = y[0].strip()
-                                    bd[x[0].strip() + '_avg'] = re.sub('[\\(|\\)]', '', y[1].strip())
-                                else:
-                                    bd[x[0].strip()] = re.sub('[\\[|\\]]', '', x[1].strip())
-                            except:
-                                pass
-                    vl.append(bd)
+                    bd = parse_line(line)
+                    vl_t.append(bd)
+                    epoch = bd['Epoch'] # Store epoch value for printing during eval/test log parse
+                if 'Test' in line and 'eta' in line:
+                    bd = parse_line(line)
+                    bd['Epoch'] = epoch # Test logs dont have epoch. so use the epoch number from train
+                    vl_v.append(bd)
+
 
         # writing data into respective csv file
-        out_file = os.path.join(args.out_dir, log_info_list[k][0] + '.csv')
-        k = k + 1
-        headers = vl[0].keys()# vl is an array of dicts of parsed log entries. use the first entry to get the header
-        with open(out_file, 'w', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=headers)
-            writer.writeheader()
-            writer.writerows(vl)
-        out_file_list.append(out_file)
-    return out_file_list
+        out_file_t = os.path.join(args.out_dir, log_info_list[k][0] + '_train.csv')
+        write_csv_file(out_file_t, vl_t)
+        out_file_list_t.append(out_file_t)
+
+        out_file_v = os.path.join(args.out_dir, log_info_list[k][0] + '_val.csv')
+        write_csv_file(out_file_v, vl_v)
+        out_file_list_v.append(out_file_v)
+
+    return out_file_list_t, out_file_list_v
 
 
 
@@ -106,59 +125,63 @@ def CumulativeSum(lists):
 
 def main(args):
 
+    def plot_graph(out_file_list, tag):
+        fig1 = plt.figure(1)
+        fig2 = plt.figure(2)
+        fig3 = plt.figure(3)
+        tag_l = tag.lower()
+
+        i = 0
+        for log_info in log_info_list:
+            out_file = out_file_list[i]
+            i = i +1
+            dataframe = pd.read_csv(out_file)
+            #dataframe = dataframe.dropna()
+            x = pd.to_numeric(dataframe.Epoch)
+            y = pd.to_numeric(dataframe.loss_avg)
+            w = pd.to_numeric(dataframe.acc1_avg)
+            z = pd.to_numeric(dataframe.acc5_avg)
+            plt.figure(1)
+            plt.plot(y, label='id %s' % y)
+            plt.xlabel(x.name)
+            plt.ylabel(y.name)
+            plt.legend([i[0] for i in log_info_list], loc="upper right")
+            tick_values = list(CountFrequency(x.values.tolist()).values())
+            xaxis_values = tuple(set(x))
+            plt.xticks(np.array(CumulativeSum(tick_values)), xaxis_values)
+            plt.grid(True, linestyle='dashed')
+            plt.title(tag + ' : '+ y.name + ' ' + log_info[1])
+            plt.savefig(os.path.join(args.out_dir, y.name + '_plot_' + tag_l + '.png'), dpi=300)
+            plt.figure(2)
+            plt.plot(w, label='id %s' % w)
+            plt.xlabel(x.name)
+            plt.ylabel(w.name)
+            plt.legend([i[0] for i in log_info_list], loc="lower right")
+            tick_values = list(CountFrequency(x.values.tolist()).values())
+            xaxis_values = tuple(set(x))
+            plt.xticks(np.array(CumulativeSum(tick_values)), xaxis_values)
+            plt.grid(True, linestyle='dashed')
+            plt.title(tag + ' : '+ w.name + ' ' + log_info[1])
+            plt.savefig(os.path.join(args.out_dir, w.name + '_plot_' + tag_l + '.png'), dpi=300)
+            plt.figure(3)
+            plt.plot(z, label='id %s' % z)
+            plt.xlabel(x.name)
+            plt.ylabel(z.name)
+            plt.legend([i[0] for i in log_info_list], loc="lower right")
+            tick_values = list(CountFrequency(x.values.tolist()).values())
+            xaxis_values = tuple(set(x))
+            plt.xticks(np.array(CumulativeSum(tick_values)), xaxis_values)
+            plt.grid(True, linestyle='dashed')
+            plt.title(tag + ' : '+ z.name + ' ' + log_info[1])
+            plt.savefig(os.path.join(args.out_dir, z.name + '_plot_' + tag_l + '.png'), dpi=300)
+        plt.close(fig1)
+        plt.close(fig2)
+        plt.close(fig3)
+
     os.makedirs(args.out_dir, exist_ok=True)
-    out_file_list = process(args)
-
-    fig1 = plt.figure(1)
-    fig2 = plt.figure(2)
-    fig3 = plt.figure(3)
-
-    i = 0
-    for log_info in log_info_list:
-        out_file = out_file_list[i]
-        i = i +1
-        dataframe = pd.read_csv(out_file)
-        dataframe = dataframe.dropna()
-        x = pd.to_numeric(dataframe.Epoch)
-        y = pd.to_numeric(dataframe.loss_avg)
-        w = pd.to_numeric(dataframe.acc1_avg)
-        z = pd.to_numeric(dataframe.acc5_avg)
-        plt.figure(1)
-        plt.plot(y, label='id %s' % y)
-        plt.xlabel(x.name)
-        plt.ylabel(y.name)
-        plt.legend([i[0] for i in log_info_list], loc="upper right")
-        tick_values = list(CountFrequency(x.values.tolist()).values())
-        xaxis_values = tuple(set(x))
-        plt.xticks(np.array(CumulativeSum(tick_values)), xaxis_values)
-        plt.grid(True, linestyle='dashed')
-        plt.title(y.name + ' ' + log_info[1])
-        plt.savefig(os.path.join(args.out_dir, x.name + '_' + y.name + '_plot.png'), dpi=300)
-        plt.figure(2)
-        plt.plot(w, label='id %s' % w)
-        plt.xlabel(x.name)
-        plt.ylabel(w.name)
-        plt.legend([i[0] for i in log_info_list], loc="lower right")
-        tick_values = list(CountFrequency(x.values.tolist()).values())
-        xaxis_values = tuple(set(x))
-        plt.xticks(np.array(CumulativeSum(tick_values)), xaxis_values)
-        plt.grid(True, linestyle='dashed')
-        plt.title(w.name + ' ' + log_info[1])
-        plt.savefig(os.path.join(args.out_dir, x.name + '_' + w.name + '_plot.png'), dpi=300)
-        plt.figure(3)
-        plt.plot(z, label='id %s' % z)
-        plt.xlabel(x.name)
-        plt.ylabel(z.name)
-        plt.legend([i[0] for i in log_info_list], loc="lower right")
-        tick_values = list(CountFrequency(x.values.tolist()).values())
-        xaxis_values = tuple(set(x))
-        plt.xticks(np.array(CumulativeSum(tick_values)), xaxis_values)
-        plt.grid(True, linestyle='dashed')
-        plt.title(z.name + ' ' + log_info[1])
-        plt.savefig(os.path.join(args.out_dir, x.name + '_' + z.name + '_plot.png'), dpi=300)
-    plt.close(fig1)
-    plt.close(fig2)
-    plt.close(fig3)
+    out_file_list_t, out_file_list_v = process(args)
+    plot_graph(out_file_list_t, 'Training')
+    plot_graph(out_file_list_v, 'Validation')
 
 
 if __name__ == '__main__':
