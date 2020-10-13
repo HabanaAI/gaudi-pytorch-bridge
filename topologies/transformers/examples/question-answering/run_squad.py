bcfffdf9a6a89ca4dd77ea7d2a3e1315a5608191
Internal Developer <internal-developer@intel.com>
2020-10-13 14:27:21 +0300

    :Move feature_index tensor to CPU
    enumerate() is a native function which is available on CPU ,
    So feature_index has to be transferred such a way that enumerate
    executed on CPU
    
    Change-Id: I5df83be01c6198a33a2815715831f1b0d17beffc
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- topologies/transformers/examples/question-answering/run_squad.py
+++ topologies/transformers/examples/question-answering/run_squad.py
@@ -383,6 +383,7 @@ def evaluate(args, model, tokenizer, prefix=""):
                     )
 
             outputs = model(**inputs)
+            feature_indices = feature_indices.to("cpu")
 
         for i, feature_index in enumerate(feature_indices):
             eval_feature = features[feature_index.item()]
