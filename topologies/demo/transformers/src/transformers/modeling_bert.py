2baf0597ae33a7cc08f52b0a40a351e7179313f0
Internal Developer <internal-developer@intel.com>
2020-10-28 17:12:37 +0200

    Select op implemention change
    Select Op is implemented using index_select and
    squeeze, since in the backward path we use select
    backward which requires strided mem copy
    
    Change-Id: Ia382a3da64f289374caa1e7d14b158f611575211
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- topologies/demo/transformers/src/transformers/modeling_bert.py
+++ topologies/demo/transformers/src/transformers/modeling_bert.py
@@ -464,7 +464,8 @@ class BertPooler(nn.Module):
     def forward(self, hidden_states):
         # We "pool" the model by simply taking the hidden state corresponding
         # to the first token.
-        first_token_tensor = hidden_states[:, 0]
+        first_token_tensor = hidden_states.index_select(1,
+                torch.tensor([0]).to(torch.device(hidden_states.device))).squeeze(1)
         pooled_output = self.dense(first_token_tensor)
         pooled_output = self.activation(pooled_output)
         return pooled_output
