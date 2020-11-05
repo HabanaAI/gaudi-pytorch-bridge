12755c145875a16cc0571587693cc9e2118aef93
Internal Developer <internal-developer@intel.com>
2020-11-05 08:34:22 +0200

    Enable Pin memory usage in Resnet
    [Resnet] turn on the pin_memory flag and
    also updated to import hb_torch only when device
    is habana
    
    Change-Id: Ie3c81cb0e047646ab8138016f863c9fbbc6c2fe4
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- topologies/resnet/train.py
+++ topologies/resnet/train.py
@@ -141,11 +141,12 @@ def _get_cache_path(filepath):
 
 def enable_tracing(device):
     with torch.jit.optimized_execution(True):
-        import hb_torch
         torch._C._jit_override_can_fuse_on_cpu(False)
         torch._C._jit_set_profiling_executor(False)
         torch._C._jit_set_profiling_mode(False)
-        hb_torch.enable()
+        if(device==torch.device('habana')):
+            import hb_torch
+            hb_torch.enable()
         sample_trace_tensor = torch.zeros(args.batch_size, 3, 224, 224).to(device)
         return sample_trace_tensor
 
@@ -289,11 +290,11 @@ def main(args):
                                                                        args.cache_dataset, args.distributed)
         data_loader = torch.utils.data.DataLoader(
             dataset, batch_size=args.batch_size,
-            sampler=train_sampler, num_workers=args.workers, worker_init_fn=dl_worker_init_fn(seed), pin_memory=False, drop_last=True)
+            sampler=train_sampler, num_workers=args.workers, worker_init_fn=dl_worker_init_fn(seed), pin_memory=True, drop_last=True)
 
         data_loader_test = torch.utils.data.DataLoader(
             dataset_test, batch_size=test_batch_size,
-            sampler=test_sampler, num_workers=args.workers, worker_init_fn=dl_worker_init_fn(seed), pin_memory=False, drop_last=True)
+            sampler=test_sampler, num_workers=args.workers, worker_init_fn=dl_worker_init_fn(seed), pin_memory=True, drop_last=True)
     else:
         data_loader      = ImageRandomDataLoader(batch_size=args.batch_size, train=True, drop_last=True)
         data_loader_test = ImageRandomDataLoader(batch_size=test_batch_size, train=False, drop_last=True)
