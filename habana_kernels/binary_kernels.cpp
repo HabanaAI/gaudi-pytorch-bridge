149c9c04da57a409dc6b74260e6d02e86a18c972
Internal Developer <internal-developer@intel.com>
2020-12-04 08:00:15 +0200

    Fix masked_scale cast issue
    The cast call in masked_call shouldn't be
    attempted if self and mask dtypes are same.
    
    Change-Id: I8134ee9ba07be8eb378cc513d89cbc7cce97d455

--- habana_kernels/binary_kernels.cpp
+++ habana_kernels/binary_kernels.cpp
@@ -891,9 +891,12 @@ Tensor pow_scalar_tensor_hpu(Scalar other, const Tensor& self) {
 Tensor masked_scale_hpu(const Tensor& self, const Tensor& mask, double scale) {
   PT_KERNEL_BEGIN;
 
-  auto tt_mul_out =
-      at::mul(self, habana_helpers::hpu_cast_tensor(mask, self.dtype()));
-  auto output = at::mul(tt_mul_out, static_cast<float>(scale));
+  auto tt_mul_out = at::mul(
+      self,
+      (self.dtype() != mask.dtype())
+          ? habana_helpers::hpu_cast_tensor(mask, self.dtype())
+          : mask);
+  auto output = at::mul(tt_mul_out, Scalar(scale));
   PT_KERNEL_END;
   return output;
 }
