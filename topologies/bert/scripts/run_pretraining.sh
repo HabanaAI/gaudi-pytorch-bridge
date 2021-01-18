b68c5454531b893096452202c814a481e5ac5630
Internal Developer <internal-developer@intel.com>
2021-01-18 11:21:42 +0200

    BERT:PT:Set batch size seen by optimizer on single chip as 8192
    Update the train_batchsize and gradient_acc_size to make effective
    batch size seen by optimizer as 8192 at the same time keeping the
    per device batch size as 32. This is done to better match the
    original GPU setting for optimizer batch size.
    
    Change-Id: I70d85b031219320221bf3727db5e7cf29eb72bc2
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- topologies/bert/scripts/run_pretraining.sh
+++ topologies/bert/scripts/run_pretraining.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-train_batch_size=${1:-4096}
+train_batch_size=${1:-8192}
 learning_rate=${2:-"6e-3"}
 precision=${3:-"fp32"}
 n_pu=${4:-1} #Number of processing units
@@ -23,7 +23,7 @@ save_checkpoint_steps=${7:-200}
 resume_training=${8:-"false"}
 create_logfile=${9:-"true"}
 accumulate_gradients=${10:-"true"}
-gradient_accumulation_steps=${11:-128}
+gradient_accumulation_steps=${11:-256}
 seed=${12:-12439}
 job_name=${13:-"bert_lamb_pretraining"}
 allreduce_post_accumulation=${14:-"true"}
