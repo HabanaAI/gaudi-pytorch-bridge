c45371c077754f19a158088acd372d98bfdf8a75
Internal Developer <internal-developer@intel.com>
2021-01-20 17:16:08 +0200

    lt scalar support for lazy mode
    Change-Id: I25aa16967d6ce18ba2787544fc46e27102c3281d
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- habana_kernels/lazy_kernels.cpp
+++ habana_kernels/lazy_kernels.cpp
@@ -889,14 +889,54 @@ Tensor eq_tensor_scalar_hpu_lazy(Tensor& self, Scalar other) {
   HABANA_ASSERT(0);
   return eq_tensor_scalar_hpu(self, other);
 };
+
 Tensor lt_scalar_hpu_lazy(Tensor& self, Scalar other) {
-  HABANA_ASSERT(0);
-  return lt_scalar_hpu(self, other);
+  PT_LAZY_TRACE;
+  auto hl_self = habana_lazy::GetOrCreateHbLazyTensor(self, c10::kHABANA);
+  auto hl_other = habana_lazy::GetIrValueForScalar(other);
+
+  auto node = habana_lazy::ir::Node::Create(
+      Symbol::fromQualString("aten::lt"), {hl_self.GetIrValue(), hl_other});
+
+  auto result = at::native::empty_hpu_lazy(
+      self.sizes(),
+      self.options().dtype(c10::ScalarType::Bool),
+      self.suggest_memory_format(),
+      false);
+
+  auto hl_result = habana_lazy::GetHbLazyTensor(result);
+  habana_lazy::ir::Value& out = hl_result.CurrentIrValue();
+  out.m_index = 0;
+  out.SetNode(node);
+
+  std::vector<at::Tensor> input_pt_vec{self};
+  node->AddInputPtTensors(input_pt_vec);
+
+  return result;
 };
+
 Tensor lt_tensor_hpu_lazy(Tensor& self, Tensor& other) {
-  HABANA_ASSERT(0);
-  return lt_tensor_hpu(self, other);
+  PT_LAZY_TRACE;
+  auto hl_self = habana_lazy::GetOrCreateHbLazyTensor(self, c10::kHABANA);
+  auto hl_other = habana_lazy::GetOrCreateHbLazyTensor(other, c10::kHABANA);
+
+  auto node = habana_lazy::ir::Node::Create(
+      Symbol::fromQualString("aten::lt"),
+      {hl_self.GetIrValue(), hl_other.GetIrValue()});
+  auto result = at::native::empty_hpu_lazy(
+      self.sizes(),
+      self.options().dtype(c10::ScalarType::Bool),
+      self.suggest_memory_format(),
+      false);
+  auto hl_result = habana_lazy::GetHbLazyTensor(result);
+  habana_lazy::ir::Value& out = hl_result.CurrentIrValue();
+  out.m_index = 0;
+  out.SetNode(node);
+  std::vector<at::Tensor> input_pt_vec{self, other};
+  node->AddInputPtTensors(input_pt_vec);
+  return result;
 };
+
 Tensor convolution_hpu_lazy(
     const Tensor& input,
     const Tensor& weight,
--- tests/cpp/lazy_compare_kernel_tests.cpp
+++ tests/cpp/lazy_compare_kernel_tests.cpp
@@ -23,3 +23,40 @@ class LazyCompareKernelTest : public ::testing::Test {
   }
 };
 
+TEST_F(LazyCompareKernelTest, LtScalarTest) {
+  setenv("PT_HPU_LAZY_MODE", "1", 1);
+
+  torch::Tensor A = torch::rand({2, 2}, torch::requires_grad(false));
+  float compVal = 1.1f;
+  auto out_cpu = torch::lt(A, compVal);
+
+  auto hA = A.to(torch::kHABANA);
+  auto result = torch::lt(hA, compVal);
+  torch::Tensor out_hpu = result.to(torch::kCPU);
+
+  EXPECT_EQ(
+      allclose(out_cpu.to(torch::kFloat), out_hpu.to(torch::kFloat)), true);
+
+  unsetenv("PT_HPU_LAZY_MODE");
+}
+
+TEST_F(LazyCompareKernelTest, LtTensorTest) {
+  setenv("PT_HPU_LAZY_MODE", "1", 1);
+
+  const std::vector<int64_t> dimentions{5, 3, 4};
+
+  torch::Tensor A = torch::randn(dimentions);
+  torch::Tensor B = torch::randn(dimentions);
+
+  auto expected = torch::lt(A, B);
+  auto hA = A.to(torch::kHABANA);
+  auto hB = B.to(torch::kHABANA);
+
+  auto result = torch::lt(hA, hB);
+  torch::Tensor habanaGenerated = result.to(torch::kCPU);
+
+  EXPECT_EQ(
+      allclose(expected.to(torch::kInt8), habanaGenerated.to(torch::kInt8)),
+      true);
+  unsetenv("PT_HPU_LAZY_MODE");
+}
