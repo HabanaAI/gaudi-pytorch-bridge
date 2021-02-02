9e6886b5ed893e578c0380d2b46ca82dae559449
Internal Developer <internal-developer@intel.com>
2021-02-02 12:13:43 +0200

    NLLloss UT ignore_index changes
    Add support for ignore_index in python UT
    
    Change-Id: I3ad0f8eb55b77858317b7333081189f068c8be1c

--- tests/test_hpu_loss.py
+++ tests/test_hpu_loss.py
@@ -8,31 +8,54 @@ test_case_list = [
     (500, 10,),
 ]
 
-
 @pytest.mark.parametrize("N, C", test_case_list)
-def test_hpu_nllloss(N, C):
+@pytest.mark.parametrize("ignore_index", [-1, -100])
+def test_hpu_nllloss(N, C, ignore_index):
     # TODO: extend that test to all features
     kernel = F.nll_loss
+    ignore_index_val = ignore_index
+    t1 = torch.randint(low=0, high=C - 1, size=(N,))
+    t_in = torch.empty_like(t1)
+    if ignore_index_val == -100:
+        t_in = t1
+    else:
+        #Fill a percentage of target tensor wih ignore_index_val
+        fill_thresh = round(0.3*C)
+        t2 = t1 < torch.tensor(fill_thresh)
+        t_in = t1.masked_fill(t2, ignore_index_val)
+
     kernel_params = {
         "input": torch.randn(N, C),
-        "target": torch.randint(low=0, high=C - 1, size=(N,)),
+        "target": t_in,
+        "ignore_index": ignore_index_val,
     }
     evaluate_fwd_kernel(kernel=kernel, kernel_params=kernel_params)
 
-
 @pytest.mark.parametrize("N, C", test_case_list)
-def test_hpu_nllloss_fwd_bwd(N, C):
+@pytest.mark.parametrize("ignore_index", [-1, -100])
+def test_hpu_nllloss_fwd_bwd(N, C, ignore_index):
     # TODO: extend that test to all features
     kernel = F.nll_loss
+    ignore_index_val = ignore_index
+    t1 = torch.randint(low=0, high=C - 1, size=(N,))
+    t_in = torch.empty_like(t1)
+    if ignore_index_val == -100:
+        t_in = t1
+    else:
+        #Fill a percentage of target tensor wih ignore_index_val
+        fill_thresh = round(0.3*C)
+        t2 = t1 < torch.tensor(fill_thresh)
+        t_in = t1.masked_fill(t2, ignore_index_val)
+
     kernel_params_fwd = {
         "input": torch.randn(N, C, requires_grad=True),
-        "target": torch.randint(low=0, high=C - 1, size=(N,)),
+        "target": t_in,
+        "ignore_index": ignore_index_val,
     }
     bwd_tensors = [torch.randn(1)]
     evaluate_fwd_bwd_kernel(
         kernel=kernel, tensor_list_bwd=bwd_tensors, kernel_params_fwd=kernel_params_fwd
     )
-
 
 @pytest.mark.parametrize("N, C", test_case_list)
 @pytest.mark.parametrize("mode", ["mean", "sum", "none"])
