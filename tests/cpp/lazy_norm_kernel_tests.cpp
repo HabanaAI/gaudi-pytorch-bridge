c70efe4ba0056b7f941c9b5798612ef513bf684e
Internal Developer <internal-developer@intel.com>
2021-04-09 11:28:59 +0530

    Add instance norm test
    Change-Id: I702a316062c169f3d6d0b26bdf26472835e93a42

--- tests/cpp/lazy_norm_kernel_tests.cpp
+++ tests/cpp/lazy_norm_kernel_tests.cpp
@@ -209,6 +209,32 @@ TEST_F(LazyNormKernelTest, BatchNormBackwardExecute) {
   EXPECT_EQ(allclose(result_lazy, result_cpu, 0.01, 0.01), true);
 }
 
+TEST_F(LazyNormKernelTest, InstanceNorm) {
+  auto input_tensor = torch::randn(
+      {10, 3, 4, 2}, torch::dtype(torch::kFloat).requires_grad(false));
+  torch::Tensor tHabanaX = input_tensor.to(torch::kHABANA);
+  at::Tensor weight =
+      torch::randn(3, torch::dtype(torch::kFloat).requires_grad(false));
+  torch::Tensor tWeight = weight.to(torch::kHABANA);
+  at::Tensor bias =
+      torch::randn(3, torch::dtype(torch::kFloat).requires_grad(false));
+  torch::Tensor tBias = bias.to(torch::kHABANA);
+  auto mean = torch::randn(3, torch::dtype(torch::kFloat).requires_grad(false));
+  torch::Tensor tHabanaMean = mean.to(torch::kHABANA);
+  auto var = torch::ones(3, torch::dtype(torch::kFloat).requires_grad(false));
+  torch::Tensor tHabanaVar = var.to(torch::kHABANA);
+
+  constexpr float mom = 0.1;
+  constexpr float eps = 1e-5;
+  auto result_cpu = torch::instance_norm(
+      input_tensor, weight, bias, mean, var, true, mom, eps, false);
+
+  auto result_lazy = torch::instance_norm(
+      tHabanaX, tWeight, tBias, tHabanaMean, tHabanaVar, true, mom, eps, false);
+
+  EXPECT_EQ(allclose(result_lazy.to("cpu"), result_cpu, 0.01, 0.01), true);
+}
+
 TEST_F(LazyNormKernelTest, NormScalarTest) {
   torch::Tensor A = torch::randn({2, 2}, torch::requires_grad(false));
   torch::Tensor hA = A.to(torch::kHABANA);
