786d18154cf2856f7fda1741919901bbbf0600bc
Internal Developer <internal-developer@intel.com>
2021-06-01 19:02:28 +0300

    Dataload profile tool update
    Change-Id: Iee10aea8a8fb11b622d6ab66a11c7e8001eb8548
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- pytorch_helpers/dataloader/profile_data_loading.py
+++ pytorch_helpers/dataloader/profile_data_loading.py
@@ -1,4 +1,11 @@
 # Copyright (c) 2021, Habana Labs Ltd.  All rights reserved.
+#
+# help:
+#  mpirun -n 8 --bind-to core --map-by slot:PE=7 --report-bindings \
+#       --allow-run-as-root python -u \
+#       pytorch-integration/pytorch_helpers/dataloader/profile_data_loading.py  \
+#       --dl-type='MP' --dl-workers=5 --profile 2>&1 | tee 8-worker-dl.log
+#
 
 import torch
 import torch.utils.data as torch_data
@@ -7,46 +14,168 @@ import torchvision.datasets as datasets
 import pathlib
 import os
 import time
-import matplotlib
-import matplotlib.pyplot as plt
+import sys
+import cProfile, pstats
+import json
+import argparse
 
-X = []
-Y = []
+from mpi4py import MPI
+global mpi_comm
+mpi_comm = MPI.COMM_WORLD
 
-def save_points(x, y):
-    X.append(x)
-    Y.append(y)
-    print(x, y)
+dl_args = {}
 
-def profile_pytorch_data_loader_for_resnet():
-    matplotlib.use( 'tkagg' )
+class ImageRandomDataLoader():
+    def __init__(self, batch_size, num_steps, train=True, drop_last=False):
+
+        def get_val(env_var, default_val):
+            val = default_val
+            val_str = os.environ.get(env_var)
+            if val_str is not None:
+                val = int(val_str)
+            return val
+
+        self.batch_size = batch_size
+        self.channels   = C = get_val('CDL_IMG_NUM_CHANNELS', 3)
+        self.height     = H = get_val('CDL_IMG_HEIGHT', 224)
+        self.width      = W = get_val('CDL_IMG_WIDTH', 224)
+        self.num_classes = get_val('CDL_NUM_CLASSES', 1000)
+        if train:
+            phase = 'train'
+            self.dataset_size = get_val('CDL_TRAIN_DATASET_SIZE', batch_size*num_steps) #num images; default 10 iterations
+
+        self.train = train
+        self.num_batches = self.dataset_size // self.batch_size
+        self.curr_batch_no = 0
+        self.partial_batch_size = 0
+        if not drop_last:
+            self.partial_batch_size = self.dataset_size % self.batch_size
+
+        if self.partial_batch_size:
+            self.num_batches = self.num_batches + 1
+        print("ImageRandomDataLoader: ", "phase=", phase, " N=", batch_size, " C=", C, " H=", H," W=", W,
+                " num classes=", self.num_classes, " dataset size=", self.dataset_size,
+                "num batches(iterations) per epoch=",self.num_batches, " drop_last =", drop_last)
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        return self.num_batches
+
+    def __next__(self):
+        self.curr_batch_no = self.curr_batch_no + 1
+        if self.curr_batch_no <= self.num_batches :
+            batch_size = self.batch_size
+            if self.curr_batch_no == self.num_batches and self.partial_batch_size:
+                batch_size = self.partial_batch_size
+
+            target = torch.randint(0, self.num_classes, (batch_size,))
+            image = torch.randn(batch_size, self.channels, self.height, self.width)
+            return image, target
+        else:
+            self.curr_batch_no = 0
+            raise StopIteration
+
+def init_data_loader():
+    wsize = mpi_comm.Get_size()
+    rank = mpi_comm.Get_rank()
+    print(" world_size :: ", wsize)
+    print(" local rank :: ", rank)
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
     transform = transforms.Compose([
-        transforms.RandomResizedCrop(224),
+        transforms.RandomResizedCrop(224,interpolation=2),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
+        normalize
     ])
 
-    train_dir = pathlib.Path('/software/data/pytorch/imagenet/ILSVRC2012/')
-    train = datasets.ImageFolder(train_dir, transform)
+    #torch.multiprocessing.set_start_method('forkserver')
+    #torch.multiprocessing.set_start_method('spawn')
+    train_dir = pathlib.Path('/root/data/pytorch/imagenet/ILSVRC2012/')
+    dataset = datasets.ImageFolder(train_dir, transform)
+    bs = 256
+    total_images = 1280000
+    num_steps = total_images/bs/wsize
+    workers = dl_args['dl-workers']
+    print("number of DL workers :: ", workers)
+    print("number of steps :: ", num_steps)
+    print("batch size :: ", bs)
 
-    dataloader = torch.utils.data.DataLoader(train, batch_size=128, shuffle=True, num_workers=32)
+    dl_type = dl_args['dl-type']
+    if dl_type == 'SYN':
+        print("synthetic dataset selected")
+        dataloader = ImageRandomDataLoader(batch_size=bs,num_steps=num_steps, train=True)
+    elif dl_type == 'MT':
+        try:
+            import habana_torch_dataloader
+        except ImportError:
+            assert False, "Could Not import habana_torch_dataloader"
+        print("Multi-Threading DL with imagenet dataset selected")
+        dataloader = habana_torch_dataloader.DataLoader(dataset, batch_size=bs, num_workers=workers, shuffle=True)
+    else:
+        print("Multi-process DL with imagenet dataset selected")
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=bs, num_workers=workers, shuffle=True)
 
+    return dataloader, num_steps, bs, rank
+
+
+def test_pytorch_data_loader_for_resnet(dataloader, num_steps, bs, rank):
     t_sum = 0
-    last_time = time.time()
+    t_ips = 0
+    start_time = time.time()
 
     for i, data in enumerate(dataloader):
-        t = time.time()
-        t_diff = t - last_time
+        dl_time = time.time()
+        t_diff = dl_time - start_time
         t_sum += t_diff
-        ##save_points(t_diff, t_sum)
-        save_points(i, t_diff)
-        last_time = t
+        start_time = dl_time
+        ips =  bs/t_diff
+        t_ips += ips
 
-        if i >= 100:
+        if rank == 0:
+            print("iteration : ",i, " ips : ",ips)
+
+        if i >= num_steps:
             break
 
+    print("Avg ips per card = ", t_ips/num_steps)
     print("Total time take = ", t_sum)
 
+def handle_args():
+    parser = argparse.ArgumentParser(description="""Run DataLoader test""")
+
+    parser.add_argument("--dl-type", type=str, default='MP',
+                        help="supported types ::, 'MT','MP' (default),'SYN'")
+    parser.add_argument("--dl-workers", type=int, default='5',
+                        help="number of DL read workers, default:5")
+    parser.add_argument("--profile", action="store_true",
+                        help="enable cprofile for the parent function")
+    args = parser.parse_args()
+    if args.dl_type:
+        dl_args['dl-type'] = args.dl_type
+    if args.dl_workers:
+        dl_args['dl-workers'] = args.dl_workers
+    if args.profile:
+        dl_args['profile'] = True
+    else:
+        dl_args['profile'] = False
+
 if __name__=='__main__':
-    profile_pytorch_data_loader_for_resnet()
-    #plt.plot(X,Y)
-    #plt.show()
+    handle_args()
+    can_profile = dl_args['profile']
+    print("profiling :: ", can_profile)
+
+    dataloader, num_steps, bs, rank = init_data_loader()
+
+    if can_profile:
+       pr = cProfile.Profile()
+       pr.enable()
+       test_pytorch_data_loader_for_resnet(dataloader, num_steps, bs, rank)
+       pr.disable()
+       stats = pstats.Stats(pr).sort_stats('tottime')
+       if mpi_comm.Get_rank() == 0:
+          stats.print_stats()
+    else:
+       test_pytorch_data_loader_for_resnet(dataloader, num_steps, bs, rank)
