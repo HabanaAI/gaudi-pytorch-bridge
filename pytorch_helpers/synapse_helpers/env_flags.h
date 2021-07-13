6bda020283b5f6b8be63fd87430a6070b2fe6eeb
Internal Developer <internal-developer@intel.com>
2021-07-13 15:59:32 +0300

    Enable pool strategy default to 5
    Enable pool strategy 5 as default for
    device memory pool.
    
    Change-Id: Ie9cb37c08a69de32ca90b6cefe6458a0ddc83d90
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- pytorch_helpers/synapse_helpers/env_flags.h
+++ pytorch_helpers/synapse_helpers/env_flags.h
@@ -95,7 +95,7 @@ struct PT_HABANA_POOL_SIZE : public std::numeric_limits<unsigned long> {
 };
 
 struct PT_HPU_POOL_STRATEGY : public std::numeric_limits<unsigned> {
-  static constexpr unsigned default_value = 3;
+  static constexpr unsigned default_value = 5;
 };
 
 struct PT_HPU_POOL_MAX_MERGE_COUNT : public std::numeric_limits<unsigned> {
