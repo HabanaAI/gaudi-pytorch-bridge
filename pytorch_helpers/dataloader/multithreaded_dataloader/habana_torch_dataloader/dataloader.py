7930e3fc1e92254fa3314c7939690fc8cd13d935
Internal Developer <internal-developer@intel.com>
2021-08-10 15:32:57 +0300

    align usage of torch._six with pytorch v1.9
    For pytorch v1.9 some redundant code was cleaned up
    and required alignment with the usage of torch._six
    in dataloader.py and worker.py.
    
    Change-Id: I2767b6e213966d23c46fef480e7d0e28e9a212d7
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- pytorch_helpers/dataloader/multithreaded_dataloader/habana_torch_dataloader/dataloader.py
+++ pytorch_helpers/dataloader/multithreaded_dataloader/habana_torch_dataloader/dataloader.py
@@ -16,7 +16,7 @@ import multiprocessing as python_multiprocessing
 import torch
 import torch.multiprocessing as multiprocessing
 from torch._utils import ExceptionWrapper
-from torch._six import queue, string_classes
+import queue
 
 from torch.utils.data.dataset import IterableDataset, Dataset
 from torch.utils.data.sampler import Sampler, SequentialSampler, RandomSampler, BatchSampler
