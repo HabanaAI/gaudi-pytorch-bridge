6db4663171863a171977ec42c551fe4ce6f78260
Internal Developer <internal-developer@intel.com>
2021-09-24 10:15:51 +0300

    Implements div_rounding_mode(...) in lazy mode with  autogen
    Change-Id: I92c6af4d8c67e6bdbc4128e445d86229739539ab
    Signed-off-by: Internal Developer <internal-developer@intel.com>

---
+++ hpu_ops/div_round_gen.cpp
@@ -0,0 +1,190 @@
+/******************************************************************************
+ * Copyright (C) 2021 HabanaLabs, Ltd.
+ * All Rights Reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ ******************************************************************************
+ */
+
+#include "../habana_kernels/lazy_kernels_declarations.h"
+#include "generated/hpu_op.h"
+#include "habana_kernels/binary_kernels.h"
+
+// TODO: Need to fetch pytorch default type dynamically
+// pytorch default type and handle other types as default type
+#define PYTORCH_DEFAULT_TYPE c10::ScalarType::Float
+
+// Except bfloat16, all other types are computed in following type
+#define COMMON_COMPUTATION_TYPE_TPC c10::ScalarType::Float
+
+// For use in div_rounding_mode
+#define StrModeTrue ""
+#define StrModeFloor "floor"
+#define StrModeTruncate "trunc"
+
+namespace habana {
+
+template <>
+LazyDiv<at::Tensor>::LazyDiv(
+    const std::string& qualstring,
+    const std::vector<at::IValue>& inputs,
+    const std::function<sizes_vec(const at::Stack&, bool)>& out_shapes_fn)
+    : habana_lazy::LazyOp<at::Tensor>(qualstring, inputs, out_shapes_fn, -1) {
+  c10::optional<std::string> rounding_mode =
+      inputs[2].toOptional<std::string>();
+  TORCH_CHECK(
+      !rounding_mode.has_value() or (*rounding_mode == StrModeTruncate) or
+          (*rounding_mode == StrModeFloor),
+      "div expected rounding_mode to be one of None, '",
+      StrModeTruncate,
+      "', or '",
+      StrModeFloor,
+      "' "
+      "but found '",
+      *rounding_mode,
+      "'");
+}
+
+template <>
+at::Tensor LazyDiv<at::Tensor>::get_result_overrideable() {
+  auto inputs = LazyOp<at::Tensor>::get_inputs();
+  auto self = inputs[0].toTensor();
+  auto other = inputs[1].toTensor();
+  c10::optional<std::string> rounding_mode =
+      inputs[2].toOptional<std::string>();
+  // If types of both Tensors are same, then first Tensor's type is used.
+  // This was required since this this case, the call  to
+  // type_promotion_for_two_tensor_inputs(...) does not change the
+  // result_dtype's existing value.
+  c10::ScalarType result_dtype = inputs[0].toTensor().scalar_type();
+
+  // TODO: Take this type promo code to a separate function to avoid
+  // duplicattion
+  int pos = -1;
+  habana_helpers::type_promotion_for_two_tensor_inputs(
+      inputs, pos, result_dtype);
+
+  // In true mode, div of integral types results in default type
+  const std::string strRroundingMode = rounding_mode.value_or(StrModeTrue);
+  if ((strRroundingMode == StrModeTrue) and
+      (c10::isIntegralType(result_dtype, true))) {
+    // TODO: default type is assumed to be Float,
+    // need to check using APIs
+    result_dtype = PYTORCH_DEFAULT_TYPE;
+  }
+
+  auto shape_out = BinaryOperator::compute_output_shape(self, other);
+
+  auto result = habana_lazy::empty_hpu_lazy(
+      shape_out,
+      self.options().dtype(result_dtype),
+      self.suggest_memory_format(),
+      false);
+
+  return result;
+}
+
+void DivRoundModeOperator::AddNode(
+    synapse_helpers::graph& graph,
+    at::Stack& stack,
+    const std::vector<bool>& is_output_persistent_list) {
+  static_cast<void>(is_output_persistent_list);
+  static_cast<void>(graph);
+
+  const at::Tensor self = stack_tensor(stack, 0);
+  const at::Tensor other = stack_tensor(stack, 1);
+  std::string rounding_mode =
+      stack[2].isNone() ? StrModeTrue : stack[2].toStringRef();
+
+  std::vector<at::Tensor> tensors = {self, other};
+
+  // Find the result type
+  const at::ScalarType& final_result_type = at::result_type(self, other);
+
+  // Computation is always done in float
+  const at::ScalarType& computation_type =
+      (c10::ScalarType::BFloat16 == final_result_type)
+      ? c10::ScalarType::BFloat16
+      : COMMON_COMPUTATION_TYPE_TPC;
+  const std::string opStringSuffix =
+      "_fwd_" + habana_helpers::name_suffix_from_type(computation_type);
+
+  // Remove the additional input from stack
+  stack.pop_back();
+
+  // Initialization
+  constexpr unsigned int cNoOfInputTensors = 2;
+  std::vector<synapse_helpers::tensor> divOp, cast[cNoOfInputTensors],
+      makeIntegerOp, castToReturnTypeOp;
+  std::vector<synTensor> binaryop_inputs{syn_in(0), syn_in(1)};
+
+  // Convert each tensor to float/bfloat16 (if not already in)
+  std::pair<c10::ScalarType, c10::ScalarType> type_key;
+  std::string strNode_type;
+
+  for (unsigned char i = 0; i < cNoOfInputTensors; ++i) {
+    if (tensors.at(i).scalar_type() == computation_type) {
+      continue;
+    }
+
+    type_key = std::make_pair(tensors.at(i).scalar_type(), computation_type);
+    auto iter = habana_helpers::cast_map.find(type_key);
+    strNode_type = iter->second;
+    cast[i] = BuildOp(
+        graph,
+        strNode_type,
+        {syn_in(i)},
+        {{stack_tensor(stack, 1).sizes(), computation_type}});
+    binaryop_inputs.at(i) = cast[i].at(0).get();
+  }
+
+  // Check if mode is other than default "true", i.e. "floor" or "trunc"
+  bool bOtherThanTrueMode = (StrModeTrue != rounding_mode);
+
+  // Final cast is required, if result type is not float
+  bool bNeedToCastFinalResult = (final_result_type != computation_type);
+
+  divOp = BuildOp(
+      graph,
+      "div" + opStringSuffix,
+      binaryop_inputs,
+      {{stack_tensor(stack, 0).sizes(),
+        computation_type,
+        bOtherThanTrueMode ? false : is_output_persistent_list[0],
+        not bOtherThanTrueMode}});
+  if (!bOtherThanTrueMode) {
+    syn_out(0) = std::move(divOp[0]);
+    return;
+  }
+
+  // If in "floor" or "trunc" mode, need to apply that
+  makeIntegerOp = BuildOp(
+      graph,
+      rounding_mode + opStringSuffix,
+      {divOp.at(0).get()},
+      {{stack_tensor(stack, 0).sizes(),
+        computation_type,
+        bNeedToCastFinalResult ? false : is_output_persistent_list[0],
+        not bNeedToCastFinalResult}});
+  if (!bNeedToCastFinalResult) {
+    syn_out(0) = std::move(makeIntegerOp[0]);
+    return;
+  }
+
+  type_key = std::make_pair(computation_type, final_result_type);
+  auto iter = habana_helpers::cast_map.find(type_key);
+  strNode_type = iter->second;
+  castToReturnTypeOp = BuildOp(
+      graph,
+      strNode_type,
+      {makeIntegerOp.at(0).get()},
+      {{stack_tensor(stack, 1).sizes(),
+        computation_type,
+        is_output_persistent_list[0],
+        true}});
+  syn_out(0) = std::move(castToReturnTypeOp[0]);
+}
+
+} // namespace habana
---
+++ tests/cpp/hpu_ops/div_roundoption.cpp
@@ -0,0 +1,98 @@
+/******************************************************************************
+ * Copyright (C) 2021 HabanaLabs, Ltd.
+ * All Rights Reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ ******************************************************************************
+ */
+
+#include "util.h"
+
+// Limits for generated values of variants of Int
+#define MIN_INT_VALUE_GENERATED -360
+#define MAX_INT_VALUE_GENERATED 360
+
+#define MIN_INT8_VALUE_GENERATED -50
+#define MAX_INT8_VALUE_GENERATED 50
+
+class HpuOpTest : public HpuOpTestUtil {};
+
+TEST_F(HpuOpTest, divroundTrueDouble) {
+  GenerateInputs(2, torch::kDouble);
+  c10::optional<std::string> mode = c10::nullopt;
+  auto expected = torch::div(GetCpuInput(0), GetCpuInput(1), mode);
+  auto result = torch::div(GetHpuInput(0), GetCpuInput(1), mode);
+
+  Compare(expected, result);
+}
+
+TEST_F(HpuOpTest, divroundTrueBFloat16) {
+  GenerateInputs(2, torch::kBFloat16);
+  c10::optional<std::string> mode = c10::nullopt;
+  auto expected = torch::div(GetCpuInput(0), GetCpuInput(1), mode);
+  auto result = torch::div(GetHpuInput(0), GetCpuInput(1), mode);
+  // TPC Kernel's precision, slightly differs fro CPU version for bfloat16
+  // Hence increased tolerance
+  Compare(expected, result, 0.1, 0.1);
+}
+
+TEST_F(HpuOpTest, divroundFloor) {
+  GenerateInputs(2);
+  torch::ScalarType dtype = torch::kFloat;
+
+  auto expected = torch::div(GetCpuInput(0), GetCpuInput(1), "floor");
+  auto result = torch::div(GetHpuInput(0), GetCpuInput(1), "floor");
+
+  Compare(expected, result);
+}
+
+// TODO: Use Test Util class (HpuOpTestUtil) (for each Int type test ) after
+// following issue fixed: [PO-165] Random Generation Fails for Byte, Char,
+// Short, Int, Long with Test Util class (HpuOpTestUtil)
+TEST_F(HpuOpTest, divroundTrueBroadcast) {
+  auto A = torch::randn({4, 3});
+  auto B = torch::randn({1, 3});
+  auto hA = A.to("hpu");
+  auto hB = B.to("hpu");
+  c10::optional<std::string> mode = c10::nullopt;
+  auto expected = torch::div(A, B, mode);
+  auto result = torch::div(hA, hB, mode);
+  Compare(expected, result);
+}
+
+TEST_F(HpuOpTest, divroundTrueTypePromoIntFloat) {
+  const std::vector<int64_t> tensor_size = {2, 5};
+  auto A = torch::randint(
+      MIN_INT_VALUE_GENERATED,
+      MAX_INT_VALUE_GENERATED,
+      tensor_size,
+      torch::dtype(torch::kInt));
+  auto B = torch::randn(tensor_size);
+  auto hA = A.to("hpu");
+  auto hB = B.to("hpu");
+  c10::optional<std::string> mode = c10::nullopt;
+  auto expected = torch::div(A, B, mode);
+  auto result = torch::div(hA, hB, mode);
+  Compare(expected, result);
+}
+
+TEST_F(HpuOpTest, divroundTruncateTypePromoInt8Int8) {
+  const std::vector<int64_t> tensor_size = {4, 6};
+  auto A = torch::randint(
+      MIN_INT8_VALUE_GENERATED,
+      MAX_INT8_VALUE_GENERATED,
+      tensor_size,
+      torch::dtype(torch::kInt8));
+  auto B = torch::randint(
+      MIN_INT8_VALUE_GENERATED,
+      MAX_INT8_VALUE_GENERATED,
+      tensor_size,
+      torch::dtype(torch::kInt8));
+  auto hA = A.to("hpu");
+  auto hB = B.to("hpu");
+  auto expected = torch::div(A, B, "trunc");
+  auto result = torch::div(hA, hB, "trunc");
+  Compare(expected, result);
+}
