87260f84476312854db4a2ad82f1d17cccf12675
Internal Developer <internal-developer@intel.com>
2021-10-24 14:39:44 +0000

    Fix aeon dataloader setup.py after wrapping aeon packaging
    Change-Id: I74dd872cd2cb5fb33e0b02949b4253c2dc0279c3

--- pytorch_helpers/dataloader/habana_dataloader/setup.py
+++ pytorch_helpers/dataloader/habana_dataloader/setup.py
@@ -6,10 +6,6 @@ from pathlib import Path
 import os
 import sys
 
-if not os.environ.get("PYTORCH_MODULES_ROOT_PATH"):
-      print("Expected 'PYTORCH_MODULES_ROOT_PATH' to be set")
-      sys.exit(1)
-
 if not os.environ.get("BUILD_ROOT_LATEST"):
       print("Expected 'BUILD_ROOT_LATEST' to be set")
       sys.exit(1)
@@ -31,12 +27,10 @@ setup(name='habana_dataloader',
                                                 ],
                                                 libraries=['aeon'],
                                                 library_dirs=[
-                                                      os.path.dirname(os.environ["DATA_LOADER_AEON_LIB_PATH"]),
-                                                      os.path.dirname(os.environ["BUILD_ROOT_LATEST"])
+                                                      os.environ["BUILD_ROOT_LATEST"]
                                                 ],
                                                 runtime_library_dirs=[
-                                                      os.path.dirname(os.environ["DATA_LOADER_AEON_LIB_PATH"]),
-                                                      os.path.dirname(os.environ["BUILD_ROOT_LATEST"]),
+                                                      os.environ["BUILD_ROOT_LATEST"],
                                                       cpp_extension.TORCH_LIB_PATH
                                                 ]
                                               )
