83a86b162a24988b6d909667320cdb2f9d4f2851
Internal Developer <internal-developer@intel.com>
2021-11-17 10:00:01 +0200

    Updated unit test for fusedadamw
    Change-Id: I08d0529758a89c5bfb7752a65508ae25b73eb32f

--- habana_kernels/pytorch-extensions/habana_custom-extension/tests/adamw_ut.py
+++ habana_kernels/pytorch-extensions/habana_custom-extension/tests/adamw_ut.py
@@ -2,73 +2,99 @@ import numpy as np
 import os
 import sys
 import torch
+from torch.optim import AdamW
 
-from habana_frameworks.torch.hpex.optimizers import FusedAdamW
-from transformers import AdamW
 
-from habana_frameworks.torch.utils.library_loader import load_habana_module
-load_habana_module()
-habana = torch.device("hpu")
-cpu = torch.device("cpu")
+def permute_4d_5d_tensor(tensor, to_filters_last):
+    import habana_frameworks.torch.core as htcore
+    if htcore.is_enabled_weight_permute_pass() is True:
+        return tensor
+    if tensor.ndim == 4:
+        if to_filters_last:
+            tensor = tensor.permute((2, 3, 1, 0))
+        else:
+            tensor = tensor.permute((3, 2, 0, 1))  # permute RSCK to KCRS
+    elif tensor.ndim == 5:
+        if to_filters_last:
+            tensor = tensor.permute((2, 3, 4, 1, 0))
+        else:
+            tensor = tensor.permute((4, 3, 0, 1, 2))  # permute RSTCK to KCRST
+    return tensor
 
-import habana_frameworks.torch.core as htcore
-
-if __name__ == "__main__":
+def fused_adam_test( dim=4):
+    cpu = torch.device("cpu")
     torch.manual_seed(0)
-    d1, d2, lr, wd = 2, 1024, 0.1, 0.1
+    d1, d2, lr, wd = 320, 256, 0.1, 0.1
+    eps = 1e-6
+    if dim==4:
+        u = torch.rand(d1, d2,3,3)
+    elif dim==5:
+        u = torch.rand(d1, d2,3,3,3)
+    else:
+        u = torch.rand(d1, d2,3)
 
-    u = torch.rand(d1, d2)
-    v = u.clone()
-    # print('input ::\n{}'.format(u))
+    print(" Input shape", u.shape)
 
-    x = u.detach().to(cpu)
+    x = u.clone()
     x.requires_grad = True
-
+    v = u.clone()
     # Compute loss
     loss_x = x.sum()
-
-    print("loss x ", loss_x.cpu())
 
     # Compute gradients of the parameters w.r.t. the loss
     loss_x.backward()
 
     # Modify the parameters by subtracting the gradient
-    optim_x = AdamW([x], lr=lr, weight_decay = wd)
-
-    print('before adam.step x ::\n{}'.format(x.to(cpu)))
+    optim_x = AdamW([x], lr=lr, weight_decay = wd,eps=eps)
     optim_x.step()
-    x_cpu = x.to(cpu)
-    print('after  adam.step x ::\n{}'.format(x_cpu))
+
+    x_cpu = x
 
     # Enable this env to validate lazy path
     os.environ['PT_HPU_LAZY_MODE'] = "1"
+    from habana_frameworks.torch.hpex.optimizers import FusedAdamW
+    from habana_frameworks.torch.utils.library_loader import load_habana_module
+
+    habana = torch.device("hpu")
+    import habana_frameworks.torch.core as htcore
 
     y = v.detach().to(habana)
+    y = permute_4d_5d_tensor(y,True)
+    #htcore.mark_step()
+
     y.requires_grad = True
 
-    optim_y = FusedAdamW([y], lr=lr, weight_decay = wd)
+    print("Shape 1", y.shape)
+
+    optim_y = FusedAdamW([y], lr=lr, weight_decay = wd,eps=eps)
     htcore.mark_step()
 
     # Compute loss
+
     loss_y = y.sum()
 
     # Compute gradients of the parameters w.r.t. the loss
     loss_y.backward()
 
-    # htcore.mark_step()
+    htcore.mark_step()
 
-    # print('before adam_habana.step y ::\n{}'.format(y.to(cpu)))
     # Modify the parameters by subtracting the gradient
     optim_y.step()
 
     htcore.mark_step()
 
+    y = permute_4d_5d_tensor(y,False)
     y_cpu = y.to(cpu)
 
-    print("loss y ", loss_y.cpu())
-
-    print('after  adam_habana.step y ::\n{}'.format(y_cpu))
-
+    max1 = x_cpu-y_cpu
+    max1 = max1.abs()
+    print(x_cpu.max(),y_cpu.max(),max1.max())
     comp = np.allclose(x_cpu.detach().numpy(), y_cpu.detach().numpy(), atol=0.001, rtol=1.e-3, equal_nan=True)
 
     print('Optimizer output match :: {}'.format(comp))
+
+
+if __name__ == "__main__":
+    fused_adam_test(3)
+    fused_adam_test(4)
+    fused_adam_test(5)
