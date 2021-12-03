80742846ed2eabc86afc92a6671d7a7e1392b529
Internal Developer <internal-developer@intel.com>
2021-12-03 17:59:22 +0200

    Add missing roundng mode in round out variant
    1. Rounding mode had to be set to round to nearest even
    2. Check for number of inputs had to be removed since it was set to 1
       but out variant has 2 inputs.
    
    Change-Id: Ifcb90895e7861fdd43070bc46574f8d238671956
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- hpu_ops/round_gen.cpp
+++ hpu_ops/round_gen.cpp
@@ -13,16 +13,7 @@
 namespace habana {
 std::shared_ptr<void> FillRoundParams(const at::Stack& stack, size_t& size) {
   PARAMS_STUB(ns_RoundKernel::Params);
-  constexpr unsigned short constExpectedNoOfInput = 1;
-  TORCH_CHECK(
-      stack.size() == constExpectedNoOfInput,
-      "Expected ",
-      constExpectedNoOfInput,
-      " input for Round Operator"
-      " but received ",
-      stack.size(),
-      " inputs.");
-  TORCH_CHECK(stack[0].isTensor(), "Input type expected to be tensor");
+  static_cast<void>(stack);
   params->roundMode = RoundMode_t::ROUND_HALF_NEAREST_EVEN;
   return params;
 }
