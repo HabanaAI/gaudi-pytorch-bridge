0d596122f9492c374402e4dc531be6e79ee730f7
Internal Developer <internal-developer@intel.com>
2021-12-03 12:01:22 +0200

    convert python test to pytest based test
    Change-Id: I8ddee7f240f900f1f4b8c01ce91210ec9e2afdee
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- tests/test_hpu_permute.py
+++ tests/test_hpu_permute.py
@@ -1,6 +1,5 @@
 import os
 import torch
-import numpy as np
 import pytest
 import test_utils
 from test_utils import compare_tensors
@@ -11,22 +10,28 @@ except ImportError:
     assert False, "Could Not import habana_frameworks.torch.core"
 
 
-def fun(img, lbl, dev):
-    if dev == torch.device("hpu"):
-        os.environ["PT_HPU_LAZY_MODE"] = "1"
-        os.environ["PT_HPU_ENABLE_REFINE_DYNAMIC_SHAPES"] = "1"
-    img = img.to(dev)
-    # lbl = lbl.to(dev)
-    img = img.contiguous(memory_format=torch.channels_last_3d)
-    # lbl = lbl.contiguous(memory_format=torch.channels_last_3d)
-    if dev == torch.device("hpu"):
-        htcore.mark_step()
-        del os.environ["PT_HPU_LAZY_MODE"]
-        del os.environ["PT_HPU_ENABLE_REFINE_DYNAMIC_SHAPES"]
-    return img, lbl
+test_case_list = [
+    # D, H, W, * 2
+    [(132, 176, 136), (146, 175, 146)]
+]
 
-def test_hpu_permute():
-    shapes = [(132, 176, 136), (146, 175, 146)]
+@pytest.mark.parametrize("dyn_shape", test_case_list)
+def test_hpu_permute(dyn_shape):
+    def fun(img, lbl, dev):
+        if dev == torch.device("hpu"):
+            os.environ["PT_HPU_LAZY_MODE"] = "1"
+            os.environ["PT_HPU_ENABLE_REFINE_DYNAMIC_SHAPES"] = "1"
+        img = img.to(dev)
+        lbl = lbl.to(dev)
+        img = img.contiguous(memory_format=torch.channels_last_3d)
+        lbl = lbl.contiguous(memory_format=torch.channels_last_3d)
+        if dev == torch.device("hpu"):
+            htcore.mark_step()
+            del os.environ["PT_HPU_LAZY_MODE"]
+            del os.environ["PT_HPU_ENABLE_REFINE_DYNAMIC_SHAPES"]
+        return img, lbl
+
+    shapes = dyn_shape
     for i in range(len(shapes)):
         x, y, z = shapes[i]
         input = torch.randn((1, 4, x, y, z))
@@ -38,4 +43,4 @@ def test_hpu_permute():
 
 
 if __name__ == '__main__':
-    test_hpu_permute()
+    test_hpu_permute(*test_case_list[0])
