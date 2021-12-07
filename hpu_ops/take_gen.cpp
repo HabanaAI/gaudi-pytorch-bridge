365f72dca5be6ea75425a07f9411fa3f8890fe2d
Internal Developer <internal-developer@intel.com>
2021-12-07 11:21:32 +0200

    [With Build error fix] Added kernel and gtest of take, take.out ops
    Re-introduce the reverted patch and change yaml field "op_base_class"
    to "op_backend".
    
    Also added per input datatype
    
    Change-Id: I11560df0933cfcc6b83a5e84abc268fd0920c4d0
    Signed-off-by: Internal Developer <internal-developer@intel.com>
    Signed-off-by: Internal Developer <internal-developer@intel.com>

---
+++ hpu_ops/take_gen.cpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Copyright (C) 2021 HabanaLabs, Ltd.
+ * All Rights Reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ ******************************************************************************
+ */
+
+#include "generated/hpu_op.h"
+#include "hpu_op_helper.h"
+
+namespace habana {
+
+void TakeOperator::AddNode(
+    synapse_helpers::graph& graph,
+    at::Stack& stack,
+    const std::vector<bool>& is_output_persistent_list) {
+  const auto self = stack.at(0).toTensor();
+  const auto& outshape = stack_tensor(stack, 0).sizes();
+
+  ns_GatherKernel::Params params{};
+
+  // (M, N) -> (MN)
+  auto reshape_outshape = self.numel();
+  auto reshape = BuildOp(
+      graph, "reshape", {syn_in(0)}, {{reshape_outshape, ScalarType()}});
+
+  // Gathers values along an axis
+  auto gatherkernel = BuildOp(
+      graph,
+      "gather_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
+      {reshape[0].get(), syn_in(1)},
+      {{outshape, ScalarType(), is_output_persistent_list[0], true}},
+      &params,
+      sizeof(params));
+
+  syn_out(0) = std::move(gatherkernel[0]);
+}
+} // namespace habana
