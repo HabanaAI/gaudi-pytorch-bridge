b2e185a7fc12a7510481f2e7ca98518f71944c0d
Internal Developer <internal-developer@intel.com>
2021-12-08 20:34:41 +0530

    Kernel development and GTest for Square_out op
    Change-Id: Ia6be230b619fc8e44e62452620034a968a5cf693

---
+++ hpu_ops/square_gen.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+ * Copyright (C) 2021 HabanaLabs, Ltd.
+ * All Rights Reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ ******************************************************************************
+ */
+
+#include "generated/hpu_op.h"
+
+namespace habana {
+
+void Square::AddNode(
+    synapse_helpers::graph& graph,
+    at::Stack& stack,
+    const std::vector<bool>& is_output_persistent_list) {
+  const auto& outshape = stack_tensor(stack, 0).sizes();
+
+  auto square = BuildOp(
+      graph,
+      "mult_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
+      {syn_in(0), syn_in(0)},
+      {{outshape, ScalarType(), is_output_persistent_list[0], true}});
+
+  syn_out(0) = std::move(square[0]);
+}
+} // namespace habana
---
+++ tests/cpp/hpu_ops/square.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+ * Copyright (C) 2021 HabanaLabs, Ltd.
+ * All Rights Reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ ******************************************************************************
+ */
+
+#include "util.h"
+
+class HpuOpTest : public HpuOpTestUtil {};
+
+TEST_F(HpuOpTest, square_out_float) {
+  GenerateInputs(1, {{1, 3, 32, 32}}, {torch::kFloat});
+
+  torch::ScalarType dtype = torch::kFloat;
+  auto expected = torch::empty(0, dtype);
+  auto result = torch::empty(0, torch::TensorOptions(dtype).device("hpu"));
+
+  torch::square_outf(GetCpuInput(0), expected);
+  torch::square_outf(GetHpuInput(0), result);
+
+  Compare(expected, result);
+}
+
+TEST_F(HpuOpTest, square_out_int) {
+  GenerateInputs(1, {{1, 3, 3, 5, 5}}, {torch::kInt});
+
+  torch::ScalarType dtype = torch::kInt;
+  auto expected = torch::empty(0, dtype);
+  auto result = torch::empty(0, torch::TensorOptions(dtype).device("hpu"));
+
+  torch::square_outf(GetCpuInput(0), expected);
+  torch::square_outf(GetHpuInput(0), result);
+
+  Compare(expected, result);
+}
