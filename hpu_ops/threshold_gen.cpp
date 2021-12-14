17101532240dff36d13687226c6f07ed47172b9e
Internal Developer <internal-developer@intel.com>
2021-12-14 18:53:11 +0530

    ,   Kernel + Gtest for threshold, threshold_, threshold_out and threshold_backward.grad_input operators
    Change-Id: I28ba1df17fe5b286d2527b00dcf9e5ad661b88a6

--- hpu_ops/threshold_gen.cpp
+++ hpu_ops/threshold_gen.cpp
@@ -18,13 +18,15 @@ void ThresholdBackwardHabanaOperator::AddNode(
     const std::vector<bool>& is_output_persistent_list) {
   // TODO: Remove this base class once [SW-65399] is resolved
   TORCH_CHECK(
-      stack.size() == 3,
+      stack.size() == 3 || stack.size() == 4,
       "Incorrect size of inputs expected for threshold operator");
 
   TORCH_CHECK(stack[0].isTensor(), "Input arg1 type expected to be tensor");
   TORCH_CHECK(stack[1].isTensor(), "Input arg2 type expected to be tensor");
   TORCH_CHECK(stack[2].isScalar(), "Input arg3 type expected to be scalar");
-
+  if (stack.size() == 4) {
+    TORCH_CHECK(stack[3].isTensor(), "Input arg4 type expected to be tensor");
+  }
   auto threshold = stack[2].toScalar();
 
   TORCH_CHECK(
@@ -34,4 +36,40 @@ void ThresholdBackwardHabanaOperator::AddNode(
   OpBackend::AddNode(graph, stack, is_output_persistent_list);
 }
 
+void Threshold::AddNode(
+    synapse_helpers::graph& graph,
+    at::Stack& stack,
+    const std::vector<bool>& is_output_persistent_list) {
+  const float threshold = stack.at(1).toScalar().toFloat();
+  const float value = stack.at(2).toScalar().toFloat();
+  const auto& outshape = stack_tensor(stack, 0).sizes();
+
+  if (threshold == 0 && value == 0) {
+    // if threshold and value are zero then the op is same perform same as relu
+    auto output = BuildOp(
+        graph,
+        "relu_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
+        {syn_in(0)},
+        {{outshape, ScalarType(), is_output_persistent_list[0], true}});
+    syn_out(0) = std::move(output[0]);
+  } else {
+    auto threshold_gen = ConstantHelper(graph, threshold, ScalarType());
+    const at::ScalarType& result_type = c10::ScalarType::Bool;
+    // Checking wheather innput is greater than threshold returns boolean tensor
+    auto greaterthan_threshold = BuildOp(
+        graph,
+        "greater_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
+        {syn_in(0), threshold_gen.get()},
+        {{outshape, result_type}});
+    auto value_gen = ConstantHelper(graph, value, ScalarType());
+    // returns input if it is true else return the value
+    auto output = BuildOp(
+        graph,
+        "where_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
+        {greaterthan_threshold[0].get(), syn_in(0), value_gen.get()},
+        {{outshape, ScalarType(), is_output_persistent_list[0], true}});
+
+    syn_out(0) = std::move(output[0]);
+  }
+}
 } // namespace habana
