7c73d76d4a2ea94ccc26bc9da437b9a1c671fa6b
Internal Developer <internal-developer@intel.com>
2021-12-23 23:15:50 +0530

    Kernel development and gtest for mm.out variant
    Issue raised for BFloat16 assertion error: https://jira.habana-labs.com/browse/
    
    Change-Id: Iad170ee329f1f370b48af53eadaa63f08e57fc42

---
+++ hpu_ops/mm_gen.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * Copyright (C) 2021 HabanaLabs, Ltd.
+ * All Rights Reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ ******************************************************************************
+ */
+
+#include "generated/hpu_op.h"
+#include "hpu_op_helper.h"
+namespace habana {
+sizes_vec MmOutputShape(const at::Stack& stack, bool) {
+  TORCH_CHECK(
+      (stack.at(0).isTensor() && stack.at(1).isTensor()),
+      " Matmul Input type expected to be tensors");
+  auto mat1 = stack.at(0).toTensor();
+  auto mat2 = stack.at(1).toTensor();
+  return {{mat1.size(0), mat2.size(1)}};
+}
+} // namespace habana
