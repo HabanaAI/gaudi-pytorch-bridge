2923b009b3f498d4157f97e80c14d0d0577dd3e4
Internal Developer <internal-developer@intel.com>
2021-12-30 17:44:30 +0530

    , Added UT for linspace
    Change-Id: I9fc48642304671a5ebf0c15416e0ec4a159e94a1

---
+++ tests/cpp/hpu_ops/linspace.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * Copyright (C) 2021 HabanaLabs, Ltd.
+ * All Rights Reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ ******************************************************************************
+ */
+
+#include "util.h"
+
+class HpuOpTest : public HpuOpTestUtil {};
+
+TEST_F(HpuOpTest, linspace) {
+  // Not supporting for the values beyond 40
+  float start = GenerateScalar<float>(-1, 40);
+  // Not supporting for the values use beyond 40
+  float end = GenerateScalar<float>(1, 40);
+  int steps = GenerateScalar<int>();
+  auto expected = torch::linspace(start, end, steps);
+  auto result = torch::linspace(start, end, steps, "hpu");
+  Compare(expected, result);
+}
