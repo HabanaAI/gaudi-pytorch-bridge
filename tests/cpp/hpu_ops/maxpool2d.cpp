1d965fee3ea4e6ac4678c39f999d7636c4090ec9
Internal Developer <internal-developer@intel.com>
2022-01-07 13:44:22 +0530

    - max_pool3d_with_indices, max_pool3d_with_indices_backward, max_pool3d_with_indices.out, max_pool3d_with_indices_backward.grad_input  - max_pool2d_with_indices.out, max_pool2d_with_indices_backward.grad_input
    Raised Jiras:-
     - maxpool2d and maxpool3d with indices fwd ops returns Indices in different format
     - max_pool2d_with_indices_out and max_pool3d_with_indices_out  ops  expects dtype of indices as Float
    
    Change-Id: I258234621e386436046d6be386dfb606574dcda7

---
+++ tests/cpp/hpu_ops/maxpool2d.cpp
@@ -0,0 +1,144 @@
+/******************************************************************************
+ * Copyright (C) 2021 HabanaLabs, Ltd.
+ * All Rights Reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ ******************************************************************************
+ */
+
+#include "util.h"
+
+class HpuOpTest : public HpuOpTestUtil {};
+// Since the out varriant intices tensor has some issue
+// (https://jira.habana-labs.com/browse/SW-74263), so that the implementation is
+// commented till the issue got resolved.
+/**TEST_F(HpuOpTest, maxpool_2d_with_indices_out) {
+  GenerateInputs(1, {{1, 2, 7, 9}});
+  std::vector<int64_t> kernel_size = {{3, 3}};
+  std::vector<int64_t> stride = {{3, 3}};
+  std::vector<int64_t> pad_size = {{1, 1}};
+  std::vector<int64_t> dilation = {{1, 1}};
+  bool ceil_mode = false;
+  torch::ScalarType dtype = torch::kInt64;
+  torch::ScalarType dtypef = torch::kFloat;
+  auto expected_tensor = torch::empty(0, dtypef);
+  auto expected_indices = torch::empty(0, dtype);
+  auto result_tensor =
+      torch::empty(0, torch::TensorOptions(dtypef).device("hpu"));
+  auto result_indices =
+      torch::empty(0, torch::TensorOptions(dtype).device("hpu"));
+  torch::max_pool2d_with_indices_outf(
+      GetCpuInput(0),
+      kernel_size,
+      stride,
+      pad_size,
+      dilation,
+      ceil_mode,
+      expected_tensor,
+      expected_indices);
+  torch::max_pool2d_with_indices_outf(
+      GetHpuInput(0),
+      kernel_size,
+      stride,
+      pad_size,
+      dilation,
+      ceil_mode,
+      result_tensor,
+      result_indices);
+
+  // max_pool2d_with_indices will return 2 outputs Indices tensor and
+  // output tensor. But here we are comparing only output tensor because in
+  // pytorch the returend indices tensor contains  indices relative to input
+  // feature map but indices tensor from TPC contains indices relative to kernel
+  // window.
+  // Jia rased for the above issue -
+  // https://jira.habana-labs.com/browse/SW-73882
+  Compare(expected_tensor, result_tensor);
+}
+
+TEST_F(HpuOpTest, maxpool_2d_with_indices_out_backward) {
+  GenerateInputs(1, {{2, 6, 6}});
+  std::vector<int64_t> kernel_size = {{3, 3}};
+  std::vector<int64_t> stride = {{3, 3}};
+  std::vector<int64_t> pad_size = {{1, 1}};
+  std::vector<int64_t> dilation = {{1, 1}};
+  bool ceil_mode = true;
+  torch::ScalarType dtype = torch::kInt64;
+  torch::ScalarType dtypef = torch::kFloat;
+  auto expected_tensor = torch::empty(0, dtypef);
+  auto expected_indices = torch::empty(0, dtype);
+  auto result_tensor =
+      torch::empty(0, torch::TensorOptions(dtypef).device("hpu"));
+
+  // If we use Indices dtype for HPU as Int or Byte for fwd out variant, then we
+  // are getting some junk values in the indices tensor and if we increase the
+  // test case dimensions like channel greater than 1 then the test case will
+  // failed with mismatch error. But, if we change the indices dtype  of HPU to
+  // Float then what ever the test case dimension correct indices will be
+  // generated.
+  // https://jira.habana-labs.com/browse/SW-74263
+  auto result_indices =
+      torch::empty(0, torch::TensorOptions(dtype).device("hpu"));
+
+  // max_pool2d with indces will return 2 outputs Indices tensor and
+  // output tensor. In pytorch the returend indices tensor contains  indices
+  // relative to input feature map but indices tensor from TPC contains indices
+  // relative to kernel window. So for testing backward first we will execute
+  // the forward operator and then that output will be passed to backward
+  // operator and will compare the backward result.
+  // Jia rased for the above issue -
+  // https://jira.habana-labs.com/browse/SW-73882
+  torch::max_pool2d_with_indices_outf(
+      GetCpuInput(0),
+      kernel_size,
+      stride,
+      pad_size,
+      dilation,
+      ceil_mode,
+      expected_tensor,
+      expected_indices);
+  torch::max_pool2d_with_indices_outf(
+      GetHpuInput(0),
+      kernel_size,
+      stride,
+      pad_size,
+      dilation,
+      ceil_mode,
+      result_tensor,
+      result_indices);
+
+  // Backward
+  auto expected_gradinp = torch::empty(0, dtypef);
+  auto result_gradinp =
+      torch::empty(0, torch::TensorOptions(dtypef).device("hpu"));
+
+  torch::max_pool2d_with_indices_backward_outf(
+      expected_tensor,
+      GetCpuInput(0),
+      kernel_size,
+      stride,
+      pad_size,
+      dilation,
+      ceil_mode,
+      expected_indices,
+      expected_gradinp);
+
+  // Since there is a error in the indices dtype of out fwd variant the correct
+  // indices will be generated in HPU Float type. But for bwd variant the
+  // expected dtype of idices is Byte ot Int16. So we are forced to add a
+  // convertion as done below.Then only the test case is passed.
+  // https://jira.habana-labs.com/browse/SW-74263
+  torch::max_pool2d_with_indices_backward_outf(
+      result_tensor,
+      GetHpuInput(0),
+      kernel_size,
+      stride,
+      pad_size,
+      dilation,
+      ceil_mode,
+      result_indices,
+      result_gradinp);
+  Compare(expected_gradinp, result_gradinp);
+}**/
