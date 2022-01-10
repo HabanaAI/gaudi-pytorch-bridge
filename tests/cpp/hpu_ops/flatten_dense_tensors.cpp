df424d1f9f306de7c450332948763d88b165494e
Internal Developer <internal-developer@intel.com>
2022-01-10 17:08:02 +0200

    Add gtest for flatten_dense_tensors op
    Change-Id: I5d6082867947427f3ff660bd2b8b8cd4a78282b1

---
+++ tests/cpp/hpu_ops/flatten_dense_tensors.cpp
@@ -0,0 +1,76 @@
+/******************************************************************************
+ * Copyright (C) 2021 HabanaLabs, Ltd.
+ * All Rights Reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ ******************************************************************************
+ */
+
+#include "util.h"
+
+class HpuOpTest : public HpuOpTestUtil {};
+
+TEST_F(HpuOpTest, flatten_dense_tensors_2_inputs) {
+  GenerateInputs(2, {{64}, {64}}, torch::kBFloat16);
+
+  torch::ScalarType dtype = torch::kBFloat16;
+
+  auto expected =
+      torch::flatten_dense_tensors({GetCpuInput(0), GetCpuInput(1)});
+  auto result = torch::flatten_dense_tensors({GetHpuInput(0), GetHpuInput(1)});
+
+  // As no computation involved, tolerance is set to 0
+  Compare(expected, result, 0, 0);
+}
+
+TEST_F(HpuOpTest, flatten_dense_tensors_3_inputs) {
+  GenerateInputs(3, {{8, 16}, {4, 8, 2}, {2}});
+
+  auto expected = torch::flatten_dense_tensors(
+      {GetCpuInput(0), GetCpuInput(1), GetCpuInput(2)});
+  auto result = torch::flatten_dense_tensors(
+      {GetHpuInput(0), GetHpuInput(1), GetHpuInput(2)});
+
+  Compare(expected, result, 0, 0);
+}
+
+TEST_F(HpuOpTest, flatten_dense_tensors_4_inputs) {
+  GenerateInputs(4, {{8, 16, 2}, {2, 24}, {16}, {2, 5}});
+
+  auto expected = torch::flatten_dense_tensors(
+      {GetCpuInput(0), GetCpuInput(1), GetCpuInput(2), GetCpuInput(3)});
+  auto result = torch::flatten_dense_tensors(
+      {GetHpuInput(0), GetHpuInput(1), GetHpuInput(2), GetHpuInput(3)});
+
+  Compare(expected, result, 0, 0);
+}
+
+TEST_F(HpuOpTest, flatten_dense_tensors_5_inputs) {
+  GenerateInputs(5, {{8, 16, 2}, {2, 24}, {16}, {2, 5}, {2}}, torch::kBFloat16);
+
+  auto expected = torch::flatten_dense_tensors(
+      {GetCpuInput(0),
+       GetCpuInput(1),
+       GetCpuInput(2),
+       GetCpuInput(3),
+       GetCpuInput(4)});
+  auto result = torch::flatten_dense_tensors(
+      {GetHpuInput(0),
+       GetHpuInput(1),
+       GetHpuInput(2),
+       GetHpuInput(3),
+       GetHpuInput(4)});
+
+  Compare(expected, result, 0, 0);
+}
+
+TEST_F(HpuOpTest, flatten_dense_tensors_1_input) {
+  GenerateInputs(1, {{2, 16, 2, 2, 4}}, torch::kBFloat16);
+
+  auto expected = torch::flatten_dense_tensors({GetCpuInput(0)});
+  auto result = torch::flatten_dense_tensors({GetHpuInput(0)});
+
+  Compare(expected, result, 0, 0);
+}
