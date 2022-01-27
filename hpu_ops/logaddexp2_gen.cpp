1a606176c796007546fca750992afe33eddf88d1
Internal Developer <internal-developer@intel.com>
2022-01-27 14:44:15 +0200

    [SW:0] Outshape Fix for Logaddexp, Logaddexp2
    Change-Id: Ifbfe9ec77ead62e9602ec5af928a225f8d504197

--- hpu_ops/logaddexp2_gen.cpp
+++ hpu_ops/logaddexp2_gen.cpp
@@ -15,35 +15,36 @@ void LogAddExp2::AddNode(
     synapse_helpers::graph& graph,
     at::Stack& stack,
     const std::vector<bool>& is_output_persistent_list) {
-  const auto& outshape = stack_tensor(stack, 0).sizes();
-
+  const auto& outshape_self = stack_tensor(stack, 0).sizes();
+  const auto& outshape_other = stack_tensor(stack, 1).sizes();
+  auto result_outshape = ComputeOutputShapes(stack, true)[0];
   // pow on input 0
   auto pow_out_1 = BuildOp(
       graph,
       "pow2_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
       {syn_in(0)},
-      {{outshape, ScalarType(), false}});
+      {{outshape_self, ScalarType()}});
 
   // pow on input 1
   auto pow_out_2 = BuildOp(
       graph,
       "pow2_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
       {syn_in(1)},
-      {{outshape, ScalarType(), false}});
+      {{outshape_other, ScalarType()}});
 
   // add on output of add
   auto add = BuildOp(
       graph,
       "add_" + habana_helpers::name_suffix_from_type(ScalarType()),
       {pow_out_1[0].get(), pow_out_2[0].get()},
-      {{outshape, ScalarType(), false}});
+      {{result_outshape, ScalarType()}});
 
   // log on output of add
   auto log = BuildOp(
       graph,
       "log2_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
       {add[0].get()},
-      {{outshape, ScalarType(), is_output_persistent_list[0], true}});
+      {{result_outshape, ScalarType(), is_output_persistent_list[0], true}});
 
   // output of log is the output of this op
   syn_out(0) = std::move(log[0]);
--- hpu_ops/logaddexp_gen.cpp
+++ hpu_ops/logaddexp_gen.cpp
@@ -15,35 +15,36 @@ void LogAddExp::AddNode(
     synapse_helpers::graph& graph,
     at::Stack& stack,
     const std::vector<bool>& is_output_persistent_list) {
-  const auto& outshape = stack_tensor(stack, 0).sizes();
-
+  const auto& outshape_self = stack_tensor(stack, 0).sizes();
+  const auto& outshape_other = stack_tensor(stack, 1).sizes();
+  auto result_outshape = ComputeOutputShapes(stack, true)[0];
   // exp on input 0
   auto exp_0 = BuildOp(
       graph,
       "exp_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
       {syn_in(0)},
-      {{outshape, ScalarType(), false}});
+      {{outshape_self, ScalarType()}});
 
   // exp on input 1
   auto exp_1 = BuildOp(
       graph,
       "exp_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
       {syn_in(1)},
-      {{outshape, ScalarType(), false}});
+      {{outshape_other, ScalarType()}});
 
   // Add on output of exp_0, exp_1
   auto add = BuildOp(
       graph,
       "add_" + habana_helpers::name_suffix_from_type(ScalarType()),
       {exp_0[0].get(), exp_1[0].get()},
-      {{outshape, ScalarType(), false}});
+      {{result_outshape, ScalarType()}});
 
   // log on output of add
   auto log = BuildOp(
       graph,
       "log_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
       {add[0].get()},
-      {{outshape, ScalarType(), is_output_persistent_list[0], true}});
+      {{result_outshape, ScalarType(), is_output_persistent_list[0], true}});
 
   // output of log is the output of this op
   syn_out(0) = std::move(log[0]);
