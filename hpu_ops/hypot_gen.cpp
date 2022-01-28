f9257ffdc312e468d71ad682a6662998c6be7723
Internal Developer <internal-developer@intel.com>
2022-01-28 07:04:13 +0200

    Fix for broadcasting issue in Hypot
    Change-Id: Ia191f49c5cf9fb64383def997f31c8787a9e0cb8

--- hpu_ops/hypot_gen.cpp
+++ hpu_ops/hypot_gen.cpp
@@ -16,35 +16,37 @@ void Hypot::AddNode(
     synapse_helpers::graph& graph,
     at::Stack& stack,
     const std::vector<bool>& is_output_persistent_list) {
-  const auto& outshape = stack_tensor(stack, 0).sizes();
+  const auto& outshape_self = stack_tensor(stack, 0).sizes();
+  const auto& outshape_other = stack_tensor(stack, 1).sizes();
+  auto result_outshape = BinaryOutputShape(stack, true)[0];
 
   // mul on input 0
   auto mul_1 = BuildOp(
       graph,
       MULT_GUID + habana_helpers::name_suffix_from_type(ScalarType()),
       {syn_in(0), syn_in(0)},
-      {{outshape, ScalarType()}});
+      {{outshape_self, ScalarType()}});
 
   // mul on input 1
   auto mul_2 = BuildOp(
       graph,
       MULT_GUID + habana_helpers::name_suffix_from_type(ScalarType()),
       {syn_in(1), syn_in(1)},
-      {{outshape, ScalarType()}});
+      {{outshape_other, ScalarType()}});
 
   // addsquare on output of mul
   auto add = BuildOp(
       graph,
       "add_" + habana_helpers::name_suffix_from_type(ScalarType()),
       {mul_1[0].get(), mul_2[0].get()},
-      {{outshape, ScalarType()}});
+      {{result_outshape, ScalarType()}});
 
   // sqrt on output of addsquare
   auto sqrt = BuildOp(
       graph,
       "sqrt_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
       {add[0].get()},
-      {{outshape, ScalarType(), is_output_persistent_list[0], true}});
+      {{result_outshape, ScalarType(), is_output_persistent_list[0], true}});
 
   // output of sqrt is the output of this op
   syn_out(0) = std::move(sqrt[0]);
