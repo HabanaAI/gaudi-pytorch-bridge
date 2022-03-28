a349721af8bde271fa8289863af2b9885259826d
Internal Developer <internal-developer@intel.com>
2022-03-28 18:25:30 +0300

    Disable sfg for gaudi2
    Change-Id: I92722cac2b0642a179ec04a617467f85262bd69c

--- habana_bridge/kernel/hpu_habna_launch_op_opt_pass.cpp
+++ habana_bridge/kernel/hpu_habna_launch_op_opt_pass.cpp
@@ -156,5 +156,10 @@ void HabanaLaunchOpPT::runMetaDataAdjustmentPasses(
   // This pass marks tensors external if they are used as input tensors for
   // collective ops. Used for Signal From Graph to signal the tensor data is
   // ready prior to recipe completion
-  externalMarkingPass(graph_nodes);
+  // TODO: SW-80913 enable for gaudi 2
+  auto& device = synapse_helpers::HPURegistrar::get_device();
+  auto device_type = device.type();
+  if (device_type == synDeviceGaudi || device_type == synDeviceGaudiM) {
+    externalMarkingPass(graph_nodes);
+  }
 }
