ef71869d8b6a079e8b9cc42e7722328efe7b7548
Internal Developer <internal-developer@intel.com>
2022-04-06 07:39:34 +0000

    Grid Sampler DS Test Case
    Change-Id: I8ff1113ed4d8d6b9a10ba023b3d4290e375781ea

--- tests/cpp/lazy_dynamic_shapes_tests.cpp
+++ tests/cpp/lazy_dynamic_shapes_tests.cpp
@@ -1603,6 +1603,36 @@ TEST_F(LazyDynamicShapesTest, DS_PadTest_IDST) {
   UNSET_ENV_FLAG_NEW(PT_HPU_DEV_ENABLE_PAD_HOST_TENSOR);
 }
 
+TEST_F(LazyDynamicShapesTest, DS_GridSamplerTest) {
+  auto grid_sampler_test = [](std::vector<int64_t> pad_sizes,
+                              std::vector<int64_t> input_shape) {
+    torch::Tensor tensor = torch::randn(pad_sizes).to(torch::kFloat);
+    torch::Tensor tensorHabana = tensor.to(torch::kHPU);
+    torch::Tensor grid_tensor = torch::randn(input_shape).to(torch::kFloat);
+    torch::Tensor gridHabana = grid_tensor.to(torch::kHPU);
+    namespace F = torch::nn::functional;
+    auto outHabana = F::grid_sample(
+        tensorHabana,
+        gridHabana,
+        F::GridSampleFuncOptions()
+            .mode(torch::kBilinear)
+            .padding_mode(torch::kZeros)
+            .align_corners(false));
+    auto out = F::grid_sample(
+        tensor,
+        grid_tensor,
+        F::GridSampleFuncOptions()
+            .mode(torch::kBilinear)
+            .padding_mode(torch::kZeros)
+            .align_corners(false));
+    bool equal = out.allclose(outHabana.to(torch::kCPU), 0.000001, 0.000001);
+    EXPECT_EQ(equal, true);
+  };
+  grid_sampler_test({2, 3, 4, 4}, {2, 10, 10, 2});
+  grid_sampler_test({1, 1, 2, 2}, {1, 3, 3, 2});
+  grid_sampler_test({1, 1, 2, 2}, {1, 4, 4, 2});
+}
+
 void runIndexPutDynamicTestBool(int N, bool acc) {
   std::vector<int64_t> dimensions = {N};
 
