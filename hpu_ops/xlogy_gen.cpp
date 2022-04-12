977741bd8976aea72d68c688a3003637258280b0
Internal Developer <internal-developer@intel.com>
2022-04-12 16:09:13 +0300

    Resolve BF16 GC failure for xlog1py and xlogy variants
    Change-Id: I4b0d315a1bd9d8ab35055dd6713a54b680c1a35b

--- hpu_ops/xlogy_gen.cpp
+++ hpu_ops/xlogy_gen.cpp
@@ -25,45 +25,43 @@ sizes_vec XlogYOutputShape(const at::Stack& stack, bool) {
   return {at::infer_size(self.sizes(), other.sizes())};
 }
 
-template <>
-LazyXlogY<at::Tensor>::LazyXlogY(
-    const std::string& qualstring,
-    const std::vector<at::IValue>& inputs,
-    const std::function<sizes_vec(const at::Stack&, bool)>& out_shapes_fn)
-    : habana_lazy::LazyOp<at::Tensor>(qualstring, inputs, out_shapes_fn, -1) {}
-
-template <>
-at::Tensor LazyXlogY<at::Tensor>::get_result_overrideable() {
-  const auto& inputs = habana_lazy::LazyOp<at::Tensor>::get_inputs();
-  const auto& t = inputs.at(1).toTensor();
-  return habana_lazy::empty_hpu_lazy(
-      t.sizes(), t.options(), t.suggest_memory_format(), false);
-}
-
 void XlogYOperator::AddNode(
     synapse_helpers::graph& graph,
     const at::Stack& stack) {
   auto outshape = XlogYOutputShape(stack)[0];
-  if (stack.at(1).isScalar()) {
-    auto logy = BuildOp(graph, guid_, {syn_in(1)}, {{1, ScalarType()}});
+  auto self_shape = stack_tensor(stack, 0).sizes().vec();
+  auto other_shape = stack_tensor(stack, 1).sizes().vec();
 
-    auto xlogy = BuildOp(
-        graph,
-        MULT_GUID + habana_helpers::name_suffix_from_type(ScalarType()),
-        {syn_in(0), logy[0].get()},
-        {{outshape, ScalarType(), 0}});
-    syn_out(0) = std::move(xlogy[0]);
-  } else {
-    auto other_shape = stack_tensor(stack, 1).sizes();
-    auto logy =
-        BuildOp(graph, guid_, {syn_in(1)}, {{other_shape, ScalarType()}});
+  auto self = stack.at(0).toTensor();
+  auto other = stack.at(1).toTensor();
 
-    auto xlogy = BuildOp(
-        graph,
-        MULT_GUID + habana_helpers::name_suffix_from_type(ScalarType()),
-        {syn_in(0), logy[0].get()},
-        {{outshape, ScalarType(), 0}});
-    syn_out(0) = std::move(xlogy[0]);
+  std::vector<synTensor> self_input{syn_in(0)};
+  std::vector<synTensor> other_input{syn_in(1)};
+  std::unique_ptr<synapse_helpers::tensor> cast;
+
+  // TODO: To remove cast nodes after adding frontend support
+  // https://jira.habana-labs.com/browse/SW-86489
+  if (self_shape == std::vector<int64_t>({1})) { // Scalar | Tensor
+    if (ScalarType() == c10::ScalarType::BFloat16) {
+      cast = std::make_unique<synapse_helpers::tensor>(CastHelper(
+          graph, syn_in(0), self_shape, self.scalar_type(), ScalarType()));
+      self_input = {cast->get()};
+    }
+  } else if (other_shape == std::vector<int64_t>({1})) { // Tensor | Scalar
+    if (ScalarType() == c10::ScalarType::BFloat16) {
+      cast = std::make_unique<synapse_helpers::tensor>(CastHelper(
+          graph, syn_in(1), other_shape, other.scalar_type(), ScalarType()));
+      other_input = {cast->get()};
+    }
   }
+
+  auto logy = BuildOp(graph, guid_, other_input, {{other_shape, ScalarType()}});
+  auto xlogy = BuildOp(
+      graph,
+      MULT_GUID + habana_helpers::name_suffix_from_type(ScalarType()),
+      {self_input[0], logy[0].get()},
+      {{outshape, ScalarType(), 0}});
+
+  syn_out(0) = std::move(xlogy[0]);
 }
 } // namespace habana
