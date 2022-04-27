f7e8c7595ee3acf7003360f59a7499e4461dc83b
Internal Developer <internal-developer@intel.com>
2022-04-27 08:37:40 +0300

    Avoid the recompile issue when torch seed be changed.
    Change-Id: I62d3dbbd813e12f345da561e5120a28b335726bc

--- hpu_ops/exponential_gen.cpp
+++ hpu_ops/exponential_gen.cpp
@@ -21,15 +21,17 @@ std::shared_ptr<void> FillExponentialParams(
       lambd >= 0.0,
       "exponential_ expects lambda >= 0.0, but found lambda=",
       lambd);
-  uint32_t seed = stack.at(2).to<uint32_t>();
   params->beta = 1.0 / lambd;
-  params->seed = seed;
   return params;
 }
 
-void ExponentialIntSeedInput::AddNode(
+void ExponentialSeedTensorInput::AddNode(
     synapse_helpers::graph& graph,
     const at::Stack& stack) {
+  // Discard self tensor, input is seed tensor only
+  p_context_->syn_inputs_.pop_front();
+  HABANA_ASSERT(p_context_->syn_inputs_.size() == 1);
+
   auto outshape = stack_tensor(stack, 0).sizes();
   size_t size = 0;
   auto params = FillExponentialParams(stack, size);
@@ -37,7 +39,7 @@ void ExponentialIntSeedInput::AddNode(
       graph,
       "random_exponential_fwd_" +
           habana_helpers::name_suffix_from_type(ScalarType()),
-      {},
+      {syn_in(0)},
       {{outshape, ScalarType(), 0}},
       params.get(),
       size);
