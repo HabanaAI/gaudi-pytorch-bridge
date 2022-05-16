6ff37ec58e246dcbdd78d55ea896d4cb74248cda
Internal Developer <internal-developer@intel.com>
2022-05-16 17:26:05 +0300

    [PT data layout] Enable syn DL flow by default
    Change-Id: I604fb7e060c122e5a588ff7ef2e39a700e13c15b

--- pytorch_helpers/synapse_helpers/env_flags.h
+++ pytorch_helpers/synapse_helpers/env_flags.h
@@ -308,7 +308,7 @@ ENV_STRUCT_DEFINITION(PT_ENABLE_DEFRAGMENTATION_INFO, bool, false);
 // Option to pass layouts handling to new flow, which will be handled by Syanpse
 // No more need to permute layouts/weight via jit optimization passes.
 // All will be handled via Synapse
-ENV_STRUCT_DEFINITION(PT_HPU_ENABLE_SYNAPSE_LAYOUT_HANDLING, bool, false);
+ENV_STRUCT_DEFINITION(PT_HPU_ENABLE_SYNAPSE_LAYOUT_HANDLING, bool, true);
 ENV_STRUCT_DEFINITION(PT_HPU_FCD_STRIDE_OPT, bool, true);
 
 // Option to enable numeric limits check for CPU tensors when downcasting
