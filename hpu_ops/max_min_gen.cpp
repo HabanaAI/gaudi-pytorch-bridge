8d6c0828523877214b042f7994e2c3610d68dbe5
Internal Developer <internal-developer@intel.com>
2022-05-20 15:58:55 +0300

    CleanUp Reduction ops to use reduction helpers
     1. min.dim
     2. Nansum
    
    Change-Id: Ic430724886134f351c5b45ce68ed4957274ccfed

--- hpu_ops/max_min_gen.cpp
+++ hpu_ops/max_min_gen.cpp
@@ -13,6 +13,25 @@
 
 namespace habana {
 
+sizes_vec MinMaxOutputShape(const at::Stack& stack, bool) {
+  const torch::Tensor& self = stack_tensor(stack, 0);
+  auto dim = stack.at(1).toInt();
+  auto keepdim = stack.at(2).toBool();
+
+  auto shapes = ReductionOutputShape(self, dim, keepdim)[0];
+  return {shapes, shapes};
+}
+
+std::shared_ptr<void> FillMinMaxParams(const at::Stack& stack, size_t& size) {
+  PARAMS_STUB(ns_Reduction::Params);
+  auto dim = stack.at(1).toInt();
+  dim = (dim >= 0) ? static_cast<int>(stack.at(0).toTensor().dim()) - 1 - dim
+                   : -(dim + 1);
+
+  params->reductionDimension = dim;
+  return params;
+}
+
 void MinMaxOut::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
   auto self = stack.at(0).toTensor();
   auto dim = stack.at(1).toInt();
--- hpu_ops/nansum_gen.cpp
+++ hpu_ops/nansum_gen.cpp
@@ -29,17 +29,6 @@ sizes_vec NanSumIntListOutputShape(const at::Stack& stack, bool) {
   return {shape};
 }
 
-std::shared_ptr<void> NanSumParams(
-    const at::Stack& stack,
-    size_t& size,
-    int64_t index) {
-  PARAMS_STUB(ns_Reduction::Params);
-  auto ndim = static_cast<int>(stack.at(0).toTensor().dim());
-  auto reduction_dim = ndim - 1 - index;
-  params->reductionDimension = reduction_dim;
-  return params;
-}
-
 void NansumList::AddNode(
     synapse_helpers::graph& graph,
     const at::Stack& stack) {
@@ -88,31 +77,10 @@ void Nansum::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
   const auto& outshape = stack_tensor(stack, 0).sizes();
   auto dtype = c10::ScalarType::Char;
 
-  struct Parameters {
-    std::shared_ptr<void> param_list;
-    size_t size_list;
-    std::vector<int64_t> shape_list;
-  };
-
   auto self = stack.at(0).toTensor();
-
   std::vector<int64_t> new_shape{1};
-  std::vector<int64_t> orig_shape{self.sizes().vec()};
-
-  std::vector<synTensor> reduce_sum_list;
-  std::vector<synapse_helpers::tensor> reduce_sum_itr;
-  std::vector<Parameters> parameters;
-  Parameters p;
   auto guid =
       guidReducesum + habana_helpers::name_suffix_from_type(ScalarType());
-
-  for (int64_t dimIndex = orig_shape.size() - 1; dimIndex >= 0; dimIndex--) {
-    orig_shape[dimIndex] = 1;
-
-    size_t size = 0;
-    auto params = NanSumParams(stack, size, dimIndex);
-    parameters.push_back({params, size, orig_shape});
-  }
 
   // isNan on input
   auto is_nan = BuildOp(
@@ -130,44 +98,16 @@ void Nansum::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
       {is_nan[0].get(), zero_constant.get(), syn_in(0)},
       {{outshape, ScalarType()}});
 
-  int64_t len = parameters.size();
-
-  auto reduce_sum = BuildOp(
+  auto reduce_sum = HandleReductionDimAndKeepdim(
+      this,
       graph,
-      guid,
+      self,
       {where[0].get()},
-      {{parameters[0].shape_list, ScalarType()}},
-      parameters[0].param_list.get(),
-      parameters[0].size_list);
-  reduce_sum_list.emplace_back(reduce_sum[0].get());
+      {},
+      false,
+      guid,
+      {{new_shape, ScalarType(), 0}});
 
-  if (len > 1) {
-    // Iterating over the for loop when multiple dim values are passed
-    for (int i = 1; i <= len - 1; i++) {
-      reduce_sum_itr = BuildOp(
-          graph,
-          guid,
-          {reduce_sum_list[i - 1]},
-          {{parameters[i].shape_list, ScalarType()}},
-          parameters[i].param_list.get(),
-          parameters[i].size_list);
-
-      // Reshape the last node to final output shape
-      if (i == len - 1) {
-        auto reshape = ReshapeHelper(
-            graph, reduce_sum_itr[0].get(), new_shape, ScalarType(), 0);
-
-        syn_out(0) = std::move(reshape);
-      }
-      reduce_sum_list.push_back(reduce_sum_itr[0].get());
-    }
-  }
-
-  if (len == 1) {
-    auto reshape =
-        ReshapeHelper(graph, reduce_sum[0].get(), new_shape, ScalarType(), 0);
-
-    syn_out(0) = std::move(reshape);
-  }
+  syn_out(0) = std::move(reduce_sum[0]);
 }
 } // namespace habana
