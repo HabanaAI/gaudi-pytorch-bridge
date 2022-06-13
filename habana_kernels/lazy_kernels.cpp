3cc7b84f95b84367716ed469bcb55fcdec9a3063
Internal Developer <internal-developer@intel.com>
2022-06-13 12:16:33 +0300

    disable weight permute if DS flag is on
    Change-Id: Ia621f88283f544268bd9c30b633c3f6af10ad36c

--- habana_kernels/lazy_kernels.cpp
+++ habana_kernels/lazy_kernels.cpp
@@ -1769,7 +1769,8 @@ Tensor convolution_hpu_lazy(
   }
 
   if (GET_ENV_FLAG_NEW(PT_HPU_ENABLE_SYNAPSE_LAYOUT_HANDLING) &&
-      GET_ENV_FLAG_NEW(PT_HPU_ENABLE_WEIGHT_CPU_PERMUTE)) {
+      GET_ENV_FLAG_NEW(PT_HPU_ENABLE_WEIGHT_CPU_PERMUTE) &&
+      !GET_ENV_FLAG_NEW(PT_HPU_ENABLE_REFINE_DYNAMIC_SHAPES)) {
     habana_lazy::PermuteTensors::permuteWeight(weight_hpu);
   }
 
