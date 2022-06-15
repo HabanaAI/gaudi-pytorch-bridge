ceda52f2361dcec25fdfd8e01285a5974c58bde4
Internal Developer <internal-developer@intel.com>
2022-06-15 12:48:22 +0300

    Falllback to cpu if dim >= 5
    Change-Id: I93b6e4ae59f4b187e5a97943fcd94113582a65f5

--- habana_kernels/kernels_register.cpp
+++ habana_kernels/kernels_register.cpp
@@ -1727,6 +1727,14 @@ Tensor hpu_wrap::binary_cross_entropy_with_logits(
       IValue(reduction)};
   check_handle->hpu_check_ivalues("binary_cross_entropy_with_logits", op_stack);
 
+  int64_t sz = (int64_t)(self.sizes().size());
+
+  if (sz >= 5) {
+    FALLBACK_IF_UNSUPPORTED_OP2(
+        binary_cross_entropy_with_logits,
+        PARAMS2(self, target, weight, pos_weight, reduction))
+  }
+
   FALLBACK_IF_UNSUPPORTED_OP1(
       binary_cross_entropy_with_logits,
       PARAMS1(
