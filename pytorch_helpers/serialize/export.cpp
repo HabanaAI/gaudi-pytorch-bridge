07d27742f91ccb97f69a4a6f53faabb2d58ce3da
Internal Developer <internal-developer@intel.com>
2022-06-22 08:00:15 +0300

    Fix compilation failure for PT 1.12 upgrade
    Change-Id: I9476912c165d014e374de1567e5fb97a4932ce04
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- pytorch_helpers/serialize/export.cpp
+++ pytorch_helpers/serialize/export.cpp
@@ -10,16 +10,37 @@
  *
  *******************************************************************************
  */
-
-#include <torch/csrc/jit/serialization/export.h>
-
-#include <string>
 #include "export.h"
+#include <torch/csrc/api/include/torch/version.h>
+#include <string>
+
+#if ((TORCH_VERSION_MAJOR == 1) && (TORCH_VERSION_MINOR == 12))
+#include <torch/csrc/onnx/onnx.h>
+#include <map>
+#include <memory>
+namespace torch {
+namespace jit {
+extern std::string pretty_print_onnx(
+    const std::shared_ptr<Graph>& graph,
+    const std::map<std::string, at::Tensor>& initializers,
+    int64_t onnx_opset_version,
+    bool defer_weight_export,
+    ::torch::onnx::OperatorExportTypes operator_export_type,
+    bool google_printer,
+    bool keep_initializers_as_inputs,
+    const std::map<std::string, int>& custom_opsets,
+    bool add_node_names);
+
+}
+} // namespace torch
+
+#else
+#include <torch/csrc/jit/serialization/export.h>
+#endif // Torch 1.12 specific hack. Remove on PT 1.13 upgrade
 
 namespace serialize {
 
 constexpr int64_t kONNXOpsetVersion = 8;
-
 std::string GraphToProtoString(const GraphPtr& graph) {
   return torch::jit::pretty_print_onnx(
       graph,
