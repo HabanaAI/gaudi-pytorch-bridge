17493cb6a2ed15dcc3cb8af31a61739cb1280b0a
Internal Developer <internal-developer@intel.com>
2022-06-28 19:23:20 +0000

    Revert " use duplicate_tensor_in_memory_section in memcpy operator"
    This reverts commit 98e95a3c8f9b288f5f7de68bd37597fc143df925.
    
    Reason for revert: regression in pytorch lightning 
    
    Change-Id: Ia21ee0deb013e9f9f246d96fd73f7a404864f670

--- habana_kernels/basic_kernels.cpp
+++ habana_kernels/basic_kernels.cpp
@@ -654,9 +654,8 @@ void MemCopyOperator::AllocateAndAddSynapseNode(
   at::Tensor output;
   if (inputs.size() == 2) {
     output = inputs[1].toTensor();
-    p_context_->syn_outputs_.emplace_back(
-        habana_helpers::duplicate_tensor_in_memory_section(
-            p_context_->syn_inputs_[1], graph, output_metadata.at(0).external));
+    synapse_helpers::tensor& output_tensor = p_context_->syn_inputs_[1];
+    p_context_->syn_outputs_.emplace_back(output_tensor);
     p_context_->pt_outputs_.emplace_back(output);
   } else {
     output =
