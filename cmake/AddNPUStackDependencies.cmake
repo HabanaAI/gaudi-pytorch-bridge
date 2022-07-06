608f5589ed49c15dd4dd9276efa33bc85660de43
Internal Developer <internal-developer@intel.com>
2022-07-06 20:48:52 +0300

    Add option to build with/without shim
    OFF by default.
    
    Change-Id: I85505577804d91e795a75c599a9bc3ef5dced6d2
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- cmake/AddNPUStackDependencies.cmake
+++ cmake/AddNPUStackDependencies.cmake
@@ -8,13 +8,22 @@
 ################################################################################
 cmake_minimum_required(VERSION 3.0)
 
+if (MANYLINUX)
 add_library(Synapse INTERFACE IMPORTED)
 list(APPEND LIB_INCLUDE_DIRS "$ENV{SYNAPSE_ROOT}/include")
 list(APPEND LIB_INCLUDE_DIRS "$ENV{HCL_ROOT}/include")
 set_target_properties(Synapse PROPERTIES
   INTERFACE_INCLUDE_DIRECTORIES "${LIB_INCLUDE_DIRS}"
   )
-
+else ()
+add_library(Synapse SHARED IMPORTED)
+list(APPEND LIB_INCLUDE_DIRS "$ENV{SYNAPSE_ROOT}/include")
+list(APPEND LIB_INCLUDE_DIRS "$ENV{HCL_ROOT}/include")
+set_target_properties(Synapse PROPERTIES
+  IMPORTED_LOCATION "$ENV{BUILD_ROOT_LATEST}/libSynapse.so"
+  INTERFACE_INCLUDE_DIRECTORIES "${LIB_INCLUDE_DIRS}"
+  )
+endif ()
 add_library(tpc_kernels SHARED IMPORTED)
 set_target_properties(tpc_kernels PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "$ENV{SPECS_EXT_ROOT}")
