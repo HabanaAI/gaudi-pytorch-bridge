9b48d630cf3837f8fa8b11d8a7d824eda2481f57
Internal Developer <internal-developer@intel.com>
2022-07-08 05:54:02 +0000

    Support GaudiM user stream
    in cluster02, the synDeviceType is synDeviceGaudiM instead of
    synDeviceGaudi. Hence add check for that in get_compute_stream_count()
    
    Change-Id: I6d9edda6578c81687508e33e1f2e7d08d0ba2edd

--- pytorch_helpers/synapse_helpers/device.h
+++ pytorch_helpers/synapse_helpers/device.h
@@ -388,7 +388,7 @@ class device {
 
   int get_compute_stream_count() {
     // FIXME need to get the info from synapse.
-    if (type_ == synDeviceGaudi)
+    if (type_ == synDeviceGaudi || type_ == synDeviceGaudiM)
       return 2;
     if (type_ == synDeviceGaudi2)
       return 4;
