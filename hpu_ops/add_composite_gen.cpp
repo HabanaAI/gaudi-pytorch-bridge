c0650177ed32c837483ad8199e5952836b61580e
Internal Developer <internal-developer@intel.com>
2022-08-01 17:31:25 +0300

    Enable caching for addcdiv/addcmul variants
    addcdiv and addcmul are defined as :
    out = input + value * (other1 op other2), where 'op' is div or
    mul and 'value' is a scalar.
    
    This patch converts 'value' from scalar to tensor to avoid graph
    cache misses when its value changes. Further the resulting tensor
    is cached for DMA transfer to avoid redundant DMAs.
    
    Change-Id: Ia2032a2e05d1759a3450a5892c9a5607dd84a6bf
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- hpu_ops/add_composite_gen.cpp
+++ hpu_ops/add_composite_gen.cpp
@@ -11,57 +11,87 @@
 #include "generated/addcdiv.h"
 #include "generated/addcmul.h"
 
+constexpr int inp_idx = 0; // index of input(self)
+constexpr int oth1_idx = 1; // index of other1
+constexpr int oth2_idx = 2; // index of other2
+constexpr int val_idx = 3; // index of value
+
 namespace habana {
 
 sizes_vec AddCOpsOutputShape(const at::Stack& stack, bool) {
-  const torch::Tensor& self = stack_tensor(stack, 0);
-  const torch::Tensor& other1 = stack_tensor(stack, 1);
-  const torch::Tensor& other2 = stack_tensor(stack, 2);
+  const torch::Tensor& self = stack_tensor(stack, inp_idx);
+  const torch::Tensor& other1 = stack_tensor(stack, oth1_idx);
+  const torch::Tensor& other2 = stack_tensor(stack, oth2_idx);
   auto tmp = at::infer_size(self.sizes(), other1.sizes());
   return {at::infer_size(tmp, other2.sizes())};
 }
 
-void AddCOpOut::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
-  const at::Tensor self = stack_tensor(stack, 0);
-  const at::Tensor other1 = stack_tensor(stack, 1);
-  const at::Tensor other2 = stack_tensor(stack, 2);
+static void convert_scalar_val_to_tensor(std::vector<at::IValue>& inputs) {
+  auto self = inputs[inp_idx].toTensor();
+  auto s = inputs[val_idx].toScalar();
+  double val = s.to<double>();
+  at::Tensor val_t;
+  if (val != 1.0)
+    val_t = habana_lazy::get_tensor_for_scalar(val, self.options());
+  c10::optional<at::Tensor> val_t_opt = c10::make_optional(val_t);
+  inputs[val_idx] = c10::IValue(val_t_opt);
+}
 
-  std::vector<synapse_helpers::tensor> mul, constant, variable_op;
-  std::vector<synTensor> vectSynTensor{syn_in(0), syn_in(1), syn_in(2)};
+template <>
+AddCOpFE<at::Tensor&>::AddCOpFE(
+    const std::string& qualstring,
+    const std::vector<at::IValue>& inputs,
+    const std::function<sizes_vec(const at::Stack&, bool)>& out_shapes_fn)
+    : habana_lazy::LazyOp<at::Tensor&>(qualstring, inputs, out_shapes_fn) {
+  auto x = get_inputs();
+  // convert "value" scalar to tensor to avoid cache misses
+  convert_scalar_val_to_tensor(x);
+  set_inputs(x);
+}
 
+template <>
+AddCOpFE<at::Tensor>::AddCOpFE(
+    const std::string& qualstring,
+    const std::vector<at::IValue>& inputs,
+    const std::function<sizes_vec(const at::Stack&, bool)>& out_shapes_fn)
+    : habana_lazy::LazyOp<at::Tensor>(qualstring, inputs, out_shapes_fn) {
+  auto x = get_inputs();
+  // convert "value" scalar to tensor to avoid cache misses
+  convert_scalar_val_to_tensor(x);
+  set_inputs(x);
+}
+template <>
+at::Tensor& AddCOpFE<at::Tensor&>::get_result_overrideable() {
+  return LazyOp<at::Tensor&>::get_result_overrideable();
+}
+template <>
+at::Tensor AddCOpFE<at::Tensor>::get_result_overrideable() {
+  return LazyOp<at::Tensor>::get_result_overrideable();
+}
+
+void AddCOpBE::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
+  const at::Tensor self = stack_tensor(stack, inp_idx);
+  const at::Tensor other1 = stack_tensor(stack, oth1_idx);
+  const at::Tensor other2 = stack_tensor(stack, oth2_idx);
+
+  std::vector<synapse_helpers::tensor> mul, variable_op;
+  std::vector<synTensor> vectSynTensor{
+      syn_in(inp_idx), syn_in(oth1_idx), syn_in(oth2_idx)};
+
+  // variable_op_inputs = {other1, other2}
   std::vector<synTensor> variable_op_inputs{
-      vectSynTensor.at(1), vectSynTensor.at(2)};
-  // if scalar mul if required, only then do cast and do the mult
-  if (ScalarInputs().size() and
-      ScalarInputs().at(ScalarId()[0]).toFloat() != 1.) {
-    size_t size = 0;
-    const at::Scalar& scalarVal = ScalarInputs().at(ScalarId()[0]);
-    PARAMS_STUB(ns_ConstantKernel::Params);
-    if (scalarVal.isIntegral(/*include bools*/ false)) {
-      get<int>(params->constant) = scalarVal.to<int>();
-    } else {
-      get<float>(params->constant) = scalarVal.to<float>();
-    }
-
-    // Cast the scalar into target "ScalarType()" and create a tensor with
-    // scalar's value
-    constant = BuildOp(
-        graph,
-        "constant_" + habana_helpers::name_suffix_from_type(ScalarType()),
-        {},
-        {{1, ScalarType()}},
-        params.get(),
-        size);
-
-    // Do the multiplication with Generated tensor
-    mul = BuildOp(
+      vectSynTensor.at(oth1_idx), vectSynTensor.at(oth2_idx)};
+  // if necessary (i.e, if value != 1 in y = input + value * (other1 op other2)
+  // where op = mul or div) do multiplication with value
+  if (!stack.at(val_idx).isNone()) {
+    mul = BuildOp( // other1 * value
         graph,
         MULT_GUID + habana_helpers::name_suffix_from_type(ScalarType()),
-        {vectSynTensor.at(1), constant[0].get()},
+        {vectSynTensor.at(oth1_idx), syn_in(val_idx)},
         {{other1.sizes(), ScalarType()}});
-    variable_op_inputs = {mul[0].get(), vectSynTensor.at(2)};
-
-  } // if(ScalarInputs().size()
+    // variable_op_inputs = {other1 * value, other2}
+    variable_op_inputs = {mul[0].get(), vectSynTensor.at(oth2_idx)};
+  }
 
   // Based on the guid_, do mult/div/other binary op
   auto outsize_variable_op = at::infer_size(other1.sizes(), other2.sizes());
@@ -70,7 +100,7 @@ void AddCOpOut::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
 
   // Finally add op with self
   std::vector<synTensor> add_op_inputs{
-      vectSynTensor.at(0), variable_op[0].get()};
+      vectSynTensor.at(inp_idx), variable_op[0].get()};
   auto outshape = AddCOpsOutputShape(stack, true)[0];
 
   auto add_op = BuildOp(
