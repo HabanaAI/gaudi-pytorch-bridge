42125a5161aadb6cc96874d944685721090888dc
Internal Developer <internal-developer@intel.com>
2022-08-17 13:04:09 +0300

    Set rounding mode for FP16 to BF16
    Change-Id: Ibe01575eef52d6f13fcba987e817055e0e8c8bf1

--- habana_kernels/kernel_utils.h
+++ habana_kernels/kernel_utils.h
@@ -25,12 +25,13 @@
 #include "habana_helpers/logging.h"
 #include "habana_kernels/habana_operator.h"
 
-// Incase of F32 to BF16 use round to nearest even. Use truncation for all F32
-// to any int type conversion. Rounding mode is not relevant for other cases
+// Incase of F32/FP16 to BF16 use round to nearest even. Use truncation for all
+// F32 to any int type conversion. Rounding mode is not relevant for other cases
 // like integral  to integral or integral to floating type conversion.
-#define SET_CAST_ROUNDING_MODE(guid)                            \
-  params.round_mode = (guid == std::string("cast_f32_to_bf16")) \
-      ? CAST_ROUND_HALF_NE                                      \
+#define SET_CAST_ROUNDING_MODE(guid)                              \
+  params.round_mode = (guid == std::string("cast_f32_to_bf16") || \
+                       guid == std::string("cast_f16_to_bf16"))   \
+      ? CAST_ROUND_HALF_NE                                        \
       : CAST_ROUND_ZERO;
 namespace habana_helpers {
 std::optional<std::string> direct_cast_guid(
