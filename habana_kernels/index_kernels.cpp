07ac91072de41b3ccd01bd6d368b02599545ac6e
Internal Developer <internal-developer@intel.com>
2022-08-23 21:43:27 +0300

    Handle slice fallback for improved cache hit
    Change-Id: Ic65a770cf7ded6330d84935fd005b4029bbfb51f
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- habana_kernels/index_kernels.cpp
+++ habana_kernels/index_kernels.cpp
@@ -2141,6 +2141,29 @@ void SliceOperator::AllocateAndAddSynapseNode(
     auto step = inputs[2].toTensor().sizes().vec();
     auto start = inputs[3].toTensor().sizes().vec();
 
+    if (habana::ShapeInference::GetCurrentPass() ==
+        habana::ShapeInfo::InferencePass::MAX_SHAPE) {
+      for (uint64_t i = 0; i < inp_shape.size(); i++) {
+        out_shape[i] =
+            out_shape[i] < inp_shape[i] ? out_shape[i] : inp_shape[i];
+        /*
+        (end - start)/step = output
+        Assuming end max is input
+        (input - start)/step = output
+        input - start = output * step
+        start = input - output * step
+        */
+        start[i] = inp_shape[i] - (out_shape[i] * step[i]);
+      }
+      // Modify the start and output shape in name shape map to create valid
+      // ranges
+      synapse_helpers::tensor& syn_tensor_output = p_context_->syn_inputs_[1];
+      habana::ShapeInference::UpdateShapeInfo(
+          graph, syn_tensor_output.id(), out_shape);
+      synapse_helpers::tensor& syn_tensor_start = p_context_->syn_inputs_[3];
+      habana::ShapeInference::UpdateShapeInfo(
+          graph, syn_tensor_start.id(), start);
+    }
     ValidateSliceInputs(inp_shape, out_shape, step, start);
   } else {
     TORCH_CHECK(
