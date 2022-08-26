024a3c8fc668c83fbaab6510a961e56d3fc653f8
Internal Developer <internal-developer@intel.com>
2022-08-26 08:17:15 +0530

    Cosmic Tagger doesn't have Op for aten::fill_ issue fix
    Change-Id: I44f790e97ac0f6c570f23b75d7504b8e11d62adf

--- hpu_ops/fill_gen.cpp
+++ hpu_ops/fill_gen.cpp
@@ -11,6 +11,23 @@
 #include "generated/fill.h"
 
 namespace habana {
+template <>
+FillFE<at::Tensor&>::FillFE(
+    const std::string& qualstring,
+    const std::vector<at::IValue>& inputs,
+    const std::function<sizes_vec(const at::Stack&, bool)>& out_shapes_fn)
+    : habana_lazy::LazyOp<at::Tensor&>(qualstring, inputs, out_shapes_fn) {
+  // NOTE: Workaround due to https://github.com/pytorch/pytorch/issues/75465
+  if (inputs[1].isBool()) {
+    get_inputs()[1] = static_cast<int>(inputs[1].toBool());
+  }
+}
+
+template <>
+at::Tensor& FillFE<at::Tensor&>::get_result_overrideable() {
+  return habana_lazy::LazyOp<at::Tensor&>::get_result_overrideable();
+}
+
 void Fill::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
   const auto& outshape = stack_tensor(stack, 0).sizes();
 
