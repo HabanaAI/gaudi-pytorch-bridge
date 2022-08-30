e7e8aa7ecb104fd0ab518133da2766eee4c33bb1
Internal Developer <internal-developer@intel.com>
2022-08-30 12:43:41 +0300

    HPU Graph Backward, along with test.
    Change-Id: I121f1a7413db58ba8bffaaa23c35b2a43393acc3

--- tests/test_hpu_graph.py
+++ tests/test_hpu_graph.py
@@ -26,5 +26,23 @@ def test_graph_capture_simple():
             warp_func(False)
     ht.hpu.synchronize()
 
+def test_graph_training():
+    #N, D_in, H, D_out = 640, 4096, 2048, 1024
+    N, D_in, H, D_out = 2, 2, 2, 2
+    module1 = torch.nn.Linear(D_in, H).to('hpu')
+    loss_fn = torch.nn.MSELoss()
+    optimizer = torch.optim.SGD(module1.parameters(),lr=0.1)
+    x = torch.randn(N, D_in, device='hpu')
+    module1 = ht.hpu.make_graphed_callables(module1, (x,))
+    real_inputs = [torch.rand_like(x) for _ in range(10)]
+    real_targets = [torch.randn(N, D_out, device="hpu") for _ in range(10)]
+    for data, target in zip(real_inputs, real_targets):
+        optimizer.zero_grad(set_to_none=True)
+        tmp = module1(data)
+        loss = loss_fn(tmp, target)
+        loss.backward()
+        optimizer.step()
+
 if __name__ == "__main__":
     test_graph_capture_simple()
+    test_graph_training()
