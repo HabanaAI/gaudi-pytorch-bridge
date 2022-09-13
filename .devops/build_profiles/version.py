d598eaafa14802981405d3655cf6f0394b57a5a3
Internal Developer <internal-developer@intel.com>
2022-09-13 16:23:22 +0200

    Add (multi-)version comparison
    Based on TFs mechanism
    
    Change-Id: I8f977a2fef02617bccfb2cdea0f03ffefc4105dc

---
+++ .devops/build_profiles/version.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+###############################################################################
+# Copyright (C) 2021-2022 Habana Labs, Ltd. an Intel Company
+# All Rights Reserved.
+#
+# Unauthorized copying of this file or any element(s) within it, via any medium
+# is strictly prohibited.
+# This file contains Habana Labs, Ltd. proprietary and confidential information
+# and is subject to the confidentiality and license agreements under which it
+# was provided.
+#
+###############################################################################
+
+from __future__ import annotations
+import sys
+from typing import Any, Optional, Type, Union
+
+import packaging.version
+from pkg_resources import get_platform
+
+
+class Version(packaging.version.Version):
+    """A PEP-440-compliant version with extensions for version matching, labels,
+    comparisons, etc.
+
+    PyPA reference for PEP-440: https://packaging.pypa.io/en/stable/version.html
+    """
+
+    def __init__(
+        self, version: Union[str, Type(sys.version_info)], label: Optional[str] = None
+    ) -> None:
+        """
+        :param version a string in PEP-440 format or a Python system version
+        :param label   optional custom symbolic label of the version. Used to generate @see Version.label.
+        """
+        self._label = label
+        if isinstance(version, str):
+            super().__init__(version)
+        elif isinstance(version, type(sys.version_info)):
+            version = f"{version.major}.{version.minor}.{version.micro}"
+            super().__init__(version)
+        else:
+            raise TypeError(
+                f"Version must be a string or a sys.version_info: {version}"
+            )
+
+    def __eq__(self, other: Any) -> bool:
+        try:
+            rhs = Version(other) if isinstance(other, str) else other
+            return super().__eq__(rhs)
+        except packaging.version.InvalidVersion:
+            return False
+
+    def __hash__(self):
+        return hash(str(self))
+
+    @property
+    def label(self):
+        """
+        Either a customized version name like "nightly" or version string built
+        out of version numbers. For usage @see prepare_venv in build.py
+        """
+        return self._label if self._label else str(self)
+
+    def _platform_matches(self, candidate: Version) -> bool:
+        def get_platform(plat: Optional[str]):
+            if not plat or plat.startswith("git"):
+                return "cpu"
+            return plat
+
+        lhs_platform = get_platform(self.local)
+        rhs_platform = get_platform(candidate.local)
+        return lhs_platform == rhs_platform
+
+    def significant_matches(self, candidate: Version) -> bool:
+        """Checks if all version components of wildcard (i.e. self) and the
+        compute platform match those from candidate.
+
+        For instance:
+        Version(" ").significant_matches(candidate=Version("2.2.3.4a0")) == True
+        Version("1.3+cpu").significant_matches(Version("1.3.0+cpu")) == True
+
+        Assume git hashes in local part of version mean a CPU platform.
+        """
+        wildcard, candidate_ver = self.release, candidate.release
+        assert len(wildcard) <= len(candidate_ver)
+        release_matches = all(w == c for w, c in zip(wildcard, candidate_ver))
+
+        if not self._platform_matches(candidate):
+            return False
+
+        if not self.is_prerelease:
+            return release_matches
+        else:
+            return release_matches and self.pre == candidate.pre
+
+
+def is_official_stable_cpu_version(pt_ver: Version) -> bool:
+    return (
+        not pt_ver.is_prerelease and pt_ver.local == "cpu" and not pt_ver.is_devrelease
+    )
+
+
+def is_official_nightly_cpu_version(pt_ver: Version) -> bool:
+    return (
+        not pt_ver.is_prerelease
+        and pt_ver.local == "cpu"
+        and len(str(pt_ver.dev)) == len("20190731")
+    )
+
+
+def is_pt_fork_version(pt_ver: Version) -> bool:
+    return pt_ver.pre == ("a", 0) and pt_ver.local.startswith("git")
