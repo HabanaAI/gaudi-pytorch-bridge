8569755fce1f9ebee804e4cfcb0ef469d8b22c2d
Internal Developer <internal-developer@intel.com>
2022-09-13 16:10:11 +0530

    Re-enable trace for ops
    Change-Id: I23d0f209a23860bcb985772c126345a054c3471d

--- scripts/gen_op.py
+++ scripts/gen_op.py
@@ -550,6 +550,7 @@ def get_return_type_str(t, orig_sig):
 def generate_entry_debug_code(t, fname, params):
     # Emits debug code for a given intercepted function.
     code = "  PT_OP_TRACE;\n"
+    code += "  PT_LAZY_TRACE;\n"
     code += '  PT_OP_INFO("HpuOp {} :"'.format(fname)
     for p in params:
         ptype = param_type(p)
