1b725282323c62c31ed75f8f5b7786957e1a5dc3
Internal Developer <internal-developer@intel.com>
2022-09-15 09:25:08 +0300

    [MCW-1474] Add error message for Complex Ops
    * For ops that involve tensors of complex types, error message
      is thrown from habana_helpers::is_supported_type function that is used in
      tensor creation and Host-Device copies.
    
    Change-Id: Ieaf2b7f9cc3ebd2171e14f07465e03a1acc8b7bd

--- pytorch_helpers/habana_helpers/tensor_utils.cpp
+++ pytorch_helpers/habana_helpers/tensor_utils.cpp
@@ -1780,6 +1780,12 @@ bool habana_helpers::is_supported_type(c10::ScalarType type) {
       }
       return device_type == synDeviceGaudi2 || device_type == synDeviceGreco;
     }
+    case c10::ScalarType::ComplexHalf:
+    case c10::ScalarType::ComplexFloat:
+    case c10::ScalarType::ComplexDouble: {
+      TORCH_CHECK(false, "Complex datatype is not supported on HPU device.");
+      return false;
+    }
 #if IS_PYTORCH_FORK_AT_LEAST(1, 0)
     case c10::ScalarType::Fp8r152: {
       auto device_type{synapse_helpers::HPURegistrar::get_device().type()};
