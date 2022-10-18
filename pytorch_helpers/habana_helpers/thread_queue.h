eaf91f406725eee62d34acaec55a6ff360cb3bf4
Internal Developer <internal-developer@intel.com>
2022-10-18 18:03:14 +0300

    changing the default queue for lazy-eager
    This is to enable the lazy-eager tests in CI with default
    queue as lock-free:
    
    For lazy-eager:
      PT_HPU_LAZY_EAGER_QUEUE_MODE = 0/2 Lock Free Queue (Preallocated)
      PT_HPU_LAZY_EAGER_QUEUE_MODE = 1   Thread Safe Queue (Preallocated)
    For lazy:
      PT_HPU_LAZY_EAGER_QUEUE_MODE = 0   Lock Free Queue (Preallocated)
      PT_HPU_LAZY_EAGER_QUEUE_MODE = 1   Thread Safe Queue (Preallocated)
      PT_HPU_LAZY_EAGER_QUEUE_MODE = 2   Standard Queue
    
    Change-Id: I3506d9b8c63041d0a3e34c0462c08bdd35a42436
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- pytorch_helpers/habana_helpers/thread_queue.h
+++ pytorch_helpers/habana_helpers/thread_queue.h
@@ -22,6 +22,7 @@
 #include <mutex>
 #include <queue>
 #include "pytorch_helpers/habana_helpers/logging.h"
+#include "pytorch_helpers/synapse_helpers/env_flags.h"
 
 namespace habana_helpers {
 
@@ -221,11 +222,28 @@ class StdQueue : public Queue<T> {
 
 template <typename T>
 Queue<T>* Queue<T>::Create(QueueType type, size_t size) {
-  if (type == QT_LockFree) {
+  QueueType ltype = type;
+  if (GET_ENV_FLAG_NEW(PT_HPU_LAZY_MODE) == 2) {
+    // the default behavior to pick queue in thread-pool
+    // of lazy-eager and lazy mode
+    // For lazy-eager:
+    //   PT_HPU_LAZY_EAGER_QUEUE_MODE = 0/2 Lock Free Queue (Preallocated)
+    //   PT_HPU_LAZY_EAGER_QUEUE_MODE = 1   Thread Safe Queue (Preallocated)
+    // For lazy:
+    //   PT_HPU_LAZY_EAGER_QUEUE_MODE = 0   Lock Free Queue (Preallocated)
+    //   PT_HPU_LAZY_EAGER_QUEUE_MODE = 1   Thread Safe Queue (Preallocated)
+    //   PT_HPU_LAZY_EAGER_QUEUE_MODE = 2   Standard Queue
+    if ((type == QT_LockFree) || (type == QT_Standard)) {
+      ltype = QT_LockFree;
+    } else if (type == QT_WithLock) {
+      ltype = QT_WithLock;
+    }
+  }
+  if (ltype == QT_LockFree) {
     return new ThreadQueueLockFree<T>{size};
-  } else if (type == QT_WithLock) {
+  } else if (ltype == QT_WithLock) {
     return new ThreadQueueWithLock<T>{size};
-  } else if (type == QT_Standard) {
+  } else if (ltype == QT_Standard) {
     return new StdQueue<T>;
   } else {
     return NULL;
