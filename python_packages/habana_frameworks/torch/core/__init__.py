2f2c797874f53e984081809566e77684ece502e5
Internal Developer <internal-developer@intel.com>
2022-11-08 13:26:30 +0200

    Add init_process_group into cache
    *Adding default process group created during init_process_group into PG cache.
    *Once creating a new process group with the whole world, it will get
    default proccess group from cache.
    
    Change-Id: I2fc3e82d5b3793f3540bd6d7803cc6e5e37c1b05

--- python_packages/habana_frameworks/torch/core/__init__.py
+++ python_packages/habana_frameworks/torch/core/__init__.py
@@ -4,6 +4,7 @@ from .step_closure import *
 from collections import deque
 from functools import wraps
 from typing import Union
+import datetime
 import habana_frameworks.torch.utils.debug as htdebug
 import habana_frameworks.torch.utils.experimental as htexp
 from habana_frameworks.torch.utils import _experimental_C
@@ -117,6 +118,7 @@ from torch.distributed.constants import default_pg_timeout
 
 ranks_cache = {}
 new_group_orig = torch.distributed.new_group
+init_process_group_orig = torch.distributed.init_process_group
 
 @wraps(torch.distributed.new_group)
 def wrap_new_group(ranks=None, timeout=default_pg_timeout, backend=None, pg_options=None):
@@ -132,7 +134,20 @@ def wrap_new_group(ranks=None, timeout=default_pg_timeout, backend=None, pg_opti
     ranks_cache[ranks_tuple] = new_group_orig(ranks, timeout, backend, pg_options)
     return ranks_cache[ranks_tuple]
 
+@wraps(torch.distributed.init_process_group)
+def wrap_init_process_group(backend, init_method=None, timeout=datetime.timedelta(seconds=1800), world_size=- 1, rank=- 1, store=None, group_name='', pg_options=None):
+    global ranks_cache
+
+    init_process_group_orig(backend, init_method, timeout, world_size, rank, store, group_name, pg_options)
+
+    actual_world_size = torch.distributed.distributed_c10d.get_world_size()
+    ranks_tuple = tuple(list(range(0, actual_world_size)))
+
+    ranks_cache[ranks_tuple] = torch.distributed.distributed_c10d._get_default_group()
+    return ranks_cache[ranks_tuple]
+
 torch.distributed.new_group = wrap_new_group
+torch.distributed.init_process_group = wrap_init_process_group
 
 
 module_set_attr_orig = torch.nn.Module.__setattr__
