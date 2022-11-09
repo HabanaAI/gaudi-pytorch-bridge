a38e6c2fbaa50ec2f73e16a16420a8b08f5fc75f
Internal Developer <internal-developer@intel.com>
2022-11-09 13:21:58 +0200

    isposinf, isneginf
    Implement ops from the title.
    Additionally implement similar op isinf.
    
    All with gtest and pytest.
    
    Change-Id: Id3a032af3166731e50136abb656913aed4bfb7ed

--- hpu_ops/bool_output_gen.cpp
+++ hpu_ops/bool_output_gen.cpp
@@ -1,13 +1,15 @@
-/******************************************************************************
- * Copyright (C) 2022 HabanaLabs, Ltd.
+/*******************************************************************************
+ * Copyright (C) 2022 Habana Labs, Ltd. an Intel Company
  * All Rights Reserved.
  *
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
+ * Unauthorized copying of this file or any element(s) within it, via any medium
+ * is strictly prohibited.
+ * This file contains Habana Labs, Ltd. proprietary and confidential information
+ * and is subject to the confidentiality and license agreements under which it
+ * was provided.
  *
- ******************************************************************************
+ *******************************************************************************
  */
-
 #include "generated/logical_and.h"
 #include "generated/logical_not.h"
 #include "generated/logical_or.h"
@@ -15,14 +17,14 @@
 
 namespace habana {
 template <>
-LazyLogical<at::Tensor>::LazyLogical(
+LazyBoolOutput<at::Tensor>::LazyBoolOutput(
     const std::string& qualstring,
     const std::vector<at::IValue>& inputs,
     const std::function<sizes_vec(const at::Stack&)>& out_shapes_fn)
     : habana_lazy::LazyOp<at::Tensor>(qualstring, inputs, out_shapes_fn, -1) {}
 
 template <>
-at::Tensor LazyLogical<at::Tensor>::get_result_overrideable() {
+at::Tensor LazyBoolOutput<at::Tensor>::get_result_overrideable() {
   const auto& inputs = habana_lazy::LazyOp<at::Tensor>::get_inputs();
   const auto& t = inputs.at(0).toTensor();
   auto shape =
@@ -30,4 +32,5 @@ at::Tensor LazyLogical<at::Tensor>::get_result_overrideable() {
   return habana_lazy::empty_hpu_lazy(
       shape, t.options().dtype(at::kBool), t.suggest_memory_format(), false);
 }
+
 } // namespace habana
--- scripts/gen_op.py
+++ scripts/gen_op.py
@@ -1,3 +1,14 @@
+# ******************************************************************************
+# Copyright (C) 2022 Habana Labs, Ltd. an Intel Company
+# All Rights Reserved.
+#
+# Unauthorized copying of this file or any element(s) within it, via any medium
+# is strictly prohibited.
+# This file contains Habana Labs, Ltd. proprietary and confidential information
+# and is subject to the confidentiality and license agreements under which it
+# was provided.
+#
+# ******************************************************************************
 from __future__ import print_function
 
 import argparse
