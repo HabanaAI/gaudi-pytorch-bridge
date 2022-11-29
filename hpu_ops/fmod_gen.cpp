36d91a2b8bb418fafb6866c0e5c2c2b1a0a7cb53
Internal Developer <internal-developer@intel.com>
2022-11-29 16:28:33 +0530

    Fmod for integral types to use div_mod guid
    Change-Id: Ia71adb14d9ae204170743b51222fcbe0b638717d

--- hpu_ops/fmod_gen.cpp
+++ hpu_ops/fmod_gen.cpp
@@ -15,34 +15,17 @@ namespace habana {
 void Fmod::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
   const auto& result_type = ScalarType();
 
-  if (result_type == at::kFloat or result_type == at::kBFloat16) {
+  if (c10::isFloatingType(result_type)) {
     return OpBackend::AddNode(graph, stack);
   }
-  // TPC supports only float and bfloat, for other types cast to float and
-  // perform fmod and then, cast back to original type
-
-  auto cast0 = CastHelper(
-      graph,
-      syn_in(0),
-      stack.at(0).isTensor() ? stack_tensor(stack, 0).sizes() : 1,
-      result_type,
-      torch::kFloat);
-  auto cast1 = CastHelper(
-      graph,
-      syn_in(1),
-      stack.at(1).isTensor() ? stack_tensor(stack, 1).sizes() : 1,
-      result_type,
-      torch::kFloat);
-
+  // TPC mod_fwd supports only floating types. For integral types, use div_mod's
+  // remainder output
   const auto outshape = BinaryOutputShape(stack)[0];
-  auto fmod = BuildOp(
+  auto mod = BuildOp(
       graph,
-      "mod_fwd_f32",
-      {cast0.get(), cast1.get()},
-      {{outshape, torch::kFloat}});
-
-  auto final_cast =
-      CastHelper(graph, fmod[0].get(), outshape, torch::kFloat, result_type, 0);
-  syn_out(0) = std::move(final_cast);
+      "div_mod_" + habana_helpers::name_suffix_from_type(result_type),
+      {syn_in(0), syn_in(1)},
+      {{outshape, result_type}, {outshape, result_type, 0}});
+  syn_out(0) = std::move(mod[1]);
 }
 } // namespace habana
