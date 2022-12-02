2d399474d4cf381d9aba529f01e2cb9c1810d331
Internal Developer <internal-developer@intel.com>
2022-12-02 08:51:09 +0000

    Revert " isposinf, isneginf"
    This reverts commit 6a6b72dc9da391f70e109eaf963b5f2697af3cb6.
    
    Change-Id: Ida358479509b8a6faac7081b411e9886d87ddfe5

--- hpu_ops/logical_gen.cpp
+++ hpu_ops/logical_gen.cpp
@@ -1,15 +1,13 @@
-/*******************************************************************************
- * Copyright (C) 2022 Habana Labs, Ltd. an Intel Company
+/******************************************************************************
+ * Copyright (C) 2022 HabanaLabs, Ltd.
  * All Rights Reserved.
  *
- * Unauthorized copying of this file or any element(s) within it, via any medium
- * is strictly prohibited.
- * This file contains Habana Labs, Ltd. proprietary and confidential information
- * and is subject to the confidentiality and license agreements under which it
- * was provided.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
  *
- *******************************************************************************
+ ******************************************************************************
  */
+
 #include "generated/logical_and.h"
 #include "generated/logical_not.h"
 #include "generated/logical_or.h"
@@ -17,14 +15,14 @@
 
 namespace habana {
 template <>
-LazyBoolOutput<at::Tensor>::LazyBoolOutput(
+LazyLogical<at::Tensor>::LazyLogical(
     const std::string& qualstring,
     const std::vector<at::IValue>& inputs,
     const std::function<sizes_vec(const at::Stack&)>& out_shapes_fn)
     : habana_lazy::LazyOp<at::Tensor>(qualstring, inputs, out_shapes_fn, -1) {}
 
 template <>
-at::Tensor LazyBoolOutput<at::Tensor>::get_result_overrideable() {
+at::Tensor LazyLogical<at::Tensor>::get_result_overrideable() {
   const auto& inputs = habana_lazy::LazyOp<at::Tensor>::get_inputs();
   const auto& t = inputs.at(0).toTensor();
   auto shape =
@@ -32,5 +30,4 @@ at::Tensor LazyBoolOutput<at::Tensor>::get_result_overrideable() {
   return habana_lazy::empty_hpu_lazy(
       shape, t.options().dtype(at::kBool), t.suggest_memory_format(), false);
 }
-
 } // namespace habana
