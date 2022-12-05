54479277f09672beb096518cb2f1895f5fe4c844
Internal Developer <internal-developer@intel.com>
2022-12-05 08:28:56 +0000

    Allow init comm group once if cache on
    Change-Id: I4119ba31c8e3e38e5fbb1847b1ba98e9fcd1eb6a

--- python_packages/habana_frameworks/torch/core/__init__.py
+++ python_packages/habana_frameworks/torch/core/__init__.py
@@ -153,15 +153,19 @@ def wrap_new_group(ranks=None, timeout=default_pg_timeout, backend=None, pg_opti
 @wraps(torch.distributed.init_process_group)
 def wrap_init_process_group(backend, init_method=None, timeout=datetime.timedelta(seconds=1800), world_size=- 1, rank=- 1, store=None, group_name='', pg_options=None):
     global ranks_cache
-    init_process_group_orig(backend, init_method, timeout, world_size, rank, store, group_name, pg_options)
     cache_enable = environ.get('PT_ENABLE_COMM_GROUP_CACHE', "False")
     if cache_enable.lower() == "true":
+        if len(ranks_cache) == 0:
+            init_process_group_orig(backend, init_method, timeout, world_size, rank, store, group_name, pg_options)
         actual_world_size = torch.distributed.distributed_c10d.get_world_size()
         ranks_tuple = tuple(list(range(0, actual_world_size)))
+        if ranks_tuple in ranks_cache:
+            return ranks_cache[ranks_tuple]
         ranks_cache[ranks_tuple] = torch.distributed.distributed_c10d._get_default_group()
         return ranks_cache[ranks_tuple]
     else:
-        return torch.distributed.distributed_c10d._get_default_group()
+        return init_process_group_orig(backend, init_method, timeout, world_size, rank, store, group_name, pg_options)
+
 
 torch.distributed.new_group = wrap_new_group
 torch.distributed.init_process_group = wrap_init_process_group
