2ab30a7b0f63b25a81439bc0d8191a6a9f6d59a1
Internal Developer <internal-developer@intel.com>
2022-12-08 16:59:22 +0100

    Add acc thread pool sync to IrReconnectAsInputNode
    Change-Id: Id50b5dd300ba51006b9049edc3075a686261ddbe

--- habana_lazy/hpu_lazy_tensors.cpp
+++ habana_lazy/hpu_lazy_tensors.cpp
@@ -487,6 +487,7 @@ void HbLazyTensor::IrInitAsInputNode() const {
 }
 
 void HbLazyTensor::IrReconnectAsInputNode() const {
+  habana_lazy::SyncAccThreadPool();
   ir::Value val = createIrValueFromData();
   ir::NodePtr node = std::make_shared<ir::Input>(*this);
   val.SetNode(node, GetDevice(), GetSizes(), dtype_optional());
