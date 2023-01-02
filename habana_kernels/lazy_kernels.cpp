482b5ba3854b8512b92e3c8feb440f7140832c1e
Internal Developer <internal-developer@intel.com>
2023-01-02 05:46:04 +0200

    Avoid partial eval for d2h views
    Change-Id: Id2cb852f35aee7462309c3b5b4ac20393dc8530b
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- habana_kernels/lazy_kernels.cpp
+++ habana_kernels/lazy_kernels.cpp
@@ -800,6 +800,24 @@ at::Tensor handleWeightTensorLayout(const Tensor& src) {
   return tensor_data;
 }
 
+void d2h_maybe_eval(const Tensor& src) {
+  if (GET_ENV_FLAG_NEW(PT_SBS) == SBSModes::SBS_MODE_DISABLED) {
+    auto hl_t = GetHbLazyTensor(src);
+    auto id = hl_t.getTensorUniqueId();
+    auto context =
+        habana_lazy::habana_lazy_executor.getDeviceExecutionContext(0);
+    StrideParams* params_ptr = context->viewContext.GetViewTableEntry(id);
+    if (params_ptr != nullptr) {
+      hl_t = GetHbLazyTensor(params_ptr->base);
+    }
+
+    if (hl_t.CurrentIrValue() && !hl_t.CurrentIrValue().IsHpuInputNode()) {
+      PT_LAZY_DEBUG("Triggering mark_step before D2H copy");
+      HbLazyTensor::StepMarker({});
+    }
+  }
+}
+
 Tensor& copy_hpu_lazy_D2H(Tensor& self, const Tensor& src, bool non_blocking) {
   PT_LAZY_TRACE;
 
@@ -817,13 +835,8 @@ Tensor& copy_hpu_lazy_D2H(Tensor& self, const Tensor& src, bool non_blocking) {
   // Remove this SBS check, as of now prepare sbs inputs on calls .to operator
   // and on inplace ops that triggers this mark step, which leads to graph
   // evaluation and we end up losing input tensor.
-  if (GET_ENV_FLAG_NEW(PT_SBS) == SBSModes::SBS_MODE_DISABLED) {
-    auto hl_t = GetHbLazyTensor(src);
-    if (hl_t.CurrentIrValue() && !hl_t.CurrentIrValue().IsHpuInputNode()) {
-      PT_LAZY_DEBUG("Triggering mark_step before D2H copy");
-      HbLazyTensor::StepMarker({});
-    }
-  }
+  d2h_maybe_eval(src);
+
   // handle views
   auto _src = HbLazyTensorViews::HandleViewsD2H(src);
   auto hb_tensor = GetHbLazyTensor(_src);
