621d197ec6d0f4641042ff000790ed750ebdce4a
Internal Developer <internal-developer@intel.com>
2023-01-05 13:07:58 +0200

    Move isnan op to cguid
    Change-Id: I81ff71f02a59be0e545a53287547cebaa3dccb81

--- hpu_ops/isnan_gen.cpp
+++ hpu_ops/isnan_gen.cpp
@@ -25,24 +25,4 @@ template <>
 at::Tensor LazyIsNan<at::Tensor>::get_result_overrideable() {
   return {};
 }
-
-void IsNanOperator::AddNode(
-    synapse_helpers::graph& graph,
-    const at::Stack& stack) {
-  const at::Tensor& self = stack_tensor(stack, 0);
-  const auto& outshape = self.sizes();
-
-  if (c10::isFloatingType(self.scalar_type())) {
-    auto result = BuildOp(
-        graph,
-        "isnan_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
-        {syn_in(0)},
-        {{outshape, torch::kBool, 0}});
-    syn_out(0) = std::move(result[0]);
-  } else {
-    auto false_val = ConstantHelper(graph, false, at::kBool, outshape, 0);
-    syn_out(0) = std::move(false_val);
-  }
-}
-
 } // namespace habana
