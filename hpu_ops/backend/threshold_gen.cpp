076b991c561a3eb4175d2624996a39767f602070
Internal Developer <internal-developer@intel.com>
2023-01-23 16:15:36 +0200

    Frontend changes for Threshold
    Change-Id: Idb15483963e1cd56019bde4f577eec2cae26950f

--- hpu_ops/backend/threshold_gen.cpp
+++ hpu_ops/backend/threshold_gen.cpp
@@ -15,38 +15,13 @@
 
 namespace habana {
 
-void Threshold::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
-  const float threshold = stack.at(1).toScalar().toFloat();
-  const float value = stack.at(2).toScalar().toFloat();
-  const auto& outshape = stack_tensor(stack, 0).sizes();
-
-  if (threshold == 0 && value == 0) {
-    // if threshold and value are zero then the op is same perform same as relu
-    auto output = BuildOp(
-        graph,
-        "relu_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
-        {syn_in(0)},
-        {{outshape, ScalarType(), 0}});
-    syn_out(0) = std::move(output[0]);
-  } else {
-    auto threshold_gen = ConstantHelper(graph, threshold, ScalarType());
-    const at::ScalarType& result_type = c10::ScalarType::Bool;
-    // Checking wheather innput is greater than threshold returns boolean tensor
-    auto greaterthan_threshold = BuildOp(
-        graph,
-        "greater_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
-        {syn_in(0), threshold_gen.get()},
-        {{outshape, result_type}});
-    auto value_gen = ConstantHelper(graph, value, ScalarType());
-    // returns input if it is true else return the value
-    auto output = BuildOp(
-        graph,
-        "where_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
-        {greaterthan_threshold[0].get(), syn_in(0), value_gen.get()},
-        {{outshape, ScalarType(), 0}});
-
-    syn_out(0) = std::move(output[0]);
-  }
+std::shared_ptr<void> FillThresholdParams(
+    const at::Stack& stack,
+    size_t& size) {
+  PARAMS_STUB(ns_ReluKernel::ParamsV2);
+  params->threshold.f = stack.at(1).toScalar().to<float>();
+  params->replacementValue.f = stack.at(2).toScalar().to<float>();
+  return params;
 }
 
 void ThresholdBackward::AddNode(
