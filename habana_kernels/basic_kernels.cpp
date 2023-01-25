a000c990af40247e943afab9f692014fab7cf008
Internal Developer <internal-developer@intel.com>
2023-01-25 07:06:11 +0000

    Revert " Fix control edge: memcopy-concat, 2nd approach"
    This reverts commit 58f81993769cee08a1944a00d015a89e774ea3a1.
    
    Change-Id: I9b4f7dfbebb6f2ad1d81295f9dd199361813123e

--- habana_kernels/basic_kernels.cpp
+++ habana_kernels/basic_kernels.cpp
@@ -1,14 +1,11 @@
-/*******************************************************************************
- * Copyright (C) 2020-2023 Habana Labs, Ltd. an Intel Company
+/******************************************************************************
+ * Copyright (C) 2020 HabanaLabs, Ltd.
  * All Rights Reserved.
  *
- * Unauthorized copying of this file or any element(s) within it, via any medium
- * is strictly prohibited.
- * This file contains Habana Labs, Ltd. proprietary and confidential information
- * and is subject to the confidentiality and license agreements under which it
- * was provided.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
  *
- *******************************************************************************
+ ******************************************************************************
  */
 #include <ATen/ATen.h>
 #include <ATen/CPUFunctions.h>
@@ -369,10 +366,6 @@ void MemCopyOperator::AllocateAndAddSynapseNode(
   at::Tensor output;
   if (inputs.size() == 2) {
     output = inputs[1].toTensor();
-    // Important:
-    // Conditions for calling 'duplicate_tensor_in_memory_section' below
-    // must match conditions in 'inplaceInputId' function in
-    // jitgraph_utils.cpp
     p_context_->syn_outputs_.emplace_back(
         habana_helpers::duplicate_tensor_in_memory_section(
             p_context_->syn_inputs_[1], graph, output_metadata.at(0).external));
--- pytorch_helpers/util/jitgraph_utils.h
+++ pytorch_helpers/util/jitgraph_utils.h
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (C) 2021-2023 Habana Labs, Ltd. an Intel Company
+ * Copyright (C) 2021 Habana Labs, Ltd. an Intel Company
  * All Rights Reserved.
  *
  * Unauthorized copying of this file or any element(s) within it, via any medium
@@ -10,6 +10,7 @@
  *
  *******************************************************************************
  */
+
 #pragma once
 #include <torch/csrc/jit/ir/ir.h>
 
@@ -26,9 +27,5 @@ torch::jit::Node* GetUnpackNodeFromTensorList(const torch::jit::Value* val);
 bool isInGraphOutputs(const torch::jit::Node* node, size_t index);
 bool isInGraphOutputs(const torch::jit::Value* value);
 bool isListNode(const torch::jit::Node* node);
-int inplaceInputId(const torch::jit::Node* node);
-
-inline bool isInplace(const torch::jit::Node* node) {
-  return inplaceInputId(node) >= 0;
-}
+bool isInplace(const torch::jit::Node* node);
 } // namespace jitgraph_utils
