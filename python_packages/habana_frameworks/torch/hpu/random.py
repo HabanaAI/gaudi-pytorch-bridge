774c5914f3befca60ee6b98ce9ceae03dd89c115
Internal Developer <internal-developer@intel.com>
2023-01-25 10:35:24 +0200

    Revert " HPU random API's to use single generator"
    This reverts commit fd860441313afd9693979658df05c82505b0f37f.
    
    Change-Id: Iedd2c47b3005a71344ed3497915d615772b1f122

--- python_packages/habana_frameworks/torch/hpu/random.py
+++ python_packages/habana_frameworks/torch/hpu/random.py
@@ -6,10 +6,9 @@ from ._utils import _get_device_index
 from . import device_count, current_device
 from torch import Tensor
 
-# Keeping default_generators as a list with single instance to keep aligned to cuda
-default_generators: List[torch._C.Generator] = [htcore._get_default_generator()]
-def _default_generator():
-    return default_generators[0]
+default_generators: List[torch._C.Generator] = []
+for i in range(device_count()):
+    default_generators.append(htcore._get_default_generator())
 
 __all__ = ['get_rng_state', 'get_rng_state_all',
            'set_rng_state', 'set_rng_state_all',
@@ -18,38 +17,47 @@ __all__ = ['get_rng_state', 'get_rng_state_all',
 
 def get_rng_state(device: Union[int, str, torch.device] = 'hpu') -> Tensor:
     device_index = _get_device_index(device)
-    if device_index != 0:
-        raise RuntimeError("hpu get_rng_state supports only device 0")
-    return _default_generator().get_state()
+    return default_generators[device_index].get_state()
 
 def set_rng_state(new_state: torch.Tensor, device: Union[int, str, torch.device] = 'hpu') -> None:
     device_index = _get_device_index(device)
-    if device_index != 0:
-        raise RuntimeError("hpu set_rng_state supports only device 0")
-    _default_generator().set_state(new_state)
+    default_generators[device_index].set_state(new_state)
 
 def manual_seed(seed) -> torch._C.Generator:
+    device_index = current_device()
     seed = int(seed)
-    return _default_generator().manual_seed(seed)
+    return default_generators[device_index].manual_seed(seed)
 
 def seed() -> int:
-    return _default_generator().seed()
+    device_index = current_device()
+    return default_generators[device_index].seed()
 
 def initial_seed() -> int:
-    return _default_generator().initial_seed()
+    device_index = current_device()
+    return default_generators[device_index].initial_seed()
 
 def get_rng_state_all() -> List[Tensor]:
-    return List[get_rng_state(0)]
+    results = []
+    for device_index in range(device_count()):
+        results.append(get_rng_state(device_index))
+    return results
 
 def set_rng_state_all(new_states: Iterable[Tensor]) -> None:
-    if len(new_states) != 1:
-        raise RuntimeError("hpu set_rng_state_all supports only states len of 1")
-    for state in new_states:
-        set_rng_state(state, 0)
+    for device_index, state in enumerate(new_states):
+        set_rng_state(state, device_index)
 
 def manual_seed_all(seed: int) -> None:
-    manual_seed(seed)
+    seed = int(seed)
+    for device_index in range(device_count()):
+        default_generators[device_index].manual_seed(seed)
 
 def seed_all() -> None:
-    _default_generator().seed()
-    _default_generator().initial_seed()
+    random_seed = 0
+    seeded = False
+    for device_index in range(device_count()):
+        if not seeded:
+            default_generators[device_index].seed()
+            random_seed = default_generators[device_index].initial_seed()
+            seeded = True
+        else:
+            default_generators[device_index].manual_seed(random_seed)
