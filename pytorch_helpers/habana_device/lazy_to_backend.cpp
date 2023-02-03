7ec8dab0b7c6dd920b7cc9ade6e25d171b8f3514
Internal Developer <internal-developer@intel.com>
2023-02-03 18:22:52 +0200

    lazy_to_backend u22 fixup
    Change-Id: I5b5a100ff31ab2f0a8cd9ef116c03f2916a8ed8d

--- pytorch_helpers/habana_device/lazy_to_backend.cpp
+++ pytorch_helpers/habana_device/lazy_to_backend.cpp
@@ -25,14 +25,14 @@ std::tuple<synapse_helpers::layouts::MemoryPermutation, bool> lazy_to_backend::
   if (GET_ENV_FLAG_NEW(PT_HPU_EAGER_OPS)) {
     PT_BRIDGE_WARN(
         "Skipping permutations for EagerOp with duplicate inputs...");
-    return {{}, true};
+    return {synapse_helpers::layouts::MemoryPermutation{}, true};
   }
   auto hb_weight_impl = habana_lazy::GetHbInternalTensorImpl(tensor);
   if (hb_weight_impl)
     return {
         hb_weight_impl->GetMemoryPermutation(),
         hb_weight_impl->GetDontAllowPermutation()};
-  return {{}, true};
+  return {synapse_helpers::layouts::MemoryPermutation{}, true};
 }
 
 bool lazy_to_backend::is_lazy_inference_call_context() {
