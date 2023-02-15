f20bebdffedc3a8bf79220c6f7f1965332e5c1e8
Internal Developer <internal-developer@intel.com>
2023-02-15 19:28:49 +0200

    [,] extend lazy_to_backend
    get/set host ptr
    set_memory_permutation
    is_shape_tensor
    tensor debug log macro
    
    Change-Id: I9254a9b6eee8f12fbf33ce8f17cef5e9b6a7dcaf

--- backend/lazy_to_backend.h
+++ backend/lazy_to_backend.h
@@ -13,13 +13,77 @@
 
 #pragma once
 #include <ATen/Tensor.h>
+#include <absl/strings/str_format.h>
+#include "backend/helpers/layout.h"
+#include "backend/helpers/tensor_info.h"
 #include "backend/synapse_helpers/layout_utils.h"
+#include "habana_helpers/logging.h"
+
 namespace lazy_to_backend {
 bool is_const_tensor(const at::Tensor& tensor);
 void* host_ptr_for_const_tensor(const at::Tensor& tensor);
 
 std::tuple<synapse_helpers::layouts::MemoryPermutation, bool>
 get_memory_permutation(const at::Tensor& tensor);
+void set_memory_permutations(
+    at::Tensor& tensor,
+    synapse_helpers::layouts::MemoryPermutation permutation,
+    const synRetrievedLaunchTensorInfoExt* info = nullptr);
+
+void set_tensor_layout_format(at::Tensor& tensor, habana::LayoutFormat format);
+habana::LayoutFormat get_tensor_layout_format(const at::Tensor& tensor);
 
 bool is_lazy_inference_call_context();
+bool is_shape_tensor(const at::Tensor& tensor);
+at::Tensor create_empty_tensor(const PtTensorInfo& ti);
+void set_host_ptr(const at::Tensor& tensor, void* host_ptr);
+void* get_host_ptr(const at::Tensor& tensor);
+
+/**
+ * Tokens that can be passed to PT_BACKEND_DEBUG_TENSOR to insert
+ * stringified value of some internal tensor field without exposing
+ * underlying class of the tensor.
+ */
+enum FormatTokens { Permutations = 1, Layout = 2, ImplPtr = 3, DataPtr = 4 };
+
+namespace detail {
+template <typename T, class Enable = void>
+struct InternalFormatter final {};
+
+template <typename T>
+struct InternalFormatter<T> {
+  static const T& format(const at::Tensor&, const T& t) {
+    return t;
+  }
+};
+
+template <>
+struct InternalFormatter<FormatTokens> {
+  static std::string format(const at::Tensor&, FormatTokens t);
+};
+
+} // namespace detail
+
+template <typename... Args>
+void debug_log_internal_tensor(
+    const at::Tensor& tensor,
+    std::string_view format_string,
+    Args... args) {
+  PT_BRIDGE_DEBUG(absl::StrFormat(
+      format_string,
+      lazy_to_backend::detail::InternalFormatter<Args>::format(
+          tensor, args)...));
+}
 } // namespace lazy_to_backend
+
+/**
+ * Macro to eliminate explicit dependency of the debug logs in backend on
+ * the GetHbInternalTensorImpl. This macro has printf semantics and all the
+ * formatted args are passed as is to absl::StrFormat, except for the
+ * FormatTokens that are converted into a string representation of the
+ * requested tensor field.
+ */
+#define PT_BACKEND_DEBUG_TENSOR(tensor, format_string, args...)              \
+  if (IS_MOD_DEBUG_ENABLED(PtLogger::ModuleMask::BRIDGE)) {                  \
+    lazy_to_backend::debug_log_internal_tensor(tensor, format_string, args); \
+  }
--- pytorch_helpers/habana_device/lazy_to_backend.cpp
+++ pytorch_helpers/habana_device/lazy_to_backend.cpp
@@ -1,4 +1,4 @@
-/******************************************************************************
+/*******************************************************************************
  * Copyright (C) 2023 Habana Labs, Ltd. an Intel Company
  * All Rights Reserved.
  *
@@ -41,6 +41,14 @@ std::tuple<synapse_helpers::layouts::MemoryPermutation, bool> lazy_to_backend::
   return {synapse_helpers::layouts::MemoryPermutation{}, false};
 }
 
+bool lazy_to_backend::is_shape_tensor(const at::Tensor& tensor) {
+  if (GET_ENV_FLAG_NEW(PT_HPU_EAGER_OPS)) {
+    return false;
+  }
+  auto hb_weight_impl = habana_lazy::GetHbInternalTensorImpl(tensor);
+  return hb_weight_impl->isShapeTensor();
+}
+
 bool lazy_to_backend::is_lazy_inference_call_context() {
   if (GET_ENV_FLAG_NEW(PT_HPU_LAZY_MODE) != 0) {
     if (!habana_lazy::isDeviceInLoweringMode()) {
@@ -49,4 +57,163 @@ bool lazy_to_backend::is_lazy_inference_call_context() {
     }
   }
   return false;
+}
+
+at::Tensor lazy_to_backend::create_empty_tensor(const PtTensorInfo& ti) {
+  auto pt_tensor = at::empty(ti.get_shape(), ti.get_topts(), ti.get_mf());
+  if (GET_ENV_FLAG_NEW(PT_HPU_EAGER_OPS)) {
+    return pt_tensor;
+  }
+  auto hb_internal_tensor = habana_lazy::GetHbInternalTensorImpl(pt_tensor);
+  PT_BRIDGE_DEBUG(
+      "Cache created a BE tensor, HbInternal address: ", hb_internal_tensor);
+  TORCH_CHECK(
+      hb_internal_tensor != nullptr,
+      "Tensor for ",
+      ti.get_ir_name(),
+      " does not have HbInternalTensor");
+  auto internal_lf = hb_internal_tensor->GetTensorLayout();
+  auto internal_lf_new = ti.getHbInternalLayoutFormat();
+  if (internal_lf != internal_lf_new) {
+    PT_BRIDGE_DEBUG(
+        "For ",
+        ti.get_ir_name(),
+        " updating HbInternalTensorImpl layout from ",
+        internal_lf,
+        " to ",
+        internal_lf_new);
+    hb_internal_tensor->SetTensorLayout(internal_lf_new);
+  }
+  if (GET_ENV_FLAG_NEW(PT_HPU_ENABLE_SYNAPSE_LAYOUT_HANDLING)) {
+    PT_BRIDGE_DEBUG(
+        "Setting synapse permutation as saved in the cache to the output tensor id: ",
+        ti.get_tensor_id(),
+        " permutation: ",
+        VecToString(ti.getHbInternalPermute()));
+    hb_internal_tensor->SetMemoryPermutation(ti.getHbInternalPermute());
+  }
+  return pt_tensor;
+}
+
+void lazy_to_backend::set_memory_permutations(
+    at::Tensor& tensor,
+    synapse_helpers::layouts::MemoryPermutation permutation,
+    const synRetrievedLaunchTensorInfoExt* info) {
+  // It should be handled in SW-122018
+  if (GET_ENV_FLAG_NEW(PT_HPU_EAGER_OPS)) {
+    PT_EAGER_DEBUG("Skipping permutations for EagerOp...");
+    return;
+  }
+  auto impl = habana_lazy::GetHbInternalTensorImpl(tensor);
+  TORCH_CHECK(
+      impl,
+      "Failed to set the permutation because the BE tensor has no internal impl");
+
+  PT_BRIDGE_DEBUG(
+      "Updating the PT tensor HbInternalTensorImpl address: ",
+      impl,
+      " storage address : ",
+      impl->data(),
+      " with permutation: ",
+      VecToString(permutation),
+      " old permutation was: ",
+      VecToString(impl->GetMemoryPermutation()));
+
+  if (permutation.size() != tensor.sizes().size()) {
+    if (!permutation.empty()) {
+      if (info)
+        PT_BRIDGE_WARN(
+            "wrong permute size - info.tensorId=",
+            info->tensorId,
+            " tensor name: ",
+            info->tensorName,
+            "  permute_vec.size = ",
+            permutation.size(),
+            "  PT tensor shape.dims =",
+            tensor.sizes().size(),
+            " PT shape: ",
+            VecToString(tensor.sizes().vec()),
+            " synapse returned tensor dims: ",
+            info->tensorDims,
+            " synapse returned tensor shape: ",
+            VecToString(std::vector<uint64_t>(
+                info->tensorMaxSize, info->tensorMaxSize + info->tensorDims)));
+      HABANA_ASSERT(false);
+    }
+  }
+  impl->SetMemoryPermutation(permutation);
+}
+
+void lazy_to_backend::set_tensor_layout_format(
+    at::Tensor& tensor,
+    habana::LayoutFormat layout) {
+  // It should be handled in SW-122018
+  if (GET_ENV_FLAG_NEW(PT_HPU_EAGER_OPS)) {
+    PT_EAGER_DEBUG("Skipping setting layout for EagerOp...");
+    return;
+  }
+  auto impl = habana_lazy::GetHbInternalTensorImpl(tensor);
+  TORCH_CHECK(
+      impl,
+      "Failed to set tensor layout because the BE tensor has no internal impl");
+  impl->SetTensorLayout(layout);
+}
+
+habana::LayoutFormat lazy_to_backend::get_tensor_layout_format(
+    const at::Tensor& tensor) {
+  // It should be handled in SW-122018
+  if (GET_ENV_FLAG_NEW(PT_HPU_EAGER_OPS)) {
+    PT_EAGER_DEBUG("Returning fixed NCHW layout for EagerOp...");
+    return habana::LayoutFormat::NCHW;
+  }
+  auto impl = habana_lazy::GetHbInternalTensorImpl(tensor);
+  TORCH_CHECK(
+      impl,
+      "Failed to get tensor layout because the BE tensor has no internal impl");
+  return impl->GetTensorLayout();
+}
+
+void lazy_to_backend::set_host_ptr(const at::Tensor& tensor, void* host_ptr) {
+  if (GET_ENV_FLAG_NEW(PT_HPU_EAGER_OPS)) {
+    PT_EAGER_DEBUG("Skipping host ptr for EagerOp...");
+    return;
+  }
+  auto impl = habana_lazy::GetHbInternalTensorImpl(tensor);
+  TORCH_CHECK(impl, "not a lazy tensor");
+  return impl->set_host_ptr(host_ptr);
+}
+
+void* lazy_to_backend::get_host_ptr(const at::Tensor& tensor) {
+  if (GET_ENV_FLAG_NEW(PT_HPU_EAGER_OPS)) {
+    PT_EAGER_DEBUG("returning null host ptr for EagerOp...");
+    return nullptr;
+  }
+  auto impl = habana_lazy::GetHbInternalTensorImpl(tensor);
+  TORCH_CHECK(impl, "not a lazy tensor");
+  return impl->get_host_ptr();
+};
+
+std::string lazy_to_backend::detail::
+    InternalFormatter<lazy_to_backend::FormatTokens>::format(
+        const at::Tensor& tensor,
+        lazy_to_backend::FormatTokens token) {
+  if (GET_ENV_FLAG_NEW(PT_HPU_EAGER_OPS)) {
+    PT_EAGER_DEBUG("Skipping permutations for EagerOp...");
+    return "<EAGER>";
+  }
+  auto impl = habana_lazy::GetHbInternalTensorImpl(tensor);
+  if (!impl) {
+    return "<NOT_A_LAZY_TENSOR>";
+  }
+  switch (token) {
+    case lazy_to_backend::FormatTokens::Permutations:
+      return VecToString(impl->GetMemoryPermutation());
+    case lazy_to_backend::FormatTokens::Layout:
+      return habana::DebugString(impl->GetTensorLayout());
+    case lazy_to_backend::FormatTokens::ImplPtr:
+      return absl::StrCat(absl::Hex(impl, absl::kZeroPad8));
+    case lazy_to_backend::FormatTokens::DataPtr:
+      return absl::StrCat(absl::Hex(impl->data(), absl::kZeroPad8));
+  }
+  return "";
 }
