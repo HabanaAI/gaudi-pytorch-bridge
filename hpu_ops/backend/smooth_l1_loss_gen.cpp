c7cc3d49e9fa468d98df71c7b71c3f71ed9fa1e7
Internal Developer <internal-developer@intel.com>
2023-02-20 08:13:39 +0200

    Implement SmoothL1Loss CGUID in Frontend
    Change-Id: I719be91a8e2704d85e2b3f2cc276733856c7ff95

--- hpu_ops/backend/smooth_l1_loss_gen.cpp
+++ hpu_ops/backend/smooth_l1_loss_gen.cpp
@@ -13,7 +13,25 @@
 
 namespace habana {
 
+std::shared_ptr<void> FillSmoothL1LossFwdParams(
+    const at::Stack& stack,
+    size_t& size) {
+  PARAMS_STUB(ns_SmoothL1LossKernel::Params);
+  auto mode = stack.at(2).toInt();
+  if (mode == at::Reduction::Reduction::Mean)
+    params->mode = LossMode_t::LOSS_REDUCTION_MODE_MEAN;
+  else if (mode == at::Reduction::Reduction::Sum)
+    params->mode = LossMode_t::LOSS_REDUCTION_MODE_SUM;
+  else
+    params->mode = LossMode_t::LOSS_REDUCTION_MODE_NONE;
+  params->beta = stack.at(3).toScalar().to<float>();
+  return params;
+}
+
 sizes_vec SmoothL1LossOutputShape(const at::Stack& stack) {
+  float beta = stack.at(3).toScalar().to<float>();
+  TORCH_CHECK(
+      beta >= 0, "smooth_l1_loss does not support negative values for beta.")
   const torch::Tensor& self = stack_tensor(stack, 0);
   int64_t reduction = stack.at(2).toInt();
   if (reduction == at::Reduction::Reduction::None) {
@@ -143,151 +161,6 @@ void SmoothL1LossBwdOperator::AddNode(
       {{inputshape, ScalarType(), 0}});
 
   syn_out(0) = std::move(grad_in.at(0));
-  return;
-}
-
-void SmoothL1LossOperator::AddNode(
-    synapse_helpers::graph& graph,
-    const at::Stack& stack) {
-  const auto& inputshape = stack_tensor(stack, 0).sizes();
-  auto mode = stack.at(2).toInt();
-  float beta = stack.at(3).toScalar().to<float>();
-  TORCH_CHECK(
-      beta >= 0, "smooth_l1_loss does not support negative values for beta.")
-
-  auto sub = BuildOp(
-      graph,
-      "sub_" + habana_helpers::name_suffix_from_type(ScalarType()),
-      {syn_in(0), syn_in(1)},
-      {{inputshape, ScalarType()}});
-
-  if (beta == 0) {
-    if (mode == at::Reduction::Reduction::None) {
-      auto t_absdiff = BuildOp(
-          graph,
-          "abs_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
-          {sub.at(0).get()},
-          {{inputshape, ScalarType(), 0}});
-
-      syn_out(0) = std::move(t_absdiff.at(0));
-      return;
-    }
-
-    auto t_absdiff = BuildOp(
-        graph,
-        "abs_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
-        {sub.at(0).get()},
-        {{inputshape, ScalarType()}});
-
-    auto n_dims = stack_tensor(stack, 0).dim();
-    std::vector<synTensor> reduction_inputs = {t_absdiff[0].get()};
-    std::vector<synapse_helpers::tensor> t_absdiff_flat;
-
-    if (n_dims > 1) {
-      auto reshape_outshape = stack_tensor(stack, 0).numel();
-      t_absdiff_flat.emplace_back(ReshapeHelper(
-          graph, t_absdiff.at(0).get(), reshape_outshape, ScalarType()));
-      reduction_inputs = {t_absdiff_flat[0].get()};
-    }
-
-    std::string reduction_guid =
-        (mode == at::Reduction::Mean) ? "reduce_mean_fwd_" : "reduce_sum_fwd_";
-    ns_Reduction::Params node_params{};
-    node_params.reductionDimension = 0;
-    auto t_out = BuildOp(
-        graph,
-        reduction_guid + habana_helpers::name_suffix_from_type(ScalarType()),
-        reduction_inputs,
-        {{1, ScalarType(), 0}},
-        &node_params,
-        sizeof(node_params));
-
-    syn_out(0) = std::move(t_out.at(0));
-    return;
-  }
-
-  auto t_absdiff = BuildOp(
-      graph,
-      "abs_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
-      {sub.at(0).get()},
-      {{inputshape, ScalarType()}});
-
-  auto t_beta = ConstantHelper(graph, beta, ScalarType(), inputshape);
-
-  auto mask = BuildOp(
-      graph,
-      "less_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
-      {t_absdiff.at(0).get(), t_beta.get()},
-      {{inputshape, at::kBool}});
-
-  auto loss_params = std::make_shared<ns_MSELossKernel::Params>();
-  loss_params->mode = MSELossMode_t::MSE_LOSS_REDUCTION_MODE_NONE;
-  auto t_mse = BuildOp(
-      graph,
-      "mse_loss_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
-      {syn_in(0), syn_in(1)},
-      {{inputshape, ScalarType()}},
-      loss_params.get(),
-      sizeof(ns_MSELossKernel::Params));
-
-  auto t_mse_scale =
-      ConstantHelper(graph, 0.5 / beta, ScalarType(), inputshape);
-
-  auto t_l2 = BuildOp(
-      graph,
-      MULT_GUID + habana_helpers::name_suffix_from_type(ScalarType()),
-      {t_mse.at(0).get(), t_mse_scale.get()},
-      {{inputshape, ScalarType()}});
-
-  auto t_b = ConstantHelper(graph, 0.5 * beta, ScalarType(), inputshape);
-
-  auto t_l1 = BuildOp(
-      graph,
-      "sub_" + habana_helpers::name_suffix_from_type(ScalarType()),
-      {t_absdiff.at(0).get(), t_b.get()},
-      {{inputshape, ScalarType()}});
-
-  if (mode == at::Reduction::Reduction::None) {
-    auto t_wh = BuildOp(
-        graph,
-        "where_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
-        {mask.at(0).get(), t_l2.at(0).get(), t_l1.at(0).get()},
-        {{inputshape, ScalarType(), 0}});
-
-    syn_out(0) = std::move(t_wh.at(0));
-    return;
-  }
-
-  auto t_wh = BuildOp(
-      graph,
-      "where_fwd_" + habana_helpers::name_suffix_from_type(ScalarType()),
-      {mask.at(0).get(), t_l2.at(0).get(), t_l1.at(0).get()},
-      {{inputshape, ScalarType()}});
-
-  auto n_dims = stack_tensor(stack, 0).dim();
-  std::vector<synTensor> reduction_inputs = {t_wh[0].get()};
-  std::vector<synapse_helpers::tensor> t_wh_flat;
-
-  if (n_dims > 1) {
-    auto reshape_outshape = stack_tensor(stack, 0).numel();
-    t_wh_flat.emplace_back(
-        ReshapeHelper(graph, t_wh.at(0).get(), reshape_outshape, ScalarType()));
-    reduction_inputs = {t_wh_flat[0].get()};
-  }
-
-  std::string reduction_guid =
-      (mode == at::Reduction::Mean) ? "reduce_mean_fwd_" : "reduce_sum_fwd_";
-  ns_Reduction::Params node_params{};
-  node_params.reductionDimension = 0;
-  auto t_out = BuildOp(
-      graph,
-      reduction_guid + habana_helpers::name_suffix_from_type(ScalarType()),
-      reduction_inputs,
-      {{1, ScalarType(), 0}},
-      &node_params,
-      sizeof(node_params));
-
-  syn_out(0) = std::move(t_out.at(0));
   return;
 }
 } // namespace habana
