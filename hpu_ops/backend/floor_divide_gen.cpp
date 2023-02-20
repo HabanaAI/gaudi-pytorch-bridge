4be111fa6e43018ed6ed618b202442c0c3bbbe26
Internal Developer <internal-developer@intel.com>
2023-02-20 07:57:00 +0200

    Add Floor divide CGUID changes in frontend
    Change-Id: I8aa5c72b500ee8124550b19c3f0a5685f100b820

--- hpu_ops/backend/floor_divide_gen.cpp
+++ hpu_ops/backend/floor_divide_gen.cpp
@@ -12,53 +12,25 @@
 #include "habana_kernels/binary_kernels.h"
 #include "pytorch_helpers/habana_helpers/pt_version_check.h"
 
-// Except bfloat16, all other types are computed in following type
-#define COMMON_COMPUTATION_TYPE_TPC c10::ScalarType::Float
-
 namespace habana {
 
-void FloorDivideOperator::AddNode(
-    synapse_helpers::graph& graph,
-    const at::Stack& stack) {
+std::shared_ptr<void> FillFloorDivideParams(
+    const at::Stack& stack,
+    size_t& size) {
   _TORCH_WARN_ONCE(
       "floor_divide is deprecated, and will be removed in a future version of pytorch."
       "It currently rounds toward 0 (like the \'trunc\' function NOT \'floor\')."
       "This results in incorrect rounding for negative values."
       "To keep the current behavior, use torch.div(a, b, rounding_mode=\'trunc\'),"
       "or for actual floor division, use torch.div(a, b, rounding_mode=\'floor\'). (function operator())");
-
-  const at::Tensor self = stack_tensor(stack, 0);
-  const at::Tensor other = stack_tensor(stack, 1);
+  static_cast<void>(stack);
+  PARAMS_STUB(ns_DivModKernel::ParamsV2);
 #if IS_PYTORCH_OLDER_THAN(1, 13)
-  const std::string rounding_mode = "trunc";
+  params->isTruncRoundingMode = true;
 #else
-  const std::string rounding_mode = "floor";
+  params->isTruncRoundingMode = false;
 #endif
-  std::vector<at::Tensor> tensors = {self, other};
-
-  const at::ScalarType& final_result_type = at::result_type(self, other);
-
-  const at::ScalarType& computation_type =
-      (c10::ScalarType::BFloat16 == final_result_type)
-      ? c10::ScalarType::BFloat16
-      : COMMON_COMPUTATION_TYPE_TPC;
-  const std::string opStringSuffix =
-      "_fwd_" + habana_helpers::name_suffix_from_type(computation_type);
-
-  auto shape_out = BinaryOperator::compute_output_shape(self, other);
-
-  auto divOp = BuildOp(
-      graph,
-      "div" + opStringSuffix,
-      {syn_in(0), syn_in(1)},
-      {{shape_out, computation_type}});
-
-  auto makeIntegerOp = BuildOp(
-      graph,
-      rounding_mode + opStringSuffix,
-      {divOp.at(0).get()},
-      {{shape_out, computation_type, 0}});
-
-  syn_out(0) = std::move(makeIntegerOp[0]);
+  return params;
 }
+
 } // namespace habana
