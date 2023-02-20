2fbca3b2daa4ed7726447cf2145acabdffe3bc85
Internal Developer <internal-developer@intel.com>
2023-02-20 17:42:02 +0000

    Revert addcomposite CGUID FE changes
    Revert " Implement addcomposite complex guid frontend"
    
    This reverts commit ba4e7b556a2e1daad431b4905668714f72fb67e4.
    
    Change-Id: I3d12c7cc77fc6c37c84ce4862a979a11947505a4

--- hpu_ops/backend/add_composite_gen.cpp
+++ hpu_ops/backend/add_composite_gen.cpp
@@ -22,34 +22,6 @@ constexpr int val_idx = 3; // index of value
 
 namespace habana {
 
-enum modes { mul, div };
-
-std::shared_ptr<void> FillAddCompositeParams(
-    const at::Stack& stack,
-    enum modes mode_t,
-    size_t& size) {
-  PARAMS_STUB(ns_BinaryWithAlphaKernel::Params);
-  auto value = stack.at(3).toScalar();
-  if (stack_tensor(stack, 0).scalar_type() == c10::ScalarType::Int) {
-    params->alpha.i = value.to<int32_t>();
-  } else {
-    params->alpha.f = value.to<float>();
-  }
-  if (mode_t == mul)
-    params->mode = BinaryWithAlphaMode_t::BINARY_WITH_ALPHA_MODE_CMUL;
-  else if (mode_t == div)
-    params->mode = BinaryWithAlphaMode_t::BINARY_WITH_ALPHA_MODE_CDIV;
-  return params;
-}
-
-std::shared_ptr<void> FillAddcmulParams(const at::Stack& stack, size_t& size) {
-  return FillAddCompositeParams(stack, mul, size);
-}
-
-std::shared_ptr<void> FillAddcdivParams(const at::Stack& stack, size_t& size) {
-  return FillAddCompositeParams(stack, div, size);
-}
-
 sizes_vec AddCOpsOutputShape(const at::Stack& stack) {
   const torch::Tensor& self = stack_tensor(stack, inp_idx);
   const torch::Tensor& other1 = stack_tensor(stack, oth1_idx);
@@ -58,19 +30,44 @@ sizes_vec AddCOpsOutputShape(const at::Stack& stack) {
   return {at::infer_size(tmp, other2.sizes())};
 }
 
-// // void AddCOpBE::AddNode(synapse_helpers::graph& graph, const at::Stack&
-// stack) {
-// //   const at::Tensor self = stack_tensor(stack, inp_idx);
-// //   const at::Tensor other1 = stack_tensor(stack, oth1_idx);
-// //   const at::Tensor other2 = stack_tensor(stack, oth2_idx);
+void AddCOpBE::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
+  const at::Tensor self = stack_tensor(stack, inp_idx);
+  const at::Tensor other1 = stack_tensor(stack, oth1_idx);
+  const at::Tensor other2 = stack_tensor(stack, oth2_idx);
 
-// //   std::vector<synapse_helpers::tensor> mul, variable_op;
+  std::vector<synapse_helpers::tensor> mul, variable_op;
 
-// sizes_vec AddCOpsOutputShape(const at::Stack& stack) {
-//   const torch::Tensor& self = stack_tensor(stack, inp_idx);
-//   const torch::Tensor& other1 = stack_tensor(stack, oth1_idx);
-//   const torch::Tensor& other2 = stack_tensor(stack, oth2_idx);
-//   auto tmp = at::infer_size(self.sizes(), other1.sizes());
-//   return {at::infer_size(tmp, other2.sizes())};
-// }
+  // variable_op_inputs = {other1, other2}
+  std::vector<synTensor> variable_op_inputs{syn_in(oth1_idx), syn_in(oth2_idx)};
+  // if necessary (i.e, if value != 1 in y = input + value * (other1 op other2)
+  // where op = mul or div) do multiplication with value
+  if (!stack.at(val_idx).isNone()) {
+    mul = BuildOp( // other1 * value
+        graph,
+        MULT_GUID + habana_helpers::name_suffix_from_type(ScalarType()),
+        {syn_in(oth1_idx), syn_in(val_idx)},
+        {{other1.sizes(), ScalarType()}});
+    // variable_op_inputs = {other1 * value, other2}
+    variable_op_inputs = {mul[0].get(), syn_in(oth2_idx)};
+  }
+
+  // Based on the guid_, do mult/div/other binary op
+  auto outsize_variable_op = at::infer_size(other1.sizes(), other2.sizes());
+  variable_op = BuildOp(
+      graph, guid_, variable_op_inputs, {{outsize_variable_op, ScalarType()}});
+
+  // Finally add op with self
+  std::vector<synTensor> add_op_inputs{syn_in(inp_idx), variable_op[0].get()};
+  auto outshape = AddCOpsOutputShape(stack)[0];
+
+  auto add_op = BuildOp(
+      graph,
+      "add_" + habana_helpers::name_suffix_from_type(ScalarType()),
+      add_op_inputs,
+      {{outshape, ScalarType(), 0}});
+
+  // output
+  syn_out(0) = std::move(add_op[0]);
+}
+
 } // namespace habana
