eb6d6da68737ee311a444ebb7552b7d6280a63bd
Internal Developer <internal-developer@intel.com>
2023-02-24 14:51:47 +0000

    Revert " Implement Addr complex guid frontend"
    This reverts commit e4fbb9b3159e3d5d96e0afdb2110e8e904be619c.
    
    Change-Id: I1cde4efb3b3e96d124032028866871314e375a85

--- hpu_ops/backend/addr_gen.cpp
+++ hpu_ops/backend/addr_gen.cpp
@@ -27,12 +27,85 @@ sizes_vec AddROutshape(const at::Stack& stack) {
   return {outshape};
 }
 
-std::shared_ptr<void> FillAddrParams(const at::Stack& stack, size_t& size) {
-  PARAMS_STUB(ns_AddrKernel::Params);
-  const float beta = stack.at(3).toScalar().toFloat();
-  const float alpha = stack.at(4).toScalar().toFloat();
-  params->beta = beta;
-  params->alpha = alpha;
-  return params;
+void AddR::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
+  auto vec1 = stack_tensor(stack, 1);
+  auto vec2 = stack_tensor(stack, 2);
+  const float alpha_val = stack.at(4).toScalar().toFloat();
+  const float beta_val = stack.at(3).toScalar().toFloat();
+
+  auto vec1_reshaped = ReshapeHelper(
+      graph,
+      syn_in(1),
+      {1, vec1.sizes()[0], 1},
+      ScalarType()); // (n,) -> (1, n, 1)
+
+  auto vec2_reshaped = ReshapeHelper(
+      graph,
+      syn_in(2),
+      {1, 1, vec2.sizes()[0]},
+      ScalarType()); // (m,) -> (1, 1, m)
+
+  synGEMMParams vecmul_params{};
+
+  std::vector<int64_t> vecmul_outshape = {
+      1, vec1.sizes()[0], vec2.sizes()[0]}; // (1, n, 1)@(1, 1, m) -> (1, n, m)
+
+  // Matrix Multiplication of vec1 and vec2
+  auto vecmul = BuildOp(
+      graph,
+      "batch_gemm",
+      {vec1_reshaped.get(), vec2_reshaped.get()},
+      {{vecmul_outshape, ScalarType()}},
+      &vecmul_params,
+      sizeof(vecmul_params));
+
+  auto alpha = ConstantHelper(graph, alpha_val, ScalarType(), vecmul_outshape);
+
+  auto addr_unsqueezed = BuildOp(
+      graph,
+      MULT_GUID + habana_helpers::name_suffix_from_type(ScalarType()),
+      {vecmul[0].get(), alpha.get()},
+      {{vecmul_outshape, ScalarType()}});
+
+  if (beta_val != 0.0) {
+    auto self = stack_tensor(stack, 0);
+    std::vector<int64_t> self_reshaped_outshape;
+    if (self.dim() == 2) {
+      self_reshaped_outshape = {1, self.sizes()[0], self.sizes()[1]};
+    } else {
+      self_reshaped_outshape = {1, 1, self.sizes()[0]};
+    }
+    std::vector<synapse_helpers::tensor> self_reshaped;
+    self_reshaped.emplace_back(ReshapeHelper(
+        graph,
+        syn_in(0),
+        self_reshaped_outshape,
+        ScalarType())); // (n,m) -> (1, n, m)
+
+    if (beta_val != 1.0) {
+      auto beta =
+          ConstantHelper(graph, beta_val, ScalarType(), vecmul_outshape);
+
+      self_reshaped = BuildOp(
+          graph,
+          MULT_GUID + habana_helpers::name_suffix_from_type(ScalarType()),
+          {self_reshaped[0].get(), beta.get()},
+          {{vecmul_outshape, ScalarType()}});
+    }
+    addr_unsqueezed = BuildOp(
+        graph,
+        "add_" + habana_helpers::name_suffix_from_type(ScalarType()),
+        {self_reshaped[0].get(), addr_unsqueezed[0].get()},
+        {{vecmul_outshape, ScalarType()}});
+  }
+  auto outshape = AddROutshape(stack)[0];
+
+  auto addr_out = BuildOp(
+      graph,
+      "squeeze",
+      {addr_unsqueezed[0].get()},
+      {{outshape, ScalarType(), 0}});
+
+  syn_out(0) = std::move(addr_out[0]);
 }
 } // namespace habana
