0a6d8382006bee74ef2aa11a0e6c70e8830c9d7d
Internal Developer <internal-developer@intel.com>
2023-03-06 06:58:27 +0200

    One shot tool to find divergence in dynamic workloads
    Change-Id: I9a4818e714f326df35af26b5876ee097c8546897
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- python_packages/habana_frameworks/torch/utils/debug/DivergenceTool.py
+++ python_packages/habana_frameworks/torch/utils/debug/DivergenceTool.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import sqlite3
+import subprocess
 
 def compare_databases(db_file1, db_file2):
     conn1 = sqlite3.connect(db_file1)
@@ -26,34 +27,27 @@ def compare_databases(db_file1, db_file2):
         return False
 
     # Check if the data in each table is the same in both databases
-    differing_tables = []
-    for table_name in tables1:
+    differing_tables = {}
+    for table_name in tables1[2:]:
         cursor1.execute(f"SELECT * FROM \"{table_name[0]}\"")
         rows1 = cursor1.fetchall()
         cursor2.execute(f"SELECT * FROM \"{table_name[0]}\"")
         rows2 = cursor2.fetchall()
-
-        if rows1 != rows2:
-            # Get the primary key of the table
-            cursor1.execute(f"PRAGMA table_info(\"{table_name[0]}\")")
-            table_info = cursor1.fetchall()
-            pk_column = [info[1] for info in table_info if info[5] == 1][0]
-            differing_tables.append((table_name[0], pk_column))
+        if len(rows1) != len(rows2):
+            print(table_name[0] + " has different tensor numbers in static and dynamic not comparing")
+        else :
+            row1_data = [lis[9] for lis in rows1]
+            row2_data = [lis[9] for lis in rows2]
+            if row1_data != row2_data:
+                differing_tables[table_name] = []
+                for i in range(len(rows1)):
+                    if(rows1[i][9] != rows2[i][9]):
+                       differing_tables[table_name].append(rows1[i][3])
 
     if differing_tables:
         print("The following tables have different data:")
-        for table in differing_tables:
-            print(f"{table[0]} (primary key: {table[1]})")
-            print(f"Data in {db_file1}:")
-            cursor1.execute(f"SELECT * FROM \"{table[0]}\"")
-            rows1 = cursor1.fetchall()
-            for row in rows1:
-                print(row)
-            print(f"Data in {db_file2}:")
-            cursor2.execute(f"SELECT * FROM \"{table[0]}\"")
-            rows2 = cursor2.fetchall()
-            for row in rows2:
-                print(row)
+        for key, value in differing_tables.items():
+            print(key, value)
         return False
     else:
         return True
@@ -66,6 +60,7 @@ def get_synrec_path():
     if synapse_root is not None :
         file_path = os.path.join(synapse_root, "scripts", "synrec.py")
     possible_paths = [
+        "/root/repos/synapse/scripts/synrec.py"
         "/root/synapse/scripts/synrec.py",
         file_path
     ]
@@ -84,23 +79,31 @@ parser.add_argument('--cmd', help='the workload command to dump and compare stat
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    current_dir = os.getcwd()
+    dump_dir = '/tmp'
     SYNREC_PATH=get_synrec_path()
     # Loop through all files in the current directory
-    for file in os.listdir(current_dir):
+    for file in os.listdir(dump_dir):
         if file.endswith(".json") or file.endswith(".db") or file.endswith(".lock"):
-            print('Deleting file ' + file)
-            os.remove(os.path.join(current_dir, file))
-    synrec_command_static = f'PT_HPU_LAZY_ACC_PAR_MODE=0 PT_HPU_PGM_ENABLE_CACHE=0 PT_HPU_ENABLE_REFINE_DYNAMIC_SHAPES=1 PT_HPU_ENABLE_MIN_MAX_AS_CURRENT=1 {SYNREC_PATH} -t -p ./StaticSynRec --ignore-errors --overwrite -- {args.cmd}'
-    synrec_command_dynamic = f'PT_HPU_LAZY_ACC_PAR_MODE=0 PT_HPU_PGM_ENABLE_CACHE=0 PT_HPU_ENABLE_REFINE_DYNAMIC_SHAPES=1 {SYNREC_PATH} -t -p ./DynamicSynRec --ignore-errors --overwrite -- {args.cmd}'
+            print('Deleting file ' + dump_dir + '/' + file)
+            os.remove(os.path.join(dump_dir, file))
+    synrec_command_static = f'PT_HPU_LAZY_ACC_PAR_MODE=0 PT_HPU_PGM_ENABLE_CACHE=0 PT_HPU_ENABLE_REFINE_DYNAMIC_SHAPES=1 PT_HPU_ENABLE_MIN_MAX_AS_CURRENT=1 {SYNREC_PATH} -t -p /tmp/StaticSynRec --ignore-errors --overwrite -- {args.cmd}'
+    synrec_command_dynamic = f'PT_HPU_LAZY_ACC_PAR_MODE=0 PT_HPU_PGM_ENABLE_CACHE=0 PT_HPU_ENABLE_REFINE_DYNAMIC_SHAPES=1 {SYNREC_PATH} -t -p /tmp/DynamicSynRec --ignore-errors --overwrite -- {args.cmd}'
     script_attr = "script -q -c"
     for cmd in [synrec_command_static, synrec_command_dynamic]:
         print(cmd)
-        process = os.popen(script_attr + '"' + cmd + '"')
-        output = process.read()
-
-    is_equal =  compare_databases('./StaticSynRec.db', './DynamicSynRec.db')
-    if is_equal:
-        print("The static and dynamic runs are equal")
+        process = subprocess.Popen(script_attr + '"' + cmd + '"', stdout=subprocess.PIPE, shell=True)
+        while True:
+            output = process.stdout.readline().decode()
+            if not output:
+                break
+            print(output, end='')
+    static_path = '/tmp/StaticSynRec.db'
+    dynmaic_path = '/tmp/DynamicSynRec.db'
+    if os.path.getsize(static_path) == 0 or os.path.getsize(dynmaic_path) == 0:
+        print("The synrec didn't run correctly, db files are empty")
     else:
-        print("There is divergence in static and dynamic runs")
+        is_equal =  compare_databases(static_path, dynmaic_path)
+        if is_equal:
+            print("The static and dynamic runs are equal")
+        else:
+            print("There is divergence in static and dynamic runs")
