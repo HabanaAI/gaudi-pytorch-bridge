0a1191c0908bde0df7060ab0e4966cafd660e4ca
Internal Developer <internal-developer@intel.com>
2023-03-07 14:03:41 +0200

    Move topk.values to BE
    Change-Id: Ibd23ca5b5f791edf00096bcd2f2cbfd3d1a2ff43

--- hpu_ops/lazy/topk_gen.cpp
+++ hpu_ops/lazy/topk_gen.cpp
@@ -53,25 +53,4 @@ std::tuple<at::Tensor, at::Tensor> LazyTopk<
   return {values, indices};
 }
 
-template <>
-LazyTopkOut<std::tuple<at::Tensor&, at::Tensor&>>::LazyTopkOut(
-    const std::string& qualstring,
-    const std::vector<at::IValue>& inputs,
-    const std::function<sizes_vec(const at::Stack&)>& out_shapes_fn)
-    : habana_lazy::LazyOp<std::tuple<at::Tensor&, at::Tensor&>>(
-          qualstring,
-          inputs,
-          out_shapes_fn,
-          -1) {
-  auto input = get_inputs();
-  convert_k_to_tensor(input);
-  set_inputs(input);
-}
-
-template <>
-std::tuple<at::Tensor&, at::Tensor&> LazyTopkOut<
-    std::tuple<at::Tensor&, at::Tensor&>>::get_result_overrideable() {
-  return LazyOp<
-      std::tuple<at::Tensor&, at::Tensor&>>::get_result_overrideable();
-}
 } // namespace habana
