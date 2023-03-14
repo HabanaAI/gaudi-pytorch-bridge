aad7e56a3fac8d835588e74a7c9c8ac54e2a7c38
Internal Developer <internal-developer@intel.com>
2023-03-14 13:47:19 +0200

    masked_scatter_hpu_lazy_ in BE - implementation
    Change-Id: I07da2a0f7f828b835107d7eed32e10aa8aaa74ad

--- hpu_ops/op_backend.h
+++ hpu_ops/op_backend.h
@@ -263,6 +263,22 @@ class OpBackend : public HabanaOperator {
       at::ScalarType dtype,
       c10::optional<int> final_result_index = c10::nullopt);
 
+  static std::vector<synapse_helpers::tensor> BuildNonZero(
+      OpBackend*,
+      synapse_helpers::graph&,
+      synapse_helpers::tensor&,
+      at::IntArrayRef,
+      at::ScalarType,
+      c10::optional<int> = c10::nullopt);
+
+  static synapse_helpers::tensor BuildScatterNDOnnx(
+      OpBackend*,
+      synapse_helpers::graph&,
+      const std::vector<std::reference_wrapper<synapse_helpers::tensor>>&,
+      at::IntArrayRef,
+      at::ScalarType,
+      c10::optional<int> = c10::nullopt);
+
   struct TensorsPair {
     const at::Tensor& pt_t;
     synapse_helpers::tensor& sh_t;
