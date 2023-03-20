a8841ebf652519b7f1c3d0ee37577e35ffa8e30a
Internal Developer <internal-developer@intel.com>
2023-03-20 18:51:32 +0200

    expose HabanaExtraMeta to python
    Change-Id: Ib42915ba82b3e9aaa116db7e9f654f10861cb826

--- python_packages/habana_frameworks/torch/core/bindings.cpp
+++ python_packages/habana_frameworks/torch/core/bindings.cpp
@@ -12,6 +12,7 @@
  */
 #include <pybind11/chrono.h>
 #include <torch/extension.h>
+#include "backend/backend_meta.h"
 #include "backend/kernel/hpu_habana_cache.h"
 #include "habana_kernels/fallback_helper.h"
 #include "habana_kernels/random_gen_kernels.h"
@@ -24,6 +25,55 @@ int GetCurrentThreadDevice() {
   return d.id();
 }
 } // namespace
+
+class SharedTensorExtraMeta {
+ public:
+  auto get_memory_permutation() {
+    return get().get_memory_permutation();
+  }
+  auto get_dont_allow_permutation() const {
+    return get().get_dont_allow_permutation();
+  }
+  const habana::TensorExtraMeta& get() const {
+    return tmeta_;
+  }
+  static std::optional<SharedTensorExtraMeta> create(const at::Tensor& tensor) {
+#if HAVE_TORCH_BACKEND_META_SUPPORT
+    auto impl{tensor.unsafeGetTensorImpl()};
+    c10::intrusive_ptr<habana::BaseTensorExtraMeta> meta{
+        impl->get_backend_meta()};
+    if (!meta)
+      return {};
+    auto tmeta_ptr{dynamic_cast<habana::TensorExtraMeta*>(meta.get())};
+    PT_EAGER_DEBUG(
+        "Producing SharedTensorExtraMeta for impl : ",
+        impl,
+        " tensor meta at address ",
+        tmeta_ptr,
+        " storage address : ",
+        tensor.data_ptr());
+
+    TORCH_CHECK(
+        tmeta_ptr != nullptr,
+        "Got BackendMeta ",
+        meta.get(),
+        " but it is not habana::TensorExtraMeta");
+    return std::optional<SharedTensorExtraMeta>(
+        SharedTensorExtraMeta(meta, *tmeta_ptr));
+#else
+    return {};
+#endif
+  }
+
+ private:
+  c10::intrusive_ptr<habana::BaseTensorExtraMeta> tmeta_ref_holder_;
+  habana::TensorExtraMeta& tmeta_;
+
+  SharedTensorExtraMeta(
+      c10::intrusive_ptr<habana::BaseTensorExtraMeta> tmeta_ref_holder,
+      habana::TensorExtraMeta& tmeta)
+      : tmeta_ref_holder_{tmeta_ref_holder}, tmeta_{tmeta} {}
+};
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("_hb_get_default_device", []() { return GetCurrentThreadDevice(); });
@@ -39,4 +89,11 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     return habana::getDefaultHPUGenerator();
   });
   m.doc() = "This module registers hpu lazy api.";
+  py::class_<SharedTensorExtraMeta>(m, "TensorExtraMeta")
+      .def_property_readonly(
+          "memory_permutation", &SharedTensorExtraMeta::get_memory_permutation)
+      .def_property_readonly(
+          "dont_allow_permutation",
+          &SharedTensorExtraMeta::get_dont_allow_permutation);
+  m.def("get_tensor_extra_meta", &SharedTensorExtraMeta::create);
 }
