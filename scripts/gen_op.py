5017d982e21dcfa6e758b5d328eb715dc639d4ac
Internal Developer <internal-developer@intel.com>
2023-03-21 17:06:11 +0200

    Fix linking PT2 plugin and pt2 test (disable equal op)
    Change-Id: I220b8b739348c7cc38078a21a80c2f4b4a5d548c

--- scripts/gen_op.py
+++ scripts/gen_op.py
@@ -1344,7 +1344,7 @@ def is_eager_op(fname, rtype, sig, ctxop):
         elif "TensorList" in sig:
             return False
         elif rtype == "bool":
-            return True
+            return False
         elif rtype == "void":
             return False
         else:
