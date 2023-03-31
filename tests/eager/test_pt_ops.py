3bc6430e63aadfc50006672ba5445da3059447d5
Internal Developer <internal-developer@intel.com>
2023-03-31 08:17:57 +0800

    register aten::argmax
    Change-Id: Ie61ae8d2f8c60f3593c419c9fd64a1facd3ff95e

---
+++ tests/eager/test_pt_ops.py
@@ -0,0 +1,26 @@
+import functools
+import torch
+import habana_frameworks.torch.core as htcore
+import habana_frameworks.torch.dynamo.compile_backend
+
+def test_argmax(mode):
+    def test(func, cpu_tensor):
+        hpu_tensor = cpu_tensor.to("hpu")
+
+        if mode == "graph":
+            result_nocompile = func(hpu_tensor).to("cpu")
+            compiled_function_training = torch.compile(func, backend="aot_hpu_training_backend")
+            result_compile = compiled_function_training(hpu_tensor).to("cpu")
+            result_compile = result_compile.to(torch.int64)    # TODO: SW-140031
+            assert torch.allclose(result_nocompile, result_compile, rtol=0, atol=0)
+        else:
+            result_cpu = func(cpu_tensor)
+            result_hpu = func(hpu_tensor).to("cpu")
+            assert torch.allclose(result_cpu, result_hpu, rtol=0, atol=0)
+
+    B0 = 4
+    test(lambda x: torch.argmax(x), torch.randn(B0))
+    test(lambda x: torch.argmax(x), torch.randn(B0, 2, 3))
+    test(lambda x: torch.argmax(x, dim=0), torch.randn(B0, 2, 3))
+    test(lambda x: torch.argmax(x, dim=-1), torch.randn(B0, 2, 3))
+    test(lambda x: torch.argmax(x, dim=2, keepdim=True), torch.randn(B0, 2, 3))
