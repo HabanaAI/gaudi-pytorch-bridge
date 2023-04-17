d75ef4f306f9729dd3516513e6be36368717b74c
Internal Developer <internal-developer@intel.com>
2023-04-17 14:25:28 +0300

    Integrate dynamic shape detection tool
    Change-Id: I7782157668ded4934f9fc76c9cbde92d9e64ef1d

--- python_packages/habana_frameworks/torch/utils/experimental/__init__.py
+++ python_packages/habana_frameworks/torch/utils/experimental/__init__.py
@@ -3,6 +3,7 @@ import habana_frameworks.torch.hpu as hpu
 from habana_frameworks.torch.utils._experimental_C import synDeviceType
 from habana_frameworks.torch.utils import _experimental_C
 import habana_frameworks.torch.hpu.memory as htmem
+from habana_frameworks.torch.utils.experimental.detect_recompilation import detect_recompilation_auto_model, data_dynamicity, const_shape_dataloader
 
 _model_params_initialized = False
 _optim_state_initialized = False
