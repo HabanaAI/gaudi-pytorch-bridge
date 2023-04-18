fa4d995c7f4f92c2ca501eca2d6049ada3b2ae88
Internal Developer <internal-developer@intel.com>
2023-04-18 09:39:08 +0300

    reintroduce PT_HPU_USE_UNSORTED_SCATTER_ADD
    Re-introduce PT_HPU_USE_UNSORTED_SCATTER_ADD flag.
    It's true by default, so no change is expected by default.
    
    Change-Id: I4b2c9386546a69a5aa782620fe41c47145f5ac9b

--- habana_kernels/index_kernels.cpp
+++ habana_kernels/index_kernels.cpp
@@ -397,8 +397,9 @@ void ScatterAddOperator::AllocateAndAddSynapseNode(
   p_context_->params_.emplace<ns_ScatterKernel::Params>(params);
   p_context_->params_size_ = sizeof(params);
 
-  if (synapse_helpers::HPURegistrar::get_device().type() !=
-      synDeviceType::synDeviceGaudi) {
+  if (GET_ENV_FLAG_NEW(PT_HPU_USE_UNSORTED_SCATTER_ADD) &&
+      synapse_helpers::HPURegistrar::get_device().type() !=
+          synDeviceType::synDeviceGaudi) {
     if (self.scalar_type() == c10::ScalarType::BFloat16) {
       auto cast_op1 = make_operator<CastOperator>(
           self.device().index(), "cast_bf16_to_f32");
