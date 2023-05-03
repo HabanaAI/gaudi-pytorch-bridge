8d6112b192005bd2749419dec9132ddce7b1332d
Internal Developer <internal-developer@intel.com>
2023-05-03 11:51:35 +0000

    Conditionally use unsorted scatter add
    Invoke unsorted scatter add for gaudi2/3 only when
    deterministic flag is false.
    
    Change-Id: I15c280af58bf6c33909c29250b01cc975a9bb857
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- habana_kernels/index_kernels.cpp
+++ habana_kernels/index_kernels.cpp
@@ -398,6 +398,7 @@ void ScatterAddOperator::AllocateAndAddSynapseNode(
   p_context_->params_size_ = sizeof(params);
 
   if (GET_ENV_FLAG_NEW(PT_HPU_USE_UNSORTED_SCATTER_ADD) &&
+      synapse_helpers::HPURegistrar::get_device().getDeterministic() == false &&
       synapse_helpers::HPURegistrar::get_device().type() !=
           synDeviceType::synDeviceGaudi) {
     if (self.scalar_type() == c10::ScalarType::BFloat16) {
