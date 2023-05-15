485c2823f597c3e90022c5a7e2c2468e6a97dfff
Internal Developer <internal-developer@intel.com>
2023-05-15 06:16:46 +0000

    Revert " calling synTensorSetHostPtr() with right arguments"
    This reverts commit 72074fcc45275c0f2b27e6d5691ab5593557a7ab.
    
    Change-Id: I41bc1b81ec1c0b95c22e2bec3890e6c70f852a56

--- backend/synapse_helpers/habana_tensor.cpp
+++ backend/synapse_helpers/habana_tensor.cpp
@@ -491,7 +491,7 @@ synapse_error_o tensor::create() {
       if (is_const_section_) {
         host_ptr_size_ = total_size_bytes_;
         status = synTensorSetHostPtr(
-            tensor_, host_ptr_, total_size_bytes_, data_type_, true);
+            tensor_, host_ptr_, total_size_bytes_, data_type_, false);
         SYNAPSE_SUCCESS_CHECK_WITH_OP(
             "synTensorSetHostPtr min sizes failed.", status, cleanup());
       }
