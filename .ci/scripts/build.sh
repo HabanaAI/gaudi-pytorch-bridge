a5c96d96e3e857e5069d4a3d0005d903709afe40
Internal Developer <internal-developer@intel.com>
2023-05-16 13:21:58 +0300

    Add build_pytorch_data macro
    Change-Id: I64295f1fa488987e30e51de4dd2ec752fe934609
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- .ci/scripts/build.sh
+++ .ci/scripts/build.sh
@@ -22,6 +22,7 @@ function pytorch_functions_help()
     echo -e "build_pytorch_tb_plugin        -   Build habana pytorch tensorboard plugin"
     echo -e "build_habana_lightning_plugins -   Build habana lightning plugin"
     echo -e "build_lightning_habana_fork    -   Build lightning habana fork"
+    echo -e "build_pytorch_data             -   Build habana pytorch data"
     echo -e "build_pytorch_text             -   Build habana pytorch text"
     echo -e "run_pytorch_qa_tests           -   Run pytorch QA tests"
     echo -e "run_pytorch_modules_tests      -   Run pytorch modules tests"
@@ -219,6 +220,21 @@ function pytorch_usage()
         echo -e "  -t,  --suite-type TYPE              Run specific suite type [all, py_tests, cpp_tests]. Default: all"
         echo -e "  -hllog LOG_LEVEL                    0-TRACE, 1-DEBUG 2-INFO, 3-WARN, 4-ERR, 5-CRITICAL"
         echo -e "  -h,  --help                         Prints this help"
+    fi
+
+    if [ $1 == "build_pytorch_data" ]; then
+        echo -e "\n usage: $1 [options]\n"
+
+        echo -e "options:\n"
+        echo -e "  -j,  --jobs <val>           Max jobs used for compilation"
+        echo -e "  -c,  --clean                clean up temporary files from 'build' command"
+        echo -e "  -r,  --release              Python only code, option ignored"
+        echo -e "  -d,  --debug                Python only code, option ignored"
+        echo -e "       --install              will install the package"
+        echo -e "       --dist                 create a wheel distribution/default"
+        echo -e "       --py-version           Python version"
+        echo -e "       --pt-data-version      PytorchData version"
+        echo -e "  -h,  --help                 Prints this help"
     fi
 
     if [ $1 == "build_pytorch_text" ]; then
@@ -2075,6 +2091,94 @@ build_pytorch_text()
         rm -rf $PYTORCH_TEXT_BUILD/pkgs
         mkdir -p $PYTORCH_TEXT_BUILD/pkgs
         cp -f ${PTT_WHL_PATH}/*.whl $PYTORCH_TEXT_BUILD/pkgs
+    fi
+
+    printf "\nElapsed time: %02u:%02u:%02u \n\n" $(($SECONDS / 3600)) $((($SECONDS / 60) % 60)) $(($SECONDS % 60))
+    restore_python_version
+    return $__result
+}
+
+build_pytorch_data()
+{
+    SECONDS=0
+    local __scriptname=$(__get_func_name)
+    local __env_vars=""
+    local __configure=""
+    local __whl_params=" bdist_wheel"
+    local __result
+    local __set_py_vers="false"
+    local __profile_getter_path="${PYTORCH_MODULES_ROOT_PATH}/.devops/profile_getter.py"
+    local __pt_data_version
+    # parameter while-loop
+    while [ -n "$1" ];
+    do
+        case $1 in
+        -j  | --jobs )
+            __env_vars+=" MAX_JOBS=$2"
+            shift
+            ;;
+        -c  | --configure )
+             __configure="yes"
+            ;;
+        -r  | --release )
+            ;;
+        -d  | --debug )
+            ;;
+        --dist )
+            __whl_params=" bdist_wheel"
+            ;;
+        --install )
+            __whl_params=" install"
+            ;;
+        --py-version )
+            set_python_version $2
+            __set_py_vers="true"
+            shift
+            ;;
+        --pt-data-version )
+            __pt_data_version="$2"
+            shift
+            ;;
+        -h  | --help )
+            usage $__scriptname
+            restore_python_version
+            return 0
+            ;;
+        esac
+        shift
+    done
+
+    rm -rf $PYTORCH_DATA_ROOT
+    mkdir -p $PYTORCH_DATA_ROOT
+    pushd $PYTORCH_DATA_ROOT
+
+    # checkout github torchaudio repo
+    if [ -z ${__pt_data_version} ]; then
+        __pt_data_version=$($__profile_getter_path --get-extras-version torchdata current)
+    fi
+    echo "get torchdata from github (tag: $__pt_data_version)"
+    git clone https://github.com/pytorch/data --branch v$__pt_data_version --single-branch --depth 1 .
+    git submodule update --init --recursive
+
+    if [ -n "$__configure" ]; then
+        $__python_cmd setup.py clean
+        git clean -fd
+    fi
+
+    echo "Build parameters ${__whl_params}"
+
+    (set -x;eval ${__env_vars} $__python_cmd setup.py ${__whl_params})
+    __result=$?
+    if [ $__result -ne 0 ]; then
+        echo "Pytorch torchdata build failed!"
+    fi
+    PTD_WHL_PATH="$PYTORCH_DATA_ROOT/dist/"
+
+    popd
+    if [[ "$__whl_params" = " bdist_wheel" ]]; then
+        rm -rf $PYTORCH_DATA_BUILD/pkgs
+        mkdir -p $PYTORCH_DATA_BUILD/pkgs
+        cp -f ${PTD_WHL_PATH}/*.whl $PYTORCH_DATA_BUILD/pkgs
     fi
 
     printf "\nElapsed time: %02u:%02u:%02u \n\n" $(($SECONDS / 3600)) $((($SECONDS / 60) % 60)) $(($SECONDS % 60))
