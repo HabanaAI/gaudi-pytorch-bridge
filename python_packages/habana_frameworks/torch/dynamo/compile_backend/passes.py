9024d56ad266935657b8dcf9d4c0381ddf8ac83a
Internal Developer <internal-developer@intel.com>
2023-05-24 16:59:06 +0300

    Enable maxpools and bathcnorm bwd for torch.compile
    Change-Id: Ie7b7444f09ccb694f4d23503c7bc448c37b16406

--- python_packages/habana_frameworks/torch/dynamo/compile_backend/passes.py
+++ python_packages/habana_frameworks/torch/dynamo/compile_backend/passes.py
@@ -406,12 +406,9 @@ def pass_mark_placement(ctx: OptimizerContext) -> bool:
             "multinomial",
             "normal",
             # Other
-            "max_pool2d_with_indices",
-            "max_pool2d_with_indices_backward",
             "convolution",  # SW-137174
             "convolution_backward",
             "_native_batch_norm_legit_functional",  # SW-137176
-            "native_batch_norm_backward",
             "addcmul",
             "_to_copy",  # SW-137099
         ]
