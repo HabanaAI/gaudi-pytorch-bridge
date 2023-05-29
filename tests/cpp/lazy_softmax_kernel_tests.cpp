28b2de875772d6c4b171448be2b4dfd2f0ec1a30
Internal Developer <internal-developer@intel.com>
2023-05-29 10:22:30 +0300

    Optimize execution time of LazySoftmaxKernelTest CPP suite
    Decrease the size of input tensors to speed up the test time execution
    
    Change-Id: I8495af1eb65fe8532dc29cc36580a128bce09f2c

--- tests/cpp/lazy_softmax_kernel_tests.cpp
+++ tests/cpp/lazy_softmax_kernel_tests.cpp
@@ -18,7 +18,7 @@ class LazySoftmaxKernelTest : public habana_lazy_test::LazyTest {};
 TEST_F(LazySoftmaxKernelTest, LogSoftMaxTest) {
   torch::Tensor input = torch::rand({64, 10}, torch::requires_grad(false));
   torch::Tensor hinput = input.to(torch::kHPU);
-  int dim = 0;
+  constexpr int dim = 0;
   torch::Tensor hout = torch::log_softmax(hinput, dim);
 
   std::vector<HbLazyTensor> tensors = {SyncAndGetHbLazyTensor(hout)};
@@ -35,7 +35,7 @@ TEST_F(LazySoftmaxKernelTest, LogSoftMaxTest4D) {
   torch::Tensor input =
       torch::rand({14, 4, 192, 160}, torch::requires_grad(false));
   torch::Tensor hinput = input.to(torch::kHPU);
-  int dim = 1;
+  constexpr int dim = 1;
   torch::Tensor hout = torch::log_softmax(hinput, dim);
 
   auto hout1 = hout.to(torch::kCPU);
@@ -47,14 +47,14 @@ TEST_F(LazySoftmaxKernelTest, LogSoftMaxTest4D) {
 
 TEST_F(LazySoftmaxKernelTest, CrossEntropyTest) {
   torch::Tensor input_tensor =
-      torch::rand({64, 128, 48, 40}, torch::requires_grad(false));
+      torch::rand({16, 32, 12, 10}, torch::requires_grad(false));
   torch::Tensor tHabanaX = input_tensor.to(torch::kHPU);
 
   torch::Tensor weight_tensor =
-      torch::rand({4, 128, 1, 1}, torch::requires_grad(false));
+      torch::rand({4, 32, 1, 1}, torch::requires_grad(false));
   torch::Tensor tHabanaW = weight_tensor.to(torch::kHPU);
 
-  auto target = torch::randint(0, 3, {64, 48, 40}, torch::kLong);
+  auto target = torch::randint(0, 3, {16, 12, 10}, torch::kLong);
   torch::Tensor htarget = target.to(torch::kHPU);
 
   torch::Tensor houtConv =
@@ -79,7 +79,7 @@ TEST_F(LazySoftmaxKernelTest, LogSoftMaxTestBackward) {
   torch::Tensor hgrad = grad.to(torch::kHPU);
   torch::Tensor houtput = output.to(torch::kHPU);
 
-  int dim = 0;
+  constexpr int dim = 0;
   auto hout_backward = torch::_log_softmax_backward_data(
       hgrad, houtput, dim, hinput.scalar_type());
 
@@ -97,7 +97,7 @@ TEST_F(LazySoftmaxKernelTest, LogSoftMaxTestBackward) {
 TEST_F(LazySoftmaxKernelTest, SoftMaxTest) {
   torch::Tensor input = torch::rand({64, 10}, torch::requires_grad(false));
   torch::Tensor hinput = input.to(torch::kHPU);
-  int dim = 0;
+  constexpr int dim = 0;
   torch::Tensor hout = torch::_softmax(hinput, dim, false);
 
   std::vector<HbLazyTensor> tensors = {SyncAndGetHbLazyTensor(hout)};
@@ -119,7 +119,7 @@ TEST_F(LazySoftmaxKernelTest, SoftMaxTestBackward) {
   torch::Tensor hgrad = grad.to(torch::kHPU);
   torch::Tensor houtput = output.to(torch::kHPU);
 
-  int dim = 0;
+  constexpr int dim = 0;
   auto hout_backward =
       torch::_softmax_backward_data(hgrad, houtput, dim, hinput.scalar_type());
 
@@ -135,31 +135,27 @@ TEST_F(LazySoftmaxKernelTest, SoftMaxTestBackward) {
 }
 
 TEST_F(LazySoftmaxKernelTest, SoftMaxTestBackward1) {
-  torch::Tensor input0 =
-      torch::rand({64, 128, 48, 40}, torch::requires_grad(false));
-  torch::Tensor hinput0 = input0.to(torch::kHPU);
+  torch::Tensor input =
+      torch::rand({32, 64, 24, 20}, torch::requires_grad(false));
+  torch::Tensor hinput = input.to(torch::kHPU);
 
   torch::Tensor weight_tensor =
-      torch::rand({4, 128, 1, 1}, torch::requires_grad(false));
+      torch::rand({4, 64, 1, 1}, torch::requires_grad(false));
   torch::Tensor tHabanaW = weight_tensor.to(torch::kHPU);
 
   torch::Tensor output =
-      torch::rand({64, 4, 48, 40}, torch::requires_grad(false));
+      torch::rand({32, 4, 24, 20}, torch::requires_grad(false));
   torch::Tensor houtput = output.to(torch::kHPU);
 
-  torch::Tensor input =
-      torch::rand({64, 4, 48, 40}, torch::requires_grad(false));
-  torch::Tensor hinput = input.to(torch::kHPU);
-
-  int dim = 1;
+  constexpr int dim = 1;
   torch::Tensor houtConv =
-      torch::conv2d(hinput0, tHabanaW, {}, {1}, at::IntArrayRef{0}, {1}, 1);
+      torch::conv2d(hinput, tHabanaW, {}, {1}, at::IntArrayRef{0}, {1}, 1);
   auto hout_backward = torch::_softmax_backward_data(
       houtConv, houtput, dim, hinput.scalar_type());
   auto hout2_back = hout_backward.to(torch::kCPU);
 
   torch::Tensor outConv =
-      torch::conv2d(input0, weight_tensor, {}, {1}, at::IntArrayRef{0}, {1}, 1);
+      torch::conv2d(input, weight_tensor, {}, {1}, at::IntArrayRef{0}, {1}, 1);
   auto cout_back =
       torch::_softmax_backward_data(outConv, output, dim, input.scalar_type());
 
