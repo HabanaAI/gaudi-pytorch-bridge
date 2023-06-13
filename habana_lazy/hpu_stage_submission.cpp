be4f6dff6086a45f1d05e070204c1a021b5f8ab4
Internal Developer <internal-developer@intel.com>
2023-06-13 08:11:42 +0300

    Avoid accthread mark steps
    Avoid processing compound op counters in acc thread. This ensures that
    1. Intermediate ops in compund ops are not counted
    2. mark step is not triggered in acc thread
    
    Change-Id: Idf5f9e6f4aa86cefffefa66d2d908489fab7e4fc
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- habana_lazy/hpu_stage_submission.cpp
+++ habana_lazy/hpu_stage_submission.cpp
@@ -9,18 +9,37 @@
  */
 #include "habana_lazy/hpu_stage_submission.h"
 #include "lazy_executor.h"
+#include "pytorch_helpers/habana_helpers/kernels_accumulation.h"
 
 namespace habana_lazy {
 
 static std::atomic<size_t> compound_op_counter = 0;
 
 PTOpTrace::PTOpTrace() {
+  if (habana_lazy::AccThread::IsAccThreadEnabled() &&
+      habana_lazy::AccThread::Get().inAccThreadContext()) {
+    // Avoid incrementing/decrementing compund op counter in acc thread. This
+    // can cause counting of internal ops. Further this ensures that StepMarker
+    // is not called from the accumulation thread. StepMarker can deallocate
+    // tensors and it can cause deadlock.
+    return;
+  }
+
   if (!habana_lazy::isDeviceInLoweringMode()) {
     compound_op_counter++;
   }
 }
 
 PTOpTrace::~PTOpTrace() {
+  if (habana_lazy::AccThread::IsAccThreadEnabled() &&
+      habana_lazy::AccThread::Get().inAccThreadContext()) {
+    // Avoid incrementing/decrementing compund op counter in acc thread. This
+    // can cause counting of internal ops. Further this ensures that StepMarker
+    // is not called from the accumulation thread. StepMarker can deallocate
+    // tensors and it can cause deadlock.
+    return;
+  }
+
   if (!habana_lazy::isDeviceInLoweringMode()) {
     compound_op_counter--;
 
@@ -33,8 +52,10 @@ PTOpTrace::~PTOpTrace() {
 void PTOpTrace::increment_compound_ops() {
   habana_lazy::StageSubmission::getInstance().incrementCompoundOps();
 
-  if (habana_lazy::StageSubmission::getInstance().isExceededMaxCompoundSize()) {
-    PT_LAZY_DEBUG("Reached max accumulated graph size, triggering a mark_step");
+  if ((habana_lazy::StageSubmission::getInstance()
+           .isExceededMaxCompoundSize())) {
+    PT_LAZY_DEBUG(
+        "Reached max accumulated compound op size, triggering a mark_step");
     bool async = GET_ENV_FLAG_NEW(PT_HPU_ENABLE_EXECUTION_THREAD);
     habana_lazy::HbLazyTensor::StepMarker({}, nullptr, {}, async);
   }
