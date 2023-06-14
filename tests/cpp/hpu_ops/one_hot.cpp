80f2d5b63d1cb21a59d095fa10678332a8e7dfe3
Internal Developer <internal-developer@intel.com>
2023-06-14 16:09:03 +0300

    Fix failing one_hot tests
    Change-Id: Ia5ce96c8ac91bf3f7aae56dd5ae29f492b97e65c

--- tests/cpp/hpu_ops/one_hot.cpp
+++ tests/cpp/hpu_ops/one_hot.cpp
@@ -35,7 +35,9 @@ TEST_P(OneHotHpuOpTestFixture, one_hot) {
   auto expected_cpu =
       torch::one_hot(abs(GetCpuInput(0)).to(torch::kLong), num_of_classes)
           .to(dtype);
-  auto expected_hpu = torch::one_hot(abs(GetHpuInput(0)), num_of_classes);
+  auto expected_hpu =
+      torch::one_hot(abs(GetHpuInput(0)).to(torch::kLong), num_of_classes)
+          .to(dtype);
   Compare(expected_cpu, expected_hpu);
 }
 
