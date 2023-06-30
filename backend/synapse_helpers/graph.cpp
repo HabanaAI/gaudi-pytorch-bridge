3ffcd094117e03b85b455971aa48c62b7b4710c4
Internal Developer <internal-developer@intel.com>
2023-06-30 13:40:31 +0300

    Fix for Resnet50 inference in Gaudi
    Added the following changes:
    1. Added the inference specific graph attribute only on valid
    graph handles
    
    Change-Id: I29da9581406fd888dc43fb354329ab349e1381e0
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- backend/synapse_helpers/graph.cpp
+++ backend/synapse_helpers/graph.cpp
@@ -126,15 +126,15 @@ synapse_error_v<graph> graph::create(
       status = synGraphCreate(&syn_graph.graph_handle_, device_type);
     }
     SYNAPSE_SUCCESS_CHECK("Graph creation failed.", status)
-  }
 
-  if (GET_ENV_FLAG_NEW(PT_HPU_INFERENCE_MODE)) {
-    synStatus status = synSuccess;
-    uint64_t values[] = {true};
-    synGraphAttribute att[] = {GRAPH_ATTRIBUTE_INFERENCE};
-    const uint32_t size = 1;
-    status = synGraphSetAttribute(syn_graph.graph_handle_, att, values, size);
-    SYNAPSE_SUCCESS_CHECK("Failed to set graph attributes.", status)
+    if (GET_ENV_FLAG_NEW(PT_HPU_INFERENCE_MODE)) {
+      synStatus status = synSuccess;
+      uint64_t values[] = {true};
+      synGraphAttribute att[] = {GRAPH_ATTRIBUTE_INFERENCE};
+      const uint32_t size = 1;
+      status = synGraphSetAttribute(syn_graph.graph_handle_, att, values, size);
+      SYNAPSE_SUCCESS_CHECK("Failed to set graph attributes.", status)
+    }
   }
 
   syn_graph.is_valid_ = true;
