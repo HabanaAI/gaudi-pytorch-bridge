298c24bc01cbdbe6e2a140ba304810fd96c227b8
Internal Developer <internal-developer@intel.com>
2023-07-02 02:02:23 +0000

    Revert " Fix Recipe Cache for random tensors"
    This reverts commit 8812a2678ab99f57954fda1be467d6b1c245aca6.
    
    Change-Id: Icde06c660ad506dfc32ad3144a641186409bb17d

--- habana_lazy/hpu_lazy_tensors.cpp
+++ habana_lazy/hpu_lazy_tensors.cpp
@@ -875,8 +875,7 @@ at::Tensor Process0DTensor(std::shared_ptr<Data>& d) {
   auto impl = habana_lazy::GetHbInternalTensorImpl(pt_tensor);
   bool is_shape_tensor = impl && impl->isShapeTensor();
   if (pt_tensor.dim() == 0 &&
-      (!pt_tensor.unsafeGetTensorImpl()->is_wrapped_number() ||
-       d->is_random_seed_tensor)) {
+      !pt_tensor.unsafeGetTensorImpl()->is_wrapped_number()) {
     HABANA_ASSERT(is_shape_tensor == false, "0D shape tensor encountered");
     pt_tensor.unsafeGetTensorImpl()->set_sizes_contiguous({1});
   }
