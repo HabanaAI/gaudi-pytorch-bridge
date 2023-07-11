266b1dc9d82f784ffbfb9318ab07eebbdc18a1c1
Internal Developer <internal-developer@intel.com>
2023-07-11 07:42:15 +0300

    Topk : Autogen code changes for dynamic shapes
    Changes:
    1. Use topk CGUID
    2. Remove usage of IDST
    
    Change-Id: Iece47bdc6ec0639320b377fc8b68fcb25b6a66f8
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- hpu_ops/backend/topk_gen.cpp
+++ hpu_ops/backend/topk_gen.cpp
@@ -46,168 +46,37 @@ void Topk::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
 
   auto outshape = TopkOutputShape(stack)[0];
 
-  auto env1 = std::getenv("ENABLE_EXPERIMENTAL_FLAGS");
-  bool enable_experimental_flags =
-      (env1 != nullptr) && (absl::string_view{env1} != "0");
-
-  auto env2 = std::getenv("ENABLE_TOPK_IN_CGUID");
-  bool enable_topk_in_cguid =
-      (env2 != nullptr) && (absl::string_view{env2} != "0");
-
-  /*
-     To support dynamic shape, the TPC kernel inputs needs to be
-     {values_tensor, indices_tensor, null, k_tensor}. The following code
-     create 3 additional ops to create the indices tensor: arrnage op ->
-     reshape op -> repeat op
-  */
-
   std::vector<synapse_helpers::tensor> result{};
 
-  if (isMetaMode() || graph.is_dynamic_graph()) {
-    std::vector<synTensor> syn_inputs{syn_in(0)};
-    if (enable_experimental_flags && enable_topk_in_cguid) {
-      syn_inputs.emplace_back(nullptr);
-      syn_inputs.emplace_back(nullptr);
-      CreateShapeTensorInput(graph, ScalarType(), {k}, syn_inputs);
+  std::vector<synTensor> syn_inputs{syn_in(0)};
+  ns_TopkNodeV2::ParamsV4 params{};
+  // It is ok to set params.bsw = k irrespective of static or DS case
+  // As per CGUID doc, bsw is ignored if params.kType = K_TENSOR_SHAPE;
+  // which is set in DS case.
+  params.bsw = k;
+  params.axis = self.dim() - dim - 1;
+  params.bottomK = !largest;
+  params.isVcData = false;
 
-      ns_TopkNodeV2::ParamsV4 params;
-      params.axis = self.dim() - dim - 1;
-      params.bottomK = !largest;
-      params.isVcData = false;
-      params.kType = K_TENSOR_SHAPE;
-      // Add topk op
-      result = BuildOp(
-          graph,
-          "topk",
-          {std::move(syn_inputs)},
-          {{outshape, ScalarType(), 0}, {outshape, c10::ScalarType::Int, 1}},
-          &params,
-          sizeof(params));
-    } else {
-      torch::jit::Stack temp_stack;
-      const auto input_shape = self.sizes();
-      int start = 0;
-      int limit = input_shape[dim];
-      int step = 1;
+  // The following 3 lines are needed only in dyn shape case.
+  // But done hre uncoditionally because, i. is ok to set null pointer
+  // inputs. CreateShapeTensorInput has check internally for DS case
+  syn_inputs.emplace_back(nullptr);
+  syn_inputs.emplace_back(nullptr);
+  CreateShapeTensorInput(graph, ScalarType(), outshape, syn_inputs);
 
-      // Add arange op - the input tensor for the arange op is also the
-      // output tensor
-      std::vector<synTensor> range_in{};
-      int input_output_depth =
-          ArangeOperator::GetOutputSize(start, limit, step);
-      std::vector<int64_t> input_output_sizes_vec{input_output_depth};
-      c10::IntArrayRef input_output_shape(
-          input_output_sizes_vec.data(), input_output_sizes_vec.size());
-
-      ns_RangeKernel::Params arrangeparam{};
-      arrangeparam.start.i = start;
-      arrangeparam.limit.i = limit;
-      arrangeparam.delta.i = step;
-      // Allocate idst if its not added from frontend.
-      std::vector<int64_t> sizes_vec{step, limit, start};
-      c10::IntArrayRef idst_sizes(sizes_vec.data(), sizes_vec.size());
-      CreateShapeTensorInput(
-          graph,
-          c10::ScalarType::Int,
-          idst_sizes,
-          range_in,
-          INPUT_DESCRIBING_SHAPE_TENSOR);
-      auto range_op = BuildOp(
-          graph,
-          "range_i32",
-          range_in,
-          {{input_output_shape, c10::ScalarType::Int}},
-          &arrangeparam,
-          sizeof(arrangeparam));
-
-      // Add reshape op
-      auto reshaped_shape = std::vector<int64_t>(self.ndimension(), 1);
-      reshaped_shape[dim] = limit;
-      auto reshape_op = ReshapeHelper(
-          graph, range_op[0].get(), reshaped_shape, c10::ScalarType::Int);
-
-      // Add repeat op
-      std::vector<int64_t> repeats = input_shape.vec();
-      repeats[dim] = 1;
-      int64_t repeat_size = repeats.size();
-      ns_TileKernel::ParamsV2 tileparams{};
-      for (int64_t i = 0; i < repeat_size; ++i) {
-        tileparams.repeat[repeat_size - i - 1] = repeats[i];
-      }
-
-      std::vector<synTensor> repeat_in = {reshape_op.get()};
-      CreateShapeTensorInput(
-          graph,
-          c10::ScalarType::Int,
-          repeats,
-          repeat_in,
-          INPUT_DESCRIBING_SHAPE_TENSOR);
-
-      std::vector<int64_t> repeat_outshape(repeats.size());
-      int64_t num_new_dimensions = repeats.size() - reshaped_shape.size();
-      std::vector<int64_t> padded_size(num_new_dimensions, 1);
-      padded_size.insert(
-          padded_size.end(), reshaped_shape.begin(), reshaped_shape.end());
-      for (size_t i = 0; i < repeats.size(); ++i) {
-        repeat_outshape[i] = padded_size[i] * repeats[i];
-      }
-
-      auto repeat_op = BuildOp(
-          graph,
-          "tile_fwd_i32",
-          repeat_in,
-          {{repeat_outshape, c10::ScalarType::Int}},
-          &tileparams,
-          sizeof(tileparams));
-      syn_inputs.emplace_back(repeat_op[0].get());
-      syn_inputs.emplace_back(nullptr);
-      CreateShapeTensorInput(graph, ScalarType(), {k}, syn_inputs);
-
-      synBeamParams params;
-      params.bsw = k;
-      params.axis = self.dim() - dim - 1;
-      params.bottomK = !largest;
-
-      // Add topk op
-      result = BuildOp(
-          graph,
-          "topk",
-          {std::move(syn_inputs)},
-          {{outshape, ScalarType(), 0}, {outshape, c10::ScalarType::Int, 1}},
-          &params,
-          sizeof(params));
-    }
-  } else {
-    if (enable_experimental_flags && enable_topk_in_cguid) {
-      ns_TopkNodeV2::ParamsV4 params;
-      params.axis = self.dim() - dim - 1;
-      params.bottomK = !largest;
-      params.bsw = k;
-      params.kType = K_TENSOR_NONE;
-      // Add topk op
-      result = BuildOp(
-          graph,
-          "topk",
-          {syn_in(0)},
-          {{outshape, ScalarType(), 0}, {outshape, c10::ScalarType::Int, 1}},
-          &params,
-          sizeof(params));
-    } else {
-      synBeamParams params;
-      params.bsw = k;
-      params.axis = self.dim() - dim - 1;
-      params.bottomK = !largest;
-
-      // Add topk op
-      result = BuildOp(
-          graph,
-          "topk",
-          {syn_in(0)},
-          {{outshape, ScalarType(), 0}, {outshape, c10::ScalarType::Int, 1}},
-          &params,
-          sizeof(params));
-    }
+  if (graph.is_dynamic_graph()) {
+    params.kType = K_TENSOR_SHAPE;
   }
+
+  // Add topk op
+  result = BuildOp(
+      graph,
+      "topk",
+      {std::move(syn_inputs)},
+      {{outshape, ScalarType(), 0}, {outshape, c10::ScalarType::Int, 1}},
+      &params,
+      sizeof(params));
   syn_out(0) = std::move(result[0]);
   syn_out(1) = std::move(result[1]);
 }
