d773ca708051a2b0c854a42288571e9a4733c42e
Internal Developer <internal-developer@intel.com>
2023-07-13 14:15:37 +0530

    cd: install.sh - remove habana-lightning-plugins
    Change-Id: I2f518b4ed693ceac711e2c5ccc17e8d1966249ec
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- .devops/cd/scripts/install.sh
+++ .devops/cd/scripts/install.sh
@@ -26,11 +26,6 @@ fi
 python${MIN_PYTHON_VER} -m pip install ./*.whl ${PIP_PYTHON_OPTIONS} --disable-pip-version-check
 python${MIN_PYTHON_VER} -m pip install $(grep "lightning" ./requirements-pytorch.txt) --no-warn-script-location --disable-pip-version-check ${PIP_PYTHON_OPTIONS}
 
-if [ -z $HABANALABS_LOCAL_DIR ]; then
-    python${MIN_PYTHON_VER} -m pip install habana-lightning-plugins=="${HABANA_RELEASE_VERSION}"."${HABANA_RELEASE_ID}" --disable-pip-version-check ${PIP_PYTHON_OPTIONS} ${EXTRA_INDEX_URL}
-else
-    python${MIN_PYTHON_VER} -m pip install ${HABANALABS_LOCAL_DIR}/habana_lightning_plugins-${HABANA_RELEASE_VERSION}.${HABANA_RELEASE_ID}*.whl --disable-pip-version-check
-fi
 python${MIN_PYTHON_VER} -m pip uninstall -y pillow
 python${MIN_PYTHON_VER} -m pip uninstall -y pillow-simd
 python${MIN_PYTHON_VER} -m pip install pillow-simd==${PILLOW_SIMD_VERSION} ${PIP_PYTHON_OPTIONS} --disable-pip-version-check
