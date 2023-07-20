8e09479cc6557d965b90c5f3187c4fac84dc42ef
Internal Developer <internal-developer@intel.com>
2023-07-20 11:43:59 +0300

    Force align input data types in RoPE
    Change-Id: I7f4e230b3a5ce501cf5a6d9f858508040592bb9b

--- python_packages/habana_frameworks/torch/hpex/kernels/RotaryPosEmbeddingHelper.py
+++ python_packages/habana_frameworks/torch/hpex/kernels/RotaryPosEmbeddingHelper.py
@@ -91,6 +91,11 @@ def apply_rotary_pos_emb(
     """
     cos, sin, offset = recalculate_params(cos, sin, position_ids, offset, mode)
 
+    if p.dtype != sin.dtype:
+        sin = sin.to(p.dtype)
+    if p.dtype != cos.dtype:
+        cos = cos.to(p.dtype)
+
     return torch.ops.hpu.rotary_pos_embedding(p, sin, cos, offset, mode.value)
 
 
