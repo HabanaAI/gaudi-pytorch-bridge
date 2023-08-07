6e832d5c09f48f598ac950ff4b4540122df8d34f
Internal Developer <internal-developer@intel.com>
2023-08-07 12:43:48 +0300

    Skip eager searchsorted tests
    Skip sporadically failing eager searchsorted tests
    
    Change-Id: I16e155986f189d8d6bd0217ecdced42d571dacb5

--- tests/pytest_working/eager/test_hpu_searchsorted_op.py
+++ tests/pytest_working/eager/test_hpu_searchsorted_op.py
@@ -24,6 +24,7 @@ dtypes = [
 ]
 
 
+@pytest.mark.skip(reason="SW-155131")
 @pytest.mark.parametrize("right", [True, False])
 @pytest.mark.parametrize("out_int32", [True, False])
 @pytest.mark.parametrize("dtype", dtypes)
@@ -56,6 +57,7 @@ def test_hpu_searchsorted_input(right, out_int32, dtype, sequence_shape, input_s
     assert hpu_results[0].dtype == cpu_results[0].dtype
 
 
+@pytest.mark.skip(reason="SW-155131")
 @pytest.mark.parametrize("out_int32", [True, False])
 @pytest.mark.parametrize("right", [False, True])
 @pytest.mark.parametrize("dtype", dtypes)
@@ -85,6 +87,7 @@ def test_hpu_searchsorted_scalar(out_int32, right, dtype):
     assert hpu_results[0].dtype == cpu_results[0].dtype
 
 
+@pytest.mark.skip(reason="SW-155131")
 @pytest.mark.parametrize(
     "right, side",
     [
@@ -123,6 +126,7 @@ def test_searchsorted_side(right, side):
     )
 
 
+@pytest.mark.skip(reason="SW-155131")
 @pytest.mark.parametrize(
     "name, value, shape",
     [
@@ -154,6 +158,7 @@ def test_searchsorted_sorter(name, value, shape):
     )
 
 
+@pytest.mark.skip(reason="SW-155131")
 def test_searchsorted_out():
     torch.manual_seed(0)
     shape = (4, 2)
