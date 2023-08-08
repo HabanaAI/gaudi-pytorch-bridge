d45c99cf31cd801054bf4f2666d2afdc53c8070c
Internal Developer <internal-developer@intel.com>
2023-08-08 10:25:45 +0300

    Fix FusedEMA
    Change-Id: I308f0559419c171f8064e32f5499b1cd6aa35049

--- python_packages/habana_frameworks/torch/hpex/movingavrg/FusedEMA.py
+++ python_packages/habana_frameworks/torch/hpex/movingavrg/FusedEMA.py
@@ -12,14 +12,12 @@
 
 import math
 from copy import deepcopy
-from typing import Callable, Iterable
 
 import habana_frameworks.torch.core as htcore
 import torch
 from torch import nn
 
 hpu = torch.device("hpu")
-cpu = torch.device("cpu")
 
 
 def is_parallel(model):
@@ -32,24 +30,17 @@ def is_parallel(model):
 def copy_attr(a, b, include=(), exclude=()):
     # Copy attributes from b to a, options to only include[...] and to exclude[...]
     for k, v in b.__dict__.items():
-        if (
-            (len(include) and k not in include)
-            or k.startswith("_")
-            or k in exclude
-        ):
+        if (len(include) and k not in include) or k.startswith("_") or k in exclude:
             continue
         else:
             setattr(a, k, v)
 
 
 class FusedEMA:
-    def __init__(
-        self, model: nn.Module, decay: float = 0.9999, updates: float = 0
-    ):
+    def __init__(self, model: nn.Module, decay: float = 0.9999, updates: float = 0):
         if not 0.0 <= decay:
             raise ValueError("Invalid decay value: {}".format(decay))
 
-        super().__init__()
         self.ema = deepcopy(
             model.module if is_parallel(model) else model
         ).eval()  # FP32 EMA
@@ -57,27 +48,20 @@ class FusedEMA:
             1 - math.exp(-x / 2000)
         )  # decay exponential ramp (to help early epochs) #decay
         self.updates = updates
-        self.model_inputs = []
-        self.updated_ema = []
-
-        for k, m_tensor in model.state_dict().items():
-            self.model_inputs.append(m_tensor)
-
-        for k, up_tensor in self.ema.state_dict().items():
-            self.updated_ema.append(up_tensor)
+        self.updated_ema = list(self.ema.state_dict().values())
 
     def update(self, model):
         htcore.step_closure._mark_step_if_lazy()
+
+        model_inputs = list(model.state_dict().values())
         with torch.no_grad():
             self.updates += 1
             decy = self.decay(self.updates)
             d = torch.tensor([decy]).to(hpu)
 
-        torch.ops.hpu.optimizer_ema(self.model_inputs, self.updated_ema, d)
+        torch.ops.hpu.optimizer_ema(model_inputs, self.updated_ema, d)
         htcore.step_closure._mark_step_if_lazy()
 
-    def update_attr(
-        self, model, include=(), exclude=("process_group", "reducer")
-    ):
+    def update_attr(self, model, include=(), exclude=("process_group", "reducer")):
         # Update EMA attributes
         copy_attr(self.ema, model, include, exclude)
--- tests/pytest_working/lazy/fused_ops/test_ema.py
+++ tests/pytest_working/lazy/fused_ops/test_ema.py
@@ -10,53 +10,46 @@
 #
 ###############################################################################
 
-import numpy as np
-from copy import deepcopy
-import torch
-from torch import nn
 import math
-import torch.nn.functional as F
-from collections import OrderedDict
-import habana_frameworks.torch.core as htcore
-from habana_frameworks.torch.hpex.movingavrg import FusedEMA
+from copy import deepcopy
+
 import pytest
-from test_utils import cpu, hpu
+import torch
+import torch.nn.functional as F
+from habana_frameworks.torch.hpex.movingavrg import FusedEMA
+from test_utils import compare_tensors, hpu
+from torch import nn
 
 
 def is_parallel(model):
-    return type(model) in (nn.parallel.DataParallel, nn.parallel.DistributedDataParallel)
+    return type(model) in (
+        nn.parallel.DataParallel,
+        nn.parallel.DistributedDataParallel,
+    )
+
 
 def copy_attr(a, b, include=(), exclude=()):
     # Copy attributes from b to a, options to only include [...] and to exclude [...]
     for k, v in b.__dict__.items():
-        if (len(include) and k not in include) or k.startswith('_') or k in exclude:
+        if (len(include) and k not in include) or k.startswith("_") or k in exclude:
             continue
         else:
             setattr(a, k, v)
 
-#permute the params from filters first (KCRS) to filters last(RSCK) or vice versa.
-#and permute from RSCK to KCRS is used for checkpoint saving
-def permute_params(model, to_filters_last):
-    with torch.no_grad():
-        for name, param in model.named_parameters():
-            if(param.ndim == 4):
-                if to_filters_last:
-                    param.data = param.data.permute((2, 3, 1, 0))
-                else:
-                    param.data = param.data.permute((3, 2, 0, 1))  # permute RSCK to KCRS
 
 class EMA:
-
     def __init__(self, model, decay, updates=0):
         # Create EMA
-        self.ema = deepcopy(model.module if is_parallel(model) else model).eval()  # FP32 EMA
-        # if next(model.parameters()).device.type != 'cpu':
-        #     self.ema.half()  # FP16 EMA
+        self.ema = deepcopy(
+            model.module if is_parallel(model) else model
+        ).eval()  # FP32 EMA
         self.updates = updates  # number of EMA updates
-        self.decay = lambda x: decay * (1 - math.exp(-x / 2000))  # decay exponential ramp (to help early epochs)
+        self.decay = lambda x: decay * (
+            1 - math.exp(-x / 2000)
+        )  # decay exponential ramp (to help early epochs)
         for p in self.ema.parameters():
             p.requires_grad_(False)
-            p.detach().to(cpu)
+            p.detach().cpu()
 
     def update(self, model):
         # Update EMA parameters
@@ -64,25 +57,18 @@ class EMA:
             self.updates += 1
             d = self.decay(self.updates)
 
-            msd = model.module.state_dict() if is_parallel(model) else model.state_dict()  # model state_dict
+            msd = (
+                model.module.state_dict() if is_parallel(model) else model.state_dict()
+            )  # model state_dict
             for k, v in self.ema.state_dict().items():
                 if v.dtype.is_floating_point:
                     v *= d
-                    v += (1. - d) * msd[k].detach()
+                    v += (1.0 - d) * msd[k].detach()
 
-    def update_attr(self, model, include=(), exclude=('process_group', 'reducer')):
+    def update_attr(self, model, include=(), exclude=("process_group", "reducer")):
         # Update EMA attributes
         copy_attr(self.ema, model, include, exclude)
 
-class lnModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.ln1 = nn.Linear(4, 4, bias=False)
-        self.ln2 = nn.Linear(2, 2)
-
-    def forward(self, x):
-        x = F.relu(self.ln1(x))
-        return F.relu(self.ln2(x))
 
 class convModel(nn.Module):
     def __init__(self):
@@ -94,74 +80,23 @@ class convModel(nn.Module):
         x = F.relu(self.conv1(x))
         return F.relu(self.conv2(x))
 
-@pytest.mark.xfail
-def test_ema():
-    decay=0.9999
-    cnt = 2
 
-    #nn_module = lnModel()
-    nn_module = convModel()
-    permute_params(nn_module,True)
+@pytest.mark.parametrize("decay", [0.9999, 0.5])
+@pytest.mark.parametrize("epochs", [1, 2])
+def test_ema(decay, epochs):
+    cpu_model = convModel()
+    cpu_ema = convModel()
+    hpu_model = deepcopy(cpu_model).to(hpu)
+    hpu_ema = deepcopy(cpu_ema).to(hpu)
 
-    optim_x = EMA(nn_module,decay)
-    permute_params(optim_x.ema,True)
+    cpu_optim = EMA(cpu_ema, decay)
+    hpu_optim = FusedEMA(hpu_ema, decay)
 
-    for i in range(0, cnt):
-        # Compute updates of the parameters
-        optim_x.update(nn_module)
-        optim_x.update_attr(nn_module)
+    for _ in range(epochs):
+        cpu_optim.update(cpu_model)
+        hpu_optim.update(hpu_model)
 
-        #params = optim_x.ema.state_dict()
-        #for i, p in params.items():
-        #    print("ema params state dict ", i, p)
+        cpu_params = list(cpu_optim.ema.state_dict().values())
+        hpu_params = list(hpu_optim.ema.state_dict().values())
 
-
-    # FusedEMA HPU computations
-    nnm = nn_module.to(hpu)
-    for k, t in nnm.state_dict().items():
-        t = t.to(hpu)
-
-    optim_y = FusedEMA(nnm, decay)
-    permute_params(optim_y.ema,True)
-
-    for i in range(0, cnt):
-        # Compute updates of the parameters
-        optim_y.update(nnm)
-        #htcore.mark_step()
-
-    x1_cpu = OrderedDict()
-    y1_cpu = OrderedDict()
-    params_x = optim_x.ema.state_dict()
-    for i, p in params_x.items():
-        #print("ema params state dict ", i, p)
-        x1_cpu[i] = p.to(cpu)
-
-    params_y = optim_y.ema.state_dict()
-    for i, up in params_y.items():
-        #print("ema params state dict ", i, up)
-        y1_cpu[i] = up.to(cpu)
-        comp1 = np.allclose(x1_cpu[i].detach().numpy(), y1_cpu[i].detach().numpy(), atol=1.e-7, rtol=1.e-5, equal_nan=True)
-        assert comp1, 'EMA Optimizer SD match'
-
-
-    #Check updated_ema
-    x1_cpu = []
-    y1_cpu = []
-    params_x = optim_x.ema.state_dict()
-
-    for k, p in params_x.items():
-        #print("ema CPU state dict ", i, p)
-        x1_cpu.append(p.to(cpu))
-
-
-    params_y = optim_y.updated_ema
-    i=0
-    for up in params_y:#.items():
-        #print("ema HPU state dict ", i, up)
-        y1_cpu.append(up.to(cpu))
-        comp1 = np.allclose(x1_cpu[i].detach().numpy(), y1_cpu[i].detach().numpy(), atol=1.e-7, rtol=1.e-5, equal_nan=True)
-        assert comp1, 'Optimizer output match'
-        i = i + 1
-
-if __name__ == "__main__":
-    test_ema()
+        compare_tensors(hpu_params, cpu_params, atol=1.0e-7, rtol=1.0e-5)
