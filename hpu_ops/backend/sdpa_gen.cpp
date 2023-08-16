121d3ce445d7de74ac90f84de2b97f96a40d3903
Internal Developer <internal-developer@intel.com>
2023-08-16 18:48:10 +0300

    FusedSDPA:Consider broadcast in Output shape computation
    q, k, v tensors are involved in matmuls (BatchGemm) in attention calc.
    PyTorch matmul allows broadcast for the batch dims.
    i.e, for dims 0, 1.. Rank - 2
    So output shape computation should consider broadcast scenarios too.
    
    Change-Id: I674c2b1e4d0f01f0a39dcfbb8563ecf339f46fd6
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- hpu_ops/backend/sdpa_gen.cpp
+++ hpu_ops/backend/sdpa_gen.cpp
@@ -30,12 +30,36 @@ sizes_vec SDPAFwdOutputShape(const at::Stack& stack) {
   std::vector<int64_t> q_shape = q.sizes().vec();
   std::vector<int64_t> k_shape = k.sizes().vec();
   std::vector<int64_t> v_shape = v.sizes().vec();
-  std::vector<int64_t> qkt_shape{q_shape.begin(), q_shape.begin() + rank - 2};
-  std::vector<int64_t> out_shape{q_shape.begin(), q_shape.begin() + rank - 1};
+
+  // q, k, v are involved in matmuls (BatchGemm) in attention calc.
+  // matmul allows broadcast for the batch dims. i.e, for dims 0, 1.. Rank - 2.
+  // So output sizes depend on broadcasting. So get sizes at batch dims for
+  // q,k v and compute sizes on these dims after broadcast is done.
+  // As per torch.nn.functional.scaled_dot_product_attention() documentation,
+  // dim0 can also be excluded from broadcast considerations since the doc
+  // mentions that dim0 will be batch size(N) for all tensors.
+  // However, in the following shape calculations, dim0 is considered for
+  // broad cast shape calc for ease of implementation
+  std::vector<int64_t> q_bdim_sizes{
+      q_shape.begin(), q_shape.begin() + rank - 2};
+  std::vector<int64_t> k_bdim_sizes{
+      k_shape.begin(), k_shape.begin() + rank - 2};
+  std::vector<int64_t> v_bdim_sizes{
+      v_shape.begin(), v_shape.begin() + rank - 2};
+
+  // Batch dim sizes of Q@K.transpose after broadcast
+  auto qkt_shape = at::infer_size(q_bdim_sizes, k_bdim_sizes);
+  // Batch dim sizes of output  i.e Q@K.transpose)@v after broadcast
+  auto out_shape = at::infer_size(qkt_shape, v_bdim_sizes);
+
+  // Append the matrix dims (last 2 dims) to batch dims to get final shape.
   int L_dim = rank - 2; // Target seq len dim
   int S_dim = rank - 2; // Source seq len dim
   int Ev_dim = rank - 1; // head_dim_v dim
+
+  out_shape.push_back(q_shape[L_dim]);
   out_shape.push_back(v_shape[Ev_dim]);
+
   qkt_shape.push_back(q_shape[L_dim]);
   qkt_shape.push_back(k_shape[S_dim]);
 
