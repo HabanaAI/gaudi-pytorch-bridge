25c26212b18a5998b714d5e696d50fcd1092a93b
Internal Developer <internal-developer@intel.com>
2023-09-21 13:53:28 +0300

    hmp depracation warning
    Change-Id: I586f5ca75aba892185bf0569dcb65e7b5149b9f3

--- python_packages/habana_frameworks/torch/hpex/hmp/__init__.py
+++ python_packages/habana_frameworks/torch/hpex/hmp/__init__.py
@@ -1,2 +1,6 @@
 from .hmp import convert
 from .utils import disable_casts
+
+import logging
+
+logging.warning("Habana Mixed Precision (HMP) module is deprecated and will be removed in version 1.13.0.\nPlease use native PyTorch autocast for mixed precision training and inference")
