0675998edfabae1066f699c405068553f2f32528
Internal Developer <internal-developer@intel.com>
2023-09-25 12:32:43 +0300

    Adaptive_avg_pool1d - tests for torch.compile
    PyTorch decomposes adaptive_avg_pool1d into unsqueeze ->
    _adaptive_avg_pool2d -> squeeze
    Adaptive_avg_pool2d is already implemented, so only tests to verify
    correct behavior have been added.
    
    Change-Id: I357628b0c51ede7ac90f86dcd564cdd46ee7b138

--- tests/pytest_working/compile/test_hpu_avg_pool.py
+++ tests/pytest_working/compile/test_hpu_avg_pool.py
@@ -30,3 +30,20 @@ def test_hpu_avg_pool1d(shape, kernel_size_and_padding, stride, dtype):
     cpu_output = cpu_compiled_fn(cpu_input)
     hpu_output = hpu_compiled_fn(hpu_input).cpu()
     assert torch.equal(cpu_output, hpu_output)
+
+@pytest.mark.parametrize("shape", [[2, 7], [2, 2, 7]])
+@pytest.mark.parametrize("output_size", [1, 6, 10])
+@pytest.mark.parametrize("dtype", [torch.float, torch.bfloat16])
+def test_hpu_adaptive_avg_pool1d(shape, output_size, dtype):
+    def fn(input):
+        return torch.ops.aten.adaptive_avg_pool1d(input, output_size)
+
+    cpu_input = torch.rand(shape, dtype=dtype)
+    hpu_input = cpu_input.to("hpu")
+
+    cpu_compiled_fn = torch.compile(fn)
+    hpu_compiled_fn = torch.compile(fn, backend="aot_hpu_training_backend")
+
+    cpu_output = cpu_compiled_fn(cpu_input)
+    hpu_output = hpu_compiled_fn(hpu_input).to("cpu")
+    assert torch.allclose(cpu_output, hpu_output)
