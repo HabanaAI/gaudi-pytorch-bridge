22c073a74d5d67cce6e878bdb365c094cf361fad
Internal Developer <internal-developer@intel.com>
2023-09-26 01:58:59 +0300

    fix invalid fp8 support check with PT >= 2.1 and non-fork PT
    Change-Id: Id2026293728c4cf417a5ce8446980c06ace32f35

--- pytorch_helpers/habana_helpers/dtype_helpers.h
+++ pytorch_helpers/habana_helpers/dtype_helpers.h
@@ -17,7 +17,8 @@
 #include "backend/helpers/get_n_bytes.h"
 #include "pytorch_helpers/habana_helpers/pt_version_check.h"
 #ifndef HAVE_FP8_SUPPORT
-#define HAVE_FP8_SUPPORT IS_PYTORCH_FORK_AT_LEAST(1, 1)
+#define HAVE_FP8_SUPPORT \
+  IS_PYTORCH_AT_LEAST(2, 1) || IS_PYTORCH_FORK_AT_LEAST(1, 1)
 #endif
 
 namespace habana_helpers {
