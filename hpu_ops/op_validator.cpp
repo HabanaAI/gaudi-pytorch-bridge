526840ebbbfc5a06968b818b4c6279cafbec770c
Internal Developer <internal-developer@intel.com>
2023-10-04 13:08:10 +0300

    fix geometry setting in shared layer
    Set geometry to proper values.
    Previously layout mistakenkly set.
    
    Layout was ignored anyway.
    
    Condider setting only geometry dims!
    
    Change-Id: I3f2e7e202d27987026abe0f30e21268335ee34ab

--- hpu_ops/op_validator.cpp
+++ hpu_ops/op_validator.cpp
@@ -110,18 +110,24 @@ bool fillSharedLayerTensorType(SharedLayer::Tensor& tensor, at::ScalarType t) {
 bool fillGuidParamInfoWithIntList(
     SharedLayer::Tensor& tensor,
     const std::vector<int64_t>& xs) {
-  tensor.geometry.dims = xs.size() - 1;
-  for (uint64_t dim = 0; dim < xs.size() - 1; ++dim) {
-    int64_t syn_dim = xs.size() - dim - 2;
-    tensor.layout.layout[syn_dim] = xs[dim];
-  }
-  if (tensor.geometry.dims == 0) {
-    tensor.geometry.dims = 1;
-    tensor.layout.layout[0] = 1;
-  }
+  // passed int vector contains dims and type, this is a reason for below
+  // indexing; refer struct habana::detail::TensorDescr @ op_validator.h
 
   if (not fillSharedLayerTensorType(tensor, (at::ScalarType)xs.back()))
     return false;
+
+  auto vec_size = xs.size();
+  tensor.geometry.dims = vec_size - 1;
+  for (uint64_t dim = 0; dim < vec_size - 1; ++dim) {
+    int64_t syn_dim = vec_size - dim - 2;
+    tensor.geometry.minSizes[syn_dim] = tensor.geometry.maxSizes[syn_dim] =
+        xs[dim];
+  }
+  if (tensor.geometry.dims == 0) {
+    // scalar size wa
+    tensor.geometry.dims = 1;
+    tensor.geometry.maxSizes[0] = tensor.geometry.minSizes[0] = 1;
+  }
 
   return true;
 }
@@ -131,9 +137,14 @@ bool fillGuidParamInfoWithTensor(
     const at::Tensor& t) {
   // todo SW-150876 missing tensor.quantizationParam setting
 
-  tensor.geometry.dims = t.dim();
-  for (int64_t dim = 0; dim < t.dim(); ++dim) {
-    int64_t syn_dim = t.dim() - dim - 1;
+  if (not fillSharedLayerTensorType(tensor, t.scalar_type()))
+    return false;
+
+  auto geometry_dims = t.dim();
+
+  tensor.geometry.dims = geometry_dims;
+  for (int64_t dim = 0; dim < geometry_dims; ++dim) {
+    int64_t syn_dim = geometry_dims - dim - 1;
     auto sym_dim = t.sym_size(dim);
     if (
 #if IS_PYTORCH_AT_LEAST(2, 1)
@@ -142,18 +153,17 @@ bool fillGuidParamInfoWithTensor(
         sym_dim.is_symbolic()
 #endif
     ) {
-      tensor.layout.layout[syn_dim] = 1;
+      tensor.geometry.minSizes[syn_dim] = tensor.geometry.maxSizes[syn_dim] = 1;
     } else {
-      tensor.layout.layout[syn_dim] = sym_dim.as_int_unchecked();
+      tensor.geometry.minSizes[syn_dim] = tensor.geometry.maxSizes[syn_dim] =
+          sym_dim.as_int_unchecked();
     }
   }
   if (tensor.geometry.dims == 0) {
     tensor.geometry.dims = 1;
-    tensor.layout.layout[0] = 1;
+    tensor.geometry.maxSizes[0] = tensor.geometry.minSizes[0] = 1;
   }
 
-  if (not fillSharedLayerTensorType(tensor, t.scalar_type()))
-    return false;
   return true;
 }
 
