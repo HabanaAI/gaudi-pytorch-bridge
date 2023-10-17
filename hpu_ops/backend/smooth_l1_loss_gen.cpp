099cf38b360421d1dcdc8d11e8063f274ba3f6dd
Internal Developer <internal-developer@intel.com>
2023-10-17 16:44:21 +0300

    Change SmoothL1LossOutputShape + bwd to output meta
    Change-Id: I3d4f4cb8c98bfeea95367e50925367070a9c2089

--- hpu_ops/backend/smooth_l1_loss_gen.cpp
+++ hpu_ops/backend/smooth_l1_loss_gen.cpp
@@ -1,9 +1,12 @@
 /******************************************************************************
- * Copyright (C) 2021 HabanaLabs, Ltd.
+ * Copyright (C) 2021-2023 Habana Labs, Ltd. an Intel Company
  * All Rights Reserved.
  *
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
+ * Unauthorized copying of this file or any element(s) within it, via any medium
+ * is strictly prohibited.
+ * This file contains Habana Labs, Ltd. proprietary and confidential information
+ * and is subject to the confidentiality and license agreements under which it
+ * was provided.
  *
  ******************************************************************************
  */
@@ -28,27 +31,34 @@ std::shared_ptr<void> FillSmoothL1LossFwdParams(
   return params;
 }
 
-sizes_vec SmoothL1LossOutputShape(const at::Stack& stack) {
+OutputMetaDataVector SmoothL1LossMeta(const at::Stack& stack) {
   float beta = stack.at(3).toScalar().to<float>();
   TORCH_CHECK(
       beta >= 0, "smooth_l1_loss does not support negative values for beta.")
   const torch::Tensor& self = stack_tensor(stack, 0);
   int64_t reduction = stack.at(2).toInt();
-  if (reduction == at::Reduction::Reduction::None) {
-    return {self.sizes().vec()};
-  }
-  return {{}};
+
+  OutputMetaData meta;
+  meta.dtype = self.scalar_type();
+  meta.shape = (reduction == at::Reduction::Reduction::None)
+      ? self.sizes().vec()
+      : std::vector<int64_t>{};
+  return {meta};
 }
 
-sizes_vec SmoothL1LossBackwardOutputShape(const at::Stack& stack) {
+OutputMetaDataVector SmoothL1LossBackwardMeta(const at::Stack& stack) {
   const torch::Tensor& self = stack_tensor(stack, 1);
-  return {self.sizes().vec()};
+
+  OutputMetaData meta;
+  meta.dtype = self.scalar_type();
+  meta.shape = self.sizes().vec();
+  return {meta};
 }
 
 void SmoothL1LossBwdOperator::AddNode(
     synapse_helpers::graph& graph,
     const at::Stack& stack) {
-  const auto& inputshape = stack_tensor(stack, 1).sizes();
+  auto meta = SmoothL1LossBackwardMeta(stack)[0];
 
   float beta = stack.at(4).toScalar().to<float>();
   TORCH_CHECK(
@@ -63,32 +73,32 @@ void SmoothL1LossBwdOperator::AddNode(
 
   auto t_diff = BuildOp(
       graph,
-      get_guid_with_precision("sub", ScalarType()),
+      get_guid_with_precision("sub", meta.dtype),
       {syn_in(1), syn_in(2)},
-      {{inputshape, ScalarType()}});
+      {{meta.shape, meta.dtype}});
 
   if (mode == at::Reduction::Reduction::Mean) {
     auto t_norm_factor =
-        ConstantHelper(graph, norm_factor, ScalarType(), inputshape);
+        ConstantHelper(graph, norm_factor, meta.dtype, meta.shape);
 
     auto t_mul = BuildOp(
         graph,
-        get_guid_with_precision("mult", ScalarType()),
+        get_guid_with_precision("mult", meta.dtype),
         {syn_in(0), t_norm_factor.get()},
-        {{inputshape, ScalarType()}});
+        {{meta.shape, meta.dtype}});
 
     auto t_sign = BuildOp(
         graph,
-        get_guid_with_precision("sign_fwd", ScalarType()),
+        get_guid_with_precision("sign_fwd", meta.dtype),
         {t_diff.at(0).get()},
-        {{inputshape, ScalarType()}});
+        {{meta.shape, meta.dtype}});
 
     if (beta == 0) {
       t_l0 = BuildOp(
           graph,
-          get_guid_with_precision("mult", ScalarType()),
+          get_guid_with_precision("mult", meta.dtype),
           {t_mul.at(0).get(), t_sign.at(0).get()},
-          {{inputshape, ScalarType(), 0}});
+          {{meta.shape, meta.dtype, 0}});
 
       syn_out(0) = std::move(t_l0.at(0));
       return;
@@ -96,23 +106,23 @@ void SmoothL1LossBwdOperator::AddNode(
 
     t_l0 = BuildOp(
         graph,
-        get_guid_with_precision("mult", ScalarType()),
+        get_guid_with_precision("mult", meta.dtype),
         {t_mul.at(0).get(), t_sign.at(0).get()},
-        {{inputshape, ScalarType()}});
+        {{meta.shape, meta.dtype}});
 
   } else {
     auto t_sign = BuildOp(
         graph,
-        get_guid_with_precision("sign_fwd", ScalarType()),
+        get_guid_with_precision("sign_fwd", meta.dtype),
         {t_diff.at(0).get()},
-        {{inputshape, ScalarType()}});
+        {{meta.shape, meta.dtype}});
 
     if (beta == 0) {
       t_l0 = BuildOp(
           graph,
-          get_guid_with_precision("mult", ScalarType()),
+          get_guid_with_precision("mult", meta.dtype),
           {syn_in(0), t_sign.at(0).get()},
-          {{inputshape, ScalarType(), 0}});
+          {{meta.shape, meta.dtype, 0}});
 
       syn_out(0) = std::move(t_l0.at(0));
       return;
@@ -120,45 +130,45 @@ void SmoothL1LossBwdOperator::AddNode(
 
     t_l0 = BuildOp(
         graph,
-        get_guid_with_precision("mult", ScalarType()),
+        get_guid_with_precision("mult", meta.dtype),
         {syn_in(0), t_sign.at(0).get()},
-        {{inputshape, ScalarType()}});
+        {{meta.shape, meta.dtype}});
   }
 
   auto t_mulfactor =
-      ConstantHelper(graph, norm_factor / beta, ScalarType(), inputshape);
+      ConstantHelper(graph, norm_factor / beta, meta.dtype, meta.shape);
 
   auto t_l2_temp = BuildOp(
       graph,
-      get_guid_with_precision("mult", ScalarType()),
+      get_guid_with_precision("mult", meta.dtype),
       {syn_in(0), t_mulfactor.get()},
-      {{inputshape, ScalarType()}});
+      {{meta.shape, meta.dtype}});
 
   auto t_l2 = BuildOp(
       graph,
-      get_guid_with_precision("mult", ScalarType()),
+      get_guid_with_precision("mult", meta.dtype),
       {t_diff.at(0).get(), t_l2_temp.at(0).get()},
-      {{inputshape, ScalarType()}});
+      {{meta.shape, meta.dtype}});
 
-  auto t_mask_const = ConstantHelper(graph, beta, ScalarType(), inputshape);
+  auto t_mask_const = ConstantHelper(graph, beta, meta.dtype, meta.shape);
 
   auto t_abs = BuildOp(
       graph,
-      get_guid_with_precision("abs_fwd", ScalarType()),
+      get_guid_with_precision("abs_fwd", meta.dtype),
       {t_diff.at(0).get()},
-      {{inputshape, ScalarType()}});
+      {{meta.shape, meta.dtype}});
 
   auto mask = BuildOp(
       graph,
-      get_guid_with_precision("less_fwd", ScalarType()),
+      get_guid_with_precision("less_fwd", meta.dtype),
       {t_abs.at(0).get(), t_mask_const.get()},
-      {{inputshape, at::kBool}});
+      {{meta.shape, at::kBool}});
 
   auto grad_in = BuildOp(
       graph,
-      get_guid_with_precision("where_fwd", ScalarType()),
+      get_guid_with_precision("where_fwd", meta.dtype),
       {mask.at(0).get(), t_l2.at(0).get(), t_l0.at(0).get()},
-      {{inputshape, ScalarType(), 0}});
+      {{meta.shape, meta.dtype, 0}});
 
   syn_out(0) = std::move(grad_in.at(0));
 }
