e5c481ef97a732a694014efa908a9205d299a79d
Internal Developer <internal-developer@intel.com>
2023-10-18 13:53:28 +0300

    wrap test body in a function Any test failure was causing the whole suite to fail.
    Change-Id: I2d931eec431c3f5c5f67c98556c6e13e0b357c70

--- tests/pytest_working/compile/test_scalar_float_nan.py
+++ tests/pytest_working/compile/test_scalar_float_nan.py
@@ -2,8 +2,9 @@ import torch
 import habana_frameworks.torch.core as htcore
 import habana_frameworks.torch.dynamo.compile_backend
 
-def fn(val):
-  return torch.full((2,2), val, dtype=torch.float, device="hpu")
+def test_scalar_float_nan():
+  def fn(val):
+    return torch.full((2,2), val, dtype=torch.float, device="hpu")
 
-compiled_fn = torch.compile(fn, backend="aot_hpu_training_backend")
-compiled_fn(float('nan'))
+  compiled_fn = torch.compile(fn, backend="aot_hpu_training_backend")
+  compiled_fn(float('nan'))
