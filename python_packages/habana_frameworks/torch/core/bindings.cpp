04c61afeb224677aa8f5520abeea91e490c67e74
Internal Developer <internal-developer@intel.com>
2023-10-24 21:20:21 +0200

    Fix double free errors in Bloom FP8
    PT 2.1 provided different semantics for get_backend_meta(). They returned a
    BackendMeta* instead of the intrusive_ptr<BackendMeta>() returned by our 2.0
    fork implementation.
    By using the 2.1's get_backend_meta_intrusive_ptr() we can keep the same
    semantics.
    
    Change-Id: I66b8044fa4912e72bc7802dced846d84e2ed2571

--- python_packages/habana_frameworks/torch/core/bindings.cpp
+++ python_packages/habana_frameworks/torch/core/bindings.cpp
@@ -47,13 +47,8 @@ class SharedTensorExtraMeta {
   }
   static std::optional<SharedTensorExtraMeta> create(const at::Tensor& tensor) {
     auto impl{tensor.unsafeGetTensorImpl()};
-#if IS_PYTORCH_AT_LEAST(2, 1)
     c10::intrusive_ptr<habana::BaseTensorExtraMeta> meta(
-        impl->get_backend_meta(), {});
-#else
-    c10::intrusive_ptr<habana::BaseTensorExtraMeta> meta(
-        impl->get_backend_meta());
-#endif
+        impl->get_backend_meta_intrusive_ptr());
     if (!meta)
       return {};
     auto tmeta_ptr{dynamic_cast<habana::TensorExtraMeta*>(meta.get())};
@@ -78,28 +73,15 @@ class SharedTensorExtraMeta {
     TORCH_CHECK(
         impl != nullptr,
         "Cannot obtain the TensorImpl from the tensor provided");
-    {
-#if IS_PYTORCH_AT_LEAST(2, 1)
-      c10::intrusive_ptr<habana::BaseTensorExtraMeta> meta(
-          impl->get_backend_meta(), {});
-#else
-      c10::intrusive_ptr<habana::BaseTensorExtraMeta> meta(
-          impl->get_backend_meta());
-#endif
-      TORCH_CHECK(
-          meta == nullptr,
-          "Cannot create a new backend meta as one already exists");
-    }
+    c10::intrusive_ptr<habana::BaseTensorExtraMeta> meta(
+        impl->get_backend_meta_intrusive_ptr());
+    TORCH_CHECK(
+        meta == nullptr,
+        "Cannot create a new backend meta as one already exists");
     c10::intrusive_ptr<c10::BackendMeta> new_tmeta{
         std::unique_ptr<c10::BackendMeta>(new habana::TensorExtraMeta())};
     impl->set_backend_meta(new_tmeta);
-#if IS_PYTORCH_AT_LEAST(2, 1)
-    c10::intrusive_ptr<habana::BaseTensorExtraMeta> meta(
-        impl->get_backend_meta(), {});
-#else
-    c10::intrusive_ptr<habana::BaseTensorExtraMeta> meta(
-        impl->get_backend_meta());
-#endif
+    meta = impl->get_backend_meta_intrusive_ptr();
     TORCH_CHECK(meta == new_tmeta, "Attached meta not the same as created");
     auto tmeta_ptr{dynamic_cast<habana::TensorExtraMeta*>(meta.get())};
     TORCH_CHECK(
