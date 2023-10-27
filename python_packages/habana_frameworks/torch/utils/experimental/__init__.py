bed872ae52412bf7b7aebe63406b65624689b9f6
Internal Developer <internal-developer@intel.com>
2023-10-27 12:14:16 +0300

    Return device type without aquiring the device
    Change-Id: Ieec51d75d3e30607fd1e406ee6b5579b7e4c3e63

--- python_packages/habana_frameworks/torch/utils/experimental/__init__.py
+++ python_packages/habana_frameworks/torch/utils/experimental/__init__.py
@@ -32,13 +32,7 @@ def _data_ptr(t) -> int:
         return 0
 
 def _get_device_type() -> int:
-    if _is_available():
-        try:
-            return _experimental_C.get_device_type()
-        except:
-            return -1
-    else:
-        return -1
+    return _experimental_C.get_device_type()
 
 def _is_fp16_supported() -> bool:
     return _get_device_type() != synDeviceType.synDeviceGaudi
