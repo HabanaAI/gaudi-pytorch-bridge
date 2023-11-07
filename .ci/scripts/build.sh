88ff904938a114c8170373e1380b646c4254f4f3
Internal Developer <internal-developer@intel.com>
2023-11-07 17:25:17 +0200

    Adapt install_pytorch_whls to handle multiple pythorch-fork wheels
    Change-Id: I0ca66ecd6aa7471e89d938711318bba8460bf619

--- .ci/scripts/build.sh
+++ .ci/scripts/build.sh
@@ -2525,7 +2525,14 @@ build_pytorch_vision()
 # called from the ci/promote flow
 install_pytorch_whls() {
     __clean_pytorch_dev_py_deps
-    $__pip_cmd install -U ${PYTORCH_FORK_RELEASE_BUILD}/pkgs/*.whl
+    #temporary workaround for pytorch-fork migration to separate component SW-162985
+    if [ $(ls ${PYTORCH_FORK_RELEASE_BUILD}/pkgs/ | wc -l) != 1 ]; then
+        pyfork_revision=$(cd ${PYTORCH_FORK_ROOT} && git rev-parse --short HEAD)
+        echo "installing pyfork version ${pyfork_revision}"
+        $__pip_cmd install -U ${PYTORCH_FORK_RELEASE_BUILD}/pkgs/*${pyfork_revision}*.whl
+    else
+        $__pip_cmd install -U ${PYTORCH_FORK_RELEASE_BUILD}/pkgs/*.whl
+    fi
     $__pip_cmd install -U ${PYTORCH_VISION_FORK_BUILD}/pkgs/*.whl
     install_pillow_simd
     if [ "${GERRIT_PROJECT}" = "lightning-habana-fork" ];  then
