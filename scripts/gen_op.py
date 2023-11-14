2a3aed0a72c1e1784d12a029986063a594f723e5
Internal Developer <internal-developer@intel.com>
2023-11-14 13:18:15 +0200

    fix autocast op generation related build error on upstream
    Change-Id: Ie45b8f6249e26ea95f1aeefb20dea68ceb4d75dc

--- scripts/gen_op.py
+++ scripts/gen_op.py
@@ -287,6 +287,7 @@ using tuple_4_tensors_2_int64_2_tensors = std::tuple<at::Tensor,at::Tensor,at::T
 using tuple_4_tensors_4_int64_tensor = std::tuple<Tensor,Tensor,Tensor,Tensor,int64_t,int64_t,int64_t,int64_t,Tensor>;
 using tuple_2_tensors_2_int64_tensor = std::tuple<Tensor,Tensor,int64_t,int64_t,Tensor>;
 using tuple_4_tensors_2_int64_3_tensor = std::tuple<Tensor,Tensor,Tensor,Tensor,int64_t,int64_t,Tensor,Tensor,Tensor>;
+using tuple_4_tensors_2_int64 = std::tuple<Tensor,Tensor,Tensor,Tensor,int64_t,int64_t>;
 using tuple_5_vectors = std::tuple<::std::vector<at::Tensor>,::std::vector<at::Tensor>,::std::vector<at::Tensor>,::std::vector<at::Tensor>,::std::vector<at::Tensor>>;
 {fallback_fallthrough}
 TORCH_LIBRARY_IMPL(aten, AutocastHPU, m) {{
@@ -2206,6 +2207,11 @@ def generate_autocast_ops(fgens, args, out_dir):
             "tuple_4_tensors_int64",
         ),
         (
+            "::std::tuple<at::Tensor,at::Tensor,at::Tensor,at::Tensor,int64_t,int64_t>",
+            "tuple_4_tensors_2_int64",
+        ),
+
+        (
             "::std::tuple<at::Tensor,at::Tensor,double,int64_t>",
             "tuple_2_tensors_double_int64",
         ),
@@ -2261,6 +2267,7 @@ def generate_autocast_ops(fgens, args, out_dir):
         "sym_stride.int",
         "sym_storage_offset",
         "_scaled_dot_product_flash_attention",
+        "_efficient_attention_forward",
     ] + (["_fused_adam", "_fused_adamw"] if torch.__version__ < Version("2.1") else [])
 
     def op_to_skip(function_name, op_name):
