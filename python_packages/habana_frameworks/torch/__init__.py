8b08b591fbea6e30859740b645e098f6736073e8
Internal Developer <internal-developer@intel.com>
2023-11-16 14:06:50 +0200

    Init: add assert in order to check supported PT version
    Change-Id: I228f6557431eb7d650c7a56c27c4012865260c31

--- python_packages/habana_frameworks/torch/__init__.py
+++ python_packages/habana_frameworks/torch/__init__.py
@@ -12,12 +12,29 @@
 
 import ctypes
 import os
+from packaging.version import Version
 
 import torch
 from habana_frameworks.torch.utils.internal import is_lazy
 
+REQUIRED_VERSION_FILE = "required_version.txt"
+REQUIRED_VERSION_FILE_PATH = os.path.join(
+    os.path.dirname(__file__), REQUIRED_VERSION_FILE
+)
+
+with open(REQUIRED_VERSION_FILE_PATH) as req_ver_file:
+    compile_time_ver = Version(req_ver_file.read())
+
+run_time_ver = Version(Version(torch.__version__).base_version)
+
+assert (
+    run_time_ver == compile_time_ver
+), f"Error: Compile-time pytorch version {compile_time_ver} differs from run-time {run_time_ver}."
+
 lib_to_load = "libhabana_pytorch{}_plugin.so".format("" if is_lazy() else "2")
-ctypes.CDLL(os.path.join(os.path.dirname(__file__), "lib", lib_to_load), ctypes.RTLD_GLOBAL)
+ctypes.CDLL(
+    os.path.join(os.path.dirname(__file__), "lib", lib_to_load), ctypes.RTLD_GLOBAL
+)
 
 import habana_frameworks.torch.core
 import habana_frameworks.torch.distributed.hccl
--- python_packages/setup.py
+++ python_packages/setup.py
@@ -62,6 +62,7 @@ setup(
     zip_safe=False,
     packages=find_namespace_packages(include=["habana_frameworks.*", "torch_hpu"]),
     package_data={
+        "habana_frameworks.torch": ["*.txt"],
         "habana_frameworks.torch.hpex.hmp": ["*.txt"]},
     ext_modules=[
         PrebuiltPtExtension("habana_frameworks.torch", modules_build_dir)],
