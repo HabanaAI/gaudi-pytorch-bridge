964cbae3b37eda477ab551edb1553b2c4c10666b
Internal Developer <internal-developer@intel.com>
2023-11-16 09:17:46 +0200

    Prevent symbolic scalars from copying to hpu
    Change-Id: I831c5b875b029c38cc1aa99aacec290eae3a0af6

--- python_packages/habana_frameworks/torch/dynamo/compile_backend/passes.py
+++ python_packages/habana_frameworks/torch/dynamo/compile_backend/passes.py
@@ -31,6 +31,12 @@ from .symbolic_execution import SymExprNodeManager
 
 logger = get_compile_backend_logger()
 
+def _is_cpu_scalar_or_symbolic_scalar(node: torch.fx.Node) -> bool:
+    if node.type in [int, float]:
+        assert node.meta["output_device"] == torch.device('cpu')
+        return True
+    else:
+        return False
 
 def _is_legacy_pt():
     if Version(Version(torch.__version__).base_version) < Version("2.1"):
@@ -743,6 +749,7 @@ def pass_wa_mixed_devices(ctx: OptimizerContext) -> bool:
                 if (
                     isinstance(arg, torch.fx.Node)
                     and arg.meta["output_device"].type != "hpu"
+                    and not _is_cpu_scalar_or_symbolic_scalar(arg)
                 ):
                     nodes_to_fix_list.append(node)
                     break
@@ -836,6 +843,9 @@ def pass_mark_placement(ctx: OptimizerContext) -> bool:
                     # If you got into this assert, we might need to rewrite this part so we cluster only
                     # these OPs that also have all inputs on HPU. Or debug why this OP have mixed device
                     # tensors, that could be the original issue here.
+                    if _is_cpu_scalar_or_symbolic_scalar(arg):
+                        logger.debug("Argument {} to node {} is a scalar or a symbolic scalar", arg, node)
+                        continue
                     assert arg.meta["output_device"].type == "hpu"
 
             placement = "hpu_cluster"
@@ -848,7 +858,8 @@ def pass_mark_placement(ctx: OptimizerContext) -> bool:
         # ...it happens that placeholder nodes might be reused between FWD and BWD.
         # They are always placed in eager though, so it should not be an issue.
         if "placement" in node.meta:
-            assert node.op == "placeholder"
+            logger.debug("Node {} of type {} has had it's placement already set" ,node, node.op)
+            assert node.meta["placement"] == placement
 
         node.meta["placement"] = placement
 
