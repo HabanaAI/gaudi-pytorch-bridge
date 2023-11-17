837c069580c5fac485c287ba63583c27b8982ef3
Internal Developer <internal-developer@intel.com>
2023-11-17 10:24:35 +0200

    Move div.Tensor_mode & div.Scalar_mode  to backend & ensure compatible with torch.compile
    Change-Id: Ibb61ce4d6ae5ba0e24b18574f6be3c4adf5bf89d

---
+++ tests/pytest_working/any_mode/test_hpu_div.py
@@ -0,0 +1,45 @@
+###############################################################################
+# Copyright (C) 2023 Habana Labs, Ltd. an Intel Company
+# All Rights Reserved.
+#
+# Unauthorized copying of this file or any element(s) within it, via any medium
+# is strictly prohibited.
+# This file contains Habana Labs, Ltd. proprietary and confidential information
+# and is subject to the confidentiality and license agreements under which it
+# was provided.
+#
+###############################################################################
+import torch
+import pytest
+from test_utils import is_gaudi1, format_tc
+import habana_frameworks.torch.dynamo.compile_backend
+import habana_frameworks.torch.utils.experimental as htexp
+
+dtypes = [torch.bfloat16, torch.float]
+if not is_gaudi1():
+    dtypes.append(torch.float16)
+
+@pytest.mark.parametrize("shape", [[2,2,3], [4,2,5,2], [2,1,3,3,2]])
+@pytest.mark.parametrize("scalar", [None, 1.3, 2.123, 5.947812, 13.13541])
+@pytest.mark.parametrize("rounding_mode", ["floor", "trunc"])
+@pytest.mark.parametrize("dtype", dtypes, ids=format_tc)
+def test_hpu_div_scalar_mode(shape, scalar, rounding_mode, dtype):
+    def fn(input_tensor, other, rounding_mode):
+        return torch.div(input_tensor, other, rounding_mode=rounding_mode)
+
+    cpu_input = torch.rand(shape, dtype=dtype)
+    hpu_input = cpu_input.to("hpu")
+
+    if scalar == None:
+        cpu_other = torch.rand(shape, dtype=dtype)
+        hpu_other = cpu_other.to("hpu")
+    else:
+        cpu_other = scalar
+        hpu_other = scalar
+
+    cpu_compiled_fn = fn
+    hpu_compiled_fn = torch.compile(fn, backend="aot_hpu_training_backend") if pytest.mode == "compile" else fn
+
+    cpu_output = cpu_compiled_fn(cpu_input, cpu_other, rounding_mode)
+    hpu_output = hpu_compiled_fn(hpu_input, hpu_other, rounding_mode).cpu()
+    assert torch.allclose(cpu_output, hpu_output)
