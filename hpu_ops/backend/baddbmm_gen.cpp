ff22f1bfaf988e039e7191bb4815152adeb76f5b
Internal Developer <internal-developer@intel.com>
2023-11-27 17:04:28 +0200

    Change BaddbmmOutputShape to output meta
    Change-Id: I29b5b9b650f0b8d65793c02b1da55c1cff84027f

--- hpu_ops/backend/baddbmm_gen.cpp
+++ hpu_ops/backend/baddbmm_gen.cpp
@@ -18,10 +18,10 @@ std::vector<synapse_helpers::tensor> ComputeGEMM(
     habana::OpBackend* op,
     synapse_helpers::graph& graph,
     std::vector<synTensor> input_tensor,
-    const at::IntArrayRef gemm_output_shape,
+    const habana::OutputMetaData& meta,
     c10::optional<int> final_idx = c10::nullopt) {
   habana::NodeAttr::NodeOutputAttr gemm_node_output_attr = {
-      gemm_output_shape, op->ScalarType()};
+      meta.shape, meta.dtype};
   gemm_node_output_attr.final_result_index = final_idx;
   synGEMMParams matmul_params{};
   std::vector<synapse_helpers::tensor> gemm_out = habana::OpBackend::BuildNode(
@@ -38,7 +38,7 @@ std::vector<synapse_helpers::tensor> ComputeGEMM(
 
 namespace habana {
 
-sizes_vec BaddbmmOutputShape(const at::Stack& stack) {
+OutputMetaDataVector BaddbmmMeta(const at::Stack& stack) {
   auto self = stack_tensor(stack, 0);
   auto batch1 = stack_tensor(stack, 1);
   auto batch2 = stack_tensor(stack, 2);
@@ -50,7 +50,9 @@ sizes_vec BaddbmmOutputShape(const at::Stack& stack) {
   int64_t contraction_size = batch1_sizes[2];
   int64_t res_rows = batch1_sizes[1];
   int64_t res_cols = batch2_sizes[2];
-  std::vector<int64_t> output_size{bs, res_rows, res_cols};
+  OutputMetaData meta;
+  meta.shape = {bs, res_rows, res_cols};
+  meta.dtype = self.scalar_type();
 
   TORCH_CHECK(
       batch2_sizes[0] == bs && batch2_sizes[1] == contraction_size,
@@ -64,26 +66,26 @@ sizes_vec BaddbmmOutputShape(const at::Stack& stack) {
       batch2_sizes[1],
       "].");
 
-  return {output_size};
+  return {meta};
 }
 
 static std::vector<synapse_helpers::tensor> ComputeBetaSide(
     OpBackend* op,
     synapse_helpers::graph& graph,
     std::vector<synTensor> input_tensor,
-    const at::IntArrayRef output_shape,
+    const OutputMetaData& meta,
     const float beta_val,
     c10::optional<int> final_idx = c10::nullopt) {
-  synapse_helpers::tensor beta_tensor = OpBackend::BuildConstant(
-      op, graph, beta_val, op->ScalarType(), output_shape);
+  synapse_helpers::tensor beta_tensor =
+      OpBackend::BuildConstant(op, graph, beta_val, meta.dtype, meta.shape);
 
   std::vector<synTensor> node_inputs{input_tensor.at(0), beta_tensor.get()};
   std::vector<synapse_helpers::tensor> beta_side_out = OpBackend::BuildNode(
       op,
       graph,
-      {get_guid_with_precision("mult", op->ScalarType()),
+      {get_guid_with_precision("mult", meta.dtype),
        std::move(node_inputs),
-       {{output_shape, op->ScalarType(), final_idx}}});
+       {{meta.shape, meta.dtype, final_idx}}});
 
   return beta_side_out;
 }
@@ -92,8 +94,7 @@ static std::vector<synapse_helpers::tensor> ComputeAlphaSide(
     OpBackend* op,
     synapse_helpers::graph& graph,
     std::vector<synTensor> input_tensor,
-    const at::IntArrayRef output_shape,
-    const at::IntArrayRef gemm_output_shape,
+    const OutputMetaData& meta,
     const float alpha_val,
     c10::optional<int> final_idx = c10::nullopt) {
   c10::optional<int> is_gemm_final_node = c10::nullopt;
@@ -101,22 +102,22 @@ static std::vector<synapse_helpers::tensor> ComputeAlphaSide(
     is_gemm_final_node = final_idx;
   }
 
-  std::vector<synapse_helpers::tensor> gemm_out = ComputeGEMM(
-      op, graph, input_tensor, gemm_output_shape, is_gemm_final_node);
+  std::vector<synapse_helpers::tensor> gemm_out =
+      ComputeGEMM(op, graph, input_tensor, meta, is_gemm_final_node);
 
   if (alpha_val == 1.0) {
     return gemm_out;
   } else {
-    auto alpha_tensor = OpBackend::BuildConstant(
-        op, graph, alpha_val, op->ScalarType(), output_shape);
+    auto alpha_tensor =
+        OpBackend::BuildConstant(op, graph, alpha_val, meta.dtype, meta.shape);
     std::vector<synTensor> mul_node_inputs{
         gemm_out[0].get(), alpha_tensor.get()};
     std::vector<synapse_helpers::tensor> alpha_mul_out = OpBackend::BuildNode(
         op,
         graph,
-        {get_guid_with_precision("mult", op->ScalarType()),
+        {get_guid_with_precision("mult", meta.dtype),
          std::move(mul_node_inputs),
-         {{output_shape, op->ScalarType(), final_idx}}});
+         {{meta.shape, meta.dtype, final_idx}}});
     return alpha_mul_out;
   }
 }
@@ -126,57 +127,47 @@ static std::vector<synapse_helpers::tensor> BaddbMMCommon(
     synapse_helpers::graph& graph,
     const at::Stack& stack,
     std::vector<synTensor> input_tensor,
-    const at::IntArrayRef output_shape,
-    const at::IntArrayRef gemm_output_shape) {
+    const OutputMetaData& meta) {
   std::vector<synapse_helpers::tensor> baddbmm_out;
 
   const float beta_val = stack.at(3).toScalar().toFloat();
   const float alpha_val = stack.at(4).toScalar().toFloat();
 
   if (alpha_val == 0.0 && beta_val == 0.0) {
-    baddbmm_out.emplace_back(OpBackend::BuildConstant(
-        op, graph, 0.0, op->ScalarType(), output_shape, 0));
+    baddbmm_out.emplace_back(
+        OpBackend::BuildConstant(op, graph, 0.0, meta.dtype, meta.shape, 0));
   } else if (alpha_val == 0.0 && beta_val != 0.0) {
-    baddbmm_out = ComputeBetaSide(
-        op, graph, {input_tensor.at(0)}, output_shape, beta_val, 0);
+    baddbmm_out =
+        ComputeBetaSide(op, graph, {input_tensor.at(0)}, meta, beta_val, 0);
   } else if (alpha_val != 0.0 && beta_val == 0.0) {
     baddbmm_out = ComputeAlphaSide(
         op,
         graph,
         {input_tensor.at(1), input_tensor.at(2)},
-        output_shape,
-        gemm_output_shape,
+        meta,
         alpha_val,
         0);
   } else {
-    auto beta_out = ComputeBetaSide(
-        op, graph, {input_tensor.at(0)}, output_shape, beta_val);
+    auto beta_out =
+        ComputeBetaSide(op, graph, {input_tensor.at(0)}, meta, beta_val);
     auto alpha_out = ComputeAlphaSide(
-        op,
-        graph,
-        {input_tensor.at(1), input_tensor.at(2)},
-        output_shape,
-        gemm_output_shape,
-        alpha_val);
+        op, graph, {input_tensor.at(1), input_tensor.at(2)}, meta, alpha_val);
     std::vector<synTensor> add_node_inputs{
         beta_out[0].get(), alpha_out[0].get()};
     baddbmm_out = OpBackend::BuildNode(
         op,
         graph,
-        {get_guid_with_precision("add", op->ScalarType()),
+        {get_guid_with_precision("add", meta.dtype),
          std::move(add_node_inputs),
-         {{output_shape, op->ScalarType(), 0}}});
+         {{meta.shape, meta.dtype, 0}}});
   }
   return baddbmm_out;
 }
 
 void Baddbmm::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
-  auto outshape = BaddbmmOutputShape(stack)[0];
+  auto meta = BaddbmmMeta(stack)[0];
   std::vector<synTensor> input_tensor{syn_in(0), syn_in(1), syn_in(2)};
-  const int64_t batch_size = stack_tensor(stack, 1).sizes()[0];
-  auto gemm_outshape = {batch_size, outshape[1], outshape[2]};
-  auto baddbmm_out =
-      BaddbMMCommon(this, graph, stack, input_tensor, outshape, gemm_outshape);
+  auto baddbmm_out = BaddbMMCommon(this, graph, stack, input_tensor, meta);
 
   syn_out(0) = std::move(baddbmm_out[0]);
 }
