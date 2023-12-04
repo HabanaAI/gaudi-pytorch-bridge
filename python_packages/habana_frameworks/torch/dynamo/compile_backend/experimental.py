f1e03de69ab1ea260b832022add118d501f6d032
Internal Developer <internal-developer@intel.com>
2023-12-04 15:44:46 +0100

    enable_compile_autograd in dynamo.compile_backend.experimental
    Change-Id: I2df00eec38a221aea0a7d541d860bbf21fddc9b9

---
+++ python_packages/habana_frameworks/torch/dynamo/compile_backend/experimental.py
@@ -0,0 +1,32 @@
+###############################################################################
+# Copyright (C) 2023 Habana Labs, Ltd. an Intel Company
+# All Rights Reserved.
+#
+# Unauthorized copying of this file or any element(s) within it, via any medium
+# is strictly prohibited.
+# This file contains Habana Labs, Ltd. proprietary and confidential information
+# and is subject to the confidentiality and license agreements under which it
+# was provided.
+#
+###############################################################################
+import functools
+import torch
+from torch._dynamo import compiled_autograd
+
+
+def enable_compiled_autograd():
+    """
+    Helper function to enable compiled_autograd for hpu backend. For more
+    info on compiled autograd see:
+        https://github.com/pytorch/pytorch/pull/103822
+
+    This should be called before any invocations of torch.compile
+    """
+    def compiler_fn(gm):
+        return torch.compile(gm, backend='aot_hpu_training_backend', fullgraph=True)
+
+    torch._C._dynamo.compiled_autograd.set_autograd_compiler(
+            functools.partial(compiled_autograd.AutogradCompilerInstance, compiler_fn)
+        )
+
+    torch._dynamo.reset()
