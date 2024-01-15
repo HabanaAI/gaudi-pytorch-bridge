2551535bc30523420409622e16d6dca36d1ba2dd
Internal Developer <internal-developer@intel.com>
2024-01-15 16:42:20 +0200

    Add diagonal op in decomposition.py
    Diagonal OP as a view OP is decomposed into as_strided.
    
    Change-Id: I1487f6a366c732185eb598c5441694a522a1392e

--- python_packages/habana_frameworks/torch/dynamo/compile_backend/decomposition.py
+++ python_packages/habana_frameworks/torch/dynamo/compile_backend/decomposition.py
@@ -55,8 +55,6 @@ hpu_backend_decompositions_common = get_decompositions(
         aten.detach.default,
         aten.diag_embed.default,
         aten.diag_embed.out,
-        aten.diagonal.default,
-        aten.diagonal.Dimname,
         aten.dot.default,
         aten.dot.out,
         aten.elu.default,
@@ -313,6 +311,47 @@ def full_like(
         pin_memory=pin_memory,
         requires_grad=requires_grad,
     )
+
+@register_custom_decomposition(aten.diagonal, hpu_backend_decompositions_common)
+def diagonal(
+    self: utils.TensorLikeType,
+    offset: int = 0,
+    dim1: int = 0,
+    dim2: int = 1,
+) -> utils.TensorLikeType:
+    """
+    Reference implementation of torch.diagonal
+    """
+    num_dims = self.dim()
+    dim1 = utils.canonicalize_dim(idx=dim1, rank=num_dims)
+    dim2 = utils.canonicalize_dim(idx=dim2, rank=num_dims)
+
+    torch._check(
+        dim1 != dim2, lambda: f"diagonal dimensions cannot be identical {dim1}, {dim2}"
+    )
+
+    storage_offset = self.storage_offset()
+
+    if offset >= 0:
+        diag_size = max(min(self.size()[dim1], self.size()[dim2] - offset), 0)
+    else:
+        diag_size = max(min(self.size()[dim1] + offset, self.size()[dim2]), 0)
+
+    if diag_size > 0:
+        if offset >= 0:
+            storage_offset += offset * self.stride()[dim2]
+        else:
+            storage_offset -= offset * self.stride()[dim1]
+
+    sizes = [s for i, s in enumerate(self.size()) if i not in (dim1, dim2)]
+    sizes.append(diag_size)
+
+    strides = [s for i, s in enumerate(self.stride()) if i not in (dim1, dim2)]
+    strides.append(self.stride()[dim1] + self.stride()[dim2])
+
+    result = self.as_strided(size=sizes, stride=strides, storage_offset=storage_offset)
+
+    return result
 
 @register_custom_decomposition(aten.bernoulli.p, hpu_backend_decompositions_common)
 def bernoulli(input, p, *, generator=None):
---
+++ tests/pytest_working/compile/test_hpu_diagonal.py
@@ -0,0 +1,94 @@
+###############################################################################
+# Copyright (C) 2024 Habana Labs, Ltd. an Intel Company
+# All Rights Reserved.
+#
+# Unauthorized copying of this file or any element(s) within it, via any medium
+# is strictly prohibited.
+# This file contains Habana Labs, Ltd. proprietary and confidential information
+# and is subject to the confidentiality and license agreements under which it
+# was provided.
+#
+###############################################################################
+import torch
+import pytest
+import habana_frameworks.torch.core as htcore
+from itertools import combinations
+
+from test_utils import is_gaudi1
+
+def set_precision(dtype):
+    atol = 0
+    rtol = 0
+    if dtype == torch.float16:
+        atol = 1.0e-4
+        rtol = 1.0e-4
+    return atol, rtol
+
+supported_dtypes = [torch.bfloat16, torch.float, torch.int, torch.short]
+if not is_gaudi1():
+    supported_dtypes.append(torch.half)
+
+def generate_inputs(ndim):
+    shapes = []
+    shapes.append(tuple([1] * ndim))
+    shapes.append(tuple([ndim * 2] * ndim))
+    shapes.append(tuple(range(1, ndim * 2 + 1, 2)))
+    shapes.append(tuple(range(ndim * 2, 0, -2)))
+
+    if ndim != 3:
+        dim = [0, 1, ndim-1, -2]
+    else:
+        dim = [0, 1, -1]
+    dims = list(combinations(dim, 2))
+    offset = [0, 1, ndim, ndim+1]
+    return [shapes, dims, offset]
+
+input_5d = generate_inputs(5)
+input_4d = generate_inputs(4)
+input_3d = generate_inputs(3)
+
+def diagonal_test_generic(shape, dims, offset, dtype):
+    input = torch.rand(shape).to(dtype=dtype)
+    input_hpu = input.to('hpu')
+
+    atol, rtol = set_precision(dtype)
+
+    dim1 = dims[0]
+    dim2 = dims[1]
+
+    def fn(input, off, d1, d2):
+        x = torch.diagonal(input, dim1=d1, dim2=d2, offset=off)
+        # diagonal is classified as view OP. It requires some consuming OP
+        # (e.g. torch.mul) to work in non-leaf mode.
+        return torch.mul(x, 1)
+
+    torch._dynamo.reset()
+    cpu_compiled_fn = torch.compile(fn)
+    hpu_compiled_fn = torch.compile(fn, backend="aot_hpu_training_backend")
+
+    expected = cpu_compiled_fn(input, offset, dim1, dim2)
+    result_hpu = hpu_compiled_fn(input_hpu, offset, dim1, dim2)
+    result = result_hpu.to('cpu')
+
+    assert torch.allclose(expected, result, atol=atol, rtol=rtol)
+
+@pytest.mark.parametrize("shape", input_5d[0])
+@pytest.mark.parametrize("dims", input_5d[1])
+@pytest.mark.parametrize("offset", input_5d[2])
+@pytest.mark.parametrize("dtype", supported_dtypes)
+def test_diagonal_5d(shape, dims, offset, dtype):
+    diagonal_test_generic(shape, dims, offset, dtype)
+
+@pytest.mark.parametrize("shape", input_4d[0])
+@pytest.mark.parametrize("dims", input_4d[1])
+@pytest.mark.parametrize("offset", input_4d[2])
+@pytest.mark.parametrize("dtype", supported_dtypes)
+def test_diagonal_4d(shape, dims, offset, dtype):
+    diagonal_test_generic(shape, dims, offset, dtype)
+
+@pytest.mark.parametrize("shape", input_3d[0])
+@pytest.mark.parametrize("dims", input_3d[1])
+@pytest.mark.parametrize("offset", input_3d[2])
+@pytest.mark.parametrize("dtype", supported_dtypes)
+def test_diagonal_3d(shape, dims, offset, dtype):
+    diagonal_test_generic(shape, dims, offset, dtype)
