aaf4304dbde27ec3b842403256f912f3c69ea9e2
Internal Developer <internal-developer@intel.com>
2024-01-26 15:37:27 +0200

    special_xlog1py op - fix wrong dtype for log1p_fwd
    Change-Id: Ia2f06520fe6db779c019f1a7543d0ed85ee3001e

--- hpu_ops/backend/xlogy_gen.cpp
+++ hpu_ops/backend/xlogy_gen.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (C) 2021-2023 Habana Labs, Ltd. an Intel Company
+ * Copyright (C) 2021-2024 Habana Labs, Ltd. an Intel Company
  * All Rights Reserved.
  *
  * Unauthorized copying of this file or any element(s) within it, via any medium
@@ -18,21 +18,23 @@ namespace habana {
 
 OutputMetaDataVector XlogYMeta(const at::Stack& stack) {
   OutputMetaData meta;
-
-  if (stack.at(1).isScalar()) {
-    auto self = stack_tensor(stack, 0);
-    meta.shape = self.sizes().vec();
-    meta.dtype = self.scalar_type();
-  } else if (stack.at(0).isScalar()) {
-    auto other = stack_tensor(stack, 1);
-    meta.shape = other.sizes().vec();
-    meta.dtype = other.scalar_type();
-  } else {
-    auto self = stack_tensor(stack, 0);
-    auto other = stack_tensor(stack, 1);
-    meta.shape = at::infer_size(self.sizes(), other.sizes());
-    meta.dtype = self.scalar_type();
+  c10::optional<at::Tensor> output_tensor = c10::nullopt;
+  c10::optional<c10::ScalarType> output_type = c10::nullopt;
+  auto size = stack.size();
+  if (size > 2 && stack.at(size - 1).isTensor()) {
+    output_tensor = stack.at(size - 1).toTensor();
+    output_type = output_tensor.value().scalar_type();
   }
+
+  auto self = stack_tensor(stack, 0);
+  auto other = stack_tensor(stack, 1);
+  meta.shape = at::infer_size(self.sizes(), other.sizes());
+  meta.dtype = habana_helpers::DTypeHelper::get_compute_dtype(
+      stack,
+      output_tensor,
+      habana_helpers::DTypeHelper::DtypePromoteVariant::kPromoteToCommon,
+      false,
+      output_type);
 
   return {meta};
 }
@@ -40,15 +42,21 @@ OutputMetaDataVector XlogYMeta(const at::Stack& stack) {
 void XlogYOperator::AddNode(
     synapse_helpers::graph& graph,
     const at::Stack& stack) {
-  auto outshape = XlogYMeta(stack)[0].shape;
-  auto other_shape = stack_tensor(stack, 1).sizes().vec();
+  auto meta = XlogYMeta(stack)[0];
+  auto other = stack_tensor(stack, 1);
+  auto logyShape = other.sizes().vec();
+  auto logyDtype = other.scalar_type();
 
-  auto logy = BuildOp(graph, guid_, {syn_in(1)}, {{other_shape, ScalarType()}});
+  auto logy = BuildOp(
+      graph,
+      get_guid_with_precision("log1p_fwd", logyDtype),
+      {syn_in(1)},
+      {{logyShape, logyDtype}});
   auto xlogy = BuildOp(
       graph,
-      get_guid_with_precision("mult", ScalarType()),
+      get_guid_with_precision("mult", meta.dtype),
       {syn_in(0), logy[0].get()},
-      {{outshape, ScalarType(), 0}});
+      {{meta.shape, meta.dtype, 0}});
 
   syn_out(0) = std::move(xlogy[0]);
 }
