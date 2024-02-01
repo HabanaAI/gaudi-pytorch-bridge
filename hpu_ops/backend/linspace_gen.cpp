2d82face5bd5209cebb7f7bdf62aff3fc656cbb1
Internal Developer <internal-developer@intel.com>
2024-02-01 09:00:31 +0200

    Support Scalar_Tensor/Tensor_Scalar linspace variants in PT2.2
    * Support new variants added in PT2.2 and Long tensor(only size 0 is required) input for linspace.
    * Cast to float32 in out meta function due to int exception in synapse.
    
    Change-Id: I8fdfa535c0fa14373ef3fff3152442d8c7b83947

--- hpu_ops/backend/linspace_gen.cpp
+++ hpu_ops/backend/linspace_gen.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (C) 2021-2023 Habana Labs, Ltd. an Intel Company
+ * Copyright (C) 2021-2024 Habana Labs, Ltd. an Intel Company
  * All Rights Reserved.
  *
  * Unauthorized copying of this file or any element(s) within it, via any medium
@@ -17,20 +17,31 @@ namespace habana {
 
 OutputMetaDataVector LinspaceMeta(const at::Stack& stack) {
   OutputMetaData meta;
-  auto out_tensor = stack.at(3).toTensor();
-
-  meta.dtype = out_tensor.scalar_type();
+  auto ival = stack.at(3);
+  if (ival.isTensor()) {
+    auto out_tensor = ival.toTensor();
+    meta.dtype = out_tensor.scalar_type();
+  } else {
+    auto end_ival = stack.at(1);
+    if (end_ival.isTensor()) {
+      meta.dtype = end_ival.toTensor().scalar_type();
+    } else if (end_ival.isScalar()) {
+      meta.dtype = end_ival.toScalar().type();
+    }
+  }
   meta.shape = {stack.at(2).toInt()};
-
   return {meta};
 }
 
 std::shared_ptr<void> LinspaceRangeParams(
     const at::Stack& stack,
     size_t& size) {
-  float start = stack[0].toScalar().to<float>();
-  float end = stack[1].toScalar().to<float>();
-  int64_t step = stack[2].toScalar().to<int64_t>();
+  float start = stack[0].isScalar() ? stack[0].toScalar().to<float>()
+                                    : stack[0].toTensor().item<float>();
+  float end = stack[1].isScalar() ? stack[1].toScalar().to<float>()
+                                  : stack[1].toTensor().item<float>();
+  int64_t step = stack[2].isScalar() ? stack[2].toScalar().to<float>()
+                                     : stack[2].toTensor().item<int64_t>();
 
   float endValueModification = 0.000001;
   int64_t arange_step = step;
@@ -59,14 +70,21 @@ void LinspaceOut::AddNode(
   OutputMetaData meta = LinspaceMeta(stack)[0];
   auto outshape = meta.shape;
 
-  float start = stack[0].toScalar().to<float>();
-  float end = stack[1].toScalar().to<float>();
-  int64_t step = stack[2].toScalar().to<int64_t>();
+  float start = stack[0].isScalar() ? stack[0].toScalar().to<float>()
+                                    : stack[0].toTensor().item<float>();
+  float end = stack[1].isScalar() ? stack[1].toScalar().to<float>()
+                                  : stack[1].toTensor().item<float>();
+  int64_t step = stack[2].isScalar() ? stack[2].toScalar().to<float>()
+                                     : stack[2].toTensor().item<int64_t>();
 
+  auto dtype = ScalarType() == c10::ScalarType::Long ||
+          ScalarType() == c10::ScalarType::Int
+      ? c10::ScalarType::Float
+      : ScalarType();
   if (step == 0) {
     // return empty tensor if zero step
-    auto result = habana::OpBackend::BuildOp(
-        graph, "memset", {}, {{outshape, ScalarType(), 0}});
+    auto result =
+        habana::OpBackend::BuildOp(graph, "memset", {}, {{outshape, dtype, 0}});
     syn_out(0) = std::move(result[0]);
   } else {
     if (start != end && step != 1) {
@@ -75,16 +93,16 @@ void LinspaceOut::AddNode(
 
       auto range = BuildOp(
           graph,
-          get_guid_with_precision("range", ScalarType()),
+          get_guid_with_precision("range", dtype),
           {},
-          {{outshape, ScalarType(), 0}},
+          {{outshape, dtype, 0}},
           params.get(),
           size);
 
       syn_out(0) = std::move(range[0]);
     } else {
       // return start when start == end or step == 1
-      auto result = ConstantHelper(graph, start, ScalarType(), outshape, 0);
+      auto result = ConstantHelper(graph, start, dtype, outshape, 0);
       syn_out(0) = std::move(result);
     }
   }
--- tests/pytest_working/any_mode/test_hpu_linspace.py
+++ tests/pytest_working/any_mode/test_hpu_linspace.py
@@ -16,7 +16,8 @@ from test_utils import (
     compare_tensors,
     is_pytest_mode_compile,
     clear_t_compile_logs,
-    check_ops_executed_in_jit_ir
+    check_ops_executed_in_jit_ir,
+    is_torch_at_least
 )
 
 @pytest.mark.parametrize("start", [0.1664, 0.6964, 4.124])
@@ -44,3 +45,31 @@ def test_hpu_linspace(start, end, steps):
             check_ops_executed_in_jit_ir("full")
         else:
             check_ops_executed_in_jit_ir("arange")
+
+@pytest.mark.skipif(not is_torch_at_least("2.2.0a0"), reason="Scalar_Tensor and Tensor_scalar variants only support PyTorch version >= 2.2.0")
+@pytest.mark.parametrize("start", [0.1664, 1, 10])
+@pytest.mark.parametrize("end", [1.2032, 5])
+@pytest.mark.parametrize("steps", [0, 1, 5])
+@pytest.mark.parametrize("dtype", [torch.float, torch.int64])
+@pytest.mark.parametrize("variant", [1, 2]) # [start, end] => 0: Tensor, Scalar; 1: Scalar, Tensor
+def test_hpu_linspace_tensor_input(start, end, steps, dtype, variant):
+    def linspace(start, end, steps, device='cpu'):
+        return torch.linspace(start, end, steps, device=device)
+
+    if variant == 1:
+        start = torch.tensor(start, dtype=dtype)
+    if variant == 2:
+        end = torch.tensor(end, dtype=dtype)
+
+    args=[start, end, steps]
+    fn = linspace
+    hpu_fn = linspace
+
+    if is_pytest_mode_compile():
+        torch._dynamo.reset()
+        hpu_fn = torch.compile(fn, backend="aot_hpu_training_backend")
+
+    expected_result = fn(*args)
+    real_result = hpu_fn(*args, device="hpu")
+
+    compare_tensors([real_result], [expected_result], atol=1e-8, rtol=1e-5)
