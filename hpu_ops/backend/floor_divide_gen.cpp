fed0d717c2e0bc893b1408836c440e81bc444344
Internal Developer <internal-developer@intel.com>
2024-02-02 15:37:18 +0200

    Use cguid for floor_divide
    Change-Id: I5cc938e4293cb2c79898e5d3b4539504eb29f47c

--- hpu_ops/backend/floor_divide_gen.cpp
+++ hpu_ops/backend/floor_divide_gen.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (C) 2021-2023 Habana Labs, Ltd. an Intel Company
+ * Copyright (C) 2021-2024 Habana Labs, Ltd. an Intel Company
  * All Rights Reserved.
  *
  * Unauthorized copying of this file or any element(s) within it, via any medium
@@ -19,46 +19,10 @@
 #define COMMON_COMPUTATION_TYPE_TPC c10::ScalarType::Float
 
 namespace habana {
-
-void FloorDivideOperator::AddNode(
-    synapse_helpers::graph& graph,
-    const at::Stack& stack) {
-  _TORCH_WARN_ONCE(
-      "floor_divide is deprecated, and will be removed in a future version of pytorch."
-      "It currently rounds toward 0 (like the \'trunc\' function NOT \'floor\')."
-      "This results in incorrect rounding for negative values."
-      "To keep the current behavior, use torch.div(a, b, rounding_mode=\'trunc\'),"
-      "or for actual floor division, use torch.div(a, b, rounding_mode=\'floor\'). (function operator())");
-
-  const at::Tensor self = stack_tensor(stack, 0);
-  const auto other = stack.at(1);
-  const std::string rounding_mode = "floor";
-
-  const at::ScalarType& final_result_type = other.isScalar()
-      ? at::result_type(self, other.toScalar())
-      : at::result_type(self, other.toTensor());
-
-  const at::ScalarType& computation_type =
-      (c10::ScalarType::BFloat16 == final_result_type)
-      ? c10::ScalarType::BFloat16
-      : COMMON_COMPUTATION_TYPE_TPC;
-
-  auto shape_out = other.isScalar()
-      ? self.sizes().vec()
-      : BinaryOperator::compute_output_shape(self, other.toTensor());
-
-  auto divOp = BuildOp(
-      graph,
-      get_guid_with_precision("div_fwd", computation_type),
-      {syn_in(0), syn_in(1)},
-      {{shape_out, computation_type}});
-
-  auto makeIntegerOp = BuildOp(
-      graph,
-      get_guid_with_precision(rounding_mode + "_fwd", computation_type),
-      {divOp.at(0).get()},
-      {{shape_out, computation_type, 0}});
-
-  syn_out(0) = std::move(makeIntegerOp[0]);
+std::shared_ptr<void> FillFloorDivideParams(const at::Stack&, size_t& size) {
+  PARAMS_STUB(ns_DivModKernel::ParamsV2);
+  // using floor mode
+  params->isTruncRoundingMode = false;
+  return params;
 }
 } // namespace habana
