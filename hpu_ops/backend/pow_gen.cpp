918fe7c27bcd9ef2a5d5e4898d0778f76f6e421b
Internal Developer <internal-developer@intel.com>
2024-02-21 14:06:48 +0200

    Add missing dtype support for aten::pow.Tensor_Scalar_out
    Change-Id: I819aa3366a8adf3cdff93010bf12237a2c44d7b1

--- hpu_ops/backend/pow_gen.cpp
+++ hpu_ops/backend/pow_gen.cpp
@@ -17,34 +17,51 @@ void PowOp::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
   auto outshape = self.sizes();
   auto other = stack[1].toScalar();
 
+  auto scalar_type = ScalarType();
+
+  std::optional<synapse_helpers::tensor> cast{};
+  if (scalar_type == c10::kChar) {
+    cast = BuildCast(
+        this, graph, syn_in(0), self.sizes().vec(), scalar_type, c10::kShort);
+    scalar_type = c10::kShort;
+  }
+
   if (other.toFloat() == 2.) {
     auto result = BuildOp(
         graph,
-        get_guid_with_precision("mult_fwd", ScalarType()),
-        {syn_in(0), syn_in(0)},
-        {{outshape, ScalarType(), 0}});
+        get_guid_with_precision("mult_fwd", scalar_type),
+        {cast.has_value() ? cast.value().get() : syn_in(0),
+         cast.has_value() ? cast.value().get() : syn_in(0)},
+        {{outshape, scalar_type, 0}});
 
     syn_out(0) = std::move(result[0]);
   } else if (other.toFloat() == 3.) {
     auto temp = BuildOp(
         graph,
-        get_guid_with_precision("mult_fwd", ScalarType()),
-        {syn_in(0), syn_in(0)},
-        {{outshape, ScalarType()}});
+        get_guid_with_precision("mult_fwd", scalar_type),
+        {cast.has_value() ? cast.value().get() : syn_in(0),
+         cast.has_value() ? cast.value().get() : syn_in(0)},
+        {{outshape, scalar_type}});
     auto result = BuildOp(
         graph,
-        get_guid_with_precision("mult_fwd", ScalarType()),
-        {temp[0].get(), syn_in(0)},
-        {{outshape, ScalarType(), 0}});
+        get_guid_with_precision("mult_fwd", scalar_type),
+        {temp[0].get(), cast.has_value() ? cast.value().get() : syn_in(0)},
+        {{outshape, scalar_type, 0}});
     syn_out(0) = std::move(result[0]);
 
   } else if (other.toFloat() == 1.) {
-    auto result =
-        BuildOp(graph, "identity", {syn_in(0)}, {{outshape, ScalarType(), 0}});
+    auto result = BuildOp(
+        graph,
+        "identity",
+        {cast.has_value() ? cast.value().get() : syn_in(0)},
+        {{outshape, scalar_type, 0}});
     syn_out(0) = std::move(result[0]);
   } else {
     auto result = BuildOp(
-        graph, guid_, {syn_in(0), syn_in(1)}, {{outshape, ScalarType(), 0}});
+        graph,
+        guid_,
+        {cast.has_value() ? cast.value().get() : syn_in(0), syn_in(1)},
+        {{outshape, scalar_type, 0}});
     syn_out(0) = std::move(result[0]);
   }
 }
