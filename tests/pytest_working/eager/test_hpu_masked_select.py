e2b11e6a141ec6233936b9d2f55ce164144b1160
Internal Developer <internal-developer@intel.com>
2024-02-22 19:46:09 +0200

    Implement masked_select for eager
    Implement eager frontend for masked_select.
    
    Use broadcast and index to achieve the
    result of the masked_select op.
    
    Implement small tests to cover dtype and
    shapes to broadcast.
    
    Change-Id: I8bb7f1f5def56c9da184f4766b88c712f30b164b

---
+++ tests/pytest_working/eager/test_hpu_masked_select.py
@@ -0,0 +1,53 @@
+###############################################################################
+# Copyright (C) 2024 Habana Labs, Ltd. an Intel Company
+# All Rights Reserved.
+#
+# Unauthorized copying of this file or any element(s) within it, via any medium
+# is strictly prohibited.
+# This file contains Habana Labs, Ltd. proprietary and confidential information
+# and is subject to the confidentiality and license agreements under which it
+# was provided.
+#
+###############################################################################
+import pytest
+import torch
+import numpy as np
+import habana_frameworks.torch.dynamo.compile_backend
+from test_utils import format_tc, is_pytest_mode_compile, is_gaudi1
+
+test_shapes_dtypes = [
+    ((2, 2, 2, 2, 2), (2, 2, 1, 2), torch.float32),
+    ((2, 2, 2, 2), (2, 2, 2, 2), torch.bfloat16),
+    ((2, 2, 2, 2), (2, 2), torch.half),
+    ((2, 2, 2), (1, 1, 2), torch.int32),
+    ((2, 2, 2), (2), torch.long),
+    ((2, 2), (1), torch.bool),
+    ((2), (1), torch.int8),
+    ((2, 2, 1, 2), (2, 2, 2, 2, 2), torch.float32),
+    ((2, 2, 1, 2), (2, 2, 2, 2), torch.bfloat16),
+    ((2, 2), (2, 2, 2, 2), torch.half),
+    ((1, 1, 2), (2, 2, 2), torch.int32),
+    ((2), (2, 1, 2), torch.long),
+    ((1), (2, 2), torch.bool),
+    ((1), (2), torch.int8),
+]
+
+@pytest.mark.parametrize("self_shape, mask_shape, dtype", test_shapes_dtypes, ids=format_tc)
+class TestHpuMaskedSelect:
+  @staticmethod
+  def test_hpu_masked_select(self_shape, mask_shape, dtype):
+    if is_gaudi1() and dtype == torch.half:
+        pytest.skip("Half is not supported on Gaudi.")
+    def fn(input, mask):
+      return torch.masked_select(input, mask)
+
+    cpu_input = torch.zeros(self_shape, dtype=dtype).random_()
+    cpu_mask = torch.zeros(mask_shape, dtype=torch.bool).random_()
+
+    hpu_input = cpu_input.to('hpu')
+    hpu_mask = cpu_mask.to('hpu')
+
+    cpu_result = fn(cpu_input, cpu_mask)
+    hpu_result = fn(hpu_input, hpu_mask)
+
+    torch.allclose(cpu_result, hpu_result.cpu())
