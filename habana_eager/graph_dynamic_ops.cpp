94690251aac5d26ab63d566763310292a05e40ea
Internal Developer <internal-developer@intel.com>
2024-03-13 08:35:52 +0200

    Revert "Revert " Enable dynamic shape support for randperm""
    This reverts commit 4699343a64005f78a77fa239e7525520eeadceb1.
    
    Change-Id: Iff5905f0f25a65e0acbaa36b1b8fc741106dbb67

--- habana_eager/graph_dynamic_ops.cpp
+++ habana_eager/graph_dynamic_ops.cpp
@@ -532,7 +532,10 @@ static auto& BasicDSOpsRegistry =
         .add("aten::topk", DSOP_MID_BACKEND(TopkOperatorDS))
         .add("aten::as_strided", DSOP_MID_BACKEND(AsStridedOperatorDS))
         .add("hpu::strided_insert", DSOP_MID_BACKEND(StridedInsertOperatorDS))
-	.add("aten::select_scatter", DSOP_MID_BACKEND(SelectScatterOperatorDS))
-	.add("aten::slice_scatter", DSOP_MID_BACKEND(SliceScatterOperatorDS));
+        .add("aten::slice_scatter", DSOP_MID_BACKEND(SliceScatterOperatorDS))
+        .add(
+            "hpu::habana_randperm",
+            DSOP_MID_BACKEND(RandpermGeneratorOperatorDS))
+        .add("aten::select_scatter", DSOP_MID_BACKEND(SelectScatterOperatorDS));
 } // namespace graph
 } // namespace habana
