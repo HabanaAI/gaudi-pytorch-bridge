4a379c114197354b3df3f1129b88ac25940593bf
Internal Developer <internal-developer@intel.com>
2024-03-19 14:56:59 +0200

    Enable Long dtype in where operator
    Change-Id: I021e37ef39566e87467ad9c572c88de80390f561

--- hpu_ops/backend/where_gen.cpp
+++ hpu_ops/backend/where_gen.cpp
@@ -99,12 +99,8 @@ FALLBACK_CHECK(
     case torch::kInt32:
     case torch::kBFloat16:
     case torch::kFloat32:
-      return true;
-    // Tpc kernel doesn't support this operator with Long yet
-    // Thus we need to fallback to cpu for Long inputs
-    // When Int64 isn't supported kInt64 is actually of type Int32
     case torch::kInt64:
-      return !common::IsInt64Supported();
+      return true;
     case torch::kHalf: {
       return synapse_helpers::device_supports_fp16(
           HPURegistrar::get_device().type());
