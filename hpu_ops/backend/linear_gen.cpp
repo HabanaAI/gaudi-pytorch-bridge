af813d0ea22cd30626942471e72e874f0c372f13
Internal Developer <internal-developer@intel.com>
2024-03-20 11:17:39 +0200

    Check for incompatible input for Linear op
    * Added check to trigger runtime error if the size of
      input matrices is not compatible for matrix multiplication.
    * Added test to cover this case.
    
    Change-Id: I735beac2e7cec8a28cd7fa5c38df536f4c49a7e0
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- hpu_ops/backend/linear_gen.cpp
+++ hpu_ops/backend/linear_gen.cpp
@@ -22,6 +22,16 @@ OutputMetaDataVector LinearMeta(const at::Stack& stack) {
   meta.dtype = input.scalar_type();
   meta.shape = input.sizes().vec();
   meta.shape[input.dim() - 1] = weight.sizes().vec()[0];
+  // Condition check to detect input with incompatible shapes
+  // Number of dimensions in matrix 1 can vary
+  int mat1_dim0 = 1, dim_i = 0;
+  for (; dim_i < input.dim() - 1; ++dim_i)
+      mat1_dim0 *= input.sizes().vec()[dim_i];
+  TORCH_CHECK(
+      input.sizes().vec()[input.dim()-1] == weight.sizes().vec()[1], "matrix 1 and matrix 2 shapes cannot be multiplied (",
+      mat1_dim0, "x", input.sizes().vec()[input.dim()-1], " and ",
+      weight.sizes().vec()[1], "x", weight.sizes().vec()[0], ")");
+
   return {meta};
 }
 } // namespace habana
