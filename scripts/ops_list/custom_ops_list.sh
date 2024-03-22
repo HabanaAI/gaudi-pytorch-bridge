36bfba2870d728a775095ffc37c5e1f119b6a897
Internal Developer <internal-developer@intel.com>
2024-03-22 20:17:11 +0200

    Custom ops list
    Change-Id: I6c6fd06707d691691f77b25f780a9d34615139f5

---
+++ scripts/ops_list/custom_ops_list.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+###############################################################################
+# Copyright (C) 2024 Habana Labs, Ltd. an Intel Company
+# All Rights Reserved.
+#
+# Unauthorized copying of this file or any element(s) within it, via any medium
+# is strictly prohibited.
+# This file contains Habana Labs, Ltd. proprietary and confidential information
+# and is subject to the confidentiality and license agreements under which it
+# was provided.
+#
+###############################################################################
+
+python custom_ops_list.py -d ../.. |& tee custom_ops_list.csv
