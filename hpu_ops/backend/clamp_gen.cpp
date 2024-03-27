076ebcc4bf768fe556deeb5a40cf6f14dd596d30
Internal Developer <internal-developer@intel.com>
2024-03-27 10:44:22 +0200

    Add missing foreach binary operators
    Refactor handing of binary foreach operators, separate general
    and op specific logic.
    Add following operators with all variants for eager with tests:
    _foreach_add - missing variants
    _foreach_mul - missing variants
    _foreach_div
    _foreach_maximum
    _foreach_minimum
    _foreach_sub
    _foreach_clamp_max
    _foreach_clamp_min
    
    Change-Id: I4a62b83e5f047aa1302125d5d3c318d17212cb94

--- hpu_ops/backend/clamp_gen.cpp
+++ hpu_ops/backend/clamp_gen.cpp
@@ -11,11 +11,13 @@
  *******************************************************************************
  */
 
+#include "generated/backend/_foreach_clamp_max.h"
+#include "generated/backend/_foreach_clamp_min.h"
 #include "generated/backend/clamp.h"
 #include "generated/backend/clamp_max.h"
 #include "generated/backend/clamp_min.h"
 #include "habana_helpers/dtype_helpers.h"
-
+#include "hpu_ops/backend/foreach.h"
 namespace habana {
 
 OutputMetaDataVector ClampMeta(const at::Stack& stack) {
@@ -121,13 +123,15 @@ static synapse_helpers::tensor ClampCommon(
     OpBackend* op,
     synapse_helpers::graph& graph,
     std::vector<synTensor> inputs,
-    const OutputMetaData& meta) {
+    at::ScalarType dtype,
+    std::vector<int64_t> shape,
+    int out_index) {
   return std::move(OpBackend::BuildNode(
       op,
       graph,
-      {get_guid_with_precision("clamp_pt_fwd", meta.dtype),
+      {get_guid_with_precision("clamp_pt_fwd", dtype),
        inputs,
-       {{meta.shape, meta.dtype, 0}}})[0]);
+       {{shape, dtype, out_index}}})[0]);
 }
 
 void clampTensor::AddNode(
@@ -149,7 +153,7 @@ void clampTensor::AddNode(
   inputs.push_back(min ? min.value().syn_t : nullptr);
   inputs.push_back(max ? max.value().syn_t : nullptr);
 
-  syn_out(0) = ClampCommon(this, graph, inputs, meta);
+  syn_out(0) = ClampCommon(this, graph, inputs, meta.dtype, meta.shape, 0);
 }
 
 void clampMaxTensor::AddNode(
@@ -158,7 +162,7 @@ void clampMaxTensor::AddNode(
   auto meta = OutputMeta(stack)[0];
   std::vector<synTensor> inputs = {syn_in(0), nullptr, syn_in(1)};
 
-  syn_out(0) = ClampCommon(this, graph, inputs, meta);
+  syn_out(0) = ClampCommon(this, graph, inputs, meta.dtype, meta.shape, 0);
 }
 
 void clampMinTensor::AddNode(
@@ -167,7 +171,67 @@ void clampMinTensor::AddNode(
   auto meta = OutputMeta(stack)[0];
   std::vector<synTensor> inputs = {syn_in(0), syn_in(1)};
 
-  syn_out(0) = ClampCommon(this, graph, inputs, meta);
+  syn_out(0) = ClampCommon(this, graph, inputs, meta.dtype, meta.shape, 0);
 }
 
+static synapse_helpers::tensor createForeachClampNode(
+    OpBackend* op,
+    synapse_helpers::graph& graph,
+    const std::vector<synTensor>& syn_inputs,
+    const std::vector<at::IValue>& pt_inputs,
+    int out_index,
+    bool is_max) {
+  const at::Tensor& self = pt_inputs[0].toTensor();
+  if (pt_inputs[1].isTensor()) {
+    const at::Tensor& other = pt_inputs[1].toTensor();
+    auto result_type = at::result_type(self, other);
+    std::vector<synTensor> inputs = syn_inputs;
+    if (is_max) {
+      inputs = {syn_inputs[0], nullptr, syn_inputs[1]};
+    }
+    auto outshape = at::infer_size(self.sizes(), other.sizes());
+
+    return ClampCommon(op, graph, inputs, result_type, outshape, out_index);
+  } else {
+    const at::Scalar& other = pt_inputs[1].toScalar();
+    auto result_type = at::result_type(self, other);
+
+    auto syn_other = OpBackend::BuildConstant(op, graph, other, result_type);
+    std::vector<synTensor> inputs = {syn_inputs[0], syn_other.get()};
+    if (is_max) {
+      inputs = {syn_inputs[0], nullptr, syn_other.get()};
+    }
+
+    return ClampCommon(
+        op, graph, inputs, result_type, self.sizes().vec(), out_index);
+  }
+}
+
+void ForeachClamp::AddNode(
+    synapse_helpers::graph& graph,
+    const at::Stack& stack) {
+  const size_t size = computeInputsNumber(stack);
+  std::vector<synTensor> inputs(size);
+  for (size_t i = 0; i < size; i++) {
+    inputs[i] = syn_in(i);
+  }
+  const bool is_max = guid_.find("foreach_clamp_max") != std::string::npos;
+  NodeCreateFunction node_creator =
+      [&is_max](
+          OpBackend* op,
+          synapse_helpers::graph& graph,
+          std::string&,
+          const std::vector<synTensor>& syn_inputs,
+          const std::vector<at::IValue>& pt_inputs,
+          int out_index) {
+        return createForeachClampNode(
+            op, graph, syn_inputs, pt_inputs, out_index, is_max);
+      };
+
+  auto results =
+      CommonForeachBinary(this, guid_, inputs, graph, stack, node_creator);
+  for (size_t i = 0; i < results.size(); i++) {
+    syn_out(i) = std::move(results[i]);
+  }
+}
 } // namespace habana
--- hpu_ops/backend/foreach_gen.cpp
+++ hpu_ops/backend/foreach_gen.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (C) 2022-2023 Habana Labs, Ltd. an Intel Company
+ * Copyright (C) 2022-2024 Habana Labs, Ltd. an Intel Company
  * All Rights Reserved.
  *
  * Unauthorized copying of this file or any element(s) within it, via any medium
@@ -12,12 +12,15 @@
  */
 
 #include "generated/backend/_foreach_abs.h"
-#include "generated/backend/_foreach_acos.h"
 #include "generated/backend/_foreach_add.h"
-#include "generated/backend/_foreach_exp.h"
+#include "generated/backend/_foreach_div.h"
 #include "generated/backend/_foreach_zero.h"
+#include "hpu_ops/backend/foreach.h"
 
 namespace habana {
+const unsigned SELF_INDEX = 0;
+const unsigned OTHER_INDEX = 1;
+const unsigned ALPHA_INDEX = 2;
 
 OutputMetaDataVector ForeachMeta(const at::Stack& stack) {
   auto tensors = stack[0].toTensorList();
@@ -33,12 +36,33 @@ OutputMetaDataVector ForeachMeta(const at::Stack& stack) {
   return meta;
 }
 
-OutputMetaDataVector ForeachBinaryMeta(const at::Stack& stack) {
+static OutputMetaData MetaForSingleOutput(
+    const at::Tensor& self,
+    const at::IValue& other,
+    const bool cast_int_to_float) {
+  OutputMetaData meta;
+  if (other.isTensor()) {
+    const auto& other_tensor = other.toTensor();
+    meta.dtype = at::result_type(self, other_tensor);
+    meta.shape = at::infer_size(self.sizes(), other_tensor.sizes());
+  } else {
+    meta.dtype = at::result_type(self, other.toScalar());
+    meta.shape = self.sizes().vec();
+  }
+  if (cast_int_to_float && isIntegralType(meta.dtype, true)) {
+    meta.dtype = torch::kFloat32;
+  }
+  return meta;
+}
+
+OutputMetaDataVector CommonForeachBinaryMeta(
+    const at::Stack& stack,
+    const bool cast_int_to_float) {
   auto list1 = stack[0].toTensorList();
   OutputMetaDataVector meta;
   meta.resize(list1.size());
 
-  // Second arg could be tensorlist, scalarlist or scalar
+  // Second arg could be tensorlist, scalarlist, tensor or scalar
   if (stack.at(1).isList()) {
     const auto& list2 = stack.at(1).toList();
     TORCH_CHECK(
@@ -47,29 +71,23 @@ OutputMetaDataVector ForeachBinaryMeta(const at::Stack& stack) {
         list1.size(),
         ", != List2 size: ",
         list2.size());
-    for (auto i = 0u; i < list1.size(); ++i) {
-      auto& m = meta[i];
-      const at::Tensor& t1 = list1[i];
-      if (list2[i].isTensor()) {
-        at::Tensor t2 = list2[i].toTensor();
-        m.dtype = at::result_type(t1, t2);
-        m.shape = at::infer_size(t1.sizes(), t2.sizes());
-      } else {
-        m.dtype = at::result_type(t1, list2[i].toScalar());
-        m.shape = t1.sizes().vec();
-      }
+    for (size_t i = 0; i < list1.size(); ++i) {
+      meta[i] = MetaForSingleOutput(list1[i], list2[i], cast_int_to_float);
     }
   } else {
-    const auto& scalar = stack.at(1).toScalar();
-    for (auto i = 0u; i < list1.size(); ++i) {
-      auto& m = meta[i];
-      const at::Tensor& t1 = list1[i];
-      m.dtype = at::result_type(t1, scalar);
-      m.shape = t1.sizes().vec();
+    for (size_t i = 0; i < list1.size(); ++i) {
+      meta[i] = MetaForSingleOutput(list1[i], stack.at(1), cast_int_to_float);
     }
   }
-
   return meta;
+}
+
+OutputMetaDataVector DivForeachBinaryMeta(const at::Stack& stack) {
+  return CommonForeachBinaryMeta(stack, true);
+}
+
+OutputMetaDataVector ForeachBinaryMeta(const at::Stack& stack) {
+  return CommonForeachBinaryMeta(stack, false);
 }
 
 void Foreach::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
@@ -99,6 +117,56 @@ void ForeachZero::AddNode(
         ConstantHelper(graph, 0, tensor.scalar_type(), tensor.sizes(), i);
     syn_out(i) = std::move(out);
   }
+}
+
+size_t computeInputsNumber(const at::Stack& stack) {
+  const size_t self_size = stack[SELF_INDEX].toTensorList().size();
+  const size_t other_size = stack[OTHER_INDEX].isTensorList()
+      ? self_size
+      : stack[OTHER_INDEX].isTensor() ? 1 : 0;
+  return self_size + other_size;
+}
+
+std::vector<synapse_helpers::tensor> CommonForeachBinary(
+    OpBackend* op,
+    std::string& guid,
+    const std::vector<synTensor>& inputs,
+    synapse_helpers::graph& graph,
+    const at::Stack& stack,
+    NodeCreateFunction node_creator) {
+  std::vector<synapse_helpers::tensor> outputs;
+  const auto& selfs = stack[SELF_INDEX].toTensorList();
+  const auto& others = stack[OTHER_INDEX];
+  at::optional<at::Scalar> alpha;
+
+  if (stack.at(1).isTensorList() || stack.at(1).isTensor()) {
+    if (stack.size() > 2) {
+      alpha = stack[ALPHA_INDEX].toScalar();
+    }
+    for (size_t i = 0; i < selfs.size(); ++i) {
+      const auto& self = selfs[i];
+      const auto& other = others.isList() ? others.toList()[i] : others;
+      const size_t other_syn_index =
+          others.isTensorList() ? i + selfs.size() : selfs.size();
+
+      std::vector<at::IValue> pt_inputs = {self, other};
+      if (alpha.has_value()) {
+        pt_inputs.push_back(alpha.value());
+      }
+
+      outputs.push_back(node_creator(
+          op, graph, guid, {inputs[i], inputs[other_syn_index]}, pt_inputs, i));
+    }
+  } else {
+    for (size_t i = 0; i < selfs.size(); ++i) {
+      const auto& self = selfs[i];
+      const auto& other = others.isList() ? others.toList()[i] : others;
+
+      outputs.push_back(
+          node_creator(op, graph, guid, {inputs[i]}, {self, other}, i));
+    }
+  }
+  return outputs;
 }
 
 } // namespace habana
