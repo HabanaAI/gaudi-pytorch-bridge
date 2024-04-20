8100bd69c6e839c7919ae80c8b7ee06ce6d2ef30
Internal Developer <internal-developer@intel.com>
2024-04-20 23:43:40 +0300

    Prepare pytorch-integration to work with PT 2.3
    Change-Id: Ia640f8b0384f901f103ee11eccd75d713e0c43fc

---
+++ pt_ver/2.3/torch_ver/csrc/distributed/c10d/ProcessGroup.hpp
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (C) 2024 Habana Labs, Ltd. an Intel Company
+ * All Rights Reserved.
+ *
+ * Unauthorized copying of this file or any element(s) within it, via any medium
+ * is strictly prohibited.
+ * This file contains Habana Labs, Ltd. proprietary and confidential information
+ * and is subject to the confidentiality and license agreements under which it
+ * was provided.
+ *
+ *******************************************************************************
+ */
+
+#pragma once
+
+#include <torch/csrc/distributed/c10d/Backend.hpp>
+
+namespace c10d_ver {
+using Work = c10d::Work;
+}
