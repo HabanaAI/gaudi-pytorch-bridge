6f86b4b7fb293bca0ebf36aae7a2e0cc83690d3e
Internal Developer <internal-developer@intel.com>
2024-05-13 10:53:13 +0000

    Revert " Temp skip of nativeBatchNormLegitNoStatsTest test due to horizontal regression"
    This reverts commit 0dab98b0ad907bf01ef46b323ad3a62c03586549.
    
    Change-Id: If637dd3432083f8473487a6c57306b82af943dc6

--- tests/cpp/hpu_ops/batch_norm_tests.cpp
+++ tests/cpp/hpu_ops/batch_norm_tests.cpp
@@ -11,7 +11,6 @@
  *******************************************************************************
  */
 
-#include "../utils/device_type_util.h"
 #include "util.h"
 
 class BatchNormHpuOpTest : public HpuOpTestUtil {};
@@ -55,11 +54,6 @@ class NativeBatchNormLegitNoStatsHpuOpTest
 TEST_P(NativeBatchNormLegitNoStatsHpuOpTest, nativeBatchNormLegitNoStatsTest) {
   const auto [inputTensorShape, inputTensorType, training, momentum, epsilon] =
       GetParam();
-
-  if (isGaudi2()) {
-    GTEST_SKIP()
-        << "Temporary test skipped on Gaudi2 due to https://jira.habana-labs.com/browse/SW-184291.";
-  }
 
   if (not training)
     GTEST_SKIP(); // TODO [Jira: SW-150573] seg fault visible for CPU pytorch
