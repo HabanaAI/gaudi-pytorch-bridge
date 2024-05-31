badac64227964bd42bd4fc3f0b4435c1b6a78ed0
Internal Developer <internal-developer@intel.com>
2024-05-31 08:26:21 +0000

    Revert " support uint8/int8 sub/add/rsub and bool add."
    This reverts commit b0427a0f800d83ac4f83abbaf73cd3128348b6a0.
    
    Change-Id: I3e0253e830b4db4a6675dd5f0b8645f1efd352b2

--- hpu_ops/backend/binary_gen.cpp
+++ hpu_ops/backend/binary_gen.cpp
@@ -289,51 +289,35 @@ void BinaryWithAlpha::AddNode(
     const auto& other_scalar = stack.at(OTHER_INDEX).toScalar();
     result_type = at::result_type(self, other_scalar);
   }
-  auto compute_type = result_type;
+
   const auto& filledParams =
       std::reinterpret_pointer_cast<ns_BinaryWithAlphaKernel::Params>(params);
   const auto& alpha = filledParams->alpha;
   const auto& mode = filledParams->mode;
 
-  // binary_with_alpha_fwd* do not support i8 and u8, so we use
-  // binary_with_alpha_fwd_i16.
-  switch (result_type) {
-    case c10::ScalarType::Char:
-    case c10::ScalarType::Byte:
-    case c10::ScalarType::Bool:
-      compute_type = c10::ScalarType::Short;
-      break;
-    default:
-      break;
-  }
-
   std::vector<synTensor> inputs{syn_in(SELF_INDEX), syn_in(OTHER_INDEX)};
   std::string guid{guid_};
 
-  if ((isAlphaIntegralType ? alpha.i : alpha.f) == 1) {
-    std::string opName;
-    switch (mode) {
-      case BINARY_WITH_ALPHA_MODE_ADD:
-        opName = "add";
-        break;
-      case BINARY_WITH_ALPHA_MODE_RSUB:
-        // RSUB uses SUB kernel, but with reversed inputs
-        inputs = {syn_in(OTHER_INDEX), syn_in(SELF_INDEX)};
-        [[fallthrough]];
-      case BINARY_WITH_ALPHA_MODE_SUB:
-        opName = "sub";
-        break;
-      default:
-        opName = {};
+  if (GetExecutionMode() == habana_helpers::HabanaFrontendTypes::EAGER) {
+    if ((isAlphaIntegralType ? alpha.i : alpha.f) == 1) {
+      std::string opName;
+      switch (mode) {
+        case BINARY_WITH_ALPHA_MODE_ADD:
+          opName = "add";
+          break;
+        case BINARY_WITH_ALPHA_MODE_RSUB:
+          // RSUB uses SUB kernel, but with reversed inputs
+          inputs = {syn_in(OTHER_INDEX), syn_in(SELF_INDEX)};
+          [[fallthrough]];
+        case BINARY_WITH_ALPHA_MODE_SUB:
+          opName = "sub";
+          break;
+        default:
+          opName = {};
+      }
+      guid = get_guid_with_precision(opName, result_type);
     }
-    guid = get_guid_with_precision(opName, compute_type);
   }
-  if ((isAlphaIntegralType ? alpha.i : alpha.f) != 1 &&
-      compute_type == c10::ScalarType::Short) {
-    std::string opName = "binary_with_alpha_fwd";
-    guid = get_guid_with_precision(opName, compute_type);
-  }
-
   auto op = BuildOp(
       graph,
       std::move(guid),
