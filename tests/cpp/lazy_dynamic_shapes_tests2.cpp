009f836857a6a0c5573fd4ad39e6b31bcd289a8a
Internal Developer <internal-developer@intel.com>
2024-06-20 18:15:39 +0300

    Remove invalid "LazyDynamicShapesTest2.SliceOnChlast6dInput" test
    In the test flow, to the 6D tensor, the ChannelLast memory layout is
    applied what is incorrect as such layout can be applied only to the 4D
    tensor.
    
    Change-Id: I43428d4d4b2810f2740900860c58ae1bbaa4c0d0

--- tests/cpp/lazy_dynamic_shapes_tests2.cpp
+++ tests/cpp/lazy_dynamic_shapes_tests2.cpp
@@ -59,21 +59,6 @@ TEST_F(LazyDynamicShapesTest2, SliceOnChlast3dInput) {
   }
 }
 
-TEST_F(LazyDynamicShapesTest2, DISABLED_SliceOnChlast6dInput) {
-  int N = 2, C = 3, D = 4, H = 5, W = 6, dim6 = 6;
-  std::vector<int> in_sizes{8, 10, 12, 20};
-  for (int i = 0; i < in_sizes.size(); i++) {
-    int W = in_sizes[i];
-    torch::Tensor A = torch::randn({N, C, D, H, W, dim6})
-                          .contiguous(c10::MemoryFormat::ChannelsLast);
-    auto hA = A.to(torch::kHPU);
-    auto B = torch::slice(A, 1, 1, -1, 1);
-    auto hB = torch::slice(hA, 1, 1, -1, 1);
-    HbLazyTensor::StepMarker({});
-    EXPECT_EQ(allclose(B, hB.cpu()), true);
-  }
-}
-
 TEST_F(LazyDynamicShapesTest2, SelectOnChlast3dInput) {
   int N = 2, C = 3, D = 4, H = 5, W = 6;
   std::vector<int> in_sizes{8, 10, 12, 20};
