380f37ff7478a6ec0cdb231c45f3e24e7cd70517
Internal Developer <internal-developer@intel.com>
2024-07-06 15:06:15 +0300

    node param patching support for 'scatter'
     Some of the tests written for node params patching
     of 'topk' are also containing 'aten::scatter' in
     addition to aten::topk. So we need to support node
     params patching for 'aten::scatter' as well.
    
    Change-Id: Ifea96b5b02b5a6cf17c60a70dd90c2fb85b9588f
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- backend/jit_graph_cache.h
+++ backend/jit_graph_cache.h
@@ -153,7 +153,8 @@ class NodeParamAgnosticOpList {
         c10::Symbol::fromQualString("aten::upsample_nearest3d_backward"),
         c10::Symbol::fromQualString("aten::resize_"),
         c10::Symbol::fromQualString("aten::masked_fill_"),
-        c10::Symbol::fromQualString("aten::_efficientzerotensor")};
+        c10::Symbol::fromQualString("aten::_efficientzerotensor"),
+        c10::Symbol::fromQualString("aten::scatter")};
     return ops_list;
   }
 
