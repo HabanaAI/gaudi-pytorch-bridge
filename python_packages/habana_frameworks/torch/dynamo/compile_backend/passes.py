a555ca8ee9183b7b7b0ba4c2e3a33f956818f520
Internal Developer <internal-developer@intel.com>
2024-07-10 12:13:12 +0300

    Preventing graph scalar input to tensor conversion for aten::clamp
    Change-Id: I06ec95cc808c8013e3b9ee99e810f79992e7f26b

--- python_packages/habana_frameworks/torch/dynamo/compile_backend/passes.py
+++ python_packages/habana_frameworks/torch/dynamo/compile_backend/passes.py
@@ -150,6 +150,7 @@ def _is_cpu_scalar_copy_required(node: torch.fx.Node, node_arg: torch.fx.Node) -
         "as_strided_scatter",
         "slice",
         "_roi_align_backward",
+        "clamp",
     ]
     copy_required = True
     if node.op == "call_function":
