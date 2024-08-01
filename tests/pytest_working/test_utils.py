8b18e6724a1f58d1b05c5e9ed173fe16083fda8b
Internal Developer <internal-developer@intel.com>
2024-08-01 10:06:57 +0300

    Add cholesky op
    Add cholesky_ex op, for now implementation without input checking support
    as it is not supported in tpc_kernels.
    
    Change-Id: I839df33c0d9645c72c3cfc6d24681dab287e8ca0

--- tests/pytest_working/test_utils.py
+++ tests/pytest_working/test_utils.py
@@ -478,6 +478,15 @@ def clear_t_compile_logs():
     fallback_logger.set_store_data(True)
 
 
+def compile_function_if_compile_mode(function):
+    if is_pytest_mode_compile():
+        clear_t_compile_logs()
+        torch._dynamo.reset()
+        return torch.compile(function, backend="hpu_backend")
+    else:
+        return function
+
+
 def check_ops_executed_in_jit_ir(op_names, verbose=False, allowed_fallbacks=set()):
     import re
 
