8497718c8fb87724ee7685688ef051e11f2a9e0c
Internal Developer <internal-developer@intel.com>
2024-08-05 15:50:27 +0300

    Enable autoload for eager mode by default
    Change-Id: I6cc885ced8e556b4308c6a3d81af0c1db71cea13

--- python_packages/habana_frameworks/__init__.py
+++ python_packages/habana_frameworks/__init__.py
@@ -14,11 +14,25 @@ import os
 is_loaded = False  # A member variable of habana_frameworks module to track if our module has been imported
 
 
+def is_autoload_enabled():
+    # If this env flag is set, behave accordingly. Otherwise move on to next conditions
+    autoload_env = os.getenv("PT_HPU_AUTOLOAD", "-1")
+    if autoload_env == "1":
+        return True
+    if autoload_env == "0":
+        return False
+
+    # If PT_HPU_AUTOLOAD isn't set, disable autoload for lazy mode
+    if os.getenv("PT_HPU_LAZY_MODE", "1") == "1":
+        return False
+    return True
+
+
 def __autoload():
     # This is an entrypoint for pytorch autoload mechanism
     # If the following confition is true, that means our backend has already been loaded, either explicitly
     # or by the autoload mechanism and importing it again should be skipped to avoid circular imports
     global is_loaded
-    if is_loaded or os.getenv("PT_HPU_AUTOLOAD", 0) == 0:
+    if is_loaded or not is_autoload_enabled():
         return
     import habana_frameworks.torch
