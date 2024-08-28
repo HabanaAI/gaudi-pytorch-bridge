d198a1c871788b196680e9ae37b72427526b65e0
Internal Developer <internal-developer@intel.com>
2024-08-28 13:25:58 +0300

    cd: PT - fix user space pip enviroment
    Change-Id: Ic1532ba5b19d8df15833abd75bf385311c471b85
    Signed-off-by: Internal Developer <internal-developer@intel.com>

--- .devops/cd/scripts/install.sh
+++ .devops/cd/scripts/install.sh
@@ -25,6 +25,6 @@ else
 fi
 python${MIN_PYTHON_VER} -m pip install ./*.whl -r requirements-pytorch.txt ${PIP_PYTHON_OPTIONS} --disable-pip-version-check --no-warn-script-location
 
-python${MIN_PYTHON_VER} -m pip uninstall -y pillow
-python${MIN_PYTHON_VER} -m pip uninstall -y pillow-simd
+python${MIN_PYTHON_VER} -m pip uninstall -y pillow ${PIP_PYTHON_OPTIONS}
+python${MIN_PYTHON_VER} -m pip uninstall -y pillow-simd ${PIP_PYTHON_OPTIONS}
 python${MIN_PYTHON_VER} -m pip install pillow-simd==${PILLOW_SIMD_VERSION} ${PIP_PYTHON_OPTIONS} --disable-pip-version-check
