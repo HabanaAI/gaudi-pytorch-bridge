b3cf330ffead42b87f51c1348f3a37bf763330ce
Internal Developer <internal-developer@intel.com>
2024-08-30 11:19:52 +0300

    Use batch_norm_reshape_bwd kernel for batch norm backward
    Change-Id: I2ac801aaed9f629db3fed403af938e7dd951110c

--- backend/synapse_helpers/layout_utils.h
+++ backend/synapse_helpers/layout_utils.h
@@ -42,6 +42,7 @@ enum class SynapseLayoutFormat {
   AWHDN = 19,
   WCN = 20,
   CLN = 21,
+  LCN = 22,
   INVALID
 };
 
@@ -69,6 +70,7 @@ static const std::unordered_map<const SynapseLayoutFormat, const char*>
         {SynapseLayoutFormat::BCN, "BCN"},
         {SynapseLayoutFormat::CN, "CN"},
         {SynapseLayoutFormat::CLN, "CLN"},
+        {SynapseLayoutFormat::LCN, "LCN"},
         {SynapseLayoutFormat::WHN, "WHN"},
         {SynapseLayoutFormat::XR, "XR"},
         {SynapseLayoutFormat::AB, "AB"},
