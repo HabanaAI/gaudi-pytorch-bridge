4166b0a00a2492bea57009dc7a42207afaca56f3
Internal Developer <internal-developer@intel.com>
2024-09-06 14:52:35 +0300

    Align replace of quant-dequant mm node with new matmul decomposition
    Change-Id: I3ffc3c45a6ba6dfa954926ba1744e952673c65dc

--- python_packages/habana_frameworks/torch/core/quantize_pt2e.py
+++ python_packages/habana_frameworks/torch/core/quantize_pt2e.py
@@ -640,7 +640,7 @@ def replace_pattern_quant_dequant_mm_addmm(module: torch.fx.GraphModule):
         weight_transpose = source_fn_stack and source_fn_stack[-1][1] in [torch.nn.Linear, torch.nn.functional.linear]
 
         gemm_users_node = next(iter(gemm_node.users), None)
-        output_view_node = gemm_users_node if is_node(gemm_users_node, "view.default") else None
+        output_view_node = gemm_users_node if is_node(gemm_users_node, "_unsafe_view.default") else None
 
         is_addmm_node = is_node(gemm_node, "addmm.default")
         weight_idx = 2 if is_addmm_node else 1
