4338faa3779239e12bb32f6f16d1c11c7a650962
Internal Developer <internal-developer@intel.com>
2024-09-09 16:28:38 +0300

    Remove reshapes for repeat
    Use new cguid for repeat op to remove
    reshapes from backend
    
    Change-Id: I5931af6e95787062e72b9b3fbc5e032f1d1d1890

--- hpu_ops/backend/repeat_gen.cpp
+++ hpu_ops/backend/repeat_gen.cpp
@@ -18,41 +18,24 @@ OutputMetaDataVector RepeatMeta(const at::Stack& stack) {
                                         : stack.at(1).toIntList().vec();
 
   OutputMetaData meta{};
-
   meta.dtype = self.scalar_type();
   meta.shape = RepeatOperator::compute_output_shape(self, repeats);
 
   return {meta};
 }
 
-void RepeatOp::AddNode(synapse_helpers::graph& graph, const at::Stack& stack) {
-  auto self = stack_tensor(stack, 0);
+std::shared_ptr<void> FillRepeatFwdParams(
+    const at::Stack& stack,
+    size_t& size) {
+  PARAMS_STUB(ns_RepeatPt::Params);
   auto repeats = stack.at(1).toIntVector();
-  int64_t size = repeats.size();
-  auto out_shape = RepeatMeta(stack)[0].shape;
-  std::vector<synapse_helpers::tensor> reshape_sh_tensor;
-  std::vector<synTensor> reshape_syn_tensor;
-  // reshape input to same dims as number of entries in repeat array
-  if (size > self.ndimension()) {
-    auto reshapeSize = RepeatOperator::compute_reshape_output(self, repeats);
-    reshape_sh_tensor.emplace_back(
-        ReshapeHelper(graph, syn_in(0), reshapeSize, self.scalar_type()));
-    reshape_syn_tensor.emplace_back(
-        reshape_sh_tensor[reshape_sh_tensor.size() - 1].get());
-  } else {
-    reshape_syn_tensor.emplace_back(syn_in(0));
+
+  for (unsigned int i = 0; i < repeats.size(); i++) {
+    params->repeat[i] = repeats[i];
   }
-  ns_TileKernel::ParamsV2 params{};
-  for (int64_t i = 0; i < size; ++i) {
-    params.repeat[size - i - 1] = repeats[i];
-  }
-  auto result = BuildOp(
-      graph,
-      guid_,
-      std::move(reshape_syn_tensor),
-      {{out_shape, ScalarType(), 0}},
-      &params,
-      sizeof(params));
-  syn_out(0) = std::move(result[0]);
+  params->size = repeats.size();
+
+  return params;
 }
+
 } // namespace habana
