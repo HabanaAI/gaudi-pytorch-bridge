0f712f784547d56884b2a0ec3be570b1bec29f87
Internal Developer <internal-developer@intel.com>
2024-09-11 18:08:01 +0300

    Implement argsort.stable
    Change-Id: I3e3d8eed8f530a8468ae5c72f37f7433cbf0539b

--- hpu_ops/backend/sort_gen.cpp
+++ hpu_ops/backend/sort_gen.cpp
@@ -10,9 +10,51 @@
  *
  *******************************************************************************
  */
+#include "generated/backend/argsort.h"
 #include "generated/backend/sort.h"
+#include "hpu_ops/hpu_op_helper.h"
 
 namespace habana {
+
+bool shouldFallback(const at::Tensor& self, bool isStable, int64_t dim_) {
+  auto dim = at::maybe_wrap_dim(dim_, self.dim());
+  if (dim == 0) {
+    return !isStable;
+  }
+  if (isStable) {
+    return self.sizes()[dim] <= 37;
+  }
+  return true;
+}
+
+FALLBACK_CHECK(
+    SortStableFallbackCheck,
+    const at::Tensor& self,
+    c10::optional<bool> stable,
+    int64_t dim_,
+    [[maybe_unused]] bool descending) {
+  bool isStable = stable.has_value() ? stable.value() : false;
+
+  return shouldFallback(self, isStable, dim_);
+}
+
+FALLBACK_CHECK(
+    ArgSortStableFallbackCheck,
+    const at::Tensor& self,
+    bool stable,
+    int64_t dim_,
+    [[maybe_unused]] bool descending) {
+  return shouldFallback(self, stable, dim_);
+}
+
+OutputMetaDataVector ArgSortStableMeta(const at::Stack& stack) {
+  auto self = stack_tensor(stack, 0);
+  OutputMetaData meta;
+  meta.dtype = c10::ScalarType::Long;
+  meta.shape = self.sizes().vec();
+  meta.mem_format = self.suggest_memory_format();
+  return {meta};
+}
 
 OutputMetaDataVector SortStableMeta(const at::Stack& stack) {
   auto self = stack_tensor(stack, 0);
@@ -81,6 +123,57 @@ void SortStable::AddNode(
 
   syn_out(0) = std::move(result[0]);
   syn_out(1) = std::move(result[1]);
+}
+
+void ArgSortStable::AddNode(
+    synapse_helpers::graph& graph,
+    const at::Stack& stack) {
+  auto self = stack.at(0).toTensor();
+  auto stable = stack.at(1).toBool();
+  auto dim_ = stack.at(2).toInt();
+  auto dim = at::maybe_wrap_dim(dim_, self.dim(), /*wrap_scalar=*/true);
+  bool descending = stack.at(3).toBool();
+  auto k = self.dim() ? self.size(dim) : 1;
+
+  auto meta = SortStableMeta(stack);
+  auto meta_value = meta[0];
+  auto meta_index = meta[1];
+  auto outshape = meta_value.shape;
+  std::vector<synapse_helpers::tensor> result{};
+
+  std::vector<synTensor> syn_inputs{syn_in(0)};
+  ns_TopkNodeV2::ParamsV4 params{};
+  // It is ok to set params.bsw = k irrespective of static or DS case
+  // As per CGUID doc, bsw is ignored if params.kType = K_TENSOR_SHAPE;
+  // which is set in DS case.
+  params.bsw = k;
+  params.axis = get_dim_in_tpc_order(dim, self.dim());
+  params.bottomK = !descending;
+  params.isVcData = false;
+  params.isStable = stable;
+
+  // The following 3 lines are needed only in dyn shape case.
+  // But done hre uncoditionally because, i. is ok to set null pointer
+  // inputs. CreateShapeTensorInput has check internally for DS case
+  syn_inputs.emplace_back(nullptr);
+  syn_inputs.emplace_back(nullptr);
+  CreateShapeTensorInput(graph, ScalarType(), outshape, syn_inputs);
+
+  if (graph.is_dynamic_graph()) {
+    params.kType = K_TENSOR_SHAPE;
+  }
+
+  // Add topk op
+  result = BuildOp(
+      graph,
+      "topk",
+      {std::move(syn_inputs)},
+      {{meta_value.shape, meta_value.dtype},
+       {meta_index.shape, meta_index.dtype, 0}},
+      &params,
+      sizeof(params));
+
+  syn_out(0) = std::move(result[1]);
 }
 
 } // namespace habana
