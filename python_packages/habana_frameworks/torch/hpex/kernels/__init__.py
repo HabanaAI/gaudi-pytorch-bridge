26055e7a3574feb94238f03035d4ae9c1571d339
Internal Developer <internal-developer@intel.com>
2024-09-17 09:44:09 +0300

    Eliminate Dead code v5 (python hpex kernels)
    Change-Id: I1bc3f69e8ef55e3b26bb559b309688a2da628aaf

--- python_packages/habana_frameworks/torch/hpex/kernels/__init__.py
+++ python_packages/habana_frameworks/torch/hpex/kernels/__init__.py
@@ -12,13 +12,7 @@
 
 from habana_frameworks.torch.utils.internal import is_lazy
 
-if not is_lazy():
-    import warnings
-
-    warnings.warn(f"RoiAlignFunction, ScaledMaskedSoftmax from {__name__} are no yet supported in eager mode")
-
 from .CTCLoss import CTCLoss
-from .CustomRoiAlign import RoiAlignFunction
 from .CustomSoftmax import CustomSoftmax
 from .Fp8FusedSDPA import fp8_fused_sdpa, fp8_sdpa_bwd_wrapper, fp8_sdpa_fwd_wrapper, gqa_output_reshape
 from .FusedSDPA import FusedSDPA
@@ -30,4 +24,3 @@ from .RotaryPosEmbeddingHelper import (
     RotaryPosEmbeddingMode,
     apply_rotary_pos_emb,
 )
-from .ScaledMaskedSoftmax import ScaledMaskedSoftmax
