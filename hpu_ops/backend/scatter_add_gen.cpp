4d19a796ce59f064fd3577906914207ac8573713
Internal Developer <internal-developer@intel.com>
2024-10-08 13:11:26 +0300

    Add input shapes check in scatter_add meta
    Change-Id: I11a6c1e45093ea62610c64a66244c83736721bf9

--- hpu_ops/backend/scatter_add_gen.cpp
+++ hpu_ops/backend/scatter_add_gen.cpp
@@ -15,6 +15,8 @@ namespace habana {
 
 const unsigned SELF_INDEX = 0;
 const unsigned DIM_INDEX = 1;
+const unsigned IND_INDEX = 2;
+const unsigned SRC_INDEX = 3;
 
 std::shared_ptr<void> ScatterAddParams(const at::Stack& stack, size_t& size) {
   PARAMS_STUB(ns_ScatterKernel::ParamsV2);
@@ -29,6 +31,33 @@ std::shared_ptr<void> ScatterAddParams(const at::Stack& stack, size_t& size) {
 
 OutputMetaDataVector ScatterAddMeta(const at::Stack& stack) {
   const auto selfTensor = stack.at(SELF_INDEX).toTensor();
+  const auto dim = stack.at(DIM_INDEX).toInt();
+  const auto indexTensor = stack.at(IND_INDEX).toTensor();
+  const auto srcTensor = stack.at(SRC_INDEX).toTensor();
+
+  // https://pytorch.org/docs/stable/generated/torch.Tensor.scatter_add_.html
+  auto self_rank = selfTensor.dim();
+  auto index_rank = indexTensor.dim();
+  auto src_rank = srcTensor.dim();
+  HABANA_ASSERT(
+      (self_rank == index_rank) && (index_rank = src_rank),
+      "The self, index, and src tensors must all have the same number of dimensions");
+  int pos_dim = dim;
+  if (pos_dim < 0) {
+    pos_dim = self_rank + pos_dim;
+  }
+  for (int64_t d = 0; d < self_rank; ++d) {
+    HABANA_ASSERT(
+        srcTensor.size(d) >= indexTensor.size(d),
+        "index.size(d) > src.size(d) at d = ",
+        d);
+    if (d != pos_dim) {
+      HABANA_ASSERT(
+          selfTensor.size(d) >= indexTensor.size(d),
+          "index.size(d) > self.size(d) at d = ",
+          d);
+    }
+  }
 
   OutputMetaData meta;
   meta.dtype = selfTensor.scalar_type();
