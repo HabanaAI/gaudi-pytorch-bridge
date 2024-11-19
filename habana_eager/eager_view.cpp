8616b1fa370a13d1839a80095ef9249676b96a48
Internal Developer <internal-developer@intel.com>
2024-11-19 09:20:06 +0100

    Add cumprod and cumsum to the list of the inplace ops needing cast (#74)

--- habana_eager/eager_view.cpp
+++ habana_eager/eager_view.cpp
@@ -53,6 +53,7 @@ std::unordered_set<std::string_view> ops_needing_cast = {
     "aten::remainder"sv,   "aten::floor_divide_"sv,
     "aten::clamp"sv,       "aten::clamp_max"sv,
     "aten::clamp_min"sv,   "aten::xlogy"sv,
+    "aten::cumprod"sv,     "aten::cumsum"sv,
 };
 
 bool check_if_op_doesnt_use_input(const JitNode* node) {
