f02c34620524ec1d19d5bbc4bfaa178df8c208f7
Internal Developer <internal-developer@intel.com>
2024-11-20 13:59:36 +0530

    Handle 1D indices in ScatterAdd input dims check (#79)
    *  Handle 1D indices in ScatterAdd input dims check
    
    *  Handle 1D indices in ScatterAdd input dims check
    
    *  Handle 1D indices in ScatterAdd input dims check

--- hpu_ops/backend/scatter_add_gen.cpp
+++ hpu_ops/backend/scatter_add_gen.cpp
@@ -35,23 +35,16 @@ OutputMetaDataVector ScatterAddMeta(const at::Stack& stack) {
   const auto indexTensor = stack.at(IND_INDEX).toTensor();
   const auto srcTensor = stack.at(SRC_INDEX).toTensor();
 
+  auto index_dims = indexTensor.dim();
+  int dim_ = at::maybe_wrap_dim(dim, selfTensor.dim());
+
   // https://pytorch.org/docs/stable/generated/torch.Tensor.scatter_add_.html
-  auto self_rank = selfTensor.dim();
-  auto index_rank = indexTensor.dim();
-  auto src_rank = srcTensor.dim();
-  HABANA_ASSERT(
-      (self_rank == index_rank) && (index_rank = src_rank),
-      "The self, index, and src tensors must all have the same number of dimensions");
-  int pos_dim = dim;
-  if (pos_dim < 0) {
-    pos_dim = self_rank + pos_dim;
-  }
-  for (int64_t d = 0; d < self_rank; ++d) {
+  for (int64_t d = 0; d < index_dims; ++d) {
     HABANA_ASSERT(
         srcTensor.size(d) >= indexTensor.size(d),
         "index.size(d) > src.size(d) at d = ",
         d);
-    if (d != pos_dim) {
+    if (d != dim_) {
       HABANA_ASSERT(
           selfTensor.size(d) >= indexTensor.size(d),
           "index.size(d) > self.size(d) at d = ",
