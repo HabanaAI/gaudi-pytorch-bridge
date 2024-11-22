9d5f84500cdfc64804a5a1dcbe3af806ddf1c19c
Internal Developer <internal-developer@intel.com>
2024-11-23 00:26:02 +0530

    Fix index_add in lazy mode for repeated index values (#105)
    * Enable index_add fix for repeated index tensor values
    
    Change-Id: I36c21d979ae7f50fb01d1b58b811d1dfd8980560
    
    * Add unit test for repeated indices in index_add lazy op
    
    Change-Id: Ifc2d1020a8870411f688f8211ecaab5e1677aad2
    
    * Add tolerance values in the unit test
    
    Change-Id: I024312dd468ff8c613e7f5c2ccaf939970bd295a
    
    * Add test for case when size od index tensor is greater than the size along the dim
    
    Change-Id: I4b8afb7fb304e2ee493026d1717e2e645901ba2c

--- habana_kernels/lazy_kernels.cpp
+++ habana_kernels/lazy_kernels.cpp
@@ -2942,21 +2942,11 @@ Tensor& index_add_hpu_lazy_out(
   auto func = [self, dim, indices, source, alpha, out]() mutable {
     auto dim_ = at::maybe_wrap_dim(dim, self.dim(), true);
 
-    auto dim_size = self.numel(); // for scalar tensor case
-    if (!self.sizes().vec().empty()) {
-      // for non-scalar tensor case
-      dim_size = self.sizes().vec()[dim_];
-    }
+    // takes care of duplicate entries in index tensor and
+    // also the case where index tensor size can be greater than the self
+    // tensor size at the relevant dim
+    std::string op_name = "hpu::index_add";
 
-    std::string op_name = "aten::index_add";
-    if (indices.numel() > dim_size) {
-      // Implementation to take care of duplicate entries in index tensor and
-      // also the case where index tensor size can be greater than the self
-      // tensor size at the relevant dim. For now enable this only in the large
-      // index tensor condition. This will later be enabled as default after
-      // watching for perf impacts.
-      op_name = "hpu::index_add";
-    }
     LazyOp<Tensor> index_add_op(
         op_name, {self, dim_, indices, source, alpha}, {self.sizes().vec()}
         // out_shapes
@@ -2997,21 +2987,11 @@ Tensor& index_add_hpu_lazy_(
     auto dim_ = at::maybe_wrap_dim(dim, self.dim(), /*wrap_scalar=*/true);
     auto hl_self = GetOrCreateHbLazyTensor(self);
 
-    auto dim_size = self.numel(); // for scalar tensor case
-    if (!self.sizes().vec().empty()) {
-      // for non-scalar tensor case
-      dim_size = self.sizes().vec()[dim_];
-    }
+    // takes care of duplicate entries in index tensor and
+    // also the case where index tensor size can be greater than the self
+    // tensor size at the relevant dim
+    std::string op_name = "hpu::index_add";
 
-    std::string op_name = "aten::index_add";
-    if (indices.numel() > dim_size) {
-      // Implementation to take care of duplicate entries in index tensor and
-      // also the case where index tensor size can be greater than the self
-      // tensor size at the relevant dim. For now enable this only in the large
-      // index tensor condition. This will later be enabled as default after
-      // watching for perf impacts.
-      op_name = "hpu::index_add";
-    }
     LazyOp<Tensor> index_add_op(
         op_name, {self, dim_, indices, source, alpha}, {self.sizes().vec()}
         // out_shapes
