f2cb54bf0aeac36212934c6bb33d8a6e23ac714a
Internal Developer <internal-developer@intel.com>
2024-11-28 13:12:54 +0100

    Fix license in extra more files (#195)
     Fix license in extra more files

--- tests/cpp/hpu_ops/kthvalue.cpp
+++ tests/cpp/hpu_ops/kthvalue.cpp
@@ -13,19 +13,6 @@
 * limitations under the License.
 */
 
-//===----------------------------------------------------------------------===//
-//
-// Copyright (C) 2023 Habana Labs, Ltd. an Intel Company
-// All Rights Reserved.
-//
-// Unauthorized copying of this file or any element(s) within it, via any medium
-// is strictly prohibited.
-// This file contains Habana Labs, Ltd. proprietary and confidential information
-// and is subject to the confidentiality and license agreements under which it
-// was provided.
-//
-//===----------------------------------------------------------------------===//
-
 #include "../utils/device_type_util.h"
 #include "../utils/dtype_supported_on_device.h"
 #include "util.h"
