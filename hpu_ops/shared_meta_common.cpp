7ad1ebfc0b061c068ef7581a2ce2f368fecd83d3
Internal Developer <internal-developer@intel.com>
2024-12-12 19:10:41 +0100

    Revert " Use maxpool_2d_fwd CGUID instead of pt_maxpool_2d_fwd (#231)" (#330)
    This reverts commit 78722bd867198c8b1d019cc7edf6603757a3f1f2.

--- hpu_ops/shared_meta_common.cpp
+++ hpu_ops/shared_meta_common.cpp
@@ -527,7 +527,6 @@ SharedMetaDataVector MaxPoolWithIndicesFwdSharedMeta(
 
   SharedMetaData maxPoolWithIndicesSharedMeta{guid};
   maxPoolWithIndicesSharedMeta.inputs_data.emplace_back(rank, dtype);
-
   if (guid.find("maxpool_3d") != std::string::npos) {
     switch (dtype) {
       case c10::ScalarType::BFloat16:
@@ -538,11 +537,13 @@ SharedMetaDataVector MaxPoolWithIndicesFwdSharedMeta(
         indexType = c10::ScalarType::Byte;
         break;
     }
+    maxPoolWithIndicesSharedMeta.outputs_data = {
+        {rank, indexType}, {rank, dtype}};
   } else {
     maxPoolWithIndicesSharedMeta.options.allowLongType = true;
+    maxPoolWithIndicesSharedMeta.outputs_data = {
+        {rank, dtype}, {rank, indexType}};
   }
-  maxPoolWithIndicesSharedMeta.outputs_data = {
-      {rank, indexType}, {rank, dtype}};
 
   return {maxPoolWithIndicesSharedMeta};
 }
