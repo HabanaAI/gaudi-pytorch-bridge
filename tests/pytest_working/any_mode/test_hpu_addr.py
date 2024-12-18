0a6a29934cb428786f90da093f6cc9cfe1c1cc11
Internal Developer <internal-developer@intel.com>
2024-12-18 11:00:59 +0530

    STMeta for addr (#247)
    * Add a default STMeta to autogen code to handle scalar inputs
    
    Change-Id: Ie53db9ae96f07695f8de22b615ddfcb568690940
    
    * STMeta for Topk
    
    Change-Id: I1d9b66233bc558f03e4b41dddfe5fc566365c8bb
    
    * Replace custom STMetas with Defaults
    
    Change-Id: I8349062eda2b440e143450968cc7746deda42b6f
    
    * Add STMetas and tests for arange, sort, exponential, avg_pool2d
    
    Change-Id: Iafafbebd8bddbb963dc160dd46743a3e235f1f0a
    
    * Address review comments
    
    Change-Id: If58581d875ce163e87bfeff49fc99bfa2dcec202
    
    * Revert STMeta changes for topk
    
    * Remove sort from st_meta because of bug 
    
    * STMeta for addr (scalar inputs)
    
    * Remove dynamic=True flag for addr

--- tests/pytest_working/any_mode/test_hpu_addr.py
+++ tests/pytest_working/any_mode/test_hpu_addr.py
@@ -18,6 +18,7 @@
 import habana_frameworks.torch.dynamo.compile_backend
 import pytest
 import torch
+from compile.test_dynamo_utils import use_eager_fallback
 from test_utils import compare_tensors, format_tc, is_gaudi1, is_pytest_mode_compile
 
 dtypes = [torch.float, torch.bfloat16]
@@ -52,3 +53,34 @@ def test_hpu_addr(shapes, alpha, beta, dtype):
     tol = 1e-6 if dtype == torch.float else 1e-2
     compare_tensors(hpu_output, cpu_output, atol=tol, rtol=tol)
     # Addr is decomposed by default to another OPs, so no check if addr is present in JIT_IR
+
+
+def test_hpu_addr_st_meta(alpha=0, beta=2.2, dtype=torch.float):
+    shapes = [
+        ([1, 3], [2], [3]),
+        ([4, 9], [4], [9]),
+        ([8, 18], [8], [18]),
+        ([3, 6], [3], [6]),
+        ([7, 16], [7], [16]),
+        ([6, 24], [6], [24]),
+    ]
+
+    def fn(input, vec1, vec2):
+        return torch.addr(input, vec1, vec2, alpha=alpha, beta=beta)
+
+    hpu_fn = torch.compile(fn, backend="hpu_backend") if is_pytest_mode_compile() else fn
+
+    with use_eager_fallback():  # to allow floordiv (//) to fallback to eager
+        for input_shape, mat_shape, vec_shape in shapes:
+            cpu_input = torch.rand(input_shape, dtype=dtype)
+            hpu_input = cpu_input.to("hpu")
+            cpu_vec1 = torch.rand(mat_shape, dtype=dtype)
+            hpu_vec1 = cpu_vec1.to("hpu")
+            cpu_vec2 = torch.rand(vec_shape, dtype=dtype)
+            hpu_vec2 = cpu_vec2.to("hpu")
+
+            cpu_output = fn(cpu_input, cpu_vec1, cpu_vec2)
+            hpu_output = hpu_fn(hpu_input, hpu_vec1, hpu_vec2)
+
+            tol = 1e-6 if dtype == torch.float else 1e-2
+            compare_tensors(hpu_output, cpu_output, atol=tol, rtol=tol)
