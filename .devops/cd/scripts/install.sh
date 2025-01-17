e2c2c7f1f96c6e7fac1027803e40ee5fccaf67ce
Internal Developer <internal-developer@intel.com>
2025-01-17 14:37:44 +0100

    cd: support two types of pt dockers - fork and upstream (#574)
    *  cd: support two types of pt dockers - fork and upstream
    
    - add --upstream-compile arg to build_pytorch_modules command
    - stop building extras and remove them from pytorch_modules.tgz package
    - add .env file to pytorch_modules.tgz package with components versions
    - use .env file in install.sh to support installation of torch and extras from the internet
    - support two variants of installation: with internal or external torch
    
    *  cd: add a separate install script for PT next

--- .devops/cd/scripts/install.sh
+++ .devops/cd/scripts/install.sh
@@ -1,27 +1,49 @@
 #!/usr/bin/env bash
 : ${1?"Usage: $0 major.minor.patch revision"}
 : ${2?"Usage: $0 major.minor.patch revision"}
+
 set -e
-# note
-# script params
+
+# Script params
+# HABANA_RELEASE_VERSION - release version, e.g. '1.20.0'
+# HABANA_RELEASE_ID - build id, e.g. '123'
+# TORCH_TYPE - torch source, 'fork' or 'upstream', empty value falls back to 'fork'
 HABANA_RELEASE_VERSION=$1
 HABANA_RELEASE_ID=$2
-# env params
+TORCH_TYPE=$3
+
+# Env params
 MIN_PYTHON_VER="${PYTHON_VERSION:-3}"
 PIP_PYTHON_OPTIONS="${PYTHON_OPTIONS:-}"
 EXTRA_INDEX_URL="${PYTHON_INDEX_URL:-}"
 PYTHON_MPI_VERSION="${MPI_VERSION:-3.1.6}"
 
-if [ -z $SKIP_INSTALL_DEPENDENCIES ]; then
+if [[ -z $SKIP_INSTALL_DEPENDENCIES ]]; then
   python${MIN_PYTHON_VER} -m pip install mpi4py=="${PYTHON_MPI_VERSION}" ${PIP_PYTHON_OPTIONS}
 fi
 
-if [ -z $HABANALABS_LOCAL_DIR ]; then
+if [[ -z $HABANALABS_LOCAL_DIR ]]; then
     python${MIN_PYTHON_VER} -m pip install habana-pyhlml=="${HABANA_RELEASE_VERSION}"."${HABANA_RELEASE_ID}" ${PIP_PYTHON_OPTIONS} ${EXTRA_INDEX_URL}
 else
     python${MIN_PYTHON_VER} -m pip install ${HABANALABS_LOCAL_DIR}/habana_pyhlml-${HABANA_RELEASE_VERSION}.${HABANA_RELEASE_ID}*.whl ${PIP_PYTHON_OPTIONS} --disable-pip-version-check
 fi
-python${MIN_PYTHON_VER} -m pip install ./*.whl -r requirements-pytorch.txt ${PIP_PYTHON_OPTIONS} --disable-pip-version-check --no-warn-script-location
+
+# Get components versions
+source .env
+
+# 'fork' - install extras from the internet, but torch and everything else from internal pytorch_modules.tgz package
+# 'upstream' - install extras AND torch from the internet, everything else from internal pytorch_modules.tgz package
+if [[ -n $TORCH_TYPE && $TORCH_TYPE == "upstream" ]]; then
+  rm -f torch-*.whl
+  python${MIN_PYTHON_VER} -m pip install torch==${TORCH_VERSION} torchvision==${TORCHVISION_VERSION} torchaudio==${TORCHAUDIO_VERSION} torchtext==${TORCHTEXT_VERSION} torchdata==${TORCHDATA_VERSION} --index-url https://download.pytorch.org/whl/cpu
+  python${MIN_PYTHON_VER} -m pip install ./*.whl -r requirements-pytorch.txt ${PIP_PYTHON_OPTIONS} --disable-pip-version-check --no-warn-script-location
+else
+  python${MIN_PYTHON_VER} -m pip install torchvision==${TORCHVISION_VERSION} torchaudio==${TORCHAUDIO_VERSION} torchtext==${TORCHTEXT_VERSION} torchdata==${TORCHDATA_VERSION} --index-url https://download.pytorch.org/whl/cpu --dry-run --report pip_report
+  jq -r '.install[].download_info.url' pip_report | grep -v '/torch-' > extras_req.txt
+  python${MIN_PYTHON_VER} -m pip install -r extras_req.txt --no-dependencies
+  python${MIN_PYTHON_VER} -m pip install ./*.whl -r requirements-pytorch.txt ${PIP_PYTHON_OPTIONS} --disable-pip-version-check --no-warn-script-location
+  rm -f pip_report extras_req.txt
+fi
 
 python${MIN_PYTHON_VER} -m pip uninstall -y pillow 2>/dev/null || echo "Skip uninstalling pillow. Need SUDO permissions."
 python${MIN_PYTHON_VER} -m pip uninstall -y pillow-simd 2>/dev/null || echo "Skip uninstalling pillow-simd. Need SUDO permissions."
