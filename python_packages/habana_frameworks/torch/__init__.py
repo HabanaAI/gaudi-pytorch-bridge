5c6047a53fe7418e4a4172d0677810d1b7b7a2b6
Internal Developer <internal-developer@intel.com>
2025-01-29 11:27:42 +0100

    change fork version condition
    Co-authored-by: Internal Developer <internal-developer@intel.com>

--- python_packages/habana_frameworks/torch/__init__.py
+++ python_packages/habana_frameworks/torch/__init__.py
@@ -38,7 +38,7 @@ with open(REQUIRED_VERSION_FILE_PATH) as req_ver_file:
     compile_time_ver = Version(req_ver_file.read())
 
 run_time_ver = Version(torch.__version__)
-is_torch_fork = run_time_ver.local.startswith("git")
+is_torch_fork = run_time_ver.local.startswith("git") or run_time_ver.local.startswith("hpu")
 
 assert (
     run_time_ver.major == compile_time_ver.major and run_time_ver.minor == compile_time_ver.minor
