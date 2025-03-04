03a6fd39b4d29deaea4fe1ea43867e0fd400c977
Internal Developer <internal-developer@intel.com>
2025-03-04 09:09:25 +0100

    Run hccl coalescing within eager pipeline execute thread to synchronize properly with send/recv (#1053)

--- python_packages/habana_frameworks/torch/distributed/hccl/process_group_eager_hccl.cpp
+++ python_packages/habana_frameworks/torch/distributed/hccl/process_group_eager_hccl.cpp
@@ -548,14 +548,38 @@ void Collective_Execute_Task(
 }
 
 void ProcessGroupEagerHCCL::groupStart() {
-  initComms();
-  TORCH_CHECK(
+  auto _groupStart = [this]() {
+    initComms();
+    TORCH_CHECK(
       hcclSuccess == hcclGroupStart(), "hcclGroupStart call returned error");
+  };
+
+  const bool pipeline_flag = GET_ENV_FLAG_NEW(PT_HPU_EAGER_PIPELINE_ENABLE) &&
+      GET_ENV_FLAG_NEW(PT_HPU_EAGER_COLLECTIVE_PIPELINE_ENABLE);
+
+  if (pipeline_flag) {
+    habana::eager::PipelineTask<habana::eager::ThreadType::EXECUTE>(_groupStart);
+  } else {
+    habana::eager::JoinPendingPipelineThreads();
+    _groupStart();
+  }
 }
 
 void ProcessGroupEagerHCCL::groupEnd() {
-  TORCH_CHECK(
+  auto _groupEnd = [this]() {
+    TORCH_CHECK(
       hcclSuccess == hcclGroupEnd(), "hcclGroupEnd call returned error");
+  };
+
+  const bool pipeline_flag = GET_ENV_FLAG_NEW(PT_HPU_EAGER_PIPELINE_ENABLE) &&
+      GET_ENV_FLAG_NEW(PT_HPU_EAGER_COLLECTIVE_PIPELINE_ENABLE);
+
+  if (pipeline_flag) {
+    habana::eager::PipelineTask<habana::eager::ThreadType::EXECUTE>(_groupEnd);
+  } else {
+    habana::eager::JoinPendingPipelineThreads();
+    _groupEnd();
+  }
 }
 
 c10::intrusive_ptr<Work> ProcessGroupEagerHCCL::collective(
