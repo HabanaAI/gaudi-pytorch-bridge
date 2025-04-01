3a281141904d0f7af4bd9319b8603911f32602ef
Internal Developer <internal-developer@intel.com>
2025-04-01 22:18:30 +0530

    Added justification for the test case (#1407)
    In this PR we are describing about the test case which we didn't mention with the original PR[https://github.com//pull/1057],
    Description: In this test case einsum op is decomposed in compile mode, which generate fx graph which has bmm, view and permute op, in some bridge fx passes we are handling wrongly for bmm and view, this test will verify not to match unnecessary pattern which includes bmm and view without knowing its neighbor nodes.

--- tests/pytest_working/compile/simple_ops/test_hpu_bmm_view.py
+++ tests/pytest_working/compile/simple_ops/test_hpu_bmm_view.py
@@ -15,23 +15,33 @@
 #
 ###############################################################################
 
-###############################################################################
-# Copyright (C) 2024 Habana Labs, Ltd. an Intel Company
-# All Rights Reserved.
-#
-# Unauthorized copying of this file or any element(s) within it, via any medium
-# is strictly prohibited.
-# This file contains Habana Labs, Ltd. proprietary and confidential information
-# and is subject to the confidentiality and license agreements under which it
-# was provided.
-#
-###############################################################################
-# import copy
-# import torchvision
 import pytest
 import torch
 from compile.test_dynamo_utils import use_eager_fallback
 from test_utils import is_pytest_mode_compile
+
+# For the below test case for einsum will generate a fx graph which has
+# bmm, view and permute op, in some fx passes we are handling wrongly
+# for bmm and view, this tests will verify not to match unnecessary pattern
+# which includes bmm and view
+# fx graph :
+# class <lambda>(torch.nn.Module):
+#    def forward(self, arg0_1: "bf16[1, 108, 512]", arg1_1: "bf16[1, 128, 108, 108]"):
+#         # File: /home/pradas/qnpu/pt_/src/pytorch-integration/tests/pytest_working/compile/test_hpu_bmm_view.py:42 in fn, code: hpu_input2 = cpu_input2.to("hpu")
+#        unsqueeze: "bf16[1, 128, 108, 108, 1]" = torch.ops.aten.unsqueeze.default(arg1_1, 4);  arg1_1 = None
+#        permute: "bf16[1, 128, 108, 1, 108]" = torch.ops.aten.permute.default(unsqueeze, [0, 1, 2, 4, 3]);  unsqueeze = None
+#        unsqueeze_1: "bf16[1, 108, 512, 1]" = torch.ops.aten.unsqueeze.default(arg0_1, 3);  arg0_1 = None
+#        unsqueeze_2: "bf16[1, 108, 512, 1, 1]" = torch.ops.aten.unsqueeze.default(unsqueeze_1, 4);  unsqueeze_1 = None
+#        permute_1: "bf16[1, 1, 1, 512, 108]" = torch.ops.aten.permute.default(unsqueeze_2, [0, 3, 4, 2, 1]);  unsqueeze_2 = None
+#        permute_2: "bf16[128, 108, 108, 1, 1]" = torch.ops.aten.permute.default(permute, [1, 2, 4, 0, 3]);  permute = None
+#        view: "bf16[1, 13824, 108]" = torch.ops.aten.view.default(permute_2, [1, 13824, 108]);  permute_2 = None
+#        permute_3: "bf16[108, 1, 512, 1, 1]" = torch.ops.aten.permute.default(permute_1, [4, 0, 3, 1, 2]);  permute_1 = None
+#        view_1: "bf16[1, 108, 512]" = torch.ops.aten.view.default(permute_3, [1, 108, 512]);  permute_3 = None
+#        bmm: "bf16[1, 13824, 512]" = torch.ops.aten.bmm.default(view, view_1);  view = view_1 = None
+#        view_2: "bf16[128, 108, 1, 1, 512]" = torch.ops.aten.view.default(bmm, [128, 108, 1, 1, 512]);  bmm = None
+#        permute_4: "bf16[1, 128, 108, 512, 1]" = torch.ops.aten.permute.default(view_2, [3, 0, 1, 4, 2]);  view_2 = None
+#        view_3: "bf16[1, 128, 108, 512]" = torch.ops.aten.view.default(permute_4, [1, 128, 108, 512]);  permute_4 = None
+#        return (view_3,)
 
 
 @pytest.mark.parametrize("dtype", [torch.bfloat16])
