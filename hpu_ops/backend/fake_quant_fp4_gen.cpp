f68639214674bc2bfc2025c56fb6c374ba579bb6
Internal Developer <internal-developer@intel.com>
2025-04-10 12:43:48 +0200

    Restore previously disabled custom op (#1467)

---
+++ hpu_ops/backend/fake_quant_fp4_gen.cpp
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2025 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "generated/backend/fake_quant_fp4.h"
+
+namespace habana {
+std::shared_ptr<void> FillFakeQuantFp4Params(
+    const at::Stack& stack,
+    size_t& size) {
+  TORCH_CHECK(
+      stack.at(0).toTensor().scalar_type() == c10::ScalarType::BFloat16,
+      "Input dtype is expected to be bfloat16");
+  static const std::map<std::string_view, FakeQuantizeFp4IntermediateDtype>
+      fp4_dtypes = {
+          {"fp4_e2m1", FakeQuantizeFp4IntermediateDtype::FP4_121},
+          {"fp4_e3m0", FakeQuantizeFp4IntermediateDtype::FP4_130}};
+
+  const auto inter_dtype = fp4_dtypes.find(stack.at(3).to<std::string_view>());
+  TORCH_CHECK(
+      inter_dtype != fp4_dtypes.end(),
+      "Unrecognized FP4 dtype. Use naming \"fp4_eXmY\"");
+
+  PARAMS_STUB(ns_FakeQuantizeFp4Kernel::Params);
+  params->round = stack.at(1).toBool() ? FakeQuantizeFp4Round::FP4_SR_AND_SFTZ
+                                       : FakeQuantizeFp4Round::FP4_RNE;
+  params->axis = stack.at(2).toScalar().toInt();
+  params->inter_dt = inter_dtype->second;
+  return params;
+}
+
+} // namespace habana
