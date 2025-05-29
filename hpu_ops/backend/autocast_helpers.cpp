7084ea36ef5f14572fd0fff62c13adbb0082347e
Internal Developer <internal-developer@intel.com>
2025-05-29 12:43:16 +0200

    Revert "  Verify ops list path. (#2003)" (#2099)
    This reverts commit 5271b01dc122517723e53a6335481f8875d5446e.

--- hpu_ops/backend/autocast_helpers.cpp
+++ hpu_ops/backend/autocast_helpers.cpp
@@ -14,9 +14,6 @@
  */
 
 #include <fstream>
-#include <iomanip>
-#include <iostream>
-#include <sstream>
 
 #include "hpu_ops/autocast_helpers.h"
 #include "pytorch_helpers/habana_helpers/logging.h"
@@ -24,92 +21,20 @@
 namespace at {
 namespace autocast {
 
-namespace {
-std::filesystem::path get_top_level_directory(const std::filesystem::path& path) {
-  const auto root_path = path.root_path();
-  const auto top_level_dir = *std::next(std::begin(path));
-
-  return root_path / top_level_dir;
-}
-
-bool is_root_safe(const std::filesystem::path& path) {
-  static const std::unordered_set<std::filesystem::path> banned_roots{
-      "/bin",
-      "/etc",
-      "/usr",
-      "/var",
-      "/boot",
-      "/dev",
-      "/lib",
-      "/lost+found",
-      "/proc",
-      "/run",
-      "/sbin",
-      "/srv",
-      "/sys",
-      "/root"};
-  const auto absolute_path = std::filesystem::absolute(path);
-  const auto canonical_path = std::filesystem::canonical(absolute_path);
-  const auto top_level_dir = get_top_level_directory(canonical_path);
-
-  return banned_roots.find(top_level_dir) == std::end(banned_roots);
-}
-
-struct ops_list_status {
-  bool is_valid = true;
-  std::string reason = "";
-};
-
-const ops_list_status validate_ops_list_path(
-    const std::filesystem::path& path_to_list) {
-  if (path_to_list.empty())
-    return {false, "Empty path."};
-
-  if (not std::filesystem::exists(path_to_list)) {
-    std::stringstream reason;
-    reason << "File(" << std::quoted(path_to_list.string())
-           << ") does not exist.";
-    return {false, reason.str()};
-  }
-
-  if (std::filesystem::is_directory(path_to_list)) {
-    std::stringstream reason;
-    reason << "Path(" << std::quoted(path_to_list.string())
-           << ") is a directory";
-    return {false, reason.str()};
-  }
-
-  if (not is_root_safe(path_to_list)) {
-    std::stringstream reason;
-    reason << "Used unsafe file location(" << std::quoted(path_to_list.string())
-           << ").";
-    return {false, reason.str()};
-  }
-
-  return {};
-}
-} // namespace
-
-// This function use std::cerr for message reporting.
-// Standard logging macros doesn't work during static initialization.
 std::unordered_set<std::string> load_ops_list(
     const std::filesystem::path& path_to_list,
     const std::unordered_set<std::string>& default_list) {
-  const auto verification_status = validate_ops_list_path(path_to_list);
-  if (not verification_status.is_valid) {
-    std::cerr << "[WARNING][PT_BRIDGE][AUTOCAST] "
-              << "Verification of ops list to autocast failed: "
-              << verification_status.reason << " Loading default ops list."
-              << std::endl;
+  if (path_to_list.empty()) {
+    PT_BRIDGE_DEBUG("Loaded default autocast list.")
     return default_list;
   }
 
   std::ifstream file(path_to_list);
   if (not file.is_open()) {
-    std::cerr << "[WARNING][PT_BRIDGE][AUTOCAST] "
-              << "Opening of ops list(" << std::quoted(path_to_list.string())
-              << ") to autocast failed."
-              << " Loading default list." << std::endl;
+    PT_BRIDGE_WARN(
+        "Failed to open file with ops to autocast: ",
+        path_to_list,
+        ". Default list loaded.");
     return default_list;
   }
 
@@ -121,10 +46,7 @@ std::unordered_set<std::string> load_ops_list(
     ops += line + ", ";
   }
 
-  std::cerr << "[DEBUG][PT_BRIDGE][AUTOCAST] "
-            << "Autocast ops loaded via " << std::quoted(path_to_list.string())
-            << ": " << ops << std::endl;
-
+  PT_BRIDGE_DEBUG("Autocast ops loaded via ", path_to_list, ": ", ops);
   return list;
 }
 
