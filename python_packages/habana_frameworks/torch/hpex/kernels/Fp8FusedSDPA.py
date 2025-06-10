43e1723256954119ad39499d06e4935ab6776067
Internal Developer <internal-developer@intel.com>
2025-06-10 15:05:05 +0530

    FusedSDPA:FP8:Return correct number of grads (#2208)
    fp8_fused_sdpa takes 20 arguments, but its backward function
    returns only 10 grads. Return 20 grads to conform to autograd rule.
    
    Change-Id: I8abb05c0250198bc622b05d9bd32c0a11215682a
    
    Signed-off-by: Internal Developer <internal-developer@intel.com>
    Co-authored-by: Internal Developer <internal-developer@intel.com>

--- python_packages/habana_frameworks/torch/hpex/kernels/Fp8FusedSDPA.py
+++ python_packages/habana_frameworks/torch/hpex/kernels/Fp8FusedSDPA.py
@@ -245,7 +245,28 @@ def fp8_sdpa_bwd_wrapper(ctx, dout, *args):
             dq = gqa_output_reshape(dq)
             dk = gqa_output_reshape(dk)
             dv = gqa_output_reshape(dv)
-        return dq, dk, dv, None, None, None, None, None, None, None
+        return (
+            dq,
+            dk,
+            dv,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
     else:
         q, k, v, P, dm, fwd_out = ctx.saved_tensors
         scale = ctx.scale
@@ -259,7 +280,28 @@ def fp8_sdpa_bwd_wrapper(ctx, dout, *args):
             dq = gqa_output_reshape(dq)
             dk = gqa_output_reshape(dk)
             dv = gqa_output_reshape(dv)
-        return dq, dk, dv, None, None, None, None, None, None, None
+        return (
+            dq,
+            dk,
+            dv,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
 
 
 class Fp8FusedSDPA(torch.autograd.Function):
