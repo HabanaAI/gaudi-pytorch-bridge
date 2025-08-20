8dfd3e59dfb737c68cbc9ddb66af3559a0014b67
Internal Developer <internal-developer@intel.com>
2025-08-20 12:20:34 +0200

    Modernize CMake 1/n (#2908)

--- cmake/Targets.cmake
+++ cmake/Targets.cmake
@@ -43,15 +43,6 @@ function(set_up_warnings target_name)
     target_compile_options(${target_name} PRIVATE -Wno-error=template-id-cdtor)
   endif()
 
-  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "11.0.0")
-    message(FATAL_ERROR "TODO: remove this if, if promotion pass!")
-    # According to https://gcc.gnu.org/bugzilla/show_bug.cgi?id=80635 GCC older than 11 may trigger
-    # bugous maybe-uninitialized warning for std::optional destructor. And this was observed on d10
-    # build with gcc8.3.0.
-    # As a W/A don't emit error in this case.
-    target_compile_options(${target_name} PRIVATE -Wno-error=maybe-uninitialized)
-  endif()
-
   if(PROJECT_IS_TOP_LEVEL)
     target_compile_options(${target_name} PRIVATE -Werror -Werror=format-security)
   endif()
