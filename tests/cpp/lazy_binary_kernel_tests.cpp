aa64ff28815d764b22ed707fb7e2c96e0ed2f014
Internal Developer <internal-developer@intel.com>
2025-09-08 14:21:28 +0200

    Temp. skip AddFwdBf16, AddFwdWithScalar, test_adam_native, test_fp8_gemm_per_block and test_apply_rotary tests due to TPC Fuser regression (#3040)
    *  Temp. skip AddFwdBf16, AddFwdWithScalar,  test_adam_native, test_fp8_gemm_per_block and test_apply_rotary tests due to TPC Fuser regression
    
    * Update copyright year to 2025 in lazy_binary_kernel_tests.cpp

--- tests/cpp/lazy_binary_kernel_tests.cpp
+++ tests/cpp/lazy_binary_kernel_tests.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021-2024 Intel Corporation
+ * Copyright (c) 2021-2025 Intel Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -993,6 +993,7 @@ TEST_F(LazyBinaryKernelTest, AddFwdF32) {
 // Also validates InferOutputMeta for GUID cast_f32_to_bf16, mult_fwd_bf16,
 // add_fwd_bf16 and cast_bf16_to_f32
 TEST_F(LazyBinaryKernelTest, AddFwdBf16) {
+  GTEST_SKIP() << "https://jira.habana-labs.com/browse/SW-240117";
   if (false == GET_ENV_FLAG_NEW(PT_HPU_VALIDATE_COMPUTE_SHAPE)) {
     SET_ENV_FLAG_NEW(PT_HPU_VALIDATE_COMPUTE_SHAPE, true, 1);
   }
@@ -1045,6 +1046,7 @@ TEST_F(LazyBinaryKernelTest, AddFwdI32withCast) {
 // Also validates InferOutputMeta for GUID mult_fwd_f32/bf16/i32,
 // Constant_f32/bf16/i32 and add_fwd_f32/bf16/i32 with second argument as scalar
 TEST_F(LazyBinaryKernelTest, AddFwdWithScalar) {
+  GTEST_SKIP() << "https://jira.habana-labs.com/browse/SW-240117";
   if (false == GET_ENV_FLAG_NEW(PT_HPU_VALIDATE_COMPUTE_SHAPE)) {
     SET_ENV_FLAG_NEW(PT_HPU_VALIDATE_COMPUTE_SHAPE, true, 1);
   }
